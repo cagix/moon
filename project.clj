c24c67d2f7348887899e18c0b187bdda281dbb43
Michael Sappler <michaelsappler@gmail.com>
2024-10-23 18:04:17 +0200

    Move fx stuff in separate repo

--- project.clj
+++ project.clj
@@ -15,24 +15,14 @@
                  [com.github.damn/reduce-fsm "eb1a2c1ff0"]
                  [nrepl "0.9.0"]
                  [org.clj-commons/pretty "3.2.0"]
-                 [org.clojure/tools.namespace "1.3.0"]
-                 [org.openjfx/javafx-controls "22.0.1"]
-                 [rewrite-clj/rewrite-clj "1.1.48"]]
-
+                 [org.clojure/tools.namespace "1.3.0"]]
   :java-source-paths ["java-src"]
-
   :plugins [[lein-hiera "2.0.0"]
             [lein-codox "0.10.8"]]
-
   :target-path "target/%s/" ; https://stackoverflow.com/questions/44246924/clojure-tools-namespace-refresh-fails-with-no-namespace-foo
-
   :uberjar-name "vampire.jar"
-
-  :omit-source true
-
-  ; because dev.browser 512...
-  :jvm-opts ["-Xms512m"
-             "-Xmx512m"
+  :jvm-opts ["-Xms256m"
+             "-Xmx256m"
              "-Dvisualvm.display.name=CDQ"
              "-XX:-OmitStackTraceInFastThrow" ; disappeared stacktraces
              ; for visualvm profiling
@@ -41,21 +31,15 @@
              ;"-Dcom.sun.management.jmxremote.ssl=false"
              ;"-Dcom.sun.management.jmxremote.authenticate=false"
              ]
-
   :codox {:source-uri "https://github.com/damn/moon/blob/main/{filepath}#L{line}"
           :metadata {:doc/format :markdown}}
-
   ; this from engine, what purpose?
   ;:javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
-
   :global-vars {*warn-on-reflection* false
                 ;*unchecked-math* :warn-on-boxed
                 ;*assert* false
                 *print-level* 3}
-
-  :profiles {:javafx  {:aot [editor.javafx dev.browser]}
-             :uberjar {:aot [app.start]}}
-
+  :profiles {:uberjar {:aot [app.start]}}
   :main app.start)
 
 ; * Notes
