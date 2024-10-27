2afe5bb8bd0d8a5f2afd5f506c93fcd4e864aad5
Michael Sappler <michaelsappler@gmail.com>
2024-10-27 04:07:22 +0100

    Cleanup uf_caves gen

--- src/moon/level/uf_caves.clj
+++ src/moon/level/uf_caves.clj
@@ -15,20 +15,18 @@
 (defn- rand-0-5 []
   (get-rand-weighted-item {0 30 1 1 2 1 3 1 4 1 5 1}))
 
-(defn- uf-place-creatures! [spawn-rate tiled-map spawn-positions]
+(defn- set-creatures-tiles [spawn-rate tiled-map spawn-positions]
   (let [layer (t/add-layer! tiled-map :name "creatures" :visible false)
         creatures (db/all :properties/creatures)
         level (inc (rand-int 6))
         creatures (creatures/with-level creatures level)]
-    ;(println "Level: " level)
-    ;(println "Creatures with level: " (count creatures))
     (doseq [position spawn-positions
             :when (<= (rand) spawn-rate)]
       (t/set-tile! layer position (creatures/tile (rand-nth creatures))))))
 
-(def ^:private ->tm-tile
+(def ^:private tm-tile
   (memoize
-   (fn ->tm-tile [texture-region movement]
+   (fn [texture-region movement]
      {:pre [#{"all" "air" "none"} movement]}
      (let [tile (t/->static-tiled-map-tile texture-region)]
        (t/put! (t/m-props tile) "movement" movement)
@@ -39,21 +37,18 @@
 (defn- terrain-texture-region []
   (g/->texture-region "maps/uf_terrain.png"))
 
-(defn- ->uf-tile [& {:keys [sprite-x sprite-y movement]}]
-  (->tm-tile (g/->texture-region (terrain-texture-region)
-                                 [(* sprite-x sprite-size)
-                                  (* sprite-y sprite-size)
-                                  sprite-size
-                                  sprite-size])
-             movement))
-
-; TODO unused
-(def ^:private ground-sprites [1 (range 5 11)])
+(defn- uf-tile [& {:keys [sprite-x sprite-y movement]}]
+  (tm-tile (g/->texture-region (terrain-texture-region)
+                               [(* sprite-x sprite-size)
+                                (* sprite-y sprite-size)
+                                sprite-size
+                                sprite-size])
+           movement))
 
 (def ^:private uf-grounds
   (for [x [1 5]
         y (range 5 11)
-        :when (not= [x y] [5 5])] ;wooden
+        :when (not= [x y] [5 5])] ; wooden
     [x y]))
 
 (def ^:private uf-walls
@@ -61,14 +56,20 @@
         y [13,16,19,22,25,28]]
     [x y]))
 
-(defn- ->ground-tile [[x y]]
-  (->uf-tile :sprite-x (+ x (rand-0-3)) :sprite-y y :movement "all"))
+(defn- ground-tile [[x y]]
+  (uf-tile :sprite-x (+ x (rand-0-3))
+           :sprite-y y
+           :movement "all"))
 
-(defn- ->wall-tile [[x y]]
-  (->uf-tile :sprite-x (+ x (rand-0-5)) :sprite-y y :movement "none"))
+(defn- wall-tile [[x y]]
+  (uf-tile :sprite-x (+ x (rand-0-5))
+           :sprite-y y
+           :movement "none"))
 
-(defn- ->transition-tile [[x y]]
-  (->uf-tile :sprite-x (+ x (rand-0-5)) :sprite-y y :movement "none"))
+(defn- transition-tile [[x y]]
+  (uf-tile :sprite-x (+ x (rand-0-5))
+           :sprite-y y
+           :movement "none"))
 
 (defn- transition? [grid [x y]]
   (= :ground (get grid [x (dec y)])))
@@ -98,20 +99,18 @@
         transition-idx  [wall-x (inc wall-y)]
         position->tile (fn [position]
                          (case (get grid position)
-                           :wall (->wall-tile wall-idx)
+                           :wall (wall-tile wall-idx)
                            :transition (if (transition? grid position)
-                                         (->transition-tile transition-idx)
-                                         (->wall-tile wall-idx))
-                           :ground (->ground-tile ground-idx)))
+                                         (transition-tile transition-idx)
+                                         (wall-tile wall-idx))
+                           :ground (ground-tile ground-idx)))
         tiled-map (wgt-grid->tiled-map grid position->tile)
         can-spawn? #(= "all" (level/movement-property tiled-map %))
         _ (assert (can-spawn? start-position)) ; assuming hoping bottom left is movable
-        spawn-positions (flood-fill grid start-position can-spawn?)
-        ]
-    ; TODO don't spawn my faction vampire w. items ...
-    ; TODO don't spawn creatures on start position
-    ; (all check have HP/movement..../?? ?) (breaks potential field, targeting, ...)
-    (uf-place-creatures! spawn-rate tiled-map spawn-positions)
+        spawn-positions (flood-fill grid start-position can-spawn?)]
+    ; TODO don't spawn my faction vampire w. player items ...
+    ; FIXME - overlapping with player - don't spawn creatures on start position
+    (set-creatures-tiles spawn-rate tiled-map spawn-positions)
     {:tiled-map tiled-map
      :start-position start-position}))
 
