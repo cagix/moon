766a592d842726e12aad5c946db385e51b7c0a94
Michael Sappler <michaelsappler@gmail.com>
2024-10-28 18:20:46 +0100

    Delete moon.intern

--- src/moon/intern.clj
+++
@@ -1,9 +0,0 @@
-(ns moon.intern)
-
-(defn into-clojure.core [nmspace]
-  (in-ns 'clojure.core)
-  (require nmspace)
-  (require 'potemkin.namespaces)
-  (let [syms (map #(symbol (str nmspace) (name %))
-                  (keys (ns-publics nmspace)))]
-    (eval `(potemkin.namespaces/import-vars ~@syms))))
