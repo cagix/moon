ac774470e25fc90e09d9c1560e3a4d43d729ab8d
Michael Sappler <michaelsappler@gmail.com>
2024-11-02 12:55:50 +0100

    Check test in dev.experiment again

--- src/dev/experiment.clj
+++ src/dev/experiment.clj
@@ -14,19 +14,34 @@
   (learn-skill! :skills/convert)
   (learn-skill! :skills/blood-curse)
   (learn-skill! :skills/slow)
+  (learn-skill! :skills/bow)
   (learn-skill! :skills/double-fireball))
+
+ ; Testing effect/target-entity
+ ; stops when target not exists anymore
+ ; or out of range
+
+ ; 1. start application
+ ; 2. start world
+ ; 3. create creature
+ (post-tx! [:tx/creature {:position [35 73]
+                          :creature-id :creatures/dragon-red
+                          :components {:entity/fsm {:fsm :fsms/npc
+                                                    :initial-state :npc-sleeping}
+                                       :entity/faction :evil}}])
+
+ (learn-skill! :skills/bow) ; 1.5 seconds attacktime
+ (post-tx! [:e/destroy (moon.world.entities/get-entity 168)]) ; TODO how to get id ?
+ ; check state is normal ... omg...
 
  ; start world - small empty test room
  ; 2 creatures - player?
  ; start skill w. applicable needs target (bow)
  ; this command:
- (post-tx! [:e/destroy (entity/get-entity 68)])
  ; check skill has stopped using
+ ; => this is basically a test for 'moon.entity.active'
 
- (post-tx! [:tx/creature {:position [35 73]
-                          :creature-id :creatures/dragon-red
-                          :components {:entity/fsm [:state/npc :npc-sleeping]
-                                       :entity/faction :evil} }])
+
  )
 
 (defn- post-tx! [tx]
