81b0de60594924372bf76d59c76182de5d0415c1
Michael Sappler <michaelsappler@gmail.com>
2024-11-07 10:21:40 +0100

    move moon.stats into moon.install

--- src/moon/install.clj
+++ src/moon/install.clj
@@ -21,7 +21,6 @@
                          sound
                          string
                          val-max)
-            moon.stats
             (moon.tx entity)))
 
 (defn- namespace->component-key [ns-str]
@@ -167,3 +166,36 @@
 
 (color/put "MODIFIERS" :cyan)
 (color/put "PRETTY_NAME" [0.84 0.8 0.52])
+
+(derive :stats/aggro-range   :entity/stat)
+(derive :stats/reaction-time :entity/stat)
+
+; * TODO clamp/post-process effective-values @ stat-k->effective-value
+; * just don't create movement-speed increases too much?
+; * dont remove strength <0 or floating point modifiers  (op/int-inc ?)
+; * cast/attack speed dont decrease below 0 ??
+
+; TODO clamp between 0 and max-speed ( same as movement-speed-schema )
+;(m/form entity/movement-speed-schema)
+(derive :stats/movement-speed :entity/stat)
+
+; TODO show the stat in different color red/green if it was permanently modified ?
+; or an icon even on the creature
+; also we want audiovisuals always ...
+
+; TODO clamp into ->pos-int
+(derive :stats/strength :entity/stat)
+
+; TODO here >0
+(comment
+ (let [doc "action-time divided by this stat when a skill is being used.
+           Default value 1.
+
+           For example:
+           attack/cast-speed 1.5 => (/ action-time 1.5) => 150% attackspeed."]))
+(derive :stats/cast-speed :entity/stat)
+(derive :stats/attack-speed :entity/stat)
+
+; TODO bounds
+(derive :stats/armor-save :entity/stat)
+(derive :stats/armor-pierce :entity/stat)
