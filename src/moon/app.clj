d39fce3334d144663bae9f503c716328ffa44cb6
Michael Sappler <michaelsappler@gmail.com>
2024-11-13 10:36:59 +0100

    Move cursors into -main

--- src/moon/app.clj
+++ src/moon/app.clj
@@ -19,22 +19,6 @@
             [moon.screens.world :as world]
             moon.install))
 
-(def ^:private cursors
-  {:cursors/bag                   ["bag001"       [0   0]]
-   :cursors/black-x               ["black_x"      [0   0]]
-   :cursors/default               ["default"      [0   0]]
-   :cursors/denied                ["denied"       [16 16]]
-   :cursors/hand-before-grab      ["hand004"      [4  16]]
-   :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
-   :cursors/hand-grab             ["hand003"      [4  16]]
-   :cursors/move-window           ["move002"      [16 16]]
-   :cursors/no-skill-selected     ["denied003"    [0   0]]
-   :cursors/over-button           ["hand002"      [0   0]]
-   :cursors/sandclock             ["sandclock"    [16 16]]
-   :cursors/skill-not-usable      ["x007"         [0   0]]
-   :cursors/use-skill             ["pointer004"   [0   0]]
-   :cursors/walking               ["walking"      [16 16]]})
-
 (defn -main []
   (db/init)
   (app/start {:title "Moon"
@@ -47,7 +31,20 @@
                  (assets/init "resources/")
                  (batch/init)
                  (shape-drawer/init)
-                 (cursors/init cursors)
+                 (cursors/init {:cursors/bag                   ["bag001"       [0   0]]
+                                :cursors/black-x               ["black_x"      [0   0]]
+                                :cursors/default               ["default"      [0   0]]
+                                :cursors/denied                ["denied"       [16 16]]
+                                :cursors/hand-before-grab      ["hand004"      [4  16]]
+                                :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
+                                :cursors/hand-grab             ["hand003"      [4  16]]
+                                :cursors/move-window           ["move002"      [16 16]]
+                                :cursors/no-skill-selected     ["denied003"    [0   0]]
+                                :cursors/over-button           ["hand002"      [0   0]]
+                                :cursors/sandclock             ["sandclock"    [16 16]]
+                                :cursors/skill-not-usable      ["x007"         [0   0]]
+                                :cursors/use-skill             ["pointer004"   [0   0]]
+                                :cursors/walking               ["walking"      [16 16]]})
                  (gui-view/init {:world-width 1440
                                  :world-height 900})
                  (world-view/init {:world-width 1440
