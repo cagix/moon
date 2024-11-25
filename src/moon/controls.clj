5c064aa005c363310d14ce853f1ca7adeb638ada
Michael Sappler <michaelsappler@gmail.com>
2024-11-25 15:30:26 +0100

    Add WASD-movement-vector to moon.controls

--- src/moon/controls.clj
+++ src/moon/controls.clj
@@ -1,6 +1,7 @@
 (ns moon.controls
   (:require [gdl.graphics.camera :as cam]
-            [gdl.input :refer [key-pressed? key-just-pressed? WASD-movement-vector]]
+            [gdl.input :refer [key-pressed? key-just-pressed?]]
+            [gdl.math.vector :as v]
             [forge.app :refer [world-camera]]))
 
 (defn unpaused? []
@@ -29,6 +30,19 @@
 
 (defn toggle-visible? [window-id]
   (key-just-pressed? (get window-hotkeys window-id)))
+
+(defn- add-vs [vs]
+  (v/normalise (reduce v/add [0 0] vs)))
+
+(defn- WASD-movement-vector []
+  (let [r (when (key-pressed? :d) [1  0])
+        l (when (key-pressed? :a) [-1 0])
+        u (when (key-pressed? :w) [0  1])
+        d (when (key-pressed? :s) [0 -1])]
+    (when (or r l u d)
+      (let [v (add-vs (remove nil? [r l u d]))]
+        (when (pos? (v/length v))
+          v)))))
 
 (defn movement-vector []
   (WASD-movement-vector))
