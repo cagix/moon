979ed74bb81cd574c95e3e0dee736fa78fdd3893
Michael Sappler <michaelsappler@gmail.com>
2024-11-27 10:35:44 +0100

    Fix audio import

--- src/forge/audio.clj
+++ src/forge/audio.clj
@@ -1,6 +1,6 @@
 (ns forge.audio
   (:require [forge.assets :as assets])
-  (:require (com.badlogic.gdx.audio Sound)))
+  (:import (com.badlogic.gdx.audio Sound)))
 
 (defn play-sound [name]
   (Sound/.play (assets/get (str "sounds/" name ".wav"))))
