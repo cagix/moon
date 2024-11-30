df7805f0a5eec258a58a1126a408417e9394e95b
Michael Sappler <michaelsappler@gmail.com>
2024-11-30 12:55:18 +0100

    clojure.gdx.graphics.shape-drawer

--- src/forge/graphics/shape_drawer.clj
+++ src/forge/graphics/shape_drawer.clj
@@ -1,10 +1,10 @@
 (ns forge.graphics.shape-drawer
   (:require [clojure.gdx.graphics.color :as color]
+            [clojure.gdx.graphics.shape-drawer :as sd]
             [clojure.gdx.math :refer [degree->radians]]
             [forge.graphics.color])
   (:import (com.badlogic.gdx.graphics Texture Pixmap Pixmap$Format)
-           (com.badlogic.gdx.graphics.g2d TextureRegion)
-           (space.earlygrey.shapedrawer ShapeDrawer)))
+           (com.badlogic.gdx.graphics.g2d TextureRegion)))
 
 (defn white-pixel-texture []
   (let [pixmap (doto (Pixmap. 1 1 Pixmap$Format/RGBA8888)
@@ -15,39 +15,39 @@
     texture))
 
 (defn create [batch sd-texture]
-  (ShapeDrawer. batch (TextureRegion. sd-texture 1 0 1 1)))
+  (sd/create batch (TextureRegion. sd-texture 1 0 1 1)))
 
-(defn set-color [^ShapeDrawer sd color]
-  (.setColor sd (forge.graphics.color/munge color)))
+(defn set-color [sd color]
+  (sd/set-color sd (forge.graphics.color/munge color)))
 
-(defn ellipse [^ShapeDrawer sd [x y] radius-x radius-y]
-  (.ellipse sd (float x) (float y) (float radius-x) (float radius-y)))
+(defn ellipse [sd [x y] radius-x radius-y]
+  (sd/ellipse sd x y radius-x radius-y))
 
-(defn filled-ellipse [^ShapeDrawer sd [x y] radius-x radius-y]
-  (.filledEllipse sd (float x) (float y) (float radius-x) (float radius-y)))
+(defn filled-ellipse [sd [x y] radius-x radius-y]
+  (sd/filled-ellipse sd x y radius-x radius-y))
 
-(defn circle [^ShapeDrawer sd [x y] radius]
-  (.circle sd (float x) (float y) (float radius)))
+(defn circle [sd [x y] radius]
+  (sd/circle sd x y radius))
 
-(defn filled-circle [^ShapeDrawer sd [x y] radius]
-  (.filledCircle sd (float x) (float y) (float radius)))
+(defn filled-circle [sd [x y] radius]
+  (sd/filled-circle sd x y radius))
 
-(defn arc [^ShapeDrawer sd [centre-x centre-y] radius start-angle degree]
-  (.arc sd centre-x centre-y radius (degree->radians start-angle) (degree->radians degree)))
+(defn arc [sd [centre-x centre-y] radius start-angle degree]
+  (sd/arc sd centre-x centre-y radius (degree->radians start-angle) (degree->radians degree)))
 
-(defn sector [^ShapeDrawer sd [centre-x centre-y] radius start-angle degree]
-  (.sector sd centre-x centre-y radius (degree->radians start-angle) (degree->radians degree)))
+(defn sector [sd [centre-x centre-y] radius start-angle degree]
+  (sd/sector sd centre-x centre-y radius (degree->radians start-angle) (degree->radians degree)))
 
-(defn rectangle [^ShapeDrawer sd x y w h]
-  (.rectangle sd x y w h))
+(defn rectangle [sd x y w h]
+  (sd/rectangle sd x y w h))
 
-(defn filled-rectangle [^ShapeDrawer sd x y w h]
-  (.filledRectangle sd (float x) (float y) (float w) (float h)) )
+(defn filled-rectangle [sd x y w h]
+  (sd/filled-rectangle sd x y w h))
 
-(defn line [^ShapeDrawer sd [sx sy] [ex ey]]
-  (.line sd (float sx) (float sy) (float ex) (float ey)))
+(defn line [sd [sx sy] [ex ey]]
+  (sd/line sd sx sy ex ey))
 
-(defn grid [^ShapeDrawer sd leftx bottomy gridw gridh cellw cellh]
+(defn grid [sd leftx bottomy gridw gridh cellw cellh]
   (let [w (* (float gridw) (float cellw))
         h (* (float gridh) (float cellh))
         topy (+ (float bottomy) (float h))
@@ -59,8 +59,8 @@
             :let [liney (+ (float bottomy) (* (float idx) (float cellh)))]]
       (line sd [leftx liney] [rightx liney]))))
 
-(defn with-line-width [^ShapeDrawer sd width draw-fn]
-  (let [old-line-width (.getDefaultLineWidth sd)]
-    (.setDefaultLineWidth sd (float (* (float width) old-line-width)))
+(defn with-line-width [sd width draw-fn]
+  (let [old-line-width (sd/default-line-width sd)]
+    (sd/set-default-line-width sd (* width old-line-width))
     (draw-fn)
-    (.setDefaultLineWidth sd (float old-line-width))))
+    (sd/set-default-line-width sd old-line-width)))
