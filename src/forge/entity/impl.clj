49f0229419266caf7c5069d955d71cf3ed460079
Michael Sappler <michaelsappler@gmail.com>
2024-12-02 12:41:26 +0100

    Move entity/animation with entity impl

--- src/forge/entity/impl.clj
+++ src/forge/entity/impl.clj
@@ -311,3 +311,61 @@
                            friendly-color
                            :else
                            neutral-color)))))
+
+(defprotocol Animation
+  (animation-tick [_ delta])
+  (animation-restart [_])
+  (animation-stopped? [_])
+  (current-frame [_]))
+
+(defrecord ImmutableAnimation [frames frame-duration looping? cnt maxcnt]
+  Animation
+  (animation-tick [this delta]
+    (let [maxcnt (float maxcnt)
+          newcnt (+ (float cnt) (float delta))]
+      (assoc this :cnt (cond (< newcnt maxcnt) newcnt
+                             looping? (min maxcnt (- newcnt maxcnt))
+                             :else maxcnt))))
+
+  (animation-restart [this]
+    (assoc this :cnt 0))
+
+  (animation-stopped? [_]
+    (and (not looping?) (>= cnt maxcnt)))
+
+  (current-frame [this]
+    (frames (min (int (/ (float cnt) (float frame-duration)))
+                 (dec (count frames))))))
+
+(defn- ->animation [frames & {:keys [frame-duration looping?]}]
+  (map->ImmutableAnimation
+   {:frames (vec frames)
+    :frame-duration frame-duration
+    :looping? looping?
+    :cnt 0
+    :maxcnt (* (count frames) (float frame-duration))}))
+
+(defmethod edn->value :s/animation [_ {:keys [frames frame-duration looping?]}]
+  (->animation (map edn->image frames)
+               :frame-duration frame-duration
+               :looping? looping?))
+
+(defn- assoc-image-current-frame [entity animation]
+  (assoc entity :entity/image (current-frame animation)))
+
+(defmethods :entity/animation
+  (entity/create [[_ animation] eid]
+    (swap! eid assoc-image-current-frame animation))
+
+  (entity/tick [[k animation] eid]
+    (swap! eid #(-> %
+                    (assoc-image-current-frame animation)
+                    (assoc k (animation-tick animation world/delta))))))
+
+(defmethods :entity/delete-after-animation-stopped
+  (entity/create  [_ eid]
+    (-> @eid :entity/animation :looping? not assert))
+
+  (entity/tick [_ eid]
+    (when (animation-stopped? (:entity/animation @eid))
+      (swap! eid assoc :entity/destroyed? true))))
