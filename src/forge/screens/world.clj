834cb5a676c36da0c8bedee1e8866e9c29d77b86
Michael Sappler <michaelsappler@gmail.com>
2024-12-02 11:14:25 +0100

    Move dev-menu/entity-info-window/hp-mana/debug-render into screens.world

--- src/forge/screens/world.clj
+++ src/forge/screens/world.clj
@@ -1,25 +1,179 @@
 (ns forge.screens.world
-  (:require [forge.graphics :as g :refer [draw-tiled-map draw-on-world-view gui-mouse-position world-camera world-mouse-position]]
-            [forge.level :as level]
-            [forge.ui.error-window :refer [error-window!]]
+  (:require [forge.graphics :as g :refer [draw-tiled-map draw-on-world-view gui-mouse-position world-camera world-mouse-position draw-circle draw-rectangle draw-filled-rectangle draw-grid world-viewport-width world-viewport-height draw-text draw-image gui-viewport-width]]
             [forge.graphics.camera :as cam]
-            [forge.screen :as screen]
-            [forge.stage :as stage]
-            [forge.ui :as ui]
+            [forge.info :as info]
             [forge.controls :as controls]
             [forge.entity.components :as entity]
             [forge.entity.state :as state]
+            [forge.level :as level]
+            [forge.screen :as screen]
+            [forge.stage :as stage]
+            [forge.ui :as ui]
             [forge.ui.action-bar :as action-bar]
-            [forge.ui.dev-menu :as dev-menu]
-            [forge.ui.entity-info-window :as entity-info-window]
-            [forge.ui.hp-mana :as hp-mana]
+            [forge.ui.error-window :refer [error-window!]]
             [forge.ui.inventory :as inventory]
             [forge.ui.player-message :as player-message]
+            [forge.val-max :as val-max]
             [forge.world :as world :refer [explored-tile-corners tick-error paused? player-eid mouseover-entity]]
-            [forge.world.debug-render :as debug-render]
-            [forge.world.potential-fields :refer [update-potential-fields!]]
+            [forge.world.potential-fields :refer [update-potential-fields! factions-iterations]]
             [forge.world.raycaster :refer [ray-blocked?]])
-  (:import (com.badlogic.gdx.scenes.scene2d Actor Touchable)))
+  (:import (com.badlogic.gdx.scenes.scene2d Actor Touchable)
+           (com.kotcrab.vis.ui.widget Menu MenuItem MenuBar)))
+
+(defn- render-infostr-on-bar [infostr x y h]
+  (draw-text {:text infostr
+              :x (+ x 75)
+              :y (+ y 2)
+              :up? true}))
+
+(defn- hp-mana-bar []
+  (let [rahmen      (g/image "images/rahmen.png")
+        hpcontent   (g/image "images/hp.png")
+        manacontent (g/image "images/mana.png")
+        x (/ gui-viewport-width 2)
+        [rahmenw rahmenh] (:pixel-dimensions rahmen)
+        y-mana 80 ; action-bar-icon-size
+        y-hp (+ y-mana rahmenh)
+        render-hpmana-bar (fn [x y contentimage minmaxval name]
+                            (draw-image rahmen [x y])
+                            (draw-image (g/sub-image contentimage [0 0 (* rahmenw (val-max/ratio minmaxval)) rahmenh])
+                                        [x y])
+                            (render-infostr-on-bar (str (readable-number (minmaxval 0)) "/" (minmaxval 1) " " name) x y rahmenh))]
+    (ui/actor {:draw (fn []
+                       (let [player-entity @player-eid
+                             x (- x (/ rahmenw 2))]
+                         (render-hpmana-bar x y-hp   hpcontent   (entity/hitpoints   player-entity) "HP")
+                         (render-hpmana-bar x y-mana manacontent (entity/mana player-entity) "MP")))})))
+
+(defn- menu-item [text on-clicked]
+  (doto (MenuItem. text)
+    (.addListener (ui/change-listener on-clicked))))
+
+(defn- add-upd-label
+  ([table text-fn icon]
+   (let [icon (ui/image->widget (g/image icon) {})
+         label (ui/label "")
+         sub-table (ui/table {:rows [[icon label]]})]
+     (.addActor table (ui/actor {:act #(.setText label (text-fn))}))
+     (.expandX (.right (.add table sub-table)))))
+  ([table text-fn]
+   (let [label (ui/label "")]
+     (.addActor table (ui/actor {:act #(.setText label (text-fn))}))
+     (.expandX (.right (.add table label))))))
+
+(defn- add-update-labels [menu-bar update-labels]
+  (let [table (.getTable menu-bar)]
+    (doseq [{:keys [label update-fn icon]} update-labels]
+      (let [update-fn #(str label ": " (update-fn))]
+        (if icon
+          (add-upd-label table update-fn icon)
+          (add-upd-label table update-fn))))))
+
+(defn- add-menu [menu-bar {:keys [label items]}]
+  (let [app-menu (Menu. label)]
+    (doseq [{:keys [label on-click]} items]
+      (.addItem app-menu (menu-item label (or on-click (fn [])))))
+    (.addMenu menu-bar app-menu)))
+
+(defn- create-menu-bar [menus]
+  (let [menu-bar (MenuBar.)]
+    (run! #(add-menu menu-bar %) menus)
+    menu-bar))
+
+(defn- dev-menu* [{:keys [menus update-labels]}]
+  (let [menu-bar (create-menu-bar menus)]
+    (add-update-labels menu-bar update-labels)
+    menu-bar))
+
+(def ^:private disallowed-keys [:entity/skills
+                                #_:entity/fsm
+                                :entity/faction
+                                :active-skill])
+
+(defn- entity-info-window []
+  (let [label (ui/label "")
+        window (ui/window {:title "Info"
+                           :id :entity-info-window
+                           :visible? false
+                           :position [gui-viewport-width 0]
+                           :rows [[{:actor label :expand? true}]]})]
+    ; TODO do not change window size ... -> no need to invalidate layout, set the whole stage up again
+    ; => fix size somehow.
+    (ui/add-actor! window (ui/actor {:act (fn update-label-text []
+                                            ; items then have 2x pretty-name
+                                            #_(.setText (.getTitleLabel window)
+                                                        (if-let [entity (mouseover-entity)]
+                                                          (info/text [:property/pretty-name (:property/pretty-name entity)])
+                                                          "Entity Info"))
+                                            (.setText label
+                                                      (str (when-let [entity (mouseover-entity)]
+                                                             (info/text
+                                                              ; don't use select-keys as it loses Entity record type
+                                                              (apply dissoc entity disallowed-keys)))))
+                                            (.pack window))}))
+    window))
+
+(defn- geom-test []
+  (let [position (world-mouse-position)
+        radius 0.8
+        circle {:position position :radius radius}]
+    (draw-circle position radius [1 0 0 0.5])
+    (doseq [[x y] (map #(:position @%) (world/circle->cells circle))]
+      (draw-rectangle x y 1 1 [1 0 0 0.5]))
+    (let [{[x y] :left-bottom :keys [width height]} (circle->outer-rectangle circle)]
+      (draw-rectangle x y width height [0 0 1 1]))))
+
+(def ^:private ^:dbg-flag tile-grid? false)
+(def ^:private ^:dbg-flag potential-field-colors? false)
+(def ^:private ^:dbg-flag cell-entities? false)
+(def ^:private ^:dbg-flag cell-occupied? false)
+
+(defn- tile-debug []
+  (let [cam (world-camera)
+        [left-x right-x bottom-y top-y] (cam/frustum cam)]
+
+    (when tile-grid?
+      (draw-grid (int left-x) (int bottom-y)
+                 (inc (int world-viewport-width))
+                 (+ 2 (int world-viewport-height))
+                 1 1 [1 1 1 0.8]))
+
+    (doseq [[x y] (cam/visible-tiles cam)
+            :let [cell (world/grid [x y])]
+            :when cell
+            :let [cell* @cell]]
+
+      (when (and cell-entities? (seq (:entities cell*)))
+        (draw-filled-rectangle x y 1 1 [1 0 0 0.6]))
+
+      (when (and cell-occupied? (seq (:occupied cell*)))
+        (draw-filled-rectangle x y 1 1 [0 0 1 0.6]))
+
+      (when potential-field-colors?
+        (let [faction :good
+              {:keys [distance]} (faction cell*)]
+          (when distance
+            (let [ratio (/ distance (factions-iterations faction))]
+              (draw-filled-rectangle x y 1 1 [ratio (- 1 ratio) ratio 0.6]))))))))
+
+(def ^:private ^:dbg-flag highlight-blocked-cell? true)
+
+(defn- highlight-mouseover-tile []
+  (when highlight-blocked-cell?
+    (let [[x y] (->tile (world-mouse-position))
+          cell (get world/grid [x y])]
+      (when (and cell (#{:air :none} (:movement @cell)))
+        (draw-rectangle x y 1 1
+                        (case (:movement @cell)
+                          :air  [1 1 0 0.5]
+                          :none [1 0 0 0.5]))))))
+
+(defn- debug-render-before-entities []
+  (tile-debug))
+
+(defn- debug-render-after-entities []
+  #_(geom-test)
+  (highlight-mouseover-tile))
 
 (def ^:private explored-tile-color (gdx-color 0.5 0.5 0.5 1))
 
@@ -70,7 +224,7 @@
          "id: " (.getUserObject actor)))
 
 (defn- dev-menu-bar []
-  (dev-menu/create
+  (dev-menu*
    {:menus [{:label "Screens"
              :items [{:label "Map-editor"
                       :on-click (partial change-screen :screens/map-editor)}
@@ -128,9 +282,9 @@
               :id :action-bar-table
               :cell-defaults {:pad 2}
               :fill-parent? true})
-   (hp-mana/actor)
+   (hp-mana-bar)
    (ui/group {:id :windows
-              :actors [(entity-info-window/create)
+              :actors [(entity-info-window)
                        (inventory/create)]})
    (ui/actor {:draw #(state/draw-gui-view (entity/state-obj @player-eid))})
    (player-message/create)])
@@ -204,10 +358,10 @@
   (draw-tiled-map world/tiled-map
                   (tile-color-setter (cam/position (world-camera))))
   (draw-on-world-view (fn []
-                       (debug-render/before-entities)
+                       (debug-render-before-entities)
                        ; FIXME position DRY (from player)
                        (world/render-entities (map deref (world/active-entities)))
-                       (debug-render/after-entities))))
+                       (debug-render-after-entities))))
 
 (deftype WorldScreen []
   screen/Screen
