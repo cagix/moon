4dd60fd26625ee74c111c774e81a7ddc2dde710e
Michael Sappler <michaelsappler@gmail.com>
2024-12-04 00:54:42 +0100

    Remove clojure.* helper namespaces

--- src/clojure/awt.clj
+++
@@ -1,8 +0,0 @@
-(ns clojure.awt
-  (:require [clojure.java.io :as io])
-  (:import (java.awt Taskbar Toolkit)))
-
-(defn set-dock-icon [path]
-  (.setIconImage (Taskbar/getTaskbar)
-                 (.getImage (Toolkit/getDefaultToolkit)
-                            (io/resource path))))
