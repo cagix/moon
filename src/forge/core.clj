e691049d53ae4e7c17cb8865f07901749c3949aa
Michael Sappler <michaelsappler@gmail.com>
2024-12-03 13:32:21 +0100

    Move vars without dependencies to the top of forge.core, remove clear-screen

--- src/forge/core.clj
+++ src/forge/core.clj
@@ -33,18 +33,6 @@
            (space.earlygrey.shapedrawer ShapeDrawer)
            (forge OrthogonalTiledMapRenderer ColorSetter RayCaster)))
 
-(defprotocol Screen
-  (screen-enter   [_])
-  (screen-exit    [_])
-  (screen-render  [_])
-  (screen-destroy [_]))
-
-(defn pretty-pst [t]
-  (binding [*print-level* 3]
-    (pretty-repl/pretty-pst t 24)))
-
-(def pprint clojure.pprint/pprint)
-
 (defmacro defsystem
   {:arglists '([name docstring? params?])}
   [name-sym & args]
@@ -66,6 +54,17 @@
        (fn [[k#] & _args#]
          k#))))
 
+(defmacro defmethods [k & sys-impls]
+  `(do
+    ~@(for [[sys & fn-body] sys-impls
+            :let [sys-var (resolve sys)]]
+        `(do
+          (when (get (methods @~sys-var) ~k)
+            (println "WARNING: Overwriting defmethod" ~k "on" ~sys-var))
+          (defmethod ~sys ~k ~(symbol (str (name (symbol sys-var)) "." (name k)))
+            ~@fn-body)))
+    ~k))
+
 (defn mapvals [f m]
   (into {} (for [[k v] m]
              [k (f v)])))
@@ -78,6 +77,216 @@
 
 (defn bind-root [avar value]
   (clojure.lang.Var/.bindRoot avar value))
+
+(defn- recur-sort-map [m]
+  (into (sorted-map)
+        (zipmap (keys m)
+                (map #(if (map? %)
+                        (recur-sort-map %)
+                        %)
+                     (vals m)))))
+
+; reduce-kv?
+(defn- apply-kvs
+  "Calls for every key in map (f k v) to calculate new value at k."
+  [m f]
+  (reduce (fn [m k]
+            (assoc m k (f k (get m k)))) ; using assoc because non-destructive for records
+          m
+          (keys m)))
+
+(defn index-of [k ^clojure.lang.PersistentVector v]
+  (let [idx (.indexOf v k)]
+    (if (= -1 idx)
+      nil
+      idx)))
+
+(defn find-first
+  "Returns the first item of coll for which (pred item) returns logical true.
+  Consumes sequences up to the first match, will consume the entire sequence
+  and return nil if no match is found."
+  [pred coll]
+  (first (filter pred coll)))
+
+(defn safe-merge [m1 m2]
+  {:pre [(not-any? #(contains? m1 %) (keys m2))]}
+  (merge m1 m2))
+
+; libgdx fn is available:
+; (MathUtils/isEqual 1 (length v))
+(defn- approx-numbers [a b epsilon]
+  (<=
+    (Math/abs (float (- a b)))
+    epsilon))
+
+(defn- round-n-decimals [^double x n]
+  (let [z (Math/pow 10 n)]
+    (float
+      (/
+        (Math/round (float (* x z)))
+        z))))
+
+(defn readable-number [^double x]
+  {:pre [(number? x)]} ; do not assert (>= x 0) beacuse when using floats x may become -0.000...000something
+  (if (or
+        (> x 5)
+        (approx-numbers x (int x) 0.001)) ; for "2.0" show "2" -> simpler
+    (int x)
+    (round-n-decimals x 2)))
+
+(defn define-order [order-k-vector]
+  (apply hash-map (interleave order-k-vector (range))))
+
+(defn sort-by-order [coll get-item-order-k order]
+  (sort-by #((get-item-order-k %) order) < coll))
+
+#_(defn order-contains? [order k]
+  ((apply hash-set (keys order)) k))
+
+#_(deftest test-order
+  (is
+    (= (define-order [:a :b :c]) {:a 0 :b 1 :c 2}))
+  (is
+    (order-contains? (define-order [:a :b :c]) :a))
+  (is
+    (not
+      (order-contains? (define-order [:a :b :c]) 2)))
+  (is
+    (=
+      (sort-by-order [:c :b :a :b] identity (define-order [:a :b :c]))
+      '(:a :b :b :c)))
+  (is
+    (=
+      (sort-by-order [:b :c :null :null :a] identity (define-order [:c :b :a :null]))
+      '(:c :b :a :null :null))))
+
+(defn assoc-ks [m ks v]
+  (if (empty? ks)
+    m
+    (apply assoc m (interleave ks (repeat v)))))
+
+(defn truncate [s limit]
+  (if (> (count s) limit)
+    (str (subs s 0 limit) "...")
+    s))
+
+(defn ->edn-str [v]
+  (binding [*print-level* nil]
+    (pr-str v)))
+
+(defn indexed ; from clojure.contrib.seq-utils (discontinued in 1.3)
+  "Returns a lazy sequence of [index, item] pairs, where items come
+ from 's' and indexes count up from zero.
+
+ (indexed '(a b c d)) => ([0 a] [1 b] [2 c] [3 d])"
+  [s]
+  (map vector (iterate inc 0) s))
+
+(defn utils-positions ; from clojure.contrib.seq-utils (discontinued in 1.3)
+  "Returns a lazy sequence containing the positions at which pred
+	 is true for items in coll."
+  [pred coll]
+  (for [[idx elt] (indexed coll) :when (pred elt)] idx))
+
+(defmacro when-seq [[aseq bind] & body]
+  `(let [~aseq ~bind]
+     (when (seq ~aseq)
+       ~@body)))
+
+(defn dissoc-in [m ks]
+  (assert (> (count ks) 1))
+  (update-in m (drop-last ks) dissoc (last ks)))
+
+;; rename to 'shuffle', rand and rand-int without the 's'-> just use with require :as.
+;; maybe even remove the when coll pred?
+;; also maybe *random* instead passing it everywhere? but not sure about that
+(defn sshuffle
+  "Return a random permutation of coll"
+  ([coll random]
+    (when coll
+      (let [al (java.util.ArrayList. ^java.util.Collection coll)]
+        (java.util.Collections/shuffle al random)
+        (clojure.lang.RT/vector (.toArray al)))))
+  ([coll]
+    (sshuffle coll (java.util.Random.))))
+
+(defn srand
+  ([random] (.nextFloat ^java.util.Random random))
+  ([n random] (* n (srand random))))
+
+(defn srand-int [n random]
+  (int (srand n random)))
+
+(defn create-seed []
+  (.nextLong (java.util.Random.)))
+
+; TODO assert int?
+(defn rand-int-between
+  "returns a random integer between lower and upper bounds inclusive."
+  ([[lower upper]]
+    (rand-int-between lower upper))
+  ([lower upper]
+    (+ lower (rand-int (inc (- upper lower))))))
+
+(defn rand-float-between [[lower upper]]
+  (+ lower (rand (- upper lower))))
+
+; TODO use 0-1 not 0-100 internally ? just display it different?
+; TODO assert the number between 0 and 100
+(defn percent-chance
+  "perc is number between 0 and 100."
+  ([perc random]
+    (< (srand random)
+       (/ perc 100)))
+  ([perc]
+    (percent-chance perc (java.util.Random.))))
+; TODO Random. does not return a number between 0 and 100?
+
+(defmacro if-chance
+  ([n then]
+    `(if-chance ~n ~then nil))
+  ([n then else]
+    `(if (percent-chance ~n) ~then ~else)))
+
+(defmacro when-chance [n & more]
+  `(when (percent-chance ~n)
+     ~@more))
+
+(defn get-rand-weighted-item
+  "given a sequence of items and their weight, returns a weighted random item.
+ for example {:a 5 :b 1} returns b only in about 1 of 6 cases"
+  [weights]
+  (let [result (rand-int (reduce + (map #(% 1) weights)))]
+    (loop [r 0
+           items weights]
+      (let [[item weight] (first items)
+            r (+ r weight)]
+        (if (> r result)
+          item
+          (recur (int r) (rest items)))))))
+
+(defn get-rand-weighted-items [n group]
+  (repeatedly n #(get-rand-weighted-item group)))
+
+(comment
+  (frequencies (get-rand-weighted-items 1000 {:a 1 :b 5 :c 4}))
+  (frequencies (repeatedly 1000 #(percent-chance 90))))
+
+(defn high-weighted "for values of x 0-1 returns y values 0-1 with higher value of y than a linear function"
+  [x]
+  (- 1 (Math/pow (- 1 x) 2)))
+
+(defn high-weighted-rand-int [n]
+  (int (* n (high-weighted (rand)))))
+
+(defn high-weighted-rand-nth [coll]
+  (nth coll (high-weighted-rand-int (count coll))))
+
+(defn pretty-pst [t]
+  (binding [*print-level* 3]
+    (pretty-repl/pretty-pst t 24)))
+
+(def pprint clojure.pprint/pprint)
 
 (defn equal? [a b]
   (MathUtils/isEqual a b))
@@ -171,9 +380,6 @@
   (visible? [layer]
     (.isVisible layer)))
 
-(defn clear-screen [color]
-  (ScreenUtils/clear color))
-
 (defn internal-file ^FileHandle [path]
   (.internal Gdx/files path))
 
@@ -189,6 +395,12 @@
 (defn current-screen []
   (and (bound? #'current-screen-key)
        (current-screen-key app-screens)))
+
+(defprotocol Screen
+  (screen-enter   [_])
+  (screen-exit    [_])
+  (screen-render  [_])
+  (screen-destroy [_]))
 
 (defn change-screen
   "Calls `exit` on the current-screen and `enter` on the new screen."
@@ -326,14 +538,6 @@
              with-out-str
              (spit db-properties-file)))))))
 
-(defn- recur-sort-map [m]
-  (into (sorted-map)
-        (zipmap (keys m)
-                (map #(if (map? %)
-                        (recur-sort-map %)
-                        %)
-                     (vals m)))))
-
 (defn- async-write-to-file! []
   (->> db-properties
        vals
@@ -357,15 +561,6 @@
    :frame-duration
    :file
    :sub-image-bounds})
-
-; reduce-kv?
-(defn- apply-kvs
-  "Calls for every key in map (f k v) to calculate new value at k."
-  [m f]
-  (reduce (fn [m k]
-            (assoc m k (f k (get m k)))) ; using assoc because non-destructive for records
-          m
-          (keys m)))
 
 (declare build)
 
@@ -422,118 +617,16 @@
   (or image
       (first (:frames animation))))
 
-(defn index-of [k ^clojure.lang.PersistentVector v]
-  (let [idx (.indexOf v k)]
-    (if (= -1 idx)
-      nil
-      idx)))
-
-(defn find-first
-  "Returns the first item of coll for which (pred item) returns logical true.
-  Consumes sequences up to the first match, will consume the entire sequence
-  and return nil if no match is found."
-  [pred coll]
-  (first (filter pred coll)))
-
 (defn ->tile [position]
   (mapv int position))
 
 (defn tile->middle [position]
   (mapv (partial + 0.5) position))
 
-(defn safe-merge [m1 m2]
-  {:pre [(not-any? #(contains? m1 %) (keys m2))]}
-  (merge m1 m2))
-
-; libgdx fn is available:
-; (MathUtils/isEqual 1 (length v))
-(defn- approx-numbers [a b epsilon]
-  (<=
-    (Math/abs (float (- a b)))
-    epsilon))
-
-(defn- round-n-decimals [^double x n]
-  (let [z (Math/pow 10 n)]
-    (float
-      (/
-        (Math/round (float (* x z)))
-        z))))
-
-(defn readable-number [^double x]
-  {:pre [(number? x)]} ; do not assert (>= x 0) beacuse when using floats x may become -0.000...000something
-  (if (or
-        (> x 5)
-        (approx-numbers x (int x) 0.001)) ; for "2.0" show "2" -> simpler
-    (int x)
-    (round-n-decimals x 2)))
-
-(defn define-order [order-k-vector]
-  (apply hash-map (interleave order-k-vector (range))))
-
-(defn sort-by-order [coll get-item-order-k order]
-  (sort-by #((get-item-order-k %) order) < coll))
-
-#_(defn order-contains? [order k]
-  ((apply hash-set (keys order)) k))
-
-#_(deftest test-order
-  (is
-    (= (define-order [:a :b :c]) {:a 0 :b 1 :c 2}))
-  (is
-    (order-contains? (define-order [:a :b :c]) :a))
-  (is
-    (not
-      (order-contains? (define-order [:a :b :c]) 2)))
-  (is
-    (=
-      (sort-by-order [:c :b :a :b] identity (define-order [:a :b :c]))
-      '(:a :b :b :c)))
-  (is
-    (=
-      (sort-by-order [:b :c :null :null :a] identity (define-order [:c :b :a :null]))
-      '(:c :b :a :null :null))))
-
 (defn k->pretty-name [k]
   (str/capitalize (name k)))
 
-(defn assoc-ks [m ks v]
-  (if (empty? ks)
-    m
-    (apply assoc m (interleave ks (repeat v)))))
-
-(defn truncate [s limit]
-  (if (> (count s) limit)
-    (str (subs s 0 limit) "...")
-    s))
-
-(defn ->edn-str [v]
-  (binding [*print-level* nil]
-    (pr-str v)))
-
 (def dev-mode? (= (System/getenv "DEV_MODE") "true"))
-
-(defn indexed ; from clojure.contrib.seq-utils (discontinued in 1.3)
-  "Returns a lazy sequence of [index, item] pairs, where items come
- from 's' and indexes count up from zero.
-
- (indexed '(a b c d)) => ([0 a] [1 b] [2 c] [3 d])"
-  [s]
-  (map vector (iterate inc 0) s))
-
-(defn utils-positions ; from clojure.contrib.seq-utils (discontinued in 1.3)
-  "Returns a lazy sequence containing the positions at which pred
-	 is true for items in coll."
-  [pred coll]
-  (for [[idx elt] (indexed coll) :when (pred elt)] idx))
-
-(defmacro when-seq [[aseq bind] & body]
-  `(let [~aseq ~bind]
-     (when (seq ~aseq)
-       ~@body)))
-
-(defn dissoc-in [m ks]
-  (assert (> (count ks) 1))
-  (update-in m (drop-last ks) dissoc (last ks)))
 
 (defsystem handle [_ ctx])
 
@@ -560,91 +653,6 @@
 (defn effects-render [ctx effects]
   (run! #(render-effect % ctx)
         effects))
-
-;; rename to 'shuffle', rand and rand-int without the 's'-> just use with require :as.
-;; maybe even remove the when coll pred?
-;; also maybe *random* instead passing it everywhere? but not sure about that
-(defn sshuffle
-  "Return a random permutation of coll"
-  ([coll random]
-    (when coll
-      (let [al (java.util.ArrayList. ^java.util.Collection coll)]
-        (java.util.Collections/shuffle al random)
-        (clojure.lang.RT/vector (.toArray al)))))
-  ([coll]
-    (sshuffle coll (java.util.Random.))))
-
-(defn srand
-  ([random] (.nextFloat ^java.util.Random random))
-  ([n random] (* n (srand random))))
-
-(defn srand-int [n random]
-  (int (srand n random)))
-
-(defn create-seed []
-  (.nextLong (java.util.Random.)))
-
-; TODO assert int?
-(defn rand-int-between
-  "returns a random integer between lower and upper bounds inclusive."
-  ([[lower upper]]
-    (rand-int-between lower upper))
-  ([lower upper]
-    (+ lower (rand-int (inc (- upper lower))))))
-
-(defn rand-float-between [[lower upper]]
-  (+ lower (rand (- upper lower))))
-
-; TODO use 0-1 not 0-100 internally ? just display it different?
-; TODO assert the number between 0 and 100
-(defn percent-chance
-  "perc is number between 0 and 100."
-  ([perc random]
-    (< (srand random)
-       (/ perc 100)))
-  ([perc]
-    (percent-chance perc (java.util.Random.))))
-; TODO Random. does not return a number between 0 and 100?
-
-(defmacro if-chance
-  ([n then]
-    `(if-chance ~n ~then nil))
-  ([n then else]
-    `(if (percent-chance ~n) ~then ~else)))
-
-(defmacro when-chance [n & more]
-  `(when (percent-chance ~n)
-     ~@more))
-
-(defn get-rand-weighted-item
-  "given a sequence of items and their weight, returns a weighted random item.
- for example {:a 5 :b 1} returns b only in about 1 of 6 cases"
-  [weights]
-  (let [result (rand-int (reduce + (map #(% 1) weights)))]
-    (loop [r 0
-           items weights]
-      (let [[item weight] (first items)
-            r (+ r weight)]
-        (if (> r result)
-          item
-          (recur (int r) (rest items)))))))
-
-(defn get-rand-weighted-items [n group]
-  (repeatedly n #(get-rand-weighted-item group)))
-
-(comment
-  (frequencies (get-rand-weighted-items 1000 {:a 1 :b 5 :c 4}))
-  (frequencies (repeatedly 1000 #(percent-chance 90))))
-
-(defn high-weighted "for values of x 0-1 returns y values 0-1 with higher value of y than a linear function"
-  [x]
-  (- 1 (Math/pow (- 1 x) 2)))
-
-(defn high-weighted-rand-int [n]
-  (int (* n (high-weighted (rand)))))
-
-(defn high-weighted-rand-nth [coll]
-  (nth coll (high-weighted-rand-int (count coll))))
 
 (defn- m-v2
   (^Vector2 [[x y]] (Vector2. x y))
@@ -769,17 +777,6 @@
    (range (dec y) (+ y 2))))
 
 (def grid2d g2d/create-grid)
-
-(defmacro defmethods [k & sys-impls]
-  `(do
-    ~@(for [[sys & fn-body] sys-impls
-            :let [sys-var (resolve sys)]]
-        `(do
-          (when (get (methods @~sys-var) ~k)
-            (println "WARNING: Overwriting defmethod" ~k "on" ~sys-var))
-          (defmethod ~sys ~k ~(symbol (str (name (symbol sys-var)) "." (name k)))
-            ~@fn-body)))
-    ~k))
 
 (def val-max-schema
   (m/schema [:and
@@ -1295,7 +1292,7 @@
        (run! screen-destroy (vals app-screens)))
 
      (render []
-       (clear-screen black)
+       (ScreenUtils/clear black)
        (screen-render (current-screen)))
 
      (resize [w h]
