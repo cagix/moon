0acaa1ec0acf038b8b73dd00e5b5e3fd7b4b2ef9
Michael Sappler <michaelsappler@gmail.com>
2024-12-04 12:49:38 +0100

    db no defmethod app-create

--- src/forge/start.clj
+++ src/forge/start.clj
@@ -3,17 +3,16 @@
             [forge.app :as app]
             [forge.db :as db]))
 
-(defmethod app-create :app/db [[_ config]]
-  (db/init config))
-
 (defn -main []
   (let [{:keys [requires
                 awt
                 glfw
                 lwjgl3
+                db
                 components]} (-> "app.edn" io-resource slurp edn-read-string)]
     (run! require requires)
     (app/set-dock-icon (:dock-icon awt))
     (app/set-glfw-config glfw)
+    (db/init db)
     (app/lwjgl3-app (app/components-app components)
                     (app/lwjgl3-config lwjgl3))))
