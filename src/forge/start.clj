3444cce009f3726ac2d532dd40a7c35ef27f4e43
Michael Sappler <michaelsappler@gmail.com>
2024-12-05 16:15:03 +0100

    clojure.gdx.utils.shared-library-loader

--- src/forge/start.clj
+++ src/forge/start.clj
@@ -1,6 +1,7 @@
 (ns forge.start
   (:require [clojure.edn :as edn]
             [clojure.gdx.backends.lwjgl3 :as lwjgl3]
+            [clojure.gdx.utils.shared-library-loader :as shared-library-loader]
             [clojure.java.awt :as awt]
             [clojure.java.io :as io]
             [clojure.lwjgl :as lwjgl]
@@ -8,19 +9,17 @@
             [forge.core :refer [app-create
                                 app-dispose
                                 app-render
-                                app-resize]])
-  (:import (com.badlogic.gdx ApplicationAdapter)
-           (com.badlogic.gdx.utils SharedLibraryLoader)))
+                                app-resize]]))
 
 (defn -main []
   (let [{:keys [requires dock-icon lwjgl3-config components]}
         (-> "app.edn" io/resource slurp edn/read-string)]
     (run! require requires)
     (awt/set-dock-icon dock-icon)
-    (when SharedLibraryLoader/isMac
+    (when shared-library-loader/mac?
       (lwjgl/configure {:glfw-library-name "glfw_async"
                         :glfw-check-thread0 false}))
-    (lwjgl3/app (proxy [ApplicationAdapter] []
+    (lwjgl3/app (proxy [com.badlogic.gdx.ApplicationAdapter] []
                   (create  []    (run! app-create components))
                   (dispose []    (run! app-dispose components))
                   (render  []    (run! app-render components))
