b028cc71b6396feab7c5a7d86650bdee57b14f51
Michael Sappler <michaelsappler@gmail.com>
2024-12-05 16:12:49 +0100

    Upfactor from forge.app

--- src/forge/app.clj
+++ src/forge/app.clj
@@ -1,28 +1,5 @@
 (ns forge.app
-  (:require [clojure.gdx :as gdx]
-            [clojure.gdx.backends.lwjgl3 :as lwjgl3]
-            [clojure.java.awt :as awt]
-            [clojure.lwjgl :as lwjgl])
-  (:import (com.badlogic.gdx ApplicationAdapter)
-           (com.badlogic.gdx.utils SharedLibraryLoader)))
-
-(defprotocol Listener
-  (create  [_])
-  (dispose [_])
-  (render  [_])
-  (resize  [_ w h]))
-
-(defn start [{:keys [dock-icon lwjgl3-config]} listener]
-  (awt/set-dock-icon dock-icon)
-  (when SharedLibraryLoader/isMac
-    (lwjgl/configure {:glfw-library-name "glfw_async"
-                      :glfw-check-thread0 false}))
-  (lwjgl3/app (proxy [ApplicationAdapter] []
-                (create  []    (create  listener))
-                (dispose []    (dispose listener))
-                (render  []    (render  listener))
-                (resize  [w h] (resize  listener w h)))
-              (lwjgl3/config lwjgl3-config)))
+  (:require [clojure.gdx :as gdx]))
 
 (def exit gdx/exit)
 
--- src/forge/start.clj
+++ src/forge/start.clj
@@ -1,25 +1,28 @@
 (ns forge.start
   (:require [clojure.edn :as edn]
+            [clojure.gdx.backends.lwjgl3 :as lwjgl3]
+            [clojure.java.awt :as awt]
             [clojure.java.io :as io]
+            [clojure.lwjgl :as lwjgl]
             [forge.app :as app]
             [forge.core :refer [app-create
                                 app-dispose
                                 app-render
-                                app-resize]]))
+                                app-resize]])
+  (:import (com.badlogic.gdx ApplicationAdapter)
+           (com.badlogic.gdx.utils SharedLibraryLoader)))
 
 (defn -main []
-  (let [{:keys [requires components] :as config} (-> "app.edn" io/resource slurp edn/read-string)]
+  (let [{:keys [requires dock-icon lwjgl3-config components]}
+        (-> "app.edn" io/resource slurp edn/read-string)]
     (run! require requires)
-    (app/start config
-               (reify app/Listener
-                 (create [_]
-                   (run! app-create components))
-
-                 (dispose [_]
-                   (run! app-dispose components))
-
-                 (render [_]
-                   (run! app-render components))
-
-                 (resize [_ w h]
-                   (run! #(app-resize % w h) components))))))
+    (awt/set-dock-icon dock-icon)
+    (when SharedLibraryLoader/isMac
+      (lwjgl/configure {:glfw-library-name "glfw_async"
+                        :glfw-check-thread0 false}))
+    (lwjgl3/app (proxy [ApplicationAdapter] []
+                  (create  []    (run! app-create components))
+                  (dispose []    (run! app-dispose components))
+                  (render  []    (run! app-render components))
+                  (resize  [w h] (run! #(app-resize % w h) components)))
+                (lwjgl3/config lwjgl3-config))))
