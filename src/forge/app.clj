26aabe1dce5cfcd5440630340bd61d2ecb7ec036
Michael Sappler <michaelsappler@gmail.com>
2024-12-05 15:33:12 +0100

    Make forge.app independent of my systems

--- src/forge/app.clj
+++ src/forge/app.clj
@@ -2,33 +2,33 @@
   (:require [clojure.gdx :as gdx]
             [clojure.gdx.backends.lwjgl3 :as lwjgl3]
             [clojure.java.awt :as awt]
-            [clojure.lwjgl :as lwjgl]
-            [forge.core :refer [app-create
-                                app-dispose
-                                app-render
-                                app-resize]])
+            [clojure.lwjgl :as lwjgl])
   (:import (com.badlogic.gdx ApplicationAdapter)
            (com.badlogic.gdx.utils SharedLibraryLoader)))
 
-(defn start [{:keys [dock-icon
-                     lwjgl3-config
-                     components]}]
+(defprotocol Listener
+  (create [_])
+  (dispose [_])
+  (render [_])
+  (resize [_ w h]))
+
+(defn start [{:keys [dock-icon lwjgl3-config]} listener]
   (awt/set-dock-icon dock-icon)
   (when SharedLibraryLoader/isMac
     (lwjgl/configure {:glfw-library-name "glfw_async"
                       :glfw-check-thread0 false}))
   (lwjgl3/app (proxy [ApplicationAdapter] []
                 (create []
-                  (run! app-create components))
+                  (create listener))
 
                 (dispose []
-                  (run! app-dispose components))
+                  (dispose listener))
 
                 (render []
-                  (run! app-render components))
+                  (render listener))
 
                 (resize [w h]
-                  (run! #(app-resize % w h) components)))
+                  (resize listener w h)))
               (lwjgl3/config lwjgl3-config)))
 
 (def exit gdx/exit)
--- src/forge/start.clj
+++ src/forge/start.clj
@@ -1,9 +1,25 @@
 (ns forge.start
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
-            [forge.app :as app]))
+            [forge.app :as app]
+            [forge.core :refer [app-create
+                                app-dispose
+                                app-render
+                                app-resize]]))
 
 (defn -main []
-  (let [{:keys [requires] :as config} (-> "app.edn" io/resource slurp edn/read-string)]
+  (let [{:keys [requires components] :as config} (-> "app.edn" io/resource slurp edn/read-string)]
     (run! require requires)
-    (app/start config)))
+    (app/start config
+               (reify app/Listener
+                 (create [_]
+                   (run! app-create components))
+
+                 (dispose [_]
+                   (run! app-dispose components))
+
+                 (render [_]
+                   (run! app-render components))
+
+                 (resize [_ w h]
+                   (run! #(app-resize % w h) components))))))
