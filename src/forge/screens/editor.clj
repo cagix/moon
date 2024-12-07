511d261c62d469dd726be2ff717a51446ee09c5d
Michael Sappler <michaelsappler@gmail.com>
2024-12-07 16:55:44 +0100

    Add forge.malli and comments

--- src/forge/screens/editor.clj
+++ src/forge/screens/editor.clj
@@ -8,7 +8,6 @@
                                                add-actor!
                                                find-actor]]
             [clojure.gdx.scene2d.ui.table :refer [add-rows!]]
-            [clojure.set :as set]
             [clojure.string :as str]
             [clojure.vis-ui :as vis]
             [clojure.utils :refer [->edn-str
@@ -34,6 +33,7 @@
                                       add-tooltip!]
              :as ui]
             [forge.component :refer [info-text]]
+            [forge.malli :as malli]
             [forge.property :as property]
             [forge.screens.stage :as stage :refer [screen-stage add-actor]]
             [forge.ui :refer [background-image
@@ -41,40 +41,6 @@
             [malli.generator :as mg])
   (:import (com.badlogic.gdx.scenes.scene2d Actor Touchable)
            (com.badlogic.gdx.scenes.scene2d.ui Table)))
-
-(defn- map-keys [m-schema]
-  (let [[_m _p & ks] m-schema]
-    (for [[k m? _schema] ks]
-      k)))
-
-(defn- map-form-k->properties
-  "Given a map schema gives a map of key to key properties (like :optional)."
-  [m-schema]
-  (let [[_m _p & ks] m-schema]
-    (into {} (for [[k m? _schema] ks]
-               [k (if (map? m?) m?)]))))
-
-(defn- optional? [k map-schema]
-  (:optional (k (map-form-k->properties map-schema))))
-
-(defn- optional-keyset [m-schema]
-  (set (filter #(optional? % m-schema) (map-keys m-schema))))
-
-(comment
- (= (optional-keyset
-     [:map {:closed true}
-      [:foo]
-      [:bar]
-      [:baz {:optional true}]
-      [:boz {:optional false}]
-      [:asdf {:optional true}]])
-    [:baz :asdf])
-
- )
-
-(defn- optional-keys-left [m-schema m]
-  (seq (set/difference (optional-keyset m-schema)
-                       (set (keys m)))))
 
 (defn- widget-type [schema _]
   (let [stype (db/schema-type schema)]
@@ -361,7 +327,7 @@
 (defn- attribute-label [k m-schema table]
   (let [label (ui/label ;(str "[GRAY]:" (namespace k) "[]/" (name k))
                         (name k))
-        delete-button (when (optional? k m-schema)
+        delete-button (when (malli/optional? k m-schema)
                         (text-button "-"
                                      (fn []
                                        (Actor/.remove (find-kv-widget table k))
@@ -400,7 +366,7 @@
                            :cell-defaults {:pad 5}})
         malli-form (db/malli-form schema)
         remaining-ks (sort (remove (set (keys (->value schema map-widget-table)))
-                                   (map-keys malli-form)))]
+                                   (malli/map-keys malli-form)))]
     (add-rows!
      window
      (for [k remaining-ks]
@@ -446,7 +412,7 @@
                           (map #(component-row % (db/malli-form schema) table)
                                (sort-by component-order m)))
         colspan component-row-cols
-        opt? (optional-keys-left (db/malli-form schema) m)]
+        opt? (malli/optional-keys-left (db/malli-form schema) m)]
     (add-rows!
      table
      (concat [(when opt?
