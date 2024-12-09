da68d3a611cc1e230e91866538976f4cf04cc212
Michael Sappler <michaelsappler@gmail.com>
2024-12-09 22:57:08 +0100

    Move start & systems to anvil.app

--- src/anvil/app.clj
+++ src/anvil/app.clj
@@ -1,5 +1,32 @@
 (ns anvil.app
-  (:require [clojure.gdx.app :as app]))
+  (:require [clojure.awt :as awt]
+            [clojure.component :refer [defsystem]]
+            [clojure.gdx.app :as app]
+            [clojure.gdx.backends.lwjgl3 :as lwjgl3]
+            [clojure.gdx.utils.shared-library-loader :as shared-library-loader]
+            [clojure.lwjgl :as lwjgl]))
+
+(defsystem create)
+
+(defsystem dispose)
+(defmethod dispose :default [_])
+
+(defsystem render)
+(defmethod render :default [_])
+
+(defsystem resize)
+(defmethod resize :default [_ w h])
+
+(defn start [{:keys [dock-icon lwjgl3 app]}]
+  (awt/set-dock-icon dock-icon)
+  (when shared-library-loader/mac?
+    (lwjgl/configure-glfw-for-mac))
+  (lwjgl3/app (reify lwjgl3/Listener
+                (create  [_]     (run! create          app))
+                (dispose [_]     (run! dispose         app))
+                (render  [_]     (run! render          app))
+                (resize  [_ w h] (run! #(resize % w h) app)))
+              (lwjgl3/config lwjgl3)))
 
 (def exit app/exit)
 
--- src/forge/app.clj
+++ src/forge/app.clj
@@ -6,11 +6,9 @@
             [anvil.screen :as screen]
             [anvil.stage :as stage]
             [anvil.ui :refer [ui-actor text-button] :as ui]
-            [clojure.awt :as awt]
             [clojure.component :refer [defsystem]]
             [clojure.edn :as edn]
             [clojure.gdx.asset-manager :as manager]
-            [clojure.gdx.backends.lwjgl3 :as lwjgl3]
             [clojure.gdx.files :as files]
             [clojure.gdx.graphics :as g]
             [clojure.gdx.graphics.camera :as cam]
@@ -21,11 +19,9 @@
             [clojure.gdx.scene2d.actor :refer [visible? set-visible] :as actor]
             [clojure.gdx.scene2d.group :refer [children]]
             [clojure.gdx.scene2d.stage :as scene2d.stage]
-            [clojure.gdx.utils.disposable :as disposable]
-            [clojure.gdx.utils.shared-library-loader :as shared-library-loader]
+            [clojure.gdx.utils.disposable :refer [dispose]]
             [clojure.gdx.utils.viewport :as vp :refer [fit-viewport]]
             [clojure.java.io :as io]
-            [clojure.lwjgl :as lwjgl]
             [clojure.string :as str]
             [clojure.utils :refer [bind-root defmethods dev-mode? mapvals]]
             [clojure.vis-ui :as vis]
@@ -37,23 +33,12 @@
             [forge.world.update :refer [update-world]])
   (:import (forge OrthogonalTiledMapRenderer)))
 
-(defsystem create)
-
-(defsystem dispose)
-(defmethod dispose :default [_])
-
-(defsystem render)
-(defmethod render :default [_])
-
-(defsystem resize)
-(defmethod resize :default [_ w h])
-
 (defmethods :db
-  (create [[_ config]]
+  (app/create [[_ config]]
     (db/setup config)))
 
 (defmethods :asset-manager
-  (create [[_ folder]]
+  (app/create [[_ folder]]
     (bind-root app/asset-manager (manager/load-all
                                   (for [[asset-type exts] [[:sound   #{"wav"}]
                                                            [:texture #{"png" "bmp"}]]
@@ -61,60 +46,60 @@
                                                   (files/recursively-search folder exts))]
                                     [file asset-type]))))
 
-  (dispose [_]
-    (disposable/dispose app/asset-manager)))
+  (app/dispose [_]
+    (dispose app/asset-manager)))
 
 (defmethods :sprite-batch
-  (create [_]
+  (app/create [_]
     (bind-root app/batch (g/sprite-batch)))
 
-  (dispose [_]
-    (disposable/dispose app/batch)))
+  (app/dispose [_]
+    (dispose app/batch)))
 
 (let [pixel-texture (atom nil)]
   (defmethods :shape-drawer
-    (create [_]
+    (app/create [_]
       (reset! pixel-texture (let [pixmap (doto (g/pixmap 1 1)
                                            (.setColor color/white)
                                            (.drawPixel 0 0))
                                   texture (g/texture pixmap)]
-                              (disposable/dispose pixmap)
+                              (dispose pixmap)
                               texture))
       (bind-root app/sd (sd/create app/batch (g/texture-region @pixel-texture 1 0 1 1))))
 
-    (dispose [_]
-      (disposable/dispose @pixel-texture))))
+    (app/dispose [_]
+      (dispose @pixel-texture))))
 
 (defmethods :default-font
-  (create [[_ font]]
+  (app/create [[_ font]]
     (bind-root app/default-font (freetype/generate-font font)))
 
-  (dispose [_]
-    (disposable/dispose app/default-font)))
+  (app/dispose [_]
+    (dispose app/default-font)))
 
 (defmethods :cursors
-  (create [[_ data]]
+  (app/create [[_ data]]
     (bind-root app/cursors (mapvals (fn [[file [hotspot-x hotspot-y]]]
                                       (let [pixmap (g/pixmap (files/internal (str "cursors/" file ".png")))
                                             cursor (g/cursor pixmap hotspot-x hotspot-y)]
-                                        (disposable/dispose pixmap)
+                                        (dispose pixmap)
                                         cursor))
                                     data)))
 
-  (dispose [_]
-    (run! disposable/dispose (vals app/cursors))))
+  (app/dispose [_]
+    (run! dispose (vals app/cursors))))
 
 (defmethods :gui-viewport
-  (create [[_ [width height]]]
+  (app/create [[_ [width height]]]
     (bind-root app/gui-viewport-width  width)
     (bind-root app/gui-viewport-height height)
     (bind-root app/gui-viewport (fit-viewport width height (g/orthographic-camera))))
 
-  (resize [_ w h]
+  (app/resize [_ w h]
     (vp/update app/gui-viewport w h :center-camera? true)))
 
 (defmethods :world-viewport
-  (create [[_ [width height tile-size]]]
+  (app/create [[_ [width height tile-size]]]
     (bind-root app/world-unit-scale (float (/ tile-size)))
     (bind-root app/world-viewport-width  width)
     (bind-root app/world-viewport-height height)
@@ -124,11 +109,11 @@
                                         y-down? false]
                                     (.setToOrtho camera y-down? world-width world-height)
                                     (fit-viewport world-width world-height camera))))
-  (resize [_ w h]
+  (app/resize [_ w h]
     (vp/update app/world-viewport w h)))
 
 (defmethods :cached-map-renderer
-  (create [_]
+  (app/create [_]
     (bind-root app/cached-map-renderer
       (memoize (fn [tiled-map]
                  (OrthogonalTiledMapRenderer. tiled-map
@@ -136,7 +121,7 @@
                                               app/batch))))))
 
 (defmethods :vis-ui
-  (create [[_ skin-scale]]
+  (app/create [[_ skin-scale]]
     ; app crashes during startup before VisUI/dispose and we do clojure.tools.namespace.refresh-> gui elements not showing.
     ; => actually there is a deeper issue at play
     ; we need to dispose ALL resources which were loaded already ...
@@ -150,7 +135,7 @@
         (set! true))
     (vis/configure-tooltips {:default-appear-delay-time 0}))
 
-  (dispose [_]
+  (app/dispose [_]
     (vis/dispose)))
 
 (defsystem actors)
@@ -171,11 +156,11 @@
     (scene2d.stage/draw stage))
 
   (screen/dispose [[_ {:keys [stage sub-screen]}]]
-    (disposable/dispose stage)
+    (dispose stage)
     (screen/dispose sub-screen)))
 
 (defmethods :screens
-  (create [[_ {:keys [screens first-k]}]]
+  (app/create [[_ {:keys [screens first-k]}]]
     (screen/setup (into {}
                         (for [k screens]
                           [k [:screens/stage {:stage (scene2d.stage/create app/gui-viewport
@@ -184,10 +169,10 @@
                                               :sub-screen [k]}]]))
                   first-k))
 
-  (dispose [_]
+  (app/dispose [_]
     (screen/dispose-all))
 
-  (render [_]
+  (app/render [_]
     (g/clear-screen color/black)
     (screen/render-current)))
 
@@ -266,20 +251,9 @@
   (screen/dispose [_]
     (dispose-world)))
 
-(defn start [{:keys [dock-icon lwjgl3 app]}]
-  (awt/set-dock-icon dock-icon)
-  (when shared-library-loader/mac?
-    (lwjgl/configure-glfw-for-mac))
-  (lwjgl3/app (reify lwjgl3/Listener
-                (create  [_]     (run! create          app))
-                (dispose [_]     (run! dispose         app))
-                (render  [_]     (run! render          app))
-                (resize  [_ w h] (run! #(resize % w h) app)))
-              (lwjgl3/config lwjgl3)))
-
 (defn -main []
   (-> "app.edn"
       io/resource
       slurp
       edn/read-string
-      start))
+      app/start))
