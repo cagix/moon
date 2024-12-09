0a8deb892bafff9a25c4bcb45f109cbab2422289
Michael Sappler <michaelsappler@gmail.com>
2024-12-09 20:27:02 +0100

    Fix calling propert system at screens.stage

--- src/forge/screens/stage.clj
+++ src/forge/screens/stage.clj
@@ -1,22 +1,22 @@
 (ns forge.screens.stage
-  (:require [clojure.component :as component]
+  (:require [anvil.screen :as screen]
             [clojure.gdx.input :as input]
             [clojure.gdx.scene2d.stage :as stage]
             [clojure.gdx.utils.disposable :as disposable]))
 
 (defn enter [[_ {:keys [stage sub-screen]}]]
   (input/set-processor stage)
-  (component/enter sub-screen))
+  (screen/enter sub-screen))
 
 (defn exit [[_ {:keys [stage sub-screen]}]]
   (input/set-processor nil)
-  (component/exit sub-screen))
+  (screen/exit sub-screen))
 
 (defn render [[_ {:keys [stage sub-screen]}]]
   (stage/act stage)
-  (component/render sub-screen)
+  (screen/render sub-screen)
   (stage/draw stage))
 
 (defn dispose [[_ {:keys [stage sub-screen]}]]
   (disposable/dispose stage)
-  (component/dispose sub-screen))
+  (screen/dispose sub-screen))
