665d74c9741d8c4d5a6e562c0ccc0581ac1b2421
Michael Sappler <michaelsappler@gmail.com>
2024-12-09 08:04:25 +0100

    Add anvil.tiled-map-renderer/TiledMapRenderer protocol

--- src/anvil/tiled_map_renderer.clj
+++ src/anvil/tiled_map_renderer.clj
@@ -1,21 +1,4 @@
-(ns anvil.tiled-map-renderer
-  (:require [clojure.gdx.tiled :as tiled])
-  (:import (forge OrthogonalTiledMapRenderer ColorSetter)))
+(ns anvil.tiled-map-renderer)
 
-(defn create [tiled-map unit-scale batch]
-  (OrthogonalTiledMapRenderer. tiled-map
-                               (float unit-scale)
-                               batch))
-
-(defn render
-  [^OrthogonalTiledMapRenderer renderer tiled-map color-setter camera]
-  (.setColorSetter renderer (reify ColorSetter
-                              (apply [_ color x y]
-                                (color-setter color x y))))
-  (.setView renderer camera)
-  (->> tiled-map
-       tiled/layers
-       (filter tiled/visible?)
-       (map (partial tiled/layer-index tiled-map))
-       int-array
-       (.render renderer)))
+(defprotocol TiledMapRenderer
+  (render [_ tiled-map color-setter camera]))
--- src/forge/app/cached_map_renderer.clj
+++ src/forge/app/cached_map_renderer.clj
@@ -1,10 +1,27 @@
 (ns forge.app.cached-map-renderer
   (:require [anvil.graphics :as g]
-            [anvil.tiled-map-renderer :as tiled-map-renderer]
-            [clojure.utils :refer [bind-root]]))
+            [anvil.tiled-map-renderer :refer [TiledMapRenderer]]
+            [clojure.gdx.tiled :as tiled]
+            [clojure.utils :refer [bind-root]])
+  (:import (forge OrthogonalTiledMapRenderer ColorSetter)))
 
 (defn create [_]
-  (bind-root g/cached-map-renderer (memoize (fn [tiled-map]
-                                              (tiled-map-renderer/create tiled-map
-                                                                         g/world-unit-scale
-                                                                         g/batch)))))
+  (bind-root g/cached-map-renderer
+             (memoize (fn [tiled-map]
+                        (OrthogonalTiledMapRenderer. tiled-map
+                                                     (float g/world-unit-scale)
+                                                     g/batch)))))
+
+(extend-type OrthogonalTiledMapRenderer
+  TiledMapRenderer
+  (render [this tiled-map color-setter camera]
+    (.setColorSetter this (reify ColorSetter
+                            (apply [_ color x y]
+                              (color-setter color x y))))
+    (.setView this camera)
+    (->> tiled-map
+         tiled/layers
+         (filter tiled/visible?)
+         (map (partial tiled/layer-index tiled-map))
+         int-array
+         (.render this))))
