b140a8ee1e9d9a54f5ec1e545c39889b9b13e879
Michael Sappler <michaelsappler@gmail.com>
2024-12-09 06:15:13 +0100

    Remove unnecessary params from defsystem

--- src/clojure/utils.clj
+++ src/clojure/utils.clj
@@ -202,24 +202,13 @@
                        (component-k->namespace prefix k)
                        k)))
 
-(defmacro defsystem ; do not need params ! can re-use create/destroy of entity and pass eid there ...
-  {:arglists '([name docstring? params?])}
+(defmacro defsystem
+  {:arglists '([name docstring?])}
   [name-sym & args]
   (let [docstring (if (string? (first args))
-                    (first args))
-        params (if (string? (first args))
-                 (second args)
-                 (first args))
-        params (if (nil? params)
-                 '[_]
-                 params)]
-    (when (zero? (count params))
-      (throw (IllegalArgumentException. "First argument needs to be component.")))
-    (when-let [avar (resolve name-sym)]
-      (println "WARNING: Overwriting defsystem:" avar))
-    `(defmulti ~(vary-meta name-sym assoc :params (list 'quote params))
-       ~(str "[[defsystem]] `" (str params) "`"
-             (when docstring (str "\n\n" docstring)))
+                    (first args))]
+    `(defmulti ~name-sym
+       ~(str "[[defsystem]]" (when docstring (str "\n\n" docstring)))
        (fn [[k#] & _args#]
          k#))))
 
