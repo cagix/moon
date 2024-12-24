b39199dc6c3f2c525b9f9ff15a0434db16b55836
Michael Sappler <michaelsappler@gmail.com>
2024-12-24 21:00:04 +0100

    WIP space invaders

--- src/clojure/gdx/space_invaders.clj
+++ src/clojure/gdx/space_invaders.clj
@@ -1,31 +1,26 @@
 (ns clojure.gdx.space-invaders
-  (:require [clojure.gdx.backends.lwjgl3 :as lwjgl3])
-  (:import (com.badlogic.gdx ApplicationListener Gdx Input Input$Keys)
-           (com.badlogic.gdx.graphics GL20 OrthographicCamera)
+  (:require [clojure.gdx.files :as files]
+            [clojure.gdx.graphics.texture :as texture]
+            [clojure.gdx.backends.lwjgl3 :as lwjgl3])
+  (:import (com.badlogic.gdx Gdx Input$Keys)
+           (com.badlogic.gdx.graphics OrthographicCamera)
            (com.badlogic.gdx.graphics Texture)
            (com.badlogic.gdx.graphics.g2d SpriteBatch)
            (com.badlogic.gdx.utils ScreenUtils)))
 
-;; TODO clojure.java.interop repo ... - or in 'clojure.reflect'
-
-; clojure java interop patterns - proxy Ilookup, etc. , cool stuff, cool ideas
-; guide for writing a clojure API for a java library
-
 ; TODO release as separate repo - example - ..
 
-; TODO android / iOs - ask chatGPT for help ...
-; TODO viewport
 ; TODO aliens move /
 ; TODO win/lose
 ; TODO music/sound
 ; TODO animations
-; TODO score/screens ?
-; TODO powerups ?
+
 ; TODO collision debug / pause / overlay FPS,etc. (dev-menu?)
+
 ; TODO alien/player/bullet width/height has to fit with image
+
 ; TODO exit ?
-; TODO pass gdx app stuffs static with create as a map
-; TODO render text, show controls ...
+
 ; TODO one version w. interop / one w. your 'API' ////....
 ; TODO dev-menu for game-stat map - depending on class can inspect ...
 ; I have this already ....
@@ -33,30 +28,34 @@
 (def screen-width 800)
 (def screen-height 600)
 
-(defn create-texture [path]
-  (Texture. (.internal Gdx/files path)))
+(defn texture [{:keys [gdx/files]} path]
+  (texture/create (files/internal files path)))
 
-(defn make-player []
+(defn make-player [context]
   {:x (/ screen-width 2)
    :y 50
    :width 64
    :height 64
    :speed 300
-   :texture (create-texture "ship/1.png")
+   :texture (texture context "ship/1.png")
    :bullets []})
 
-(defn make-alien [x y]
-  {:x x :y y :width 48 :height 48
-   :texture (create-texture "alien/1.png")})
+(defn make-alien [context x y]
+  {:x x
+   :y y
+   :width 48
+   :height 48
+   :texture (texture context "alien/1.png")})
 
-(defn make-game-state []
-  {:player (make-player)
-   :aliens (for [x (range 100 700 80)
-                 y (range 400 500 60)]
-             (make-alien x y))
-   :bullets []
-   :batch (SpriteBatch.)
-   :camera (OrthographicCamera. screen-width screen-height)})
+(defn make-game-state [gdx-state]
+  (merge gdx-state
+         {:player (make-player gdx-state)
+          :aliens (for [x (range 100 700 80)
+                        y (range 400 500 60)]
+                    (make-alien gdx-state x y))
+          :bullets []
+          :batch (SpriteBatch.)
+          :camera (OrthographicCamera. screen-width screen-height)}))
 
 (defn update-player [player delta]
   (let [speed (:speed player)
@@ -90,7 +89,8 @@
 (defn handle-input [game-state]
   (if (.isKeyJustPressed Gdx/input Input$Keys/SPACE)
     (let [player (:player game-state)
-          bullet {:x (+ (:x player) 28) :y (+ (:y player) 64)}]
+          bullet {:x (+ (:x player) 28)
+                  :y (+ (:y player) 64)}]
       (update game-state :bullets conj bullet))
     game-state))
 
@@ -111,7 +111,10 @@
     (doseq [alien aliens]
       (render-entity batch alien))
     (doseq [bullet bullets]
-      (render-entity batch {:texture (create-texture "bullet.png") :x (:x bullet) :y (:y bullet)}))
+      (render-entity batch
+                     {:texture (texture "bullet.png") ; TODO texture loaded every time new ! can load them once into game-state only ?! ... noice...
+                      :x (:x bullet)
+                      :y (:y bullet)}))
     (.end batch)))
 
 (defn update-game [game-state delta]
@@ -123,13 +126,10 @@
 
 (def state (atom nil))
 
-(def gdx-stuff nil)
-
 (defn game-listener []
   (reify lwjgl3/Application
     (create [_ gdx-state]
-      (def gdx-stuff gdx-state)
-      (reset! state (make-game-state))
+      (reset! state (make-game-state gdx-state))
       (swap! state assoc :camera (OrthographicCamera. screen-width screen-height)))
 
     (dispose [_]
