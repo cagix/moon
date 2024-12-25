18ff18d66ef9363f4573a1f50328b45b034e6050
Michael Sappler <michaelsappler@gmail.com>
2024-12-25 12:30:18 +0100

    Forgot to add anvil.effect.sound

---
+++ src/anvil/effect/sound.clj
@@ -0,0 +1,13 @@
+(ns ^:no-doc anvil.effect.sound
+  (:require [anvil.component :as component]
+            [clojure.gdx.audio.sound :as sound]))
+
+(defmethods :effects/sound
+  (component/applicable? [_ _ctx]
+    true)
+
+  (component/useful? [_ _]
+    false)
+
+  (component/handle [[_ sound] _ctx]
+    (sound/play sound)))
