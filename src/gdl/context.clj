cdfdd7f4061035aa8d31e3795bcd21f9e8e98d4c
Michael Sappler <michaelsappler@gmail.com>
2024-12-26 15:32:48 +0100

    clojure.gdx/dispose

--- src/gdl/context.clj
+++ src/gdl/context.clj
@@ -1,6 +1,6 @@
 (ns gdl.context
   (:require [anvil.component :as component]
-            [clojure.gdx :as gdx :refer [play sprite-batch]]
+            [clojure.gdx :as gdx :refer [play sprite-batch dispose]]
             [clojure.gdx.graphics.camera :as camera]
             [clojure.gdx.graphics.color :as color]
             [clojure.gdx.graphics.colors :as colors]
@@ -13,7 +13,6 @@
             [clojure.gdx.graphics.g2d.texture-region :as texture-region]
             [clojure.gdx.interop :as interop]
             [clojure.gdx.math.utils :refer [clamp degree->radians]]
-            [clojure.gdx.utils.disposable :refer [dispose]]
             [clojure.gdx.utils.viewport :as viewport]
             [clojure.string :as str]
             [gdl.assets :as assets]
