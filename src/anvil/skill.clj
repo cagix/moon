fb1cccd67ce7ac37869a556f1f0c8620c52bc30f
Michael Sappler <michaelsappler@gmail.com>
2024-12-30 22:23:28 +0100

    Do not use defn-impl at skill-usable-state

--- src/anvil/skill.clj
+++ src/anvil/skill.clj
@@ -1,3 +1,21 @@
-(ns anvil.skill)
+(ns anvil.skill
+  (:require [anvil.effect :as effect]
+            [anvil.entity :as entity]))
 
-(defn usable-state [entity skill effect-ctx])
+(defn- not-enough-mana? [entity {:keys [skill/cost]}]
+  (and cost (> cost (entity/mana-val entity))))
+
+(defn usable-state
+  [entity {:keys [skill/cooling-down? skill/effects] :as skill} effect-ctx]
+  (cond
+   cooling-down?
+   :cooldown
+
+   (not-enough-mana? entity skill)
+   :not-enough-mana
+
+   (not (effect/some-applicable? effect-ctx effects))
+   :invalid-params
+
+   :else
+   :usable))
