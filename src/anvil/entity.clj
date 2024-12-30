0be99acd2972904c3146ec528ebff15d649045c0
Michael Sappler <michaelsappler@gmail.com>
2024-12-30 18:28:56 +0100

    Do not use defn-impl at entity.damage

--- src/anvil/entity.clj
+++ src/anvil/entity.clj
@@ -66,8 +66,19 @@
 (defn pay-mana-cost [entity cost])
 
 (defn damage
-  ([source damage])
-  ([source target damage]))
+  ([source damage]
+   (update damage
+           :damage/min-max
+           #(-> %
+                (apply-min-modifier source :modifier/damage-deal-min)
+                (apply-max-modifier source :modifier/damage-deal-max))))
+
+  ([source target damage]
+   (update (damage source damage)
+           :damage/min-max
+           apply-max-modifier
+           target
+           :modifier/damage-receive-max)))
 
 (defn hitpoints
   "Returns the hitpoints val-max vector `[current-value maximum]` of entity after applying max-hp modifier.
