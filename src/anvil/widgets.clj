48cf2d4e579327a8f5436e5f8cc9ab7bfac32eb8
Michael Sappler <michaelsappler@gmail.com>
2024-12-30 22:29:17 +0100

    Do not use defn-impl at action-bar

--- src/anvil/widgets.clj
+++ src/anvil/widgets.clj
@@ -1,10 +1,33 @@
-(ns anvil.widgets)
+(ns anvil.widgets
+  (:require [gdl.ui :as ui :refer [ui-actor]]
+            [gdl.ui.group :as group])
+  (:import (com.badlogic.gdx.scenes.scene2d Actor)
+           (com.badlogic.gdx.scenes.scene2d.ui Button ButtonGroup)))
 
 (defn hp-mana-bar [c])
 
 (defn dev-menu [c])
 
-(defn action-bar-table [c])
+(defn- action-bar-button-group []
+  (let [actor (ui-actor {})]
+    (.setName actor "action-bar/button-group")
+    (Actor/.setUserObject actor (ui/button-group {:max-check-count 1
+                                                  :min-check-count 0}))
+    actor))
+
+(defn- action-bar []
+  (let [group (ui/horizontal-group {:pad 2 :space 2})]
+    (.setUserObject group :ui/action-bar)
+    (group/add-actor! group (action-bar-button-group))
+    group))
+
+(defn action-bar-table [_context]
+  (ui/table {:rows [[{:actor (action-bar)
+                      :expand? true
+                      :bottom? true}]]
+             :id :action-bar-table
+             :cell-defaults {:pad 2}
+             :fill-parent? true}))
 
 (defn inventory [c])
 
--- src/anvil/widgets/action_bar.clj
+++ src/anvil/widgets/action_bar.clj
@@ -1,32 +1,10 @@
 (ns anvil.widgets.action-bar
   (:require [anvil.info :as info]
-            [anvil.widgets :as widgets]
             [cdq.context :as world]
-            [gdl.ui :refer [ui-actor add-tooltip!] :as ui]
+            [gdl.ui :refer [add-tooltip!] :as ui]
             [gdl.ui.group :refer [add-actor!]])
   (:import (com.badlogic.gdx.scenes.scene2d Actor)
            (com.badlogic.gdx.scenes.scene2d.ui Button ButtonGroup)))
-
-(defn- action-bar-button-group []
-  (let [actor (ui-actor {})]
-    (.setName actor "action-bar/button-group")
-    (Actor/.setUserObject actor (ui/button-group {:max-check-count 1
-                                                  :min-check-count 0}))
-    actor))
-
-(defn- action-bar []
-  (let [group (ui/horizontal-group {:pad 2 :space 2})]
-    (.setUserObject group :ui/action-bar)
-    (add-actor! group (action-bar-button-group))
-    group))
-
-(defn-impl widgets/action-bar-table [_context]
-  (ui/table {:rows [[{:actor (action-bar)
-                      :expand? true
-                      :bottom? true}]]
-             :id :action-bar-table
-             :cell-defaults {:pad 2}
-             :fill-parent? true}))
 
 (defn action-bar-add-skill [c {:keys [property/id entity/image] :as skill}]
   (let [{:keys [horizontal-group button-group]} (world/get-action-bar c)
