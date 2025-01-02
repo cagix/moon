95031bd834454fa3a3e10304c428d93f32d66404
Michael Sappler <michaelsappler@gmail.com>
2025-01-02 01:30:14 +0100

    Clearer names effect-context

--- src/anvil/effect.clj
+++ src/anvil/effect.clj
@@ -2,37 +2,39 @@
   (:require [cdq.context :as world]
             [clojure.component :as component]))
 
+
+
+
 ; TODO the 'ctx' is actually 'effect' itself with position/target/etc. as part of ':effect/'
+
+
+
+
 
 ; this is not necessary if effect does not need target, but so far not other solution came up.
 (defn check-update-ctx
   "Call this on effect-context if the time of using the context is not the time when context was built."
-  [c {:keys [effect/source effect/target] :as ctx}]
+  [context {:keys [effect/source effect/target] :as effect-ctx}]
   (if (and target
            (not (:entity/destroyed? @target))
-           (world/line-of-sight? c @source @target))
-    ctx
-    (dissoc ctx :effect/target)))
+           (world/line-of-sight? context @source @target))
+    effect-ctx
+    (dissoc effect-ctx :effect/target)))
 
-; TODO 'effect' with 'effect-components' which have a certain defsystem 'protocol'
-; of required and optional defsystems
-; so just do/applicable?/useful?/and grep 'effects', e.t.c. rename keywords?
-; => so sound is just a component....
+(defn filter-applicable? [effect-ctx effect]
+  (filter #(component/applicable? % effect-ctx) effect))
 
-(defn filter-applicable? [ctx effect]
-  (filter #(component/applicable? % ctx) effect))
+(defn some-applicable? [effect-ctx effect]
+  (seq (filter-applicable? effect-ctx effect)))
 
-(defn some-applicable? [ctx effect]
-  (seq (filter-applicable? ctx effect)))
-
-(defn applicable-and-useful? [c ctx effect]
+(defn applicable-and-useful? [context effect-ctx effect]
   (->> effect
-       (filter-applicable? ctx)
-       (some #(component/useful? % ctx c))))
+       (filter-applicable? effect-ctx)
+       (some #(component/useful? % effect-ctx context))))
 
-(defn do-all! [c ctx effect]
-  (run! #(component/handle % ctx c)
-        (filter-applicable? ctx effect)))
+(defn do-all! [context effect-ctx effect]
+  (run! #(component/handle % effect-ctx context)
+        (filter-applicable? effect-ctx effect)))
 
-(defn render-info [c ctx effect]
-  (run! #(component/render-effect % ctx c) effect))
+(defn render-info [context effect-ctx effect]
+  (run! #(component/render-effect % effect-ctx context) effect))
