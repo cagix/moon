7f9e099601746b04219ae60fa6e3f59a4f518546
Michael Sappler <michaelsappler@gmail.com>
2025-01-03 14:12:35 +0100

    move apply-color into info/text

--- src/gdl/info.clj
+++ src/gdl/info.clj
@@ -40,28 +40,29 @@
               :maxrange
               :entity-effects])
 
-(defn- apply-color [k->colors k info-text]
-  (if-let [color (k->colors k)]
-    (str "[" color "]" info-text "[]")
-    info-text))
-
 (declare ^:dynamic *info-text-entity*)
 
 (defsystem info)
 (defmethod info :default [_ _context])
 
-(defn text [c components]
+; TODO this is 'gdl.context' ?
+; or 'cdq.context' => yep ! ?! idk
+
+(defn text [context components]
   (->> components
        (sort-by-k-order k-order)
        (keep (fn [{k 0 v 1 :as component}]
                (str (try (binding [*info-text-entity* components]
-                           (apply-color k->colors k (info component c)))
+                           (let [info-text (info component context)]
+                             (if-let [color (k->colors k)]
+                               (str "[" color "]" info-text "[]")
+                               info-text)))
                          (catch Throwable t
                            ; calling from property-editor where entity components
                            ; have a different data schema than after component/create!
                            ; and info-text might break
                            (pr-str component)))
                     (when (map? v)
-                      (str "\n" (text c v))))))
+                      (str "\n" (text context v))))))
        (str/join "\n")
        remove-newlines))
