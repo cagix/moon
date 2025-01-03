4fdc1e6810fcc9ced36398a867fd5576a628068d
Michael Sappler <michaelsappler@gmail.com>
2025-01-03 15:36:52 +0100

    Add docs & type hints

--- src/clojure/gdx/vis_ui.clj
+++ src/clojure/gdx/vis_ui.clj
@@ -1,8 +1,12 @@
 (ns clojure.gdx.vis-ui
+  "Allows to easily load VisUI skin and change default title alignment and I18N bundles. Contains static field with VisUI version."
   (:refer-clojure :exclude [load])
-  (:import (com.kotcrab.vis.ui VisUI VisUI$SkinScale)))
+  (:import (com.badlogic.gdx.scenes.scene2d.ui Skin)
+           (com.kotcrab.vis.ui VisUI VisUI$SkinScale)))
 
-(defn load [skin-scale]
+(defn load
+  "Loads default VisUI skin with `:skin-scale/x1` or `:skin-scale/x2`."
+  [skin-scale]
   (VisUI/load (case skin-scale
                 :skin-scale/x1 VisUI$SkinScale/X1
                 :skin-scale/x2 VisUI$SkinScale/X2)))
@@ -10,8 +14,12 @@
 (defn loaded? []
   (VisUI/isLoaded))
 
-(defn dispose []
+(defn dispose
+  "Unloads skin."
+  []
   (VisUI/dispose))
 
-(defn skin []
+(defn skin
+  "Returns the `com.badlogic.gdx.scenes.scene2d.ui.Skin`."
+  ^Skin []
   (VisUI/getSkin))
