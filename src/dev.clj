3f36722746a189ecb2ac137d7d31b197017c0281
Michael Sappler <michaelsappler@gmail.com>
2025-01-04 19:17:39 +0100

    Remove dev/tools and move to dev

--- src/dev.clj
+++ src/dev.clj
@@ -1,89 +1,239 @@
 (ns dev
-  (:require [clojure.edn :as edn]
-            [clojure.java.io :as io]
-            [malli.core :as m]))
-
-; Requirements! / Tests !
-; * maybe the error message is unreadable if the whole db is one map ... ?
-; * why don't I use an existing db (datomic?)
-
-(def schema
-  [:map-of :keyword :int])
-
-[:s/map
- [:properties/audiovisuals
-  :properties/creatures
-  :properties/items
-  :properties/projectiles
-  :properties/skills
-  :properties/worlds]]
-
-(m/validate schema {:a 1
-                    :b 2
-                    :c 3
-                    })
+  (:require [cdq.context :as world]
+            [clojure.gdx :as gdx]
+            [clojure.gdx.scene2d.stage :as stage]
+            [clojure.string :as str]
+            [clojure.pprint :refer [pprint]]
+            [gdl.context :as c]
+            [gdl.ui :refer [t-node scroll-pane] :as ui]
+            [clojure.gdx.scene2d.group :refer [children]]))
 
 (comment
- (def db-data (-> "properties.edn"
-                  io/resource
-                  slurp
-                  edn/read-string))
 
- (println (count db-data))
- ; 372 properties
+ (print-vimrc-names-forge-core-publics 'forge.core "vimrc")
 
- (defn- property-type [{:keys [property/id]}]
-   (keyword "properties" (namespace id)))
+ (print-app-values-tree "app-values-tree.clj"
+                        #{"anvil", "gdl", "uf"})
 
- (for [[ptype properties] (group-by property-type db-data)]
-   [ptype (count properties)])
+ (post-runnable show-tree-view!)
+ (show-tree-view! (world/mouseover-entity @c/state))
+ (show-tree-view! (mouseover-grid-cell @c/state))
+ (show-tree-view! (ns-value-vars #{"forge"}))
 
- #_([:properties/audiovisuals 6]
-    [:properties/creatures 128]
-    [:properties/items 223]
-    [:properties/projectiles 2]
-    [:properties/skills 10]
-    [:properties/worlds 3])
-
- (defn- async-write-to-file! [m]
-   (async-pprint-spit! (io/resource "properties.edn")
-                       (recur-sort-map m)))
-
- ;(async-write-to-file! new-db-data)
-
- (def new-db-data
-   (into {}
-         (for [[ptype properties] (group-by property-type db-data)]
-           [ptype (into {}
-                        (for [{:keys [property/id] :as property} properties]
-                          [id (dissoc property :property/id)]))])))
-
- (keys new-db-data)
- (:properties/audiovisuals :properties/creatures :properties/items :properties/projectiles :properties/skills :properties/worlds)
-
- (for [[ptype id-map] new-db-data]
-   [ptype (count id-map)])
-
- (:creatures/vampire (:properties/creatures new-db-data)))
-
-;;;;;;
+ ; Idea:
+ ; * Generate the tree as data-structure first
+ ; Then pass to the ui
+ ; So can test it locally?
+ ; or some interface for tree-node & on-clicked
 
 
-; property-type remove
-; make db a map of 'different types' to 'id-property' map
+ (do
+  (learn-skill! :skills/projectile)
+  (learn-skill! :skills/spawn)
+  (learn-skill! :skills/meditation)
+  (learn-skill! :skills/death-ray)
+  (learn-skill! :skills/convert)
+  (learn-skill! :skills/blood-curse)
+  (learn-skill! :skills/slow)
+  (learn-skill! :skills/bow)
+  (learn-skill! :skills/double-fireball))
 
-; so The whole DB as one schema?
+ ; Testing effect/target-entity
+ ; stops when target not exists anymore
+ ; or out of range
 
-[:s/map
- [:properties/audiovisuals
-  :properties/creatures
-  :properties/items
-  :properties/projectiles
-  :properties/skills
-  :properties/worlds]]
+ ; 1. start application
+ ; 2. start world
+ ; 3. create creature
+ (post-runnable #(world/creature %
+                                 {:position [35 73]
+                                  :creature-id :creatures/dragon-red
+                                  :components {:entity/fsm {:fsm :fsms/npc
+                                                            :initial-state :npc-sleeping}
+                                               :entity/faction :evil}}))
 
+ (learn-skill! :skills/bow) ; 1.5 seconds attacktime
+ (post-tx! [:e/destroy (ids->eids 168)]) ; TODO how to get id ?
+ ; check state is normal ... omg...
 
-; and properties is map-of
-; id to audiovisuals properties
+ ; start world - small empty test room
+ ; 2 creatures - player?
+ ; start skill w. applicable needs target (bow)
+ ; this command:
+ ; check skill has stopped using
+ ; => this is basically a test for 'forge.entity.active'
 
-; so then properties/audiovisuals is just the propertiess
+ )
+
+(defn post-runnable [f]
+  (let [context @c/state]
+    (gdx/post-runnable context (f context))))
+
+(defn- learn-skill! [{:keys [cdq.context/player-eid] :as c} skill-id]
+  (world/add-skill c
+                   player-eid
+                   (c/build c skill-id)))
+
+(defn- create-item! [{:keys [cdq.context/player-eid] :as c} item-id]
+  (world/spawn-item c
+                    (:position @player-eid)
+                    (c/build c item-id)))
+
+(defn- mouseover-grid-cell [c]
+  @(world/grid-cell c (mapv int (c/world-mouse-position c))))
+
+(defn- class->label-str [class]
+  (case class
+    clojure.lang.LazySeq ""
+    (str class)))
+
+(defn- ->v-str [v]
+  (cond
+   (number? v) v
+   (keyword? v) v
+   (string? v) (pr-str v)
+   (boolean? v) v
+   (instance? clojure.lang.Atom v) (str "[LIME] Atom [] [GRAY]" (class @v) "[]")
+   (map? v) (str (class v))
+   (and (vector? v) (< (count v) 3)) (pr-str v)
+   (vector? v) (str "Vector " (count v))
+   (var? v) (str "[GOLD]" (:name (meta v)) "[] : " (->v-str @v))
+   :else (str "[ORANGE]" (class->label-str v) "[]")))
+
+; TODO truncate ...
+(defn- labelstr [k v]
+  (str
+   (if (keyword? k)
+     (str "[LIGHT_GRAY]:" (when-let [ns (namespace k)] (str ns "/")) " [] [WHITE]" (name k) "[]")
+     k)
+   ": [GOLD]" (str (->v-str v)) "[]"))
+
+(defn- add-elements! [node elements]
+  (doseq [element elements]
+    (.add node (t-node (ui/label (str (->v-str element)))))))
+
+#_(let [ns-sym (first (first (into {} (ns-value-vars))))]
+  ;(map ->v-str vars)
+  )
+
+(declare add-map-nodes!)
+
+(defn- children->str-map [children]
+  (zipmap (map str children)
+          children))
+
+(defn- add-nodes [node level v]
+  (when (map? v)
+    (add-map-nodes! node v (inc level)))
+
+  (when (coll? v)
+    (add-elements! node v))
+
+  (when (instance? com.badlogic.gdx.scenes.scene2d.Stage v)
+    (add-map-nodes! node (children->str-map (children (stage/root v))) level))
+
+  (when (instance? com.badlogic.gdx.scenes.scene2d.Group v)
+    (add-map-nodes! node (children->str-map (children v)) level))
+
+  (when (and (var? v)
+             (instance? com.badlogic.gdx.assets.AssetManager @v))
+    (add-map-nodes! node (bean @v) level)))
+
+(comment
+ (let [vis-image (first (children (.getRoot (stage-get))))]
+   (supers (class vis-image))
+   (str vis-image)
+   )
+ )
+
+(defn- add-map-nodes! [parent-node m level]
+  ;(println "Level: " level " - go deeper? " (< level 4))
+  (when (< level 2)
+    (doseq [[k v] (into (sorted-map) m)]
+      ;(println "add-map-nodes! k " k)
+      (try
+       (let [node (t-node (ui/label (labelstr k v)))]
+         (.add parent-node node) ; no t-node-add!: tree cannot be casted to tree-node ... , Tree itself different .add
+         #_(when (instance? clojure.lang.Atom v) ; StackOverFLow
+           (add-nodes node level @v))
+         (add-nodes node level v))
+       (catch Throwable t
+         (throw (ex-info "" {:k k :v v} t))
+         #_(.add parent-node (t-node (ui/label (str "[RED] "k " - " t))))
+
+         )))))
+
+(defn- scroll-pane-cell [rows]
+  (let [viewport (:gdl.context/viewport @c/state)
+        table (ui/table {:rows rows
+                         :cell-defaults {:pad 1}
+                         :pack? true})
+        scroll-pane (scroll-pane table)]
+    {:actor scroll-pane
+     :width (/ (:width viewport) 2)
+     :height
+     (- (:height viewport) 50)
+     #_(min (- (:height viewport) 50) (height table))}))
+
+(defn- show-tree-view! [m]
+  {:pre [(map? m)]}
+  (let [tree (ui/tree)]
+    (add-map-nodes! tree (into (sorted-map) m) 0)
+    (c/add-actor
+     (ui/window {:title "Tree View"
+                 :close-button? true
+                 :close-on-escape? true
+                 :center? true
+                 :rows [[(scroll-pane-cell [[tree]])]]
+                 :pack? true}))
+    nil))
+
+(defn get-namespaces [packages]
+  (filter #(packages (first (str/split (name (ns-name %)) #"\.")))
+          (all-ns)))
+
+(defn get-vars [nmspace condition]
+  (for [[sym avar] (ns-interns nmspace)
+       :when (condition avar)]
+    avar))
+
+(defn- protocol? [value]
+  (and (instance? clojure.lang.PersistentArrayMap value)
+       (:on value)))
+
+(defn- get-non-fn-vars [nmspace]
+  (get-vars nmspace (fn [avar]
+                      (let [value @avar]
+                        (not (or (fn? value)
+                                 (instance? clojure.lang.MultiFn value)
+                                 (protocol? value)
+                                 ; anonymous class (proxy)
+                                 (instance? java.lang.Class value)))))))
+
+(defn ns-value-vars
+  "Returns a map of ns-name to value-vars (non-function vars).
+  Use to understand the state of your application.
+
+  Example: `(ns-value-vars #{\"forge\"})`"
+  [packages]
+  (into {} (for [nmspace (get-namespaces packages)
+                 :let [value-vars (get-non-fn-vars nmspace)]
+                 :when (seq value-vars)]
+             [(ns-name nmspace) value-vars])))
+
+(defn print-app-values-tree [file namespaces-set]
+  (spit file
+        (with-out-str
+         (pprint
+          (for [[ns-name vars] (ns-value-vars namespaces-set)]
+            [ns-name (map #(:name (meta %)) vars)])))))
+
+(defn print-vimrc-names-forge-core-publics [ns-sym file]
+  (->> (find-ns ns-sym)
+       ns-publics
+       (remove (fn [[k v]]
+                 (or (:macro (meta v))
+                     (instance? java.lang.Class @v))))
+       (map (fn [s] (str "\"" (name (first s)) "\"")))
+       (str/join ", ")
+       (spit file)))
