a5e6c328d154015ce93e36672365ac78ffbf8a50
Michael Sappler <michaelsappler@gmail.com>
2025-01-08 07:39:34 +0100

    Move extend-types to the lwjgl3-backend

--- src/cdq/start.clj
+++ src/cdq/start.clj
@@ -1,7 +1,7 @@
 (ns cdq.start
-  (:require [clojure.edn :as edn]
-            [clojure.application :as application]
-            [clojure.gdx.backends.lwjgl3.application :as lwjgl3-app]
+  (:require [clojure.application :as application]
+            [clojure.edn :as edn]
+            [clojure.gdx.backends.lwjgl3.application :as lwjgl3]
             [clojure.java.io :as io]
             [cdq.app :as game])
   (:gen-class))
@@ -10,43 +10,20 @@
 
 (defn -main []
   (let [config (-> "app.edn" io/resource slurp edn/read-string)]
-    (lwjgl3-app/create (reify application/Listener
-                         (create [_ context]
-                           (reset! state (game/create context (:context config))))
+    (lwjgl3/create (reify application/Listener
+                     (create [_ context]
+                       (reset! state (game/create context (:context config))))
 
-                         (dispose [_]
-                           (game/dispose @state))
+                     (dispose [_]
+                       (game/dispose @state))
 
-                         (pause [_])
+                     (pause [_])
 
-                         (render [_]
-                           (swap! state game/render))
+                     (render [_]
+                       (swap! state game/render))
 
-                         (resize [_ width height]
-                           (game/resize @state width height))
+                     (resize [_ width height]
+                       (game/resize @state width height))
 
-                         (resume [_]))
-                       (:app config))))
-
-(extend-type com.badlogic.gdx.Files
-  clojure.files/Files
-  (internal [this path]
-    (.internal this path)))
-
-(extend-type com.badlogic.gdx.Graphics
-  clojure.graphics/Graphics
-  (delta-time [this]
-    (.getDeltaTime this))
-  (frames-per-second [this]
-    (.getFramesPerSecond this))
-  (new-cursor [this pixmap hotspot-x hotspot-y]
-    (.newCursor this pixmap hotspot-x hotspot-y))
-  (set-cursor [this cursor]
-    (.setCursor this cursor)))
-
-(extend-type com.badlogic.gdx.Application
-  clojure.application/Application
-  (exit [this]
-    (.exit this))
-  (post-runnable [this runnable]
-    (.postRunnable this runnable)))
+                     (resume [_]))
+                   (:app config))))
--- src/clojure/gdx/backends/lwjgl3/application.clj
+++ src/clojure/gdx/backends/lwjgl3/application.clj
@@ -1,7 +1,4 @@
 (ns clojure.gdx.backends.lwjgl3.application
-  "Provides an interface for creating and managing a LibGDX application using the LWJGL3 backend.
-   This namespace includes functionality for initializing the application with a listener and configuration,
-   as well as accessing the LibGDX global context."
   (:require [clojure.application :as app]
             [clojure.java.io :as io])
   (:import (java.awt Taskbar Toolkit)
@@ -11,12 +8,7 @@
            (com.badlogic.gdx.utils SharedLibraryLoader)
            (org.lwjgl.system Configuration)))
 
-(defn- context
-  "Returns a map of the LibGDX global context.
-   Call this function after the application's `create` method is executed.
-   The returned map provides convenient access to key components of the LibGDX framework,
-   such as graphics, audio, input, and file handling."
-  []
+(defn- context []
   {:clojure.gdx/app      Gdx/app
    :clojure.gdx/audio    Gdx/audio
    :clojure.gdx/files    Gdx/files
@@ -29,39 +21,10 @@
    :clojure.gdx/input    Gdx/input
    :clojure.gdx/net      Gdx/net})
 
-; TODO so what we want to achieve now is to not set 'Gdx'
-; so we have a stateless libgdx
-; => or rather do not expose functions which use Gdx internally
-; => can make PR's to libgdx to over methods w/o global state
-; or just port it
-; 1. of all don't set! the Gdx and see what breaks
-; fix that only
-
-(defn create
-  "Initializes and launches a LibGDX application using the LWJGL3 backend.
-
-   - `listener`: A record or object implementing the necessary application lifecycle methods
-     (`create`, `dispose`, `pause`, `render`, `resize`, and `resume`) to handle game logic.
-   - `config`: A map containing configuration options for the application, including:
-       - `:title` (string): The title of the application window.
-       - `:window-width` (int): The width of the application window.
-       - `:window-height` (int): The height of the application window.
-       - `:fps` (int): The desired frames per second for the application.
-       - `:icon` (string): Path to the icon image resource for the application.
-
-   This function sets up the LWJGL3 application with the specified configuration,
-   applies platform-specific settings (e.g., for macOS), and proxies lifecycle events
-   to the provided listener."
-  [listener config]
-  ; The API does _not_ do that !
-  ; just write an API - neither go into libgdx now nor into your game -
-  ; stay in the middle - not up or down -
-  ; a complete API
-  ; start with the classes you are using -
+(defn create [listener config]
   (.setIconImage (Taskbar/getTaskbar)
                  (.getImage (Toolkit/getDefaultToolkit)
                             (io/resource (:icon config))))
-  ; but what you could also do , move these 2 lines into libgdx ... ! its java code !
   (when SharedLibraryLoader/isMac
     (.set Configuration/GLFW_LIBRARY_NAME "glfw_async")
     #_(.set Configuration/GLFW_CHECK_THREAD0 false))
@@ -83,3 +46,26 @@
                         (.setWindowedMode (:window-width config)
                                           (:window-height config))
                         (.setForegroundFPS (:fps config)))))
+
+(extend-type com.badlogic.gdx.Files
+  clojure.files/Files
+  (internal [this path]
+    (.internal this path)))
+
+(extend-type com.badlogic.gdx.Graphics
+  clojure.graphics/Graphics
+  (delta-time [this]
+    (.getDeltaTime this))
+  (frames-per-second [this]
+    (.getFramesPerSecond this))
+  (new-cursor [this pixmap hotspot-x hotspot-y]
+    (.newCursor this pixmap hotspot-x hotspot-y))
+  (set-cursor [this cursor]
+    (.setCursor this cursor)))
+
+(extend-type com.badlogic.gdx.Application
+  clojure.application/Application
+  (exit [this]
+    (.exit this))
+  (post-runnable [this runnable]
+    (.postRunnable this runnable)))
