2e64ff638460e973951663b9157d9107825733ea
Michael Sappler <michaelsappler@gmail.com>
2025-01-10 17:17:04 +0100

    add comment visui dispose

--- src/cdq/app.clj
+++ src/cdq/app.clj
@@ -4,6 +4,9 @@
   (:gen-class))
 
 ; => TODO ui is not disposed - this is the VisUI skin and is global state there - so just define it in my context ?
+; then it doesn't know its disposed ? omg ...
+;:gdl.context/ui-skin (com.kotcrab.vis.ui.VisUI/getSkin)
+
 ; TODO tiled-map also dispose if new game state add
 ; this also if world restarts !!
 
