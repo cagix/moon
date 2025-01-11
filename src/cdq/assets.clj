3117feeb623f167386a7bcff41971b034e212a47
Michael Sappler <michaelsappler@gmail.com>
2025-01-11 16:04:49 +0100

    cdq.assets implements gdl.assets

--- src/cdq/assets.clj
+++ src/cdq/assets.clj
@@ -1,12 +1,42 @@
 (ns cdq.assets
-  (:require [clojure.gdx.assets :as assets]
-            [clojure.string :as str]
-            [gdl.utils.files :as files]))
+  (:require [clojure.string :as str]
+            [gdl.utils.files :as files])
+  (:import (clojure.lang IFn)
+           (com.badlogic.gdx.assets AssetManager)
+           (com.badlogic.gdx.audio Sound)
+           (com.badlogic.gdx.graphics Texture)
+           (gdl.assets Assets)))
+
+(def ^:private asset-type-class-map
+  {:sound   Sound
+   :texture Texture})
+
+(defn- asset-type->class [k]
+  (get asset-type-class-map k))
+
+(defn- class->asset-type [class]
+  (some (fn [[k v]] (when (= v class) k)) asset-type-class-map))
+
+(defn- blocking-load-all [assets]
+  (let [manager (proxy [AssetManager IFn Assets] []
+                  (invoke [^String path]
+                    (let [^AssetManager this this]
+                      (if (.contains this path)
+                        (.get this path)
+                        (throw (IllegalArgumentException. (str "Asset cannot be found: " path))))))
+                  (all_of_type [asset-type]
+                    (let [^AssetManager manager this]
+                      (filter #(= (class->asset-type (.getAssetType manager %)) asset-type)
+                              (.getAssetNames manager)))))]
+    (doseq [[file asset-type] assets]
+      (.load manager ^String file (asset-type->class asset-type)))
+    (.finishLoading manager)
+    manager))
 
 (defn create [{:keys [gdl/files] :as context} config]
   (assoc context
          :gdl/assets
-         (assets/blocking-load-all
+         (blocking-load-all
           (let [folder (::folder config)]
             (for [[asset-type exts] {:sound   #{"wav"}
                                      :texture #{"png" "bmp"}}
