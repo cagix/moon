fb745742e713aca818dceb3c4187a383638523fa
Michael Sappler <michaelsappler@gmail.com>
2025-01-11 21:19:00 +0100

    cdq.ui.actionbar/create

--- src/cdq/create.clj
+++ src/cdq/create.clj
@@ -16,27 +16,6 @@
             [cdq.val-max :as val-max]
             [cdq.widgets.inventory :as inventory]))
 
-(defn- action-bar-button-group []
-  (let [actor (ui-actor {})]
-    (.setName actor "action-bar/button-group")
-    (actor/set-user-object actor (ui/button-group {:max-check-count 1
-                                                   :min-check-count 0}))
-    actor))
-
-(defn- action-bar []
-  (let [group (ui/horizontal-group {:pad 2 :space 2})]
-    (.setUserObject group :ui/action-bar)
-    (group/add-actor! group (action-bar-button-group))
-    group))
-
-(defn- action-bar-table [_context]
-  (ui/table {:rows [[{:actor (action-bar)
-                      :expand? true
-                      :bottom? true}]]
-             :id :action-bar-table
-             :cell-defaults {:pad 2}
-             :fill-parent? true}))
-
 (defn- draw-player-message [{:keys [gdl.graphics/ui-viewport
                                     cdq.context/player-message] :as c}]
   (when-let [text (:text @player-message)]
