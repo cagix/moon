1f187c249027a8ccb1d7118089242d6bb087966a
Michael Sappler <michaelsappler@gmail.com>
2025-01-11 14:48:09 +0100

    add namespace graphics to gl

--- src/clojure/gdx.clj
+++ src/clojure/gdx.clj
@@ -2,14 +2,14 @@
   (:import (com.badlogic.gdx Gdx)))
 
 (defn context []
-  {:clojure/app      Gdx/app
-   :clojure/audio    Gdx/audio
-   :clojure/files    Gdx/files
-   :clojure/gl       Gdx/gl
-   :clojure/gl20     Gdx/gl20
-   :clojure/gl30     Gdx/gl30
-   :clojure/gl31     Gdx/gl31
-   :clojure/gl32     Gdx/gl32
-   :clojure/graphics Gdx/graphics
-   :clojure/input    Gdx/input
-   :clojure/net      Gdx/net})
+  {:clojure/app           Gdx/app
+   :clojure/audio         Gdx/audio
+   :clojure/files         Gdx/files
+   :clojure/graphics      Gdx/graphics
+   :clojure.graphics/gl   Gdx/gl
+   :clojure.graphics/gl20 Gdx/gl20
+   :clojure.graphics/gl30 Gdx/gl30
+   :clojure.graphics/gl31 Gdx/gl31
+   :clojure.graphics/gl32 Gdx/gl32
+   :clojure/input         Gdx/input
+   :clojure/net           Gdx/net})
