41f414b605ef62789045bb3517fbd41aaee82cba
Michael Sappler <michaelsappler@gmail.com>
2025-01-13 16:48:51 +0100

    forgot file

---
+++ src/cdq/db.clj
@@ -0,0 +1,15 @@
+(ns cdq.db
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.schema :as schema]))
+
+(defn create [_context _config]
+  (let [properties-file (io/resource "properties.edn")
+        schemas (-> "schema.edn" io/resource slurp edn/read-string)
+        properties (-> properties-file slurp edn/read-string)]
+    (assert (or (empty? properties)
+                (apply distinct? (map :property/id properties))))
+    (run! (partial schema/validate! schemas) properties)
+    {:db/data (zipmap (map :property/id properties) properties)
+     :db/properties-file properties-file
+     :db/schemas schemas}))
