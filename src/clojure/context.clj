0fb7726ae41184439059137feac023d6ea8c2c17
Michael Sappler <michaelsappler@gmail.com>
2025-01-13 12:47:02 +0100

    Move WASD to clojure.world

--- src/clojure/context.clj
+++ src/clojure/context.clj
@@ -7,7 +7,6 @@
             [clojure.interop :as interop]
             [clojure.graphics.tiled-map-renderer :as tiled-map-renderer]
             [clojure.scene2d.stage :as stage]
-            [clojure.math.vector2 :as v]
             [clojure.math.utils :refer [clamp]]
             [clojure.utils.viewport :as viewport]
             [clojure.graphics :as graphics]
@@ -231,13 +230,3 @@
                :close-on-escape? true
                :center? true
                :pack? true})))
-
-(defn WASD-movement-vector []
-  (let [r (when (input/key-pressed? :d) [1  0])
-        l (when (input/key-pressed? :a) [-1 0])
-        u (when (input/key-pressed? :w) [0  1])
-        d (when (input/key-pressed? :s) [0 -1])]
-    (when (or r l u d)
-      (let [v (v/add-vs (remove nil? [r l u d]))]
-        (when (pos? (v/length v))
-          v)))))
--- src/clojure/world.clj
+++ src/clojure/world.clj
@@ -483,7 +483,14 @@
                      (c/build c audiovisuals-id)))
 
 (defn player-movement-vector []
-  (c/WASD-movement-vector))
+  (let [r (when (input/key-pressed? :d) [1  0])
+        l (when (input/key-pressed? :a) [-1 0])
+        u (when (input/key-pressed? :w) [0  1])
+        d (when (input/key-pressed? :s) [0 -1])]
+    (when (or r l u d)
+      (let [v (v/add-vs (remove nil? [r l u d]))]
+        (when (pos? (v/length v))
+          v)))))
 
 (defn add-skill [c eid {:keys [property/id] :as skill}]
   {:pre [(not (entity/has-skill? @eid skill))]}
