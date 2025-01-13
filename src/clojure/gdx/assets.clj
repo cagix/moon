0a36ec1fed92d9e641bcc0841c33450b79068276
Michael Sappler <michaelsappler@gmail.com>
2025-01-13 12:52:09 +0100

    Fix clojure.gdx.assets

--- src/clojure/gdx/assets.clj
+++ src/clojure/gdx/assets.clj
@@ -1,13 +1,19 @@
-(ns cdq.assets)
+(ns clojure.gdx.assets
+  (:require [clojure.string :as str])
+  (:import (clojure.lang IFn)
+           (com.badlogic.gdx Gdx)
+           (com.badlogic.gdx.assets AssetManager)
+           (com.badlogic.gdx.audio Sound)
+           (com.badlogic.gdx.graphics Texture)))
 
 (defn manager
   ([_context _config]
    (manager
     (let [folder "resources/"]
-      (for [[asset-type extensions] {com.badlogic.gdx.audio.Sound      #{"wav"}
-                                     com.badlogic.gdx.graphics.Texture #{"png" "bmp"}}
-            file (map #(clojure.string/replace-first % folder "")
-                      (loop [[file & remaining] (.list (.internal com.badlogic.gdx.Gdx/files folder))
+      (for [[asset-type extensions] {Sound   #{"wav"}
+                                     Texture #{"png" "bmp"}}
+            file (map #(str/replace-first % folder "")
+                      (loop [[file & remaining] (.list (.internal Gdx/files folder))
                              result []]
                         (cond (nil? file)
                               result
@@ -22,9 +28,9 @@
                               (recur remaining result))))]
         [file asset-type]))))
   ([assets]
-   (let [manager (proxy [com.badlogic.gdx.assets.AssetManager clojure.lang.IFn] []
+   (let [manager (proxy [AssetManager clojure.lang.IFn] []
                    (invoke [^String path]
-                     (let [^com.badlogic.gdx.assets.AssetManager this this]
+                     (let [^AssetManager this this]
                        (if (.contains this path)
                          (.get this path)
                          (throw (IllegalArgumentException. (str "Asset cannot be found: " path)))))))]
