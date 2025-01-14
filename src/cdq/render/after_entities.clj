9f58b8fffde8bf8dc557afc43c65c4c0cb8f8b5d
Michael Sappler <michaelsappler@gmail.com>
2025-01-14 05:19:37 +0100

    cdq.render.after-entities/render

--- src/cdq/render/after_entities.clj
+++ src/cdq/render/after_entities.clj
@@ -1,1 +1,34 @@
-(ns cdq.render.after-entities)
+(ns cdq.render.after-entities
+  (:require [clojure.context :as c]
+            [clojure.graphics.shape-drawer :as sd]
+            [clojure.grid :as grid]
+            [clojure.math.shapes :refer [circle->outer-rectangle]]))
+
+(defn- geom-test [{:keys [clojure.graphics/shape-drawer
+                          clojure.context/grid]
+                   :as c}]
+  (let [position (c/world-mouse-position c)
+        radius 0.8
+        circle {:position position :radius radius}]
+    (sd/circle shape-drawer position radius [1 0 0 0.5])
+    (doseq [[x y] (map #(:position @%) (grid/circle->cells grid circle))]
+      (sd/rectangle shape-drawer x y 1 1 [1 0 0 0.5]))
+    (let [{[x y] :left-bottom :keys [width height]} (circle->outer-rectangle circle)]
+      (sd/rectangle shape-drawer x y width height [0 0 1 1]))))
+
+(def ^:private ^:dbg-flag highlight-blocked-cell? true)
+
+(defn- highlight-mouseover-tile [{:keys [clojure.graphics/shape-drawer
+                                         clojure.context/grid] :as c}]
+  (when highlight-blocked-cell?
+    (let [[x y] (mapv int (c/world-mouse-position c))
+          cell (grid [x y])]
+      (when (and cell (#{:air :none} (:movement @cell)))
+        (sd/rectangle shape-drawer x y 1 1
+                      (case (:movement @cell)
+                        :air  [1 1 0 0.5]
+                        :none [1 0 0 0.5]))))))
+
+(defn render [c]
+  #_(geom-test c)
+  (highlight-mouseover-tile c))
--- src/clojure/world/graphics.clj
+++ src/clojure/world/graphics.clj
@@ -3,10 +3,8 @@
             [clojure.context.timer :as timer]
             [clojure.error :refer [pretty-pst]]
             [clojure.graphics.shape-drawer :as sd]
-            [clojure.math.shapes :refer [circle->outer-rectangle]]
             [clojure.utils :refer [defsystem sort-by-order]]
             [clojure.val-max :as val-max]
-            [clojure.grid :as grid]
             [clojure.world :refer [active-entities
                                    render-z-order
                                    line-of-sight?
@@ -15,35 +13,6 @@
                                    world-item?
                                    item-place-position]]
             [clojure.entity :as entity]))
-
-(defn- geom-test [{:keys [clojure.graphics/shape-drawer
-                          clojure.context/grid]
-                   :as c}]
-  (let [position (c/world-mouse-position c)
-        radius 0.8
-        circle {:position position :radius radius}]
-    (sd/circle shape-drawer position radius [1 0 0 0.5])
-    (doseq [[x y] (map #(:position @%) (grid/circle->cells grid circle))]
-      (sd/rectangle shape-drawer x y 1 1 [1 0 0 0.5]))
-    (let [{[x y] :left-bottom :keys [width height]} (circle->outer-rectangle circle)]
-      (sd/rectangle shape-drawer x y width height [0 0 1 1]))))
-
-(def ^:private ^:dbg-flag highlight-blocked-cell? true)
-
-(defn- highlight-mouseover-tile [{:keys [clojure.graphics/shape-drawer
-                                         clojure.context/grid] :as c}]
-  (when highlight-blocked-cell?
-    (let [[x y] (mapv int (c/world-mouse-position c))
-          cell (grid [x y])]
-      (when (and cell (#{:air :none} (:movement @cell)))
-        (sd/rectangle shape-drawer x y 1 1
-                      (case (:movement @cell)
-                        :air  [1 1 0 0.5]
-                        :none [1 0 0 0.5]))))))
-
-(defn render-after-entities [c]
-  #_(geom-test c)
-  (highlight-mouseover-tile c))
 
 (defn- draw-skill-image [{:keys [clojure.graphics/shape-drawer] :as c} image entity [x y] action-counter-ratio]
   (let [[width height] (:world-unit-dimensions image)
