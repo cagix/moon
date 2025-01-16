8ff3c6e7782bc5656cddebdef976e85d0b5eebe9
Michael Sappler <michaelsappler@gmail.com>
2025-01-16 14:36:28 +0100

    src/cdq/world/grid.clj

---
+++ src/cdq/world/grid.clj
@@ -0,0 +1,54 @@
+(ns cdq.world.grid
+  (:require [clojure.grid :as grid]
+            [clojure.grid2d :as g2d]
+            [clojure.tiled :as tiled]
+            [clojure.utils :refer [tile->middle]]))
+
+(defrecord RCell [position
+                  middle ; only used @ potential-field-follow-to-enemy -> can remove it.
+                  adjacent-cells
+                  movement
+                  entities
+                  occupied
+                  good
+                  evil]
+  grid/Cell
+  (blocked? [_ z-order]
+    (case movement
+      :none true ; wall
+      :air (case z-order ; water/doodads
+             :z-order/flying false
+             :z-order/ground true)
+      :all false)) ; ground/floor
+
+  (blocks-vision? [_]
+    (= movement :none))
+
+  (occupied-by-other? [_ eid]
+    (some #(not= % eid) occupied))
+
+  (nearest-entity [this faction]
+    (-> this faction :eid))
+
+  (nearest-entity-distance [this faction]
+    (-> this faction :distance)))
+
+(defn ->grid-cell [position movement]
+  {:pre [(#{:none :air :all} movement)]}
+  (map->RCell
+   {:position position
+    :middle (tile->middle position)
+    :movement movement
+    :entities #{}
+    :occupied #{}}))
+
+(defn create [{:keys [clojure.context/tiled-map]}]
+  (g2d/create-grid
+   (tiled/tm-width tiled-map)
+   (tiled/tm-height tiled-map)
+   (fn [position]
+     (atom (->grid-cell position
+                        (case (tiled/movement-property tiled-map position)
+                          "none" :none
+                          "air"  :air
+                          "all"  :all))))))
--- src/clojure/create.clj
+++ src/clojure/create.clj
@@ -5,55 +5,6 @@
             [clojure.world :refer [spawn-creature]]
             [clojure.grid :as grid]))
 
-(defrecord RCell [position
-                  middle ; only used @ potential-field-follow-to-enemy -> can remove it.
-                  adjacent-cells
-                  movement
-                  entities
-                  occupied
-                  good
-                  evil]
-  grid/Cell
-  (blocked? [_ z-order]
-    (case movement
-      :none true ; wall
-      :air (case z-order ; water/doodads
-             :z-order/flying false
-             :z-order/ground true)
-      :all false)) ; ground/floor
-
-  (blocks-vision? [_]
-    (= movement :none))
-
-  (occupied-by-other? [_ eid]
-    (some #(not= % eid) occupied))
-
-  (nearest-entity [this faction]
-    (-> this faction :eid))
-
-  (nearest-entity-distance [this faction]
-    (-> this faction :distance)))
-
-(defn ->grid-cell [position movement]
-  {:pre [(#{:none :air :all} movement)]}
-  (map->RCell
-   {:position position
-    :middle (tile->middle position)
-    :movement movement
-    :entities #{}
-    :occupied #{}}))
-
-(defn create-grid [tiled-map]
-  (g2d/create-grid
-   (tiled/tm-width tiled-map)
-   (tiled/tm-height tiled-map)
-   (fn [position]
-     (atom (->grid-cell position
-                        (case (tiled/movement-property tiled-map position)
-                          "none" :none
-                          "air"  :air
-                          "all"  :all))))))
-
 (defn player-entity-props [start-position]
   {:position (tile->middle start-position)
    :creature-id :creatures/vampire
@@ -101,9 +52,6 @@
 
 (defn tiled-map* [context]
   (:tiled-map (:clojure.context/level context)))
-
-(defn grid* [context]
-  (create-grid (:clojure.context/tiled-map context)))
 
 (defn entity-ids* [_context]
   (atom {}))
