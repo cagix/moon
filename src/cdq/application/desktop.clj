c9f8b58e2b0def9837bb4ae197b98ec16941b1a5
Michael Sappler <michaelsappler@gmail.com>
2025-01-17 14:20:22 +0100

    Simplify cdq.application.desktop

--- src/cdq/application/desktop.clj
+++ src/cdq/application/desktop.clj
@@ -1,20 +1,52 @@
 (ns cdq.application.desktop
-  (:require [clojure.gdx.backends.lwjgl :as lwjgl]
-            [clojure.utils :as utils]))
+  (:require [clojure.application]
+            [clojure.gdx.backends.lwjgl]))
 
-(defn start [{:keys [config listener]}]
-  (lwjgl/application (proxy [com.badlogic.gdx.ApplicationAdapter] []
-                       (create []
-                         (utils/req-resolve-call (:create listener)))
+(defn start [{:keys [config
+                     create
+                     render
+                     resize]}]
+  (clojure.gdx.backends.lwjgl/application
+   (proxy [com.badlogic.gdx.ApplicationAdapter] []
+     (create []
+       (clojure.application/create create))
 
-                       (dispose []
-                         (utils/req-resolve-call (:dispose listener)))
+     (dispose []
+       (clojure.application/dispose))
 
-                       (render []
-                         (utils/req-resolve-call (:render listener)))
+     (render []
+       (clojure.application/render render))
 
-                       (resize [width height]
-                         (utils/req-resolve-call (:resize listener) width height)))
-                     config))
+     (resize [width height]
+       (clojure.application/resize resize width height)))
+   config))
 
 
+; Badlogic => 'move to 'clojure.gdx' or whatever name it will be...
+; * ApplicationAdapter
+; * Color
+; * SharedLibraryLoader
+; * FitViewport
+; * Viewport
+; * Gdx
+; * BitmapFont
+; * Texture / TextureRegion
+; * Camera OrthographicCamera
+; * FileHandle
+; * Pixmap
+; * ScreenUtils
+; * com.badlogic.gdx.graphics.g2d.SpriteBatch
+; * com.badlogic.gdx.graphics.Colors/put
+; * Input$Buttons / Input$Keys
+; * Align
+; com.badlogic.gdx.maps.tiled TmxMapLoader
+; * math
+; * sound
+; * disposable
+; * batch
+
+; * com.badlogic.gdx.graphics.g2d.freetype
+
+; * scene2d ...
+
+; com.badlogic.gdx.assets AssetManager
