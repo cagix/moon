1a61dafafeec9cbecc3976ec0c548da5f4c1593e
Michael Sappler <michaelsappler@gmail.com>
2025-01-17 02:28:34 +0100

    Add missing Lwjgl3Application API methods

--- src/clojure/gdx/backends/lwjgl.clj
+++ src/clojure/gdx/backends/lwjgl.clj
@@ -64,3 +64,40 @@
                         (resize [width height]
                           (utils/req-resolve-call (:resize listener) width height)))
                       (config/create config)))
+
+(defn window
+  "Creates a new Lwjgl3Window using the provided listener and Lwjgl3WindowConfiguration. This function only just instantiates a Lwjgl3Window and returns immediately. The actual window creation is postponed with Application.postRunnable(Runnable) until after all existing windows are updated."
+  [application listener window-config]
+  (Lwjgl3Application/.newWindow application listener window-config))
+
+
+(defn set-gl-debug-message-control
+  "Enables or disables GL debug messages for the specified severity level. Returns false if the severity level could not be set (e.g. the NOTIFICATION level is not supported by the ARB and AMD extensions). See Lwjgl3ApplicationConfiguration.enableGLDebugOutput(boolean, PrintStream)"
+  [severity enabled?]
+  (Lwjgl3Application/setGLDebugMessageControl severity #_(k->severity severity)
+                                              (boolean enabled?)))
+
+
+(defn display-mode
+  "the currently active Graphics.DisplayMode of the primary or the given monitor"
+  ([monitor]
+   (Lwjgl3ApplicationConfiguration/getDisplayMode monitor))
+  ([]
+   (Lwjgl3ApplicationConfiguration/getDisplayMode)))
+
+(defn display-modes
+  "the available Graphics.DisplayModes of the primary or the given monitor"
+  ([monitor]
+   (Lwjgl3ApplicationConfiguration/getDisplayModes monitor))
+  ([]
+   (Lwjgl3ApplicationConfiguration/getDisplayModes)))
+
+(defn primary-monitor
+  "the primary Graphics.Monitor"
+  []
+  (Lwjgl3ApplicationConfiguration/getPrimaryMonitor))
+
+(defn monitors
+  "the connected Graphics.Monitors"
+  []
+  (Lwjgl3ApplicationConfiguration/getMonitors))
