44b55466fed1a325d5b430bd5282082d20386eb6
Michael Sappler <michaelsappler@gmail.com>
2025-01-17 02:32:25 +0100

    Move custom lwjgl3gdx port to dropbox

--- src/clojure/gdx/backends/lwjgl.clj
+++ src/clojure/gdx/backends/lwjgl.clj
@@ -1,53 +1,10 @@
 (ns clojure.gdx.backends.lwjgl
   (:require [clojure.gdx.backends.lwjgl-config :as config]
             [clojure.utils :as utils])
-  (:import (com.badlogic.gdx ApplicationAdapter Gdx)
+  (:import (com.badlogic.gdx ApplicationAdapter)
            (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
                                              Lwjgl3ApplicationConfiguration
-                                             Lwjgl3ApplicationConfiguration$GLEmulation
-                                             Lwjgl3ApplicationLogger
-                                             Lwjgl3Clipboard
-                                             Lwjgl3NativesLoader
-                                             Lwjgl3Net
-                                             Lwjgl3Window
-                                             Sync)
-           (com.badlogic.gdx.backends.lwjgl3.audio.mock MockAudio)
-           (com.badlogic.gdx.utils GdxRuntimeException)))
-
-(defn- create [listener config]
-  (when (= (.glEmulation config) Lwjgl3ApplicationConfiguration$GLEmulation/ANGLE_GLES20)
-    (println "Lwjgl3ApplicationConfiguration$GLEmulation/ANGLE_GLES20; " Lwjgl3ApplicationConfiguration$GLEmulation/ANGLE_GLES20)
-    (Lwjgl3Application/loadANGLE))
-  (Lwjgl3Application/initializeGlfw)
-  (let [this (proxy [Lwjgl3Application] [])
-        config (Lwjgl3ApplicationConfiguration/copy config)]
-    (.setApplicationLogger this (Lwjgl3ApplicationLogger.))
-    (set! (.config this) config)
-    (set! Gdx/app this)
-    (set! (.audio this) (if (.disableAudio config)
-                          (MockAudio.)
-                          (try (.createAudio this config)
-                               (catch Throwable t
-                                 (.log this "Lwjgl3Application", "Couldn't initialize audio, disabling audio", t)
-                                 (MockAudio.)))))
-    (set! Gdx/audio (.audio this))
-    (set! (.files this) (.createFiles this))
-    (set! Gdx/files (.files this))
-    (set! (.clipboard this) (Lwjgl3Clipboard.))
-    (set! (.sync this) (Sync.))
-    (let [window (.createWindow this config listener 0)]
-      (when (= (.glEmulation config) Lwjgl3ApplicationConfiguration$GLEmulation/ANGLE_GLES20)
-        (Lwjgl3Application/postLoadANGLE))
-      (.add (.windows this) window))
-    (try
-     (.loop this)
-     (.cleanupWindows this)
-     (catch Throwable t
-       (if (instance? RuntimeException t)
-         (throw t)
-         (throw (GdxRuntimeException. t))))
-     (finally
-      (.cleanup this)))))
+                                             Lwjgl3ApplicationConfiguration$GLEmulation)))
 
 ; TODO option w/o config also possible -> taking default then
 (defn application [{:keys [config
