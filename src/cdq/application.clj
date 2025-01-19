7fb1b3408c2580baa662af2a3139fd645eb6ec6c
Michael Sappler <michaelsappler@gmail.com>
2025-01-19 19:18:54 +0100

    render/resize/dispose all in application

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -1,9 +1,8 @@
 (ns cdq.application
-  (:require cdq.application.dispose
-            cdq.application.render
-            cdq.application.resize
+  (:require cdq.utils
             clojure.gdx.application
             clojure.gdx.backends.lwjgl
+            [cdq.gdx.utils.viewport :as viewport]
             clojure.java.io))
 
 (def create-components
@@ -36,6 +35,35 @@
     [:world/potential-field-cache       cdq.create.potential-fields]
     [:cdq.context/player-eid            cdq.create.player-eid]])
 
+(def render-fns
+  '[(cdq.application.render.assoc-active-entities/render)
+    (cdq.render.camera/set-on-player)
+    (cdq.gdx.graphics/clear-screen)
+    (cdq.render.tiled-map/draw)
+    (cdq.graphics/draw-on-world-view [(cdq.render.before-entities/render)
+                                      (cdq.world.graphics/render-entities
+                                       {:below {:entity/mouseover? cdq.world.graphics/draw-faction-ellipse
+                                                :player-item-on-cursor cdq.world.graphics/draw-world-item-if-exists
+                                                :stunned cdq.world.graphics/draw-stunned-circle}
+                                        :default {:entity/image cdq.world.graphics/draw-image-as-of-body
+                                                  :entity/clickable cdq.world.graphics/draw-text-when-mouseover-and-text
+                                                  :entity/line-render cdq.world.graphics/draw-line}
+                                        :above {:npc-sleeping cdq.world.graphics/draw-zzzz
+                                                :entity/string-effect cdq.world.graphics/draw-text
+                                                :entity/temp-modifier cdq.world.graphics/draw-filled-circle-grey}
+                                        :info {:entity/hp cdq.world.graphics/draw-hpbar-when-mouseover-and-not-full
+                                               :active-skill cdq.world.graphics/draw-skill-image-and-active-effect}})
+                                      (cdq.render.after-entities/render)])
+    (cdq.render/draw-stage)
+    (cdq.render/update-stage)
+    (cdq.render/player-state-input)
+    (cdq.render/update-mouseover-entity)
+    (cdq.render/update-paused)
+    (cdq.render/when-not-paused)
+    (cdq.render/remove-destroyed-entities)
+    (cdq.render/camera-controls)
+    (cdq.render/window-controls)])
+
 (def state (atom nil))
 
 ; oh ok ! or configure a libgdx application on the fly with clojure.core/fetch-deps or fetch-libs
@@ -61,15 +89,24 @@
                                                                     create-components)))
 
                                             (dispose [_]
-                                              (cdq.application.dispose/context @state))
+                                              (doseq [[k value] @state
+                                                      :when (cdq.utils/disposable? value)]
+                                                ;(println "Disposing " k " - " value)
+                                                (cdq.utils/dispose value)))
 
                                             (pause [_])
 
                                             (render [_]
-                                              (swap! state cdq.application.render/context))
+                                              (swap! state (fn [context]
+                                                             (reduce (fn [context fn-invoc]
+                                                                       (cdq.utils/req-resolve-call fn-invoc context))
+                                                                     context
+                                                                     render-fns))))
 
                                             (resize [_ width height]
-                                              (cdq.application.resize/context @state width height))
+                                              (let [context @state]
+                                                (viewport/update (:cdq.graphics/ui-viewport    context) width height :center-camera? true)
+                                                (viewport/update (:cdq.graphics/world-viewport context) width height)))
 
                                             (resume [_]))
                                           {:title "Cyber Dungeon Quest"
--- src/cdq/application/resize.clj
+++
@@ -1,6 +0,0 @@
-(ns cdq.application.resize
-  (:require [cdq.gdx.utils.viewport :as viewport]))
-
-(defn context [context width height]
-  (viewport/update (:cdq.graphics/ui-viewport    context) width height :center-camera? true)
-  (viewport/update (:cdq.graphics/world-viewport context) width height))
