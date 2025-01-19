5feb2953aad93f73e585f78b9168b4df6fe74182
Michael Sappler <michaelsappler@gmail.com>
2025-01-19 13:58:55 +0100

    Use patched reduce-fsm lib

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [com.badlogicgames.gdx/gdx-freetype-platform "1.13.0" :classifier "natives-desktop"]
                  [com.kotcrab.vis/vis-ui "1.5.2"]
                  [metosin/malli "0.13.0"]
-                 [com.github.damn/reduce-fsm "eb1a2c1ff0"]
+                 [com.github.cdorrat/reduce-fsm "fe1c914d68"]
                  [com.github.damn/clojure.dev-loop "ef54a03"]]
   :java-source-paths ["src-java"]
   :plugins [[lein-hiera "2.0.0"]
