48c2437b358dec088ad01c36d617d8a41211ce79
Michael Sappler <michaelsappler@gmail.com>
2025-01-19 13:56:47 +0100

    Move dev-loop to separate repo

--- project.clj
+++ project.clj
@@ -7,10 +7,8 @@
                  [com.badlogicgames.gdx/gdx-freetype-platform "1.13.0" :classifier "natives-desktop"]
                  [com.kotcrab.vis/vis-ui "1.5.2"]
                  [metosin/malli "0.13.0"]
-                 [org.clj-commons/pretty "3.2.0"]
-                 [nrepl "0.9.0"]
-                 [org.clojure/tools.namespace "1.3.0"]
-                 [com.github.damn/reduce-fsm "eb1a2c1ff0"]] ; TODO use updated main version
+                 [com.github.damn/reduce-fsm "eb1a2c1ff0"]
+                 [com.github.damn/clojure.dev-loop "ef54a03"]]
   :java-source-paths ["src-java"]
   :plugins [[lein-hiera "2.0.0"]
             [lein-codox "0.10.8"]]
