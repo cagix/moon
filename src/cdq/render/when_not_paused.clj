90b3789740ab3faedd187f3dde68f4504fb394ca
Michael Sappler <michaelsappler@gmail.com>
2025-01-22 14:10:29 +0100

    req-resolve-call @ cdq.render.when-not-paused

--- src/cdq/render/when_not_paused.clj
+++ src/cdq/render/when_not_paused.clj
@@ -1,13 +1,11 @@
-(ns cdq.render.when-not-paused
-  (:require cdq.render.when-not-paused.update-time
-            cdq.render.when-not-paused.update-potential-fields
-            cdq.render.when-not-paused.tick-entities))
+(ns cdq.render.when-not-paused)
 
 (defn render [context]
   (if (:cdq.context/paused? context)
     context
-    (reduce (fn [context f] (f context))
+    (reduce (fn [context f]
+              (cdq.utils/req-resolve-call f context))
             context
-            [cdq.render.when-not-paused.update-time/render
-             cdq.render.when-not-paused.update-potential-fields/render
-             cdq.render.when-not-paused.tick-entities/render])))
+            '[(cdq.render.when-not-paused.update-time/render)
+              (cdq.render.when-not-paused.update-potential-fields/render)
+              (cdq.render.when-not-paused.tick-entities/render)])))
