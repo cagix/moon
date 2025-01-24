956790bbee962c1f12b877b0c01d710f160301ce
Michael Sappler <michaelsappler@gmail.com>
2025-01-24 06:51:43 +0100

    Move comment up

--- src/cdq/render/remove_destroyed_entities.clj
+++ src/cdq/render/remove_destroyed_entities.clj
@@ -2,7 +2,6 @@
   (:require cdq.world))
 
 (defn render [{:keys [cdq.context/entity-ids] :as c}]
-  ; do not pause this as for example pickup item, should be destroyed => make test & remove comment.
   (doseq [eid (filter (comp :entity/destroyed? deref)
                       (vals @entity-ids))]
     (cdq.world/remove-entity c eid)
