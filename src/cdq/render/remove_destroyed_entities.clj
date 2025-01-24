edd5f2d05d99547dcd5d60ba479d1ac04c66dcc1
Michael Sappler <michaelsappler@gmail.com>
2025-01-24 06:52:52 +0100

    Type out context

--- src/cdq/render/remove_destroyed_entities.clj
+++ src/cdq/render/remove_destroyed_entities.clj
@@ -1,10 +1,12 @@
 (ns cdq.render.remove-destroyed-entities
   (:require cdq.world))
 
-(defn render [{:keys [cdq.context/entity-ids] :as c}]
+(defn render [{:keys [cdq.context/entity-ids
+                      context/entity-components]
+               :as context}]
   (doseq [eid (filter (comp :entity/destroyed? deref)
                       (vals @entity-ids))]
-    (cdq.world/remove-entity c eid)
-    (doseq [component @eid]
-      (cdq.world/destroy! component eid c)))
-  c)
+    (cdq.world/remove-entity context eid)
+    (doseq [[k v] @eid]
+      (cdq.world/destroy! component eid context)))
+  context)
