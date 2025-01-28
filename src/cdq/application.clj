ab9591a9af21cb988a26dfea951cc03b42328e82
Michael Sappler <michaelsappler@gmail.com>
2025-01-28 20:02:52 +0100

    Move dispose/render/resize to cdq.application.context

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -1,118 +1,33 @@
 (ns cdq.application
-  (:require [cdq.application.context :as context]
-            [clojure.gdx.application :as application]
-            [clojure.gdx.backends.lwjgl :as lwjgl]
-            [clojure.gdx.utils :as utils]
-            [clojure.gdx.utils.viewport :as viewport]
-            [clojure.utils]))
+  (:require cdq.application.context
+            clojure.gdx.application
+            clojure.gdx.backends.lwjgl
+            clojure.gdx.utils
+            clojure.utils))
 
 (def state (atom nil))
-
-(def context-keyset
-  #{:cdq/assets
-    :cdq/db
-    :cdq/effects
-    :cdq/schemas
-    :cdq.context/content-grid
-
-    ; delta-time gets added after first frame
-    :cdq.context/delta-time
-
-    :cdq.context/elapsed-time
-    :cdq.context/entity-ids
-    :cdq.context/error
-    :cdq.context/explored-tile-corners
-    :cdq.context/factions-iterations
-    :cdq.context/grid
-    :cdq.context/level
-    :cdq.context/mouseover-eid
-    :cdq.context/paused?
-    :cdq.context/player-eid
-    :cdq.context/player-message
-    :cdq.context/raycaster
-    :cdq.context/stage
-    :cdq.context/tiled-map
-    :cdq.game/active-entities
-    :cdq.graphics/batch
-    :cdq.graphics/cursors
-    :cdq.graphics/default-font
-    :cdq.graphics/shape-drawer
-    :cdq.graphics/shape-drawer-texture
-    :cdq.graphics/tiled-map-renderer
-    :cdq.graphics/ui-viewport
-    :cdq.graphics/world-unit-scale
-    :cdq.graphics/world-viewport
-    :context/entity-components
-    :world/potential-field-cache}
-  )
-
-(comment
- (clojure.pprint/pprint (sort (keys @state)))
-
- (= context-keyset (set (keys @state)))
- ; => validate each frame !
- ; => after each render/effect ?
- )
 
 (defn -main []
   (clojure.utils/execute! (get {:mac '[(clojure.java.awt.taskbar/set-icon "moon.png")
                                        (clojure.lwjgl.system.configuration/set-glfw-library-name "glfw_async")]}
                                (clojure.gdx.utils/operating-system)))
-  (lwjgl/application (reify application/Listener
-                       (create [_]
-                         (reset! state (context/create)))
+  (clojure.gdx.backends.lwjgl/application (reify clojure.gdx.application/Listener
+                                            (create [_]
+                                              (reset! state (cdq.application.context/create)))
 
-                       (dispose [_]
-                         (comment
-                          (context/dispose @state)
-                          )
+                                            (dispose [_]
+                                              (cdq.application.context/dispose @state))
 
-                         (doseq [[k value] @state
-                                 :when (utils/disposable? value)]
-                           (utils/dispose value)))
+                                            (pause [_])
 
-                       (pause [_])
+                                            (render [_]
+                                              (swap! state cdq.application.context/render))
 
-                       (render [_]
-                         (comment
-                          (swap! state context/render)
-                          )
+                                            (resize [_ width height]
+                                              (cdq.application.context/resize @state width height))
 
-                         (swap! state (fn [context]
-                                        (reduce (fn [context f]
-                                                  (f context))
-                                                context
-                                                (for [ns-sym '[cdq.render.assoc-active-entities
-                                                               cdq.render.set-camera-on-player
-                                                               cdq.render.clear-screen
-                                                               cdq.render.tiled-map
-                                                               cdq.render.draw-on-world-view
-                                                               cdq.render.stage
-                                                               cdq.render.player-state-input
-                                                               cdq.render.update-mouseover-entity
-                                                               cdq.render.update-paused
-                                                               cdq.render.when-not-paused
-
-                                                               ; do not pause this as for example pickup item, should be destroyed => make test & remove comment.
-                                                               cdq.render.remove-destroyed-entities
-
-                                                               cdq.render.camera-controls
-                                                               cdq.render.window-controls]]
-                                                  (do
-                                                   (require ns-sym)
-                                                   (resolve (symbol (str ns-sym "/render")))))))))
-
-                       (resize [_ width height]
-                         (comment
-                          (context/resize @state width height)
-                          )
-
-                         (let [context @state]
-                           (viewport/update (:cdq.graphics/ui-viewport    context) width height :center-camera? true)
-                           (viewport/update (:cdq.graphics/world-viewport context) width height)))
-
-                       (resume [_]))
-                     {:title "Cyber Dungeon Quest"
-                      :windowed-mode {:width 1440
-                                      :height 900}
-                      :foreground-fps 60}))
+                                            (resume [_]))
+                                          {:title "Cyber Dungeon Quest"
+                                           :windowed-mode {:width 1440
+                                                           :height 900}
+                                           :foreground-fps 60}))
--- src/cdq/application/context.clj
+++ src/cdq/application/context.clj
@@ -16,7 +16,56 @@
             [cdq.create.world-viewport :as world-viewport]
             cdq.context
             cdq.impl.world.spawn-entity
-            cdq.world.context))
+            cdq.world.context
+            [clojure.gdx.utils :as utils]
+            [clojure.gdx.utils.viewport :as viewport]))
+
+(def context-keyset
+  #{:cdq/assets
+    :cdq/db
+    :cdq/effects
+    :cdq/schemas
+    :cdq.context/content-grid
+
+    ; delta-time gets added after first frame
+    :cdq.context/delta-time
+
+    :cdq.context/elapsed-time
+    :cdq.context/entity-ids
+    :cdq.context/error
+    :cdq.context/explored-tile-corners
+    :cdq.context/factions-iterations
+    :cdq.context/grid
+    :cdq.context/level
+    :cdq.context/mouseover-eid
+    :cdq.context/paused?
+    :cdq.context/player-eid
+    :cdq.context/player-message
+    :cdq.context/raycaster
+    :cdq.context/stage
+    :cdq.context/tiled-map
+    :cdq.game/active-entities
+    :cdq.graphics/batch
+    :cdq.graphics/cursors
+    :cdq.graphics/default-font
+    :cdq.graphics/shape-drawer
+    :cdq.graphics/shape-drawer-texture
+    :cdq.graphics/tiled-map-renderer
+    :cdq.graphics/ui-viewport
+    :cdq.graphics/world-unit-scale
+    :cdq.graphics/world-viewport
+    :context/entity-components
+    :world/potential-field-cache}
+  )
+
+(comment
+ (clojure.pprint/pprint (sort (keys @state)))
+
+ (= context-keyset (set (keys @state)))
+ ; => validate each frame !
+ ; => after each render/effect ?
+ )
+
 
 (defn create []
   (let [schemas (schemas/create)
@@ -40,3 +89,36 @@
                   :cdq/schemas schemas
                   :cdq.context/stage (stage/create batch ui-viewport)}) ]
     (cdq.world.context/reset context :worlds/vampire)))
+
+(defn dispose [context]
+  (doseq [[_k value] context
+          :when (utils/disposable? value)]
+    (utils/dispose value)))
+
+(defn render [context]
+  (reduce (fn [context f]
+            (f context))
+          context
+          (for [ns-sym '[cdq.render.assoc-active-entities
+                         cdq.render.set-camera-on-player
+                         cdq.render.clear-screen
+                         cdq.render.tiled-map
+                         cdq.render.draw-on-world-view
+                         cdq.render.stage
+                         cdq.render.player-state-input
+                         cdq.render.update-mouseover-entity
+                         cdq.render.update-paused
+                         cdq.render.when-not-paused
+
+                         ; do not pause this as for example pickup item, should be destroyed => make test & remove comment.
+                         cdq.render.remove-destroyed-entities
+
+                         cdq.render.camera-controls
+                         cdq.render.window-controls]]
+            (do
+             (require ns-sym)
+             (resolve (symbol (str ns-sym "/render")))))))
+
+(defn resize [context width height]
+  (viewport/update (:cdq.graphics/ui-viewport    context) width height :center-camera? true)
+  (viewport/update (:cdq.graphics/world-viewport context) width height))
