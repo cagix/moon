952432f9fb74089cadf41b6bb0adfd3f0dd6f293
Michael Sappler <michaelsappler@gmail.com>
2025-04-19 18:09:23 +0530

    Remove clojure.gdx.files.file-handle wrapper

--- src/cdq/create/assets.clj
+++ src/cdq/create/assets.clj
@@ -1,8 +1,8 @@
 (ns cdq.create.assets
   (:require [clojure.gdx.assets :as assets]
-            [clojure.gdx.files.file-handle :as fh]
             [clojure.string :as str])
-  (:import (com.badlogic.gdx Gdx)))
+  (:import (com.badlogic.gdx Gdx)
+           (com.badlogic.gdx.files FileHandle)))
 
 (defn create []
   (assets/create
@@ -10,16 +10,16 @@
      (for [[asset-type extensions] {:sound   #{"wav"}
                                     :texture #{"png" "bmp"}}
            file (map #(str/replace-first % folder "")
-                     (loop [[file & remaining] (fh/list (.internal Gdx/files folder))
+                     (loop [[^FileHandle file & remaining] (.list (.internal Gdx/files folder))
                             result []]
                        (cond (nil? file)
                              result
 
-                             (fh/directory? file)
-                             (recur (concat remaining (fh/list file)) result)
+                             (.isDirectory file)
+                             (recur (concat remaining (.list file)) result)
 
-                             (extensions (fh/extension file))
-                             (recur remaining (conj result (fh/path file)))
+                             (extensions (.extension file))
+                             (recur remaining (conj result (.path file)))
 
                              :else
                              (recur remaining result))))]
