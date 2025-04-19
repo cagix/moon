69e87d1977c4b932a1ee1b3416913de3571666b0
Michael Sappler <michaelsappler@gmail.com>
2025-04-19 18:24:26 +0530

    Remove clojure.gdx.graphics.texture

--- src/cdq/create/shape_drawer_texture.clj
+++ src/cdq/create/shape_drawer_texture.clj
@@ -1,13 +1,12 @@
 (ns cdq.create.shape-drawer-texture
   (:require [clojure.gdx.graphics.color :as color]
-            [clojure.gdx.graphics.texture :as texture]
             clojure.gdx.utils)
-  (:import (com.badlogic.gdx.graphics Pixmap Pixmap$Format)))
+  (:import (com.badlogic.gdx.graphics Texture Pixmap Pixmap$Format)))
 
 (defn create []
   (let [pixmap (doto (Pixmap. 1 1 Pixmap$Format/RGBA8888)
                  (.setColor color/white)
                  (.drawPixel 0 0))
-        texture (texture/create pixmap)]
+        texture (Texture. pixmap)]
     (clojure.gdx.utils/dispose pixmap)
     texture))
