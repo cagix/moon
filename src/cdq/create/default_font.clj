a711e9430deac8e2e2239e7bdd8ce604936cc0a2
Michael Sappler <michaelsappler@gmail.com>
2025-04-19 18:46:35 +0530

    Remove clojure.gdx.graphics.g2d.freetype

--- src/cdq/create/default_font.clj
+++ src/cdq/create/default_font.clj
@@ -1,11 +1,29 @@
 (ns cdq.create.default-font
-  (:require [clojure.gdx.graphics.g2d.freetype :as freetype])
   (:import (com.badlogic.gdx Gdx)
-           (com.badlogic.gdx.graphics.g2d BitmapFont)))
+           (com.badlogic.gdx.graphics Texture$TextureFilter)
+           (com.badlogic.gdx.graphics.g2d BitmapFont)
+           (com.badlogic.gdx.graphics.g2d.freetype FreeTypeFontGenerator
+                                                   FreeTypeFontGenerator$FreeTypeFontParameter)))
+
+(defn- ->params [{:keys [size]}]
+  (let [params (FreeTypeFontGenerator$FreeTypeFontParameter.)]
+    (set! (.size params) size)
+    ; .color and this:
+    ;(set! (.borderWidth parameter) 1)
+    ;(set! (.borderColor parameter) red)
+    (set! (.minFilter params) Texture$TextureFilter/Linear) ; because scaling to world-units
+    (set! (.magFilter params) Texture$TextureFilter/Linear)
+    params))
+
+(defn- generate-font [file-handle params]
+  (let [generator (FreeTypeFontGenerator. file-handle)
+        font (.generateFont generator (->params params))]
+    (.dispose generator)
+    font))
 
 (defn create [{:keys [file size quality-scaling]}]
-  (let [^BitmapFont font (freetype/generate-font (.internal Gdx/files file)
-                                                 {:size (* size quality-scaling)})]
+  (let [^BitmapFont font (generate-font (.internal Gdx/files file)
+                                        {:size (* size quality-scaling)})]
     (.setScale (.getData font) (float (/ quality-scaling)))
     (set! (.markupEnabled (.getData font)) true)
     (.setUseIntegerPositions font false) ; otherwise scaling to world-units (/ 1 48)px not visible
