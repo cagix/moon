3180f1c5b0336719d36316c12250221101d2928a
Michael Sappler <michaelsappler@gmail.com>
2025-04-19 11:39:46 +0530

    Remove context protocol world

--- src/cdq/game.clj
+++ src/cdq/game.clj
@@ -14,21 +14,11 @@
             [cdq.create.ui-viewport :as ui-viewport]
             [cdq.create.world-unit-scale :as world-unit-scale]
             [cdq.create.world-viewport :as world-viewport]
-            [cdq.context :as context]
-            [cdq.entity :as entity]
-            [cdq.fsm :as fsm]
-            [cdq.graphics.animation :as animation]
-            [cdq.inventory :as inventory]
-            [cdq.widgets.inventory :as widgets.inventory]
-            [cdq.world :as world]
             cdq.world.context
             clojure.gdx.application
             clojure.gdx.backends.lwjgl
             [clojure.gdx.utils :as utils]
-            [clojure.gdx.utils.viewport :as viewport]
-            [clojure.utils :refer [safe-merge]]))
-
-(defrecord Game [])
+            [clojure.gdx.utils.viewport :as viewport]))
 
 (defn- create-game []
   (let [schemas (schemas/create)
@@ -36,21 +26,20 @@
         shape-drawer-texture (shape-drawer-texture/create)
         world-unit-scale (world-unit-scale/create)
         ui-viewport (ui-viewport/create)
-        context (map->Game
-                 {:cdq/assets (assets/create)
-                  :cdq.graphics/batch batch
-                  :cdq.graphics/cursors (cursors/create)
-                  :cdq.graphics/default-font (default-font/create)
-                  :cdq.graphics/shape-drawer (shape-drawer/create batch shape-drawer-texture)
-                  :cdq.graphics/shape-drawer-texture shape-drawer-texture
-                  :cdq.graphics/tiled-map-renderer (tiled-map-renderer/create batch world-unit-scale)
-                  :cdq.graphics/ui-viewport ui-viewport
-                  :cdq.graphics/world-unit-scale world-unit-scale
-                  :cdq.graphics/world-viewport (world-viewport/create world-unit-scale)
-                  :cdq/db (db/create schemas)
-                  :context/entity-components (cdq.create.entity-components/create)
-                  :cdq/schemas schemas
-                  :cdq.context/stage (stage/create batch ui-viewport)}) ]
+        context {:cdq/assets (assets/create)
+                 :cdq.graphics/batch batch
+                 :cdq.graphics/cursors (cursors/create)
+                 :cdq.graphics/default-font (default-font/create)
+                 :cdq.graphics/shape-drawer (shape-drawer/create batch shape-drawer-texture)
+                 :cdq.graphics/shape-drawer-texture shape-drawer-texture
+                 :cdq.graphics/tiled-map-renderer (tiled-map-renderer/create batch world-unit-scale)
+                 :cdq.graphics/ui-viewport ui-viewport
+                 :cdq.graphics/world-unit-scale world-unit-scale
+                 :cdq.graphics/world-viewport (world-viewport/create world-unit-scale)
+                 :cdq/db (db/create schemas)
+                 :context/entity-components (cdq.create.entity-components/create)
+                 :cdq/schemas schemas
+                 :cdq.context/stage (stage/create batch ui-viewport)} ]
     (cdq.world.context/reset context :worlds/vampire)))
 
 (defn- dispose-game [context]
@@ -112,99 +101,3 @@
                                            :windowed-mode {:width 1440
                                                            :height 900}
                                            :foreground-fps 60}))
-
-(defrecord Body [position
-                 left-bottom
-                 width
-                 height
-                 half-width
-                 half-height
-                 radius
-                 collides?
-                 z-order
-                 rotation-angle])
-
-(defn- create-body [{[x y] :position
-                     :keys [position
-                            width
-                            height
-                            collides?
-                            z-order
-                            rotation-angle]}]
-  (assert position)
-  (assert width)
-  (assert height)
-  (assert (>= width  (if collides? world/minimum-size 0)))
-  (assert (>= height (if collides? world/minimum-size 0)))
-  (assert (or (boolean? collides?) (nil? collides?)))
-  (assert ((set world/z-orders) z-order))
-  (assert (or (nil? rotation-angle)
-              (<= 0 rotation-angle 360)))
-  (map->Body
-   {:position (mapv float position)
-    :left-bottom [(float (- x (/ width  2)))
-                  (float (- y (/ height 2)))]
-    :width  (float width)
-    :height (float height)
-    :half-width  (float (/ width  2))
-    :half-height (float (/ height 2))
-    :radius (float (max (/ width  2)
-                        (/ height 2)))
-    :collides? collides?
-    :z-order z-order
-    :rotation-angle (or rotation-angle 0)}))
-
-(defn- create-vs [components context]
-  (reduce (fn [m [k v]]
-            (assoc m k (entity/create [k v] context)))
-          {}
-          components))
-
-(defmulti create! (fn [[k] eid c]
-                    k))
-(defmethod create! :default [_ eid c])
-
-(def id-counter (atom 0))
-
-(extend-type Game
-  world/World
-  (spawn-entity [context position body components]
-    (assert (and (not (contains? components :position))
-                 (not (contains? components :entity/id))))
-    (let [eid (atom (-> body
-                        (assoc :position position)
-                        create-body
-                        (safe-merge (-> components
-                                        (assoc :entity/id (swap! id-counter inc))
-                                        (create-vs context)))))]
-      (doseq [component context]
-        (context/add-entity component eid))
-      (doseq [component @eid]
-        (create! component eid context))
-      eid)))
-
-(defmethod create! :entity/inventory
-  [[k items] eid c]
-  (swap! eid assoc k inventory/empty-inventory)
-  (doseq [item items]
-    (widgets.inventory/pickup-item c eid item)))
-
-(defmethod create! :entity/skills
-  [[k skills] eid c]
-  (swap! eid assoc k nil)
-  (doseq [skill skills]
-    (world/add-skill c eid skill)))
-
-(defmethod create! :entity/animation
-  [[_ animation] eid c]
-  (swap! eid assoc :entity/image (animation/current-frame animation)))
-
-(defmethod create! :entity/delete-after-animation-stopped?
-  [_ eid c]
-  (-> @eid :entity/animation :looping? not assert))
-
-(defmethod create! :entity/fsm
-  [[k {:keys [fsm initial-state]}] eid c]
-  (swap! eid assoc
-         k (fsm/create fsm initial-state)
-         initial-state (entity/create [initial-state eid] c)))
--- src/cdq/world.clj
+++ src/cdq/world.clj
@@ -1,5 +1,10 @@
 (ns cdq.world
-  (:require cdq.graphics
+  (:require [cdq.context :as context]
+            cdq.graphics
+            [cdq.fsm :as fsm]
+            [cdq.inventory :as inventory]
+            [cdq.widgets.inventory :as widgets.inventory]
+            [cdq.graphics.animation :as animation]
             [cdq.db :as db]
             [clojure.utils :refer [define-order safe-merge]]
             [cdq.graphics.shape-drawer :as sd]
@@ -29,8 +34,73 @@
 
 (def render-z-order (define-order z-orders))
 
-(defprotocol World
-  (spawn-entity [_ position body components]))
+(defrecord Body [position
+                 left-bottom
+                 width
+                 height
+                 half-width
+                 half-height
+                 radius
+                 collides?
+                 z-order
+                 rotation-angle])
+
+(defn- create-body [{[x y] :position
+                     :keys [position
+                            width
+                            height
+                            collides?
+                            z-order
+                            rotation-angle]}]
+  (assert position)
+  (assert width)
+  (assert height)
+  (assert (>= width  (if collides? minimum-size 0)))
+  (assert (>= height (if collides? minimum-size 0)))
+  (assert (or (boolean? collides?) (nil? collides?)))
+  (assert ((set z-orders) z-order))
+  (assert (or (nil? rotation-angle)
+              (<= 0 rotation-angle 360)))
+  (map->Body
+   {:position (mapv float position)
+    :left-bottom [(float (- x (/ width  2)))
+                  (float (- y (/ height 2)))]
+    :width  (float width)
+    :height (float height)
+    :half-width  (float (/ width  2))
+    :half-height (float (/ height 2))
+    :radius (float (max (/ width  2)
+                        (/ height 2)))
+    :collides? collides?
+    :z-order z-order
+    :rotation-angle (or rotation-angle 0)}))
+
+(defn- create-vs [components context]
+  (reduce (fn [m [k v]]
+            (assoc m k (entity/create [k v] context)))
+          {}
+          components))
+
+(defmulti create! (fn [[k] eid c]
+                    k))
+(defmethod create! :default [_ eid c])
+
+(def id-counter (atom 0))
+
+(defn spawn-entity [context position body components]
+  (assert (and (not (contains? components :position))
+               (not (contains? components :entity/id))))
+  (let [eid (atom (-> body
+                      (assoc :position position)
+                      create-body
+                      (safe-merge (-> components
+                                      (assoc :entity/id (swap! id-counter inc))
+                                      (create-vs context)))))]
+    (doseq [component context]
+      (context/add-entity component eid))
+    (doseq [component @eid]
+      (create! component eid context))
+    eid))
 
 (def ^{:doc "For effects just to have a mouseover body size for debugging purposes."
        :private true}
@@ -234,3 +304,29 @@
   (when (:entity/player? @eid)
     (action-bar-remove-skill c skill))
   (swap! eid update :entity/skills dissoc id))
+
+(defmethod create! :entity/inventory
+  [[k items] eid c]
+  (swap! eid assoc k inventory/empty-inventory)
+  (doseq [item items]
+    (widgets.inventory/pickup-item c eid item)))
+
+(defmethod create! :entity/skills
+  [[k skills] eid c]
+  (swap! eid assoc k nil)
+  (doseq [skill skills]
+    (add-skill c eid skill)))
+
+(defmethod create! :entity/animation
+  [[_ animation] eid c]
+  (swap! eid assoc :entity/image (animation/current-frame animation)))
+
+(defmethod create! :entity/delete-after-animation-stopped?
+  [_ eid c]
+  (-> @eid :entity/animation :looping? not assert))
+
+(defmethod create! :entity/fsm
+  [[k {:keys [fsm initial-state]}] eid c]
+  (swap! eid assoc
+         k (fsm/create fsm initial-state)
+         initial-state (entity/create [initial-state eid] c)))
