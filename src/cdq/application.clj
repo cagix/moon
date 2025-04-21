ade4f0e7d7492be6c247604e2fcc7027bedcd3a8
Michael Sappler <michaelsappler@gmail.com>
2025-04-21 11:17:49 +0530

    Add resources/cdq.application.edn

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -1393,36 +1393,7 @@
 (def state (atom nil))
 
 (defn -main []
-  (let [config {:mac-os {:dock-icon "moon.png"}
-                :title "Cyber Dungeon Quest"
-                :windowed-mode {:width 1440 :height 900}
-                :foreground-fps 60
-                :assets {:folder "resources/"
-                         :asset-type->extensions {:sound   #{"wav"}
-                                                  :texture #{"png" "bmp"}}}
-                :cursors {:cursors/bag                   ["bag001"       [0   0]]
-                          :cursors/black-x               ["black_x"      [0   0]]
-                          :cursors/default               ["default"      [0   0]]
-                          :cursors/denied                ["denied"       [16 16]]
-                          :cursors/hand-before-grab      ["hand004"      [4  16]]
-                          :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
-                          :cursors/hand-grab             ["hand003"      [4  16]]
-                          :cursors/move-window           ["move002"      [16 16]]
-                          :cursors/no-skill-selected     ["denied003"    [0   0]]
-                          :cursors/over-button           ["hand002"      [0   0]]
-                          :cursors/sandclock             ["sandclock"    [16 16]]
-                          :cursors/skill-not-usable      ["x007"         [0   0]]
-                          :cursors/use-skill             ["pointer004"   [0   0]]
-                          :cursors/walking               ["walking"      [16 16]]}
-                :default-font {:file "fonts/exocet/films.EXL_____.ttf"
-                               :size 16
-                               :quality-scaling 2}
-                :ui-viewport {:width 1440 :height 900}
-                :world-unit-scale 48
-                :world-viewport {:width 1440 :height 900}
-                :ui {:skin-scale :x1}
-                :schemas "schema.edn"
-                :first-level :worlds/vampire}]
+  (let [config (-> "cdq.application.edn" io/resource slurp edn/read-string)]
     (when (= SharedLibraryLoader/os Os/MacOsX)
       (.setIconImage (Taskbar/getTaskbar)
                      (.getImage (Toolkit/getDefaultToolkit)
