b774b4e159ae72aca2b81852d73fa010a386d1e0
Michael Sappler <michaelsappler@gmail.com>
2025-04-25 12:22:27 +0530

    Formatting

--- src/cdq/widgets/inventory.clj
+++ src/cdq/widgets/inventory.clj
@@ -70,12 +70,9 @@
   [21 (+ (slot->y-sprite-idx slot) 2)])
 
 (defn- slot->sprite [c slot]
-  (cdq.graphics.sprite/from-sheet (cdq.graphics.sprite/sheet c
-                                                                     "images/items.png"
-                                                                     48
-                                                                     48)
-                                      (slot->sprite-idx slot)
-                                      c))
+  (cdq.graphics.sprite/from-sheet (cdq.graphics.sprite/sheet c "images/items.png" 48 48)
+                                  (slot->sprite-idx slot)
+                                  c))
 
 (defn- slot->background [c slot]
   (let [drawable (-> (slot->sprite c slot)
