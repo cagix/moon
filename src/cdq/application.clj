2e2b86177f4014072b56b20e52de148d0f964f78
Michael Sappler <michaelsappler@gmail.com>
2025-04-25 10:22:43 +0530

    Move effects to cdq.impl.effects

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -2,10 +2,10 @@
   (:require [cdq.assets :refer [play-sound]]
             [cdq.context :as context]
             [cdq.db :as db]
-            [cdq.effect :as effect]
             [cdq.effect-context :as effect-ctx]
             [cdq.entity :as entity]
             [cdq.entity.fsm :as fsm]
+            cdq.impl.effects
             [cdq.schema :as schema]
             [cdq.property :as property]
             [cdq.timer :as timer]
@@ -24,10 +24,6 @@
                                delayed-alert
                                spawn-item
                                spawn-audiovisual
-                               spawn-creature
-                               spawn-projectile
-                               line-render
-                               projectile-size
                                world-item?
                                player-movement-vector
                                get-inventory
@@ -48,12 +44,11 @@
             [gdl.input :as input]
             [gdl.math.raycaster :as raycaster]
             [gdl.math.vector2 :as v]
-            [gdl.rand :refer [rand-int-between]]
             [gdl.ui :as ui]
             [gdl.ui.actor :as actor]
             [gdl.ui.group :as group]
             [gdl.ui.stage :as stage]
-            [gdl.utils :as utils :refer [find-first defcomponent]]
+            [gdl.utils :as utils :refer [find-first]]
             [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.pprint :refer [pprint]])
@@ -249,283 +244,6 @@
 
 (defmethod schema/edn->value :s/one-to-many [_ property-ids {:keys [cdq/db] :as context}]
   (set (map #(db/build db % context) property-ids)))
-
-(comment
- (ns cdq.components.effects.audiovisual)
-
- (defn applicable? [_ {:keys [effect/target-position]}]
-   target-position)
-
- (defn useful? [_ _ _c]
-   false)
-
- (defn handle [[_ audiovisual] {:keys [effect/target-position]} c]
-   (spawn-audiovisual c target-position audiovisual))
- )
-
-(defcomponent :effects/audiovisual
-  (effect/applicable? [_ {:keys [effect/target-position]}]
-    target-position)
-
-  (effect/useful? [_ _ _c]
-    false)
-
-  (effect/handle [[_ audiovisual] {:keys [effect/target-position]} c]
-    (spawn-audiovisual c target-position audiovisual)))
-
-(defn- projectile-start-point [entity direction size]
-  (v/add (:position entity)
-         (v/scale direction
-                  (+ (:radius entity) size 0.1))))
-
-(defcomponent :effects/projectile
-  ; TODO for npcs need target -- anyway only with direction
-  (effect/applicable? [_ {:keys [effect/target-direction]}]
-    target-direction) ; faction @ source also ?
-
-  ; TODO valid params direction has to be  non-nil (entities not los player ) ?
-  (effect/useful? [[_ {:keys [projectile/max-range] :as projectile}]
-                   {:keys [effect/source effect/target]}
-                   {:keys [cdq.context/raycaster]}]
-    (let [source-p (:position @source)
-          target-p (:position @target)]
-      ; is path blocked ereally needed? we need LOS also right to have a target-direction as AI?
-      (and (not (raycaster/path-blocked? raycaster ; TODO test
-                                         source-p
-                                         target-p
-                                         (projectile-size projectile)))
-           ; TODO not taking into account body sizes
-           (< (v/distance source-p ; entity/distance function protocol EntityPosition
-                          target-p)
-              max-range))))
-
-  (effect/handle [[_ projectile] {:keys [effect/source effect/target-direction]} c]
-    (spawn-projectile c
-                      {:position (projectile-start-point @source
-                                                         target-direction
-                                                         (projectile-size projectile))
-                       :direction target-direction
-                       :faction (:entity/faction @source)}
-                      projectile)))
-
-(comment
- ; mass shooting
- (for [direction (map math.vector/normalise
-                      [[1 0]
-                       [1 1]
-                       [1 -1]
-                       [0 1]
-                       [0 -1]
-                       [-1 -1]
-                       [-1 1]
-                       [-1 0]])]
-   [:tx/projectile projectile-id ...]
-   )
- )
-
-(defcomponent :effects/sound
-  (effect/applicable? [_ _ctx]
-    true)
-
-  (effect/useful? [_ _ _c]
-    false)
-
-  (effect/handle [[_ sound] _ctx c]
-    (sound/play sound)))
-
-(defcomponent :effects/spawn
-  (effect/applicable? [_ {:keys [effect/source effect/target-position]}]
-    (and (:entity/faction @source)
-         target-position))
-
-  (effect/handle [[_ {:keys [property/id]}]
-                  {:keys [effect/source effect/target-position]}
-                  c]
-    (spawn-creature c
-                    {:position target-position
-                     :creature-id id ; already properties/get called through one-to-one, now called again.
-                     :components {:entity/fsm {:fsm :fsms/npc
-                                               :initial-state :npc-idle}
-                                  :entity/faction (:entity/faction @source)}})))
-
-(comment
- ; TODO applicable targets? e.g. projectiles/effect s/???item entiteis ??? check
- ; same code as in render entities on world view screens/world
- ; TODO showing one a bit further up
- ; maybe world view port is cut
- ; not quite showing correctly.
- (let [targets (los/creatures-in-los-of-player)]
-   (count targets)
-   #_(sort-by #(% 1) (map #(vector (:entity.creature/name @%)
-                                   (:position @%)) targets)))
-
- )
-
-(defcomponent :effects/target-all
-  ; TODO targets projectiles with -50% hp !!
-  (effect/applicable? [_ _]
-    true)
-
-  (effect/useful? [_ _ _c]
-    ; TODO
-    false)
-
-  (effect/handle [[_ {:keys [entity-effects]}] {:keys [effect/source]} c]
-    (let [source* @source]
-      (doseq [target (los/creatures-in-los-of-player c)]
-        (line-render c
-                     {:start (:position source*) #_(start-point source* target*)
-                      :end (:position @target)
-                      :duration 0.05
-                      :color [1 0 0 0.75]
-                      :thick? true})
-        ; some sound .... or repeat smae sound???
-        ; skill do sound  / skill start sound >?
-        ; problem : nested tx/effect , we are still having direction/target-position
-        ; at sub-effects
-        ; and no more safe - merge
-        ; find a way to pass ctx / effect-ctx separate ?
-        (effect-ctx/do-all! c
-                            {:effect/source source :effect/target target}
-                            entity-effects)))))
-
-(defcomponent :effects/target-entity
-  (effect/applicable? [[_ {:keys [entity-effects]}] {:keys [effect/target] :as effect-ctx}]
-    (and target
-         (seq (effect-ctx/filter-applicable? effect-ctx entity-effects))))
-
-  (effect/useful?  [[_ {:keys [maxrange]}] {:keys [effect/source effect/target]} _c]
-    (entity/in-range? @source @target maxrange))
-
-  (effect/handle [[_ {:keys [maxrange entity-effects]}]
-                  {:keys [effect/source effect/target] :as effect-ctx}
-                  {:keys [cdq/db] :as c}]
-    (let [source* @source
-          target* @target]
-      (if (entity/in-range? source* target* maxrange)
-        (do
-         (line-render c
-                      {:start (entity/start-point source* target*)
-                       :end (:position target*)
-                       :duration 0.05
-                       :color [1 0 0 0.75]
-                       :thick? true})
-         (effect-ctx/do-all! c effect-ctx entity-effects))
-        (spawn-audiovisual c
-                           (entity/end-point source* target* maxrange)
-                           (db/build db :audiovisuals/hit-ground c))))))
-
-(defcomponent :effects.target/audiovisual
-  (effect/applicable? [_ {:keys [effect/target]}]
-    target)
-
-  (effect/useful? [_ _ _c]
-    false)
-
-  (effect/handle [[_ audiovisual] {:keys [effect/target]} c]
-    (spawn-audiovisual c
-                       (:position @target)
-                       audiovisual)))
-
-(defcomponent :effects.target/convert
-  (effect/applicable? [_ {:keys [effect/source effect/target]}]
-    (and target
-         (= (:entity/faction @target)
-            (entity/enemy @source))))
-
-  (effect/handle [_ {:keys [effect/source effect/target]} c]
-    (swap! target assoc :entity/faction (:entity/faction @source))))
-
-(defn- effective-armor-save [source* target*]
-  (max (- (or (entity/stat target* :entity/armor-save) 0)
-          (or (entity/stat source* :entity/armor-pierce) 0))
-       0))
-
-(comment
- ; broken
- (let [source* {:entity/armor-pierce 0.4}
-       target* {:entity/armor-save   0.5}]
-   (effective-armor-save source* target*))
- )
-
-
-(defn- armor-saves? [source* target*]
-  (< (rand) (effective-armor-save source* target*)))
-
-(defcomponent :effects.target/damage
-  (effect/applicable? [_ {:keys [effect/target]}]
-    (and target
-         (:entity/hp @target)))
-
-  (effect/handle [[_ damage]
-                  {:keys [effect/source effect/target]}
-                  {:keys [cdq/db] :as c}]
-    (let [source* @source
-          target* @target
-          hp (entity/hitpoints target*)]
-      (cond
-       (zero? (hp 0))
-       nil
-
-       (armor-saves? source* target*)
-       (swap! target entity/add-text-effect c "[WHITE]ARMOR")
-
-       :else
-       (let [min-max (:damage/min-max (entity/damage source* target* damage))
-             dmg-amount (rand-int-between min-max)
-             new-hp-val (max (- (hp 0) dmg-amount) 0)]
-         (swap! target assoc-in [:entity/hp 0] new-hp-val)
-         (spawn-audiovisual c
-                            (:position target*)
-                            (db/build db :audiovisuals/damage c))
-         (fsm/event c target (if (zero? new-hp-val) :kill :alert))
-         (swap! target entity/add-text-effect c (str "[RED]" dmg-amount "[]")))))))
-
-(defcomponent :effects.target/kill
-  (effect/applicable? [_ {:keys [effect/target]}]
-    (and target
-         (:entity/fsm @target)))
-
-  (effect/handle [_ {:keys [effect/target]} c]
-    (fsm/event c target :kill)))
-
-(defn- entity->melee-damage [entity]
-  (let [strength (or (entity/stat entity :entity/strength) 0)]
-    {:damage/min-max [strength strength]}))
-
-(defn- melee-damage-effect [entity]
-  [:effects.target/damage (entity->melee-damage entity)])
-
-(defcomponent :effects.target/melee-damage
-  (effect/applicable? [_ {:keys [effect/source] :as ctx}]
-    (effect/applicable? (melee-damage-effect @source) ctx))
-
-  (effect/handle [_ {:keys [effect/source] :as ctx} c]
-    (effect/handle (melee-damage-effect @source) ctx c)))
-
-(let [modifiers {:modifier/movement-speed {:op/mult -0.5}}
-      duration 5]
-  (defcomponent :effects.target/spiderweb
-    (effect/applicable? [_ _]
-      ; ?
-      true)
-
-    ; TODO stacking? (if already has k ?) or reset counter ? (see string-effect too)
-    (effect/handle [_
-                    {:keys [effect/target]}
-                    {:keys [cdq.context/elapsed-time] :as c}]
-      (when-not (:entity/temp-modifier @target)
-        (swap! target assoc :entity/temp-modifier {:modifiers modifiers
-                                                   :counter (timer/create elapsed-time duration)})
-        (swap! target entity/mod-add modifiers)))))
-
-(defcomponent :effects.target/stun
-  (effect/applicable? [_ {:keys [effect/target]}]
-    (and target
-         (:entity/fsm @target)))
-
-  (effect/handle [[_ duration] {:keys [effect/target]} c]
-    (fsm/event c target :stun duration)))
 
 (defprotocol Disposable
   (dispose! [_]))
