d8b08762b9d5a4764bc8a1f9878d6a7c4dd97ba4
Michael Sappler <michaelsappler@gmail.com>
2025-04-26 03:42:29 +0530

    Move tick! methods to cdq.impl.entity

--- src/cdq/render.clj
+++ src/cdq/render.clj
@@ -1,36 +1,23 @@
 (ns cdq.render
   (:require [cdq.context :as context]
             [cdq.db :as db]
-            [cdq.effect-context :as effect-ctx]
             [cdq.entity :as entity]
-            [cdq.entity.fsm :as fsm]
-            [cdq.schema :as schema]
-            [cdq.timer :as timer]
             cdq.error
-            [cdq.skill :as skill]
             [cdq.grid :as grid]
             [cdq.line-of-sight :as los]
             cdq.potential-fields
             cdq.time
-            [cdq.world :refer [nearest-enemy
-                               player-movement-vector
-                               friendlies-in-radius
-                               minimum-size
-                               player-movement-vector]]
-            [cdq.world.potential-field :as potential-field]
             [cdq.data.grid2d :as g2d]
             [cdq.graphics :as graphics]
-            [cdq.graphics.animation :as animation]
             [cdq.graphics.camera :as camera]
             [cdq.graphics.shape-drawer :as sd]
             [cdq.graphics.tiled-map-renderer :as tiled-map-renderer]
             [cdq.input :as input]
             [cdq.math.raycaster :as raycaster]
-            [cdq.math.vector2 :as v]
             [cdq.ui.actor :as actor]
             [cdq.ui.group :as group]
             [cdq.ui.stage :as stage]
-            [cdq.utils :as utils :refer [find-first]])
+            [cdq.utils :as utils])
   (:import (com.badlogic.gdx.graphics Color)
            (com.badlogic.gdx.graphics.g2d Batch)
            (cdq StageWithState)))
@@ -249,234 +236,6 @@
      (cdq.error/error-window context t)
      #_(bind-root ::error t))) ; FIXME ... either reduce or use an atom ...
   context)
-
-; this is not necessary if effect does not need target, but so far not other solution came up.
-(defn- update-effect-ctx
-  "Call this on effect-context if the time of using the context is not the time when context was built."
-  [context {:keys [effect/source effect/target] :as effect-ctx}]
-  (if (and target
-           (not (:entity/destroyed? @target))
-           (los/exists? context @source @target))
-    effect-ctx
-    (dissoc effect-ctx :effect/target)))
-
-(defmethod tick! :active-skill [[_ {:keys [skill effect-ctx counter]}]
-                                eid
-                                {:keys [cdq.context/elapsed-time] :as c}]
-  (cond
-   (not (effect-ctx/some-applicable? (update-effect-ctx c effect-ctx)
-                                     (:skill/effects skill)))
-   (do
-    (fsm/event c eid :action-done)
-    ; TODO some sound ?
-    )
-
-   (timer/stopped? counter elapsed-time)
-   (do
-    (effect-ctx/do-all! c effect-ctx (:skill/effects skill))
-    (fsm/event c eid :action-done))))
-
-(defn- npc-choose-skill [c entity ctx]
-  (->> entity
-       :entity/skills
-       vals
-       (sort-by #(or (:skill/cost %) 0))
-       reverse
-       (filter #(and (= :usable (skill/usable-state entity % ctx))
-                     (effect-ctx/applicable-and-useful? c ctx (:skill/effects %))))
-       first))
-
-(defn- npc-effect-context [c eid]
-  (let [entity @eid
-        target (nearest-enemy c entity)
-        target (when (and target
-                          (los/exists? c entity @target))
-                 target)]
-    {:effect/source eid
-     :effect/target target
-     :effect/target-direction (when target
-                                (entity/direction entity @target))}))
-
-(defmethod tick! :npc-idle [_ eid c]
-  (let [effect-ctx (npc-effect-context c eid)]
-    (if-let [skill (npc-choose-skill c @eid effect-ctx)]
-      (fsm/event c eid :start-action [skill effect-ctx])
-      (fsm/event c eid :movement-direction (or (potential-field/find-direction c eid) [0 0])))))
-
-(defmethod tick! :npc-moving [[_ {:keys [counter]}]
-                              eid
-                              {:keys [cdq.context/elapsed-time] :as c}]
-  (when (timer/stopped? counter elapsed-time)
-    (fsm/event c eid :timer-finished)))
-
-(defmethod tick! :npc-sleeping [_ eid {:keys [cdq.context/grid] :as c}]
-  (let [entity @eid
-        cell (grid (entity/tile entity))]
-    (when-let [distance (grid/nearest-entity-distance @cell (entity/enemy entity))]
-      (when (<= distance (entity/stat entity :entity/aggro-range))
-        (fsm/event c eid :alert)))))
-
-(defmethod tick! :player-moving [[_ {:keys [movement-vector]}] eid c]
-  (if-let [movement-vector (player-movement-vector)]
-    (swap! eid assoc :entity/movement {:direction movement-vector
-                                       :speed (entity/stat @eid :entity/movement-speed)})
-    (fsm/event c eid :no-movement-input)))
-
-(defmethod tick! :stunned [[_ {:keys [counter]}]
-                           eid
-                           {:keys [cdq.context/elapsed-time] :as c}]
-  (when (timer/stopped? counter elapsed-time)
-    (fsm/event c eid :effect-wears-off)))
-
-(defmethod tick! :entity/alert-friendlies-after-duration
-  [[_ {:keys [counter faction]}]
-   eid
-   {:keys [cdq.context/grid
-           cdq.context/elapsed-time]
-    :as c}]
-  (when (timer/stopped? counter elapsed-time)
-    (swap! eid assoc :entity/destroyed? true)
-    (doseq [friendly-eid (friendlies-in-radius grid (:position @eid) faction)]
-      (fsm/event c friendly-eid :alert))))
-
-(defmethod tick! :entity/animation
-  [[k animation] eid {:keys [cdq.context/delta-time]}]
-  (swap! eid #(-> %
-                  (assoc :entity/image (animation/current-frame animation))
-                  (assoc k (animation/tick animation delta-time)))))
-
-(defmethod tick! :entity/delete-after-duration
-  [[_ counter]
-   eid
-   {:keys [cdq.context/elapsed-time]}]
-  (when (timer/stopped? counter elapsed-time)
-    (swap! eid assoc :entity/destroyed? true)))
-
-(defn- move-position [position {:keys [direction speed delta-time]}]
-  (mapv #(+ %1 (* %2 speed delta-time)) position direction))
-
-(defn- move-body [body movement]
-  (-> body
-      (update :position    move-position movement)
-      (update :left-bottom move-position movement)))
-
-(defn- valid-position? [grid {:keys [entity/id z-order] :as body}]
-  {:pre [(:collides? body)]}
-  (let [cells* (into [] (map deref) (grid/rectangle->cells grid body))]
-    (and (not-any? #(grid/blocked? % z-order) cells*)
-         (->> cells*
-              grid/cells->entities
-              (not-any? (fn [other-entity]
-                          (let [other-entity @other-entity]
-                            (and (not= (:entity/id other-entity) id)
-                                 (:collides? other-entity)
-                                 (entity/collides? other-entity body)))))))))
-
-(defn- try-move [grid body movement]
-  (let [new-body (move-body body movement)]
-    (when (valid-position? grid new-body)
-      new-body)))
-
-; TODO sliding threshold
-; TODO name - with-sliding? 'on'
-; TODO if direction was [-1 0] and invalid-position then this algorithm tried to move with
-; direection [0 0] which is a waste of processor power...
-(defn- try-move-solid-body [grid body {[vx vy] :direction :as movement}]
-  (let [xdir (Math/signum (float vx))
-        ydir (Math/signum (float vy))]
-    (or (try-move grid body movement)
-        (try-move grid body (assoc movement :direction [xdir 0]))
-        (try-move grid body (assoc movement :direction [0 ydir])))))
-
-; set max speed so small entities are not skipped by projectiles
-; could set faster than max-speed if I just do multiple smaller movement steps in one frame
-(def ^:private max-speed (/ minimum-size
-                            cdq.time/max-delta)) ; need to make var because s/schema would fail later if divide / is inside the schema-form
-
-(def speed-schema (schema/m-schema [:and number? [:>= 0] [:<= max-speed]]))
-
-(defmethod tick! :entity/movement
-  [[_ {:keys [direction speed rotate-in-movement-direction?] :as movement}]
-   eid
-   {:keys [cdq.context/delta-time
-           cdq.context/grid] :as context}]
-  (assert (schema/validate speed-schema speed)
-          (pr-str speed))
-  (assert (or (zero? (v/length direction))
-              (v/normalised? direction))
-          (str "cannot understand direction: " (pr-str direction)))
-  (when-not (or (zero? (v/length direction))
-                (nil? speed)
-                (zero? speed))
-    (let [movement (assoc movement :delta-time delta-time)
-          body @eid]
-      (when-let [body (if (:collides? body) ; < == means this is a movement-type ... which could be a multimethod ....
-                        (try-move-solid-body grid body movement)
-                        (move-body body movement))]
-        (doseq [component context]
-          (context/position-changed component eid))
-        (swap! eid assoc
-               :position (:position body)
-               :left-bottom (:left-bottom body))
-        (when rotate-in-movement-direction?
-          (swap! eid assoc :rotation-angle (v/angle-from-vector direction)))))))
-
-(defmethod tick! :entity/projectile-collision
-  [[k {:keys [entity-effects already-hit-bodies piercing?]}]
-   eid
-   {:keys [cdq.context/grid] :as c}]
-  ; TODO this could be called from body on collision
-  ; for non-solid
-  ; means non colliding with other entities
-  ; but still collding with other stuff here ? o.o
-  (let [entity @eid
-        cells* (map deref (grid/rectangle->cells grid entity)) ; just use cached-touched -cells
-        hit-entity (find-first #(and (not (contains? already-hit-bodies %)) ; not filtering out own id
-                                     (not= (:entity/faction entity) ; this is not clear in the componentname & what if they dont have faction - ??
-                                           (:entity/faction @%))
-                                     (:collides? @%)
-                                     (entity/collides? entity @%))
-                               (grid/cells->entities cells*))
-        destroy? (or (and hit-entity (not piercing?))
-                     (some #(grid/blocked? % (:z-order entity)) cells*))]
-    (when destroy?
-      (swap! eid assoc :entity/destroyed? true))
-    (when hit-entity
-      (swap! eid assoc-in [k :already-hit-bodies] (conj already-hit-bodies hit-entity))) ; this is only necessary in case of not piercing ...
-    (when hit-entity
-      (effect-ctx/do-all! c
-                          {:effect/source eid
-                           :effect/target hit-entity}
-                          entity-effects))))
-
-(defmethod tick! :entity/delete-after-animation-stopped?
-  [_ eid c]
-  (when (animation/stopped? (:entity/animation @eid))
-    (swap! eid assoc :entity/destroyed? true)))
-
-(defmethod tick! :entity/skills
-  [[k skills]
-   eid
-   {:keys [cdq.context/elapsed-time]}]
-  (doseq [{:keys [skill/cooling-down?] :as skill} (vals skills)
-          :when (and cooling-down?
-                     (timer/stopped? cooling-down? elapsed-time))]
-    (swap! eid assoc-in [k (:property/id skill) :skill/cooling-down?] false)))
-
-(defmethod tick! :entity/string-effect
-  [[k {:keys [counter]}]
-   eid
-   {:keys [cdq.context/elapsed-time]}]
-  (when (timer/stopped? counter elapsed-time)
-    (swap! eid dissoc k)))
-
-(defmethod tick! :entity/temp-modifier
-  [[k {:keys [modifiers counter]}]
-   eid
-   {:keys [cdq.context/elapsed-time]}]
-  (when (timer/stopped? counter elapsed-time)
-    (swap! eid dissoc k)
-    (swap! eid entity/mod-remove modifiers)))
 
 (defn when-not-paused! [context]
   (if (:cdq.context/paused? context)
