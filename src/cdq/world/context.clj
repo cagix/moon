95bb7393b32f7b1cf707f17de92411cebbf5bd5b
Michael Sappler <michaelsappler@gmail.com>
2025-04-26 04:21:50 +0530

    Upfactor cdq.create.grid

--- src/cdq/world/context.clj
+++ src/cdq/world/context.clj
@@ -1,14 +1,95 @@
 (ns cdq.world.context
   (:require [cdq.context :as context]
             [cdq.create.level :as level]
-            cdq.create.grid
             [cdq.stage]
             [cdq.grid :as grid]
             [cdq.world :refer [spawn-creature]]
             [cdq.data.grid2d :as g2d]
             [cdq.tiled :as tiled]
             [cdq.ui.stage :as stage]
-            [cdq.utils :as utils :refer [defcomponent]]))
+            [cdq.utils :as utils :refer [tile->middle defcomponent]]))
+
+(defrecord RCell [position
+                  middle ; only used @ potential-field-follow-to-enemy -> can remove it.
+                  adjacent-cells
+                  movement
+                  entities
+                  occupied
+                  good
+                  evil]
+  grid/Cell
+  (blocked? [_ z-order]
+    (case movement
+      :none true ; wall
+      :air (case z-order ; water/doodads
+             :z-order/flying false
+             :z-order/ground true)
+      :all false)) ; ground/floor
+
+  (blocks-vision? [_]
+    (= movement :none))
+
+  (occupied-by-other? [_ eid]
+    (some #(not= % eid) occupied))
+
+  (nearest-entity [this faction]
+    (-> this faction :eid))
+
+  (nearest-entity-distance [this faction]
+    (-> this faction :distance)))
+
+(defn ->grid-cell [position movement]
+  {:pre [(#{:none :air :all} movement)]}
+  (map->RCell
+   {:position position
+    :middle (tile->middle position)
+    :movement movement
+    :entities #{}
+    :occupied #{}}))
+
+(defn- create-grid [tiled-map]
+  (g2d/create-grid
+   (tiled/tm-width tiled-map)
+   (tiled/tm-height tiled-map)
+   (fn [position]
+     (atom (->grid-cell position
+                        (case (tiled/movement-property tiled-map position)
+                          "none" :none
+                          "air"  :air
+                          "all"  :all))))))
+
+(defn- set-cells! [grid eid]
+  (let [cells (grid/rectangle->cells grid @eid)]
+    (assert (not-any? nil? cells))
+    (swap! eid assoc ::touched-cells cells)
+    (doseq [cell cells]
+      (assert (not (get (:entities @cell) eid)))
+      (swap! cell update :entities conj eid))))
+
+(defn- remove-from-cells! [eid]
+  (doseq [cell (::touched-cells @eid)]
+    (assert (get (:entities @cell) eid))
+    (swap! cell update :entities disj eid)))
+
+; could use inside tiles only for >1 tile bodies (for example size 4.5 use 4x4 tiles for occupied)
+; => only now there are no >1 tile entities anyway
+(defn- rectangle->occupied-cells [grid {:keys [left-bottom width height] :as rectangle}]
+  (if (or (> (float width) 1) (> (float height) 1))
+    (grid/rectangle->cells grid rectangle)
+    [(grid [(int (+ (float (left-bottom 0)) (/ (float width) 2)))
+            (int (+ (float (left-bottom 1)) (/ (float height) 2)))])]))
+
+(defn- set-occupied-cells! [grid eid]
+  (let [cells (rectangle->occupied-cells grid @eid)]
+    (doseq [cell cells]
+      (assert (not (get (:occupied @cell) eid)))
+      (swap! cell update :occupied conj eid))
+    (swap! eid assoc ::occupied-cells cells)))
+
+(defn- remove-from-occupied-cells! [eid]
+  (doseq [cell (::occupied-cells @eid)]
+    (assert (get (:occupied @cell) eid))
+    (swap! cell update :occupied disj eid)))
 
 (defn- set-arr [arr cell cell->blocked?]
   (let [[x y] (:position cell)]
@@ -73,7 +154,7 @@
 (defn reset [context {:keys [world-id] :as _config}]
   (reset-stage! context)
   (let [{:keys [tiled-map start-position] :as level} (level/create context world-id)
-        grid (cdq.create.grid/create tiled-map)
+        grid (create-grid tiled-map)
         context (merge context
                        {:cdq.context/content-grid (content-grid tiled-map)
                         :cdq.context/elapsed-time 0
@@ -121,3 +202,23 @@
 
   (context/position-changed [this eid]
     (context/add-entity this eid)))
+
+(defcomponent :cdq.context/grid
+  (context/add-entity [[_ grid] eid]
+    ; https://github.com/damn/core/issues/58
+    ;(assert (valid-position? grid @eid)) ; TODO deactivate because projectile no left-bottom remove that field or update properly for all
+    (set-cells! grid eid)
+    (when (:collides? @eid)
+      (set-occupied-cells! grid eid)))
+
+  (context/remove-entity [[_ _grid] eid]
+    (remove-from-cells! eid)
+    (when (:collides? @eid)
+      (remove-from-occupied-cells! eid)))
+
+  (context/position-changed [[_ grid] eid]
+    (remove-from-cells! eid)
+    (set-cells! grid eid)
+    (when (:collides? @eid)
+      (remove-from-occupied-cells! eid)
+      (set-occupied-cells! grid eid))))
