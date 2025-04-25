90b03b5e2013254512ae2a115d600c220b3ace5d
Michael Sappler <michaelsappler@gmail.com>
2025-04-26 04:14:08 +0530

    Upfactor modules/uf_caves

--- src/cdq/impl/level/uf_caves.clj
+++ src/cdq/impl/level/uf_caves.clj
@@ -1,10 +1,174 @@
 (ns cdq.impl.level.uf-caves
   (:require [cdq.create.level :refer [generate-level*]]
             [cdq.db :as db]
-            [cdq.level.uf-caves :as uf-caves]))
+            [cdq.mapgen :refer [creatures-with-level creature-tile wgt-grid->tiled-map adjacent-wall-positions scalegrid cave-grid flood-fill]]
+            [cdq.data.grid2d :as g2d]
+            [cdq.tiled :as tiled]
+            [cdq.rand :refer [get-rand-weighted-item]])
+  (:import (com.badlogic.gdx.graphics Texture)
+           (com.badlogic.gdx.graphics.g2d TextureRegion)))
+
+(def ^:private scaling 4)
+
+(defn- rand-0-3 []
+  (get-rand-weighted-item {0 60 1 1 2 1 3 1}))
+
+(defn- rand-0-5 []
+  (get-rand-weighted-item {0 30 1 1 2 1 3 1 4 1 5 1}))
+
+(defn- set-creatures-tiles [creature-properties spawn-rate tiled-map spawn-positions]
+  (let [layer (tiled/add-layer! tiled-map :name "creatures" :visible false)
+        level (inc (rand-int 6))
+        creatures (creatures-with-level creature-properties level)]
+    (doseq [position spawn-positions
+            :when (<= (rand) spawn-rate)]
+      (tiled/set-tile! layer position (creature-tile (rand-nth creatures))))))
+
+(def ^:private tm-tile
+  (memoize
+   (fn [texture-region movement]
+     {:pre [#{"all" "air" "none"} movement]}
+     (let [tile (tiled/static-tiled-map-tile texture-region)]
+       (tiled/put! (tiled/m-props tile) "movement" movement)
+       tile))))
+
+(def ^:private sprite-size 48)
+
+(defn- uf-tile [texture & {:keys [sprite-x sprite-y movement]}]
+  (tm-tile (TextureRegion. ^Texture texture
+                           (int (* sprite-x sprite-size))
+                           (int (* sprite-y sprite-size))
+                           (int sprite-size)
+                           (int sprite-size))
+           movement))
+
+(def ^:private uf-grounds
+  (for [x [1 5]
+        y (range 5 11)
+        :when (not= [x y] [5 5])] ; wooden
+    [x y]))
+
+(def ^:private uf-walls
+  (for [x [1]
+        y [13,16,19,22,25,28]]
+    [x y]))
+
+(defn- ground-tile [texture [x y]]
+  (uf-tile texture
+           :sprite-x (+ x (rand-0-3))
+           :sprite-y y
+           :movement "all"))
+
+(defn- wall-tile [texture [x y]]
+  (uf-tile texture
+           :sprite-x (+ x (rand-0-5))
+           :sprite-y y
+           :movement "none"))
+
+(defn- transition-tile [texture [x y]]
+  (uf-tile texture
+           :sprite-x (+ x (rand-0-5))
+           :sprite-y y
+           :movement "none"))
+
+(defn- transition? [grid [x y]]
+  (= :ground (get grid [x (dec y)])))
+
+(defn- assoc-transition-cells [grid]
+  (let [grid (reduce #(assoc %1 %2 :transition) grid
+                     (adjacent-wall-positions grid))]
+    (assert (or
+             (= #{:wall :ground :transition} (set (g2d/cells grid)))
+             (= #{:ground :transition}       (set (g2d/cells grid))))
+            (str "(set (g2d/cells grid)): " (set (g2d/cells grid))))
+    ;_ (printgrid grid)
+    ;_ (println)
+    grid))
+
+(defn- scale-grid [grid start scale]
+  (let [grid (scalegrid grid scale)]
+    ;_ (printgrid grid)
+    ;_ (println)
+    {:start-position (mapv #(* % scale) start)
+     :grid grid}))
+
+(defn- generate-tiled-map [texture grid]
+  (let [ground-idx (rand-nth uf-grounds)
+        {wall-x 0 wall-y 1 :as wall-idx} (rand-nth uf-walls)
+        transition-idx [wall-x (inc wall-y)]
+        position->tile (fn [position]
+                         (case (get grid position)
+                           :wall (wall-tile texture wall-idx)
+                           :transition (if (transition? grid position)
+                                         (transition-tile texture transition-idx)
+                                         (wall-tile texture wall-idx))
+                           :ground (ground-tile texture ground-idx)))]
+    (wgt-grid->tiled-map sprite-size grid position->tile)))
+
+; TODO don't spawn my faction vampire w. player items ...
+; FIXME - overlapping with player - don't spawn creatures on start position
+(defn create [{:keys [world/map-size world/spawn-rate]}
+              creature-properties
+              texture]
+  (let [{:keys [start grid]} (cave-grid :size map-size)
+        {:keys [start-position grid]} (scale-grid grid start scaling)
+        grid (assoc-transition-cells grid)
+        tiled-map (generate-tiled-map texture grid)
+        can-spawn? #(= "all" (tiled/movement-property tiled-map %))
+        _ (assert (can-spawn? start-position)) ; assuming hoping bottom left is movable
+        spawn-positions (flood-fill grid start-position can-spawn?)]
+    (set-creatures-tiles creature-properties
+                         spawn-rate
+                         tiled-map
+                         spawn-positions)
+    {:tiled-map tiled-map
+     :start-position start-position}))
 
 (defmethod generate-level* :world.generator/uf-caves [world {:keys [cdq/db] :as c}]
-  (uf-caves/create world
-                   (db/build-all db :properties/creatures c)
-                   ((:cdq/assets c) "maps/uf_terrain.png")))
+  (create world
+          (db/build-all db :properties/creatures c)
+          ((:cdq/assets c) "maps/uf_terrain.png")))
+
+; the problem is the implicit schema
+; and from that schema we can create an editor which exactly those properties only
+; so no tiled?
+; or fixed settings which get checked ?
+; or adapt tiled itself ???
+; or write in javafx a rpgmaker/tiled mix with start -> stats the actual game
+; or the editor is in the game itself
+; or the game itself is the editor
+
+; a series of expressions,executions on the grid/level
+; to transform it into the final form
+; with some schemas
+; and the different steps lay out
+; how does the world get created ?
+
+; you cannot make the world into a formular
+; you have to give the exact things which are available and make an editor
+; oryx rpg maker
+; not roguelike
+; actual RPGs with one time playing only
+; they are more interesting
+; where you doj;t know anything numbers or something
+; and only feeling is there
+; no widgets
+; nothing
+; no text
+; how you can control the character?
+; with keyboard/mouse thats the complicated part
+; => xbox controller make !
+; then just imaginw what should happen based on _context_
+
+; its a game like Half Life or Dark Souls
+; just a simple game with set of challenges
+
+
+
+; => data first <=
+; => level tileset oryx has what? -> make it into data <=
+; then can use
+; what is _is_ ....
+; door ?
+
 
