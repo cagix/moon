6a544bf3ebc3f36cea4333f290e7ca0a02de6af4
Michael Sappler <michaelsappler@gmail.com>
2025-04-25 10:27:11 +0530

    cdq.impl.entity

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -6,6 +6,7 @@
             [cdq.entity :as entity]
             [cdq.entity.fsm :as fsm]
             cdq.impl.effects
+            cdq.impl.entity
             [cdq.schema :as schema]
             [cdq.property :as property]
             [cdq.timer :as timer]
@@ -19,11 +20,6 @@
                                player-movement-vector
                                friendlies-in-radius
                                minimum-size
-                               item-place-position
-                               show-modal
-                               delayed-alert
-                               spawn-item
-                               spawn-audiovisual
                                world-item?
                                player-movement-vector
                                get-inventory
@@ -32,7 +28,6 @@
             cdq.world.context
             [cdq.world.potential-field :as potential-field]
             [gdl.assets :as assets]
-            [gdl.audio.sound :as sound]
             [gdl.data.grid2d :as g2d]
             [gdl.gdx.interop :as interop]
             [gdl.graphics :as graphics]
@@ -538,75 +533,6 @@
     (validate-properties! properties schemas)
     (map->DB {:db/data (zipmap (map :property/id properties) properties)
               :db/properties-file properties-file})))
-
-(defn- entity-components []
-  {:entity/destroy-audiovisual {:destroy! (fn [audiovisuals-id eid {:keys [cdq/db] :as c}]
-                                            (spawn-audiovisual c
-                                                               (:position @eid)
-                                                               (db/build db audiovisuals-id c)))}
-   :player-idle           {:pause-game? true}
-   :active-skill          {:pause-game? false
-                           :cursor :cursors/sandclock
-                           :enter (fn [[_ {:keys [eid skill]}]
-                                       {:keys [cdq.context/elapsed-time] :as c}]
-                                    (sound/play (:skill/start-action-sound skill))
-                                    (when (:skill/cooldown skill)
-                                      (swap! eid assoc-in
-                                             [:entity/skills (:property/id skill) :skill/cooling-down?]
-                                             (timer/create elapsed-time (:skill/cooldown skill))))
-                                    (when (and (:skill/cost skill)
-                                               (not (zero? (:skill/cost skill))))
-                                      (swap! eid entity/pay-mana-cost (:skill/cost skill))))}
-   :player-dead           {:pause-game? true
-                           :cursor :cursors/black-x
-                           :enter (fn [[_ {:keys [tx/sound
-                                                  modal/title
-                                                  modal/text
-                                                  modal/button-text]}]
-                                       c]
-                                    (sound/play sound)
-                                    (show-modal c {:title title
-                                                   :text text
-                                                   :button-text button-text
-                                                   :on-click (fn [])}))}
-   :player-item-on-cursor {:pause-game? true
-                           :cursor :cursors/hand-grab
-                           :enter (fn [[_ {:keys [eid item]}] c]
-                                    (swap! eid assoc :entity/item-on-cursor item))
-                           :exit (fn [[_ {:keys [eid player-item-on-cursor/place-world-item-sound]}] c]
-                                   ; at clicked-cell when we put it into a inventory-cell
-                                   ; we do not want to drop it on the ground too additonally,
-                                   ; so we dissoc it there manually. Otherwise it creates another item
-                                   ; on the ground
-                                   (let [entity @eid]
-                                     (when (:entity/item-on-cursor entity)
-                                       (sound/play place-world-item-sound)
-                                       (swap! eid dissoc :entity/item-on-cursor)
-                                       (spawn-item c
-                                                   (item-place-position c entity)
-                                                   (:entity/item-on-cursor entity)))))}
-   :player-moving         {:pause-game? false
-                           :cursor :cursors/walking
-                           :enter (fn [[_ {:keys [eid movement-vector]}] c]
-                                    (swap! eid assoc :entity/movement {:direction movement-vector
-                                                                       :speed (entity/stat @eid :entity/movement-speed)}))
-                           :exit (fn [[_ {:keys [eid]}] c]
-                                   (swap! eid dissoc :entity/movement))}
-   :stunned               {:pause-game? false
-                           :cursor :cursors/denied}
-   :npc-dead              {:enter (fn [[_ {:keys [eid]}] c]
-                                    (swap! eid assoc :entity/destroyed? true))}
-   :npc-moving            {:enter (fn [[_ {:keys [eid movement-vector]}] c]
-                                    (swap! eid assoc :entity/movement {:direction movement-vector
-                                                                       :speed (or (entity/stat @eid :entity/movement-speed) 0)}))
-                           :exit (fn [[_ {:keys [eid]}] c]
-                                   (swap! eid dissoc :entity/movement))}
-   :npc-sleeping          {:exit (fn [[_ {:keys [eid]}] c]
-                                   (delayed-alert c
-                                                  (:position       @eid)
-                                                  (:entity/faction @eid)
-                                                  0.2)
-                                   (swap! eid entity/add-text-effect c "[WHITE]!"))}})
 
 (defn- active-entities [{:keys [grid]} center-entity]
   (->> (let [idx (-> center-entity
@@ -1117,7 +1043,7 @@
                                                          :cdq.context/stage (create-stage! (:ui config) batch ui-viewport)
                                                          :cdq/schemas schemas
                                                          :cdq/db (create-db schemas)
-                                                         :context/entity-components (entity-components)}]
+                                                         :context/entity-components (cdq.impl.entity/components)}]
                                             (cdq.world.context/reset context (:first-level config)))))
 
                           (dispose []
