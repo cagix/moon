2ccf8d9dd0bf9bf91bc6f477f6a19eba0bf5ef00
Michael Sappler <michaelsappler@gmail.com>
2025-05-03 11:42:49 +0530

    Move cdq.impl.context into cdq.application

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -1,12 +1,285 @@
 (ns cdq.application
-  (:require [clojure.edn :as edn]
-            [clojure.java.io :as io])
-  (:import (com.badlogic.gdx ApplicationAdapter Gdx)
+  (:require [cdq.db :as db]
+            [cdq.schema :as schema]
+            [cdq.assets :as assets]
+            [cdq.gdx.interop :as interop]
+            cdq.graphics.animation
+            [cdq.graphics.shape-drawer :as shape-drawer]
+            cdq.graphics.sprite
+            [cdq.property :as property]
+            [cdq.ui.group :as group]
+            [cdq.utils :as utils]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.pprint :refer [pprint]])
+  (:import (cdq StageWithState OrthogonalTiledMapRenderer)
+           (clojure.lang ILookup)
+           (com.badlogic.gdx ApplicationAdapter Gdx)
            (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application Lwjgl3ApplicationConfiguration)
+           (com.badlogic.gdx.graphics Color Pixmap Pixmap$Format Texture Texture$TextureFilter OrthographicCamera)
+           (com.badlogic.gdx.graphics.g2d BitmapFont SpriteBatch TextureRegion)
+           (com.badlogic.gdx.graphics.g2d.freetype FreeTypeFontGenerator
+                                                   FreeTypeFontGenerator$FreeTypeFontParameter)
            (com.badlogic.gdx.utils Disposable SharedLibraryLoader Os)
-           (com.badlogic.gdx.utils.viewport Viewport)
+           (com.badlogic.gdx.utils.viewport FitViewport Viewport)
+           (com.kotcrab.vis.ui VisUI VisUI$SkinScale)
+           (com.kotcrab.vis.ui.widget Tooltip)
            (java.awt Taskbar Toolkit)
            (org.lwjgl.system Configuration)))
+
+(defrecord Cursors []
+  Disposable
+  (dispose [this]
+    (run! Disposable/.dispose (vals this))))
+
+(defn- load-cursors [config]
+  (map->Cursors
+   (utils/mapvals
+    (fn [[file [hotspot-x hotspot-y]]]
+      (let [pixmap (Pixmap. (.internal Gdx/files (str "cursors/" file ".png")))
+            cursor (.newCursor Gdx/graphics pixmap hotspot-x hotspot-y)]
+        (.dispose pixmap)
+        cursor))
+    config)))
+
+(defn- font-params [{:keys [size]}]
+  (let [params (FreeTypeFontGenerator$FreeTypeFontParameter.)]
+    (set! (.size params) size)
+    ; .color and this:
+    ;(set! (.borderWidth parameter) 1)
+    ;(set! (.borderColor parameter) red)
+    (set! (.minFilter params) Texture$TextureFilter/Linear) ; because scaling to world-units
+    (set! (.magFilter params) Texture$TextureFilter/Linear)
+    params))
+
+(defn- generate-font [file-handle params]
+  (let [generator (FreeTypeFontGenerator. file-handle)
+        font (.generateFont generator (font-params params))]
+    (.dispose generator)
+    font))
+
+(defn- load-font [{:keys [file size quality-scaling]}]
+  (let [^BitmapFont font (generate-font (.internal Gdx/files file)
+                                        {:size (* size quality-scaling)})]
+    (.setScale (.getData font) (float (/ quality-scaling)))
+    (set! (.markupEnabled (.getData font)) true)
+    (.setUseIntegerPositions font false) ; otherwise scaling to world-units (/ 1 48)px not visible
+    font))
+
+(defn- white-pixel-texture []
+  (let [pixmap (doto (Pixmap. 1 1 Pixmap$Format/RGBA8888)
+                 (.setColor Color/WHITE)
+                 (.drawPixel 0 0))
+        texture (Texture. pixmap)]
+    (.dispose pixmap)
+    texture))
+
+(defn- create-stage! [config batch viewport]
+  ; app crashes during startup before VisUI/dispose and we do cdq.tools.namespace.refresh-> gui elements not showing.
+  ; => actually there is a deeper issue at play
+  ; we need to dispose ALL resources which were loaded already ...
+  (when (VisUI/isLoaded)
+    (VisUI/dispose))
+  (VisUI/load (case (:skin-scale config)
+                :x1 VisUI$SkinScale/X1
+                :x2 VisUI$SkinScale/X2))
+  (-> (VisUI/getSkin)
+      (.getFont "default-font")
+      .getData
+      .markupEnabled
+      (set! true))
+  ;(set! Tooltip/DEFAULT_FADE_TIME (float 0.3))
+  ;Controls whether to fade out tooltip when mouse was moved. (default false)
+  ;(set! Tooltip/MOUSE_MOVED_FADEOUT true)
+  (set! Tooltip/DEFAULT_APPEAR_DELAY_TIME (float 0))
+  (let [stage (proxy [StageWithState ILookup] [viewport batch]
+                (valAt
+                  ([id]
+                   (group/find-actor-with-id (StageWithState/.getRoot this) id))
+                  ([id not-found]
+                   (or (group/find-actor-with-id (StageWithState/.getRoot this) id)
+                       not-found))))]
+    (.setInputProcessor Gdx/input stage)
+    stage))
+
+(defn- tiled-map-renderer [batch world-unit-scale]
+  (memoize (fn [tiled-map]
+             (OrthogonalTiledMapRenderer. tiled-map
+                                          (float world-unit-scale)
+                                          batch))))
+
+(defn- fit-viewport
+  "A ScalingViewport that uses Scaling.fit so it keeps the aspect ratio by scaling the world up to fit the screen, adding black bars (letterboxing) for the remaining space."
+  [width height camera & {:keys [center-camera?]}]
+  {:pre [width height]}
+  (proxy [FitViewport ILookup] [width height camera]
+    (valAt
+      ([key]
+       (interop/k->viewport-field this key))
+      ([key _not-found]
+       (interop/k->viewport-field this key)))))
+
+(defn- world-viewport [world-unit-scale config]
+  {:pre [world-unit-scale]}
+  (let [camera (OrthographicCamera.)
+        world-width  (* (:width  config) world-unit-scale)
+        world-height (* (:height config) world-unit-scale)
+        y-down? false]
+    (.setToOrtho camera y-down? world-width world-height)
+    (fit-viewport world-width world-height camera)))
+
+; reduce-kv?
+(defn- apply-kvs
+  "Calls for every key in map (f k v) to calculate new value at k."
+  [m f]
+  (reduce (fn [m k]
+            (assoc m k (f k (get m k)))) ; using assoc because non-destructive for records
+          m
+          (keys m)))
+
+(defmethod schema/edn->value :s/sound [_ sound-name {:keys [cdq/assets]}]
+  (assets/sound assets sound-name))
+
+(defn- edn->sprite [c {:keys [file sub-image-bounds]}]
+  (if sub-image-bounds
+    (let [[sprite-x sprite-y] (take 2 sub-image-bounds)
+          [tilew tileh]       (drop 2 sub-image-bounds)]
+      (cdq.graphics.sprite/from-sheet (cdq.graphics.sprite/sheet c file tilew tileh)
+                                      [(int (/ sprite-x tilew))
+                                       (int (/ sprite-y tileh))]
+                                      c))
+    (cdq.graphics.sprite/create c file)))
+
+(defmethod schema/edn->value :s/image [_ edn c]
+  (edn->sprite c edn))
+
+(defmethod schema/edn->value :s/animation [_ {:keys [frames frame-duration looping?]} c]
+  (cdq.graphics.animation/create (map #(edn->sprite c %) frames)
+                                     :frame-duration frame-duration
+                                     :looping? looping?))
+
+(defmethod schema/edn->value :s/one-to-one [_ property-id {:keys [cdq/db] :as context}]
+  (db/build db property-id context))
+
+(defmethod schema/edn->value :s/one-to-many [_ property-ids {:keys [cdq/db] :as context}]
+  (set (map #(db/build db % context) property-ids)))
+
+(defn- validate-properties! [properties schemas]
+  (assert (or (empty? properties)
+              (apply distinct? (map :property/id properties))))
+  (run! #(schema/validate! schemas (property/type %) %) properties))
+
+#_(def ^:private undefined-data-ks (atom #{}))
+
+(comment
+ #{:frames
+   :looping?
+   :frame-duration
+   :file ; => this is texture ... convert that key itself only?!
+   :sub-image-bounds})
+
+(defn- build* [{:keys [cdq/schemas] :as c} property]
+  (apply-kvs property
+             (fn [k v]
+               (let [schema (try (schema/schema-of schemas k)
+                                 (catch Throwable _t
+                                   #_(swap! undefined-data-ks conj k)
+                                   nil))
+                     v (if (map? v)
+                         (build* c v)
+                         v)]
+                 (try (schema/edn->value schema v c)
+                      (catch Throwable t
+                        (throw (ex-info " " {:k k :v v} t))))))))
+
+(defn- recur-sort-map [m]
+  (into (sorted-map)
+        (zipmap (keys m)
+                (map #(if (map? %)
+                        (recur-sort-map %)
+                        %)
+                     (vals m)))))
+
+(defn- async-pprint-spit! [file data]
+  (.start
+   (Thread.
+    (fn []
+      (binding [*print-level* nil]
+        (->> data
+             pprint
+             with-out-str
+             (spit file)))))))
+
+(defrecord DB []
+  db/DB
+  (async-write-to-file! [{:keys [db/data db/properties-file]}]
+    ; TODO validate them again!?
+    (->> data
+         vals
+         (sort-by property/type)
+         (map recur-sort-map)
+         doall
+         (async-pprint-spit! properties-file)))
+
+  (update [{:keys [db/data] :as db}
+           {:keys [property/id] :as property}
+           schemas]
+    {:pre [(contains? property :property/id)
+           (contains? data id)]}
+    (schema/validate! schemas (property/type property) property)
+    (clojure.core/update db :db/data assoc id property)) ; assoc-in ?
+
+  (delete [{:keys [db/data] :as db} property-id]
+    {:pre [(contains? data property-id)]}
+    (clojure.core/update db dissoc :db/data property-id)) ; dissoc-in ?
+
+  (get-raw [{:keys [db/data]} id]
+    (utils/safe-get data id))
+
+  (all-raw [{:keys [db/data]} property-type]
+    (->> (vals data)
+         (filter #(= property-type (property/type %)))))
+
+  (build [this id context]
+    (build* context (db/get-raw this id)))
+
+  (build-all [this property-type context]
+    (map (partial build* context)
+         (db/all-raw this property-type))))
+
+(defn- create-db [schemas]
+  (let [properties-file (io/resource "properties.edn")
+        properties (-> properties-file slurp edn/read-string)]
+    (validate-properties! properties schemas)
+    (map->DB {:db/data (zipmap (map :property/id properties) properties)
+              :db/properties-file properties-file})))
+
+(defn- create-initial-context! [config]
+  (let [batch (SpriteBatch.)
+        shape-drawer-texture (white-pixel-texture)
+        world-unit-scale (float (/ (:world-unit-scale config)))
+        ui-viewport (fit-viewport (:width  (:ui-viewport config))
+                                  (:height (:ui-viewport config))
+                                  (OrthographicCamera.))
+        schemas (-> (:schemas config) io/resource slurp edn/read-string)]
+    {:cdq/assets (assets/create (:assets config))
+     ;;
+     :cdq.graphics/batch batch
+     :cdq.graphics/cursors (load-cursors (:cursors config))
+     :cdq.graphics/default-font (load-font (:default-font config))
+     :cdq.graphics/shape-drawer (shape-drawer/create batch (TextureRegion. ^Texture shape-drawer-texture 1 0 1 1))
+     :cdq.graphics/shape-drawer-texture shape-drawer-texture
+     :cdq.graphics/tiled-map-renderer (tiled-map-renderer batch world-unit-scale)
+     :cdq.graphics/world-unit-scale world-unit-scale
+     :cdq.graphics/world-viewport (world-viewport world-unit-scale (:world-viewport config))
+     ;;
+     :cdq.graphics/ui-viewport ui-viewport
+     :cdq.context/stage (create-stage! (:ui config) batch ui-viewport)
+     ;;
+     :cdq/schemas schemas
+     :cdq/db (create-db schemas)
+     ;;
+     }))
 
 (def state
   "Do not call `swap!`, instead use `post-runnable!`, as the main game loop has side-effects and should not be retried.
@@ -43,7 +316,7 @@
                           (create []
                             (reset! state (reduce (fn [context f]
                                                     (f context config))
-                                                  {}
+                                                  (create-initial-context! config)
                                                   create-pipeline)))
 
                           (dispose []
