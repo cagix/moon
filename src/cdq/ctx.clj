a14c58b6b5394601dec31d75f929edaede0a27e2
Michael Sappler <michaelsappler@gmail.com>
2025-05-10 16:08:01 +0530

    Remove comments

--- src/cdq/ctx.clj
+++ src/cdq/ctx.clj
@@ -1,27 +1,12 @@
 (ns cdq.ctx)
 
-; -> all libgdx or even 'clojure.gdx' hide behind main protocols ??
-
 (declare
 
  ^{:doc "Implements [[cdq.assets/Assets]] and `clojure.lang.IFn` with one arg, like this: `(assets \"my_image.png\")`.
 
         Returns the asset or throws an exception if it cannot be found."}
- assets ; -> separate into 'audio' and 'graphics' ( ?)
+ assets
 
  ^{:doc "Instance of [[cdq.graphics/Graphics]]."}
  graphics
-
- ; stage
-
- ; world
-
- ; db
-
- ; app => post-runnable
-
- ; files -> internal file-handle  (again protocol ?)
-
- ; input -> key/button - pressed ?
-
  )
