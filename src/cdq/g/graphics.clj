284610bf24f50115b00048398afccaf9b1f0f223
Michael Sappler <michaelsappler@gmail.com>
2025-05-13 21:54:29 +0530

    Remove clojure.gdx.graphics

--- src/cdq/g/graphics.clj
+++ src/cdq/g/graphics.clj
@@ -1,17 +1,130 @@
 (ns cdq.g.graphics
   (:require cdq.graphics
-            [clojure.gdx.graphics :as graphics]
             [clojure.gdx.graphics.camera :as camera]
             [clojure.gdx.interop :as interop]
             [clojure.gdx.tiled :as tiled]
-            [clojure.gdx.math :refer [degree->radians]]
-            [clojure.utils :as utils])
+            [clojure.gdx.math :refer [clamp degree->radians]]
+            [clojure.utils :as utils]
+            [clojure.string :as str])
   (:import (com.badlogic.gdx Gdx)
-           (com.badlogic.gdx.graphics Color Texture)
-           (com.badlogic.gdx.graphics.g2d Batch SpriteBatch TextureRegion)
+           (com.badlogic.gdx.graphics Color Pixmap Pixmap$Format Texture Texture$TextureFilter OrthographicCamera)
+           (com.badlogic.gdx.graphics.g2d Batch BitmapFont SpriteBatch TextureRegion)
+           (com.badlogic.gdx.graphics.g2d.freetype FreeTypeFontGenerator
+                                                   FreeTypeFontGenerator$FreeTypeFontParameter)
+           (com.badlogic.gdx.math Vector2)
            (com.badlogic.gdx.utils Disposable)
-           (com.badlogic.gdx.utils.viewport Viewport)
+           (com.badlogic.gdx.utils.viewport FitViewport Viewport)
            (space.earlygrey.shapedrawer ShapeDrawer)))
+
+(defn- text-height [^BitmapFont font text]
+  (-> text
+      (str/split #"\n")
+      count
+      (* (.getLineHeight font))))
+
+(defn- draw-text! [{:keys [^BitmapFont font
+                           scale
+                           batch
+                           x
+                           y
+                           text
+                           h-align
+                           up?]}]
+  (let [data (.getData font)
+        old-scale (float (.scaleX data))
+        new-scale (float (* old-scale (float scale)))
+        target-width (float 0)
+        wrap? false]
+    (.setScale data new-scale)
+    (.draw font
+           batch
+           text
+           (float x)
+           (float (+ y (if up? (text-height font text) 0)))
+           target-width
+           (interop/k->align (or h-align :center))
+           wrap?)
+    (.setScale data old-scale)))
+
+(defn- draw-texture-region [^Batch batch texture-region [x y] [w h] rotation color]
+  (if color (.setColor batch color))
+  (.draw batch
+         texture-region
+         x
+         y
+         (/ (float w) 2) ; rotation origin
+         (/ (float h) 2)
+         w
+         h
+         1 ; scale-x
+         1 ; scale-y
+         rotation)
+  (if color (.setColor batch Color/WHITE)))
+
+(defn- fit-viewport
+  ([width height]
+   (fit-viewport width height (OrthographicCamera.)))
+  ([width height camera]
+   (proxy [FitViewport clojure.lang.ILookup] [width height camera]
+     (valAt
+       ([key]
+        (interop/k->viewport-field this key))
+       ([key _not-found]
+        (interop/k->viewport-field this key))))))
+
+(defn- ->world-viewport [world-unit-scale {:keys [width height]}]
+  (let [camera (OrthographicCamera.)
+        world-width  (* width world-unit-scale)
+        world-height (* height world-unit-scale)
+        y-down? false]
+    (.setToOrtho camera y-down? world-width world-height)
+    (fit-viewport world-width world-height camera)))
+
+(defn- font-params [{:keys [size]}]
+  (let [params (FreeTypeFontGenerator$FreeTypeFontParameter.)]
+    (set! (.size params) size)
+    ; .color and this:
+    ;(set! (.borderWidth parameter) 1)
+    ;(set! (.borderColor parameter) red)
+    (set! (.minFilter params) Texture$TextureFilter/Linear) ; because scaling to world-units
+    (set! (.magFilter params) Texture$TextureFilter/Linear)
+    params))
+
+(defn- generate-font [file-handle params]
+  (let [generator (FreeTypeFontGenerator. file-handle)
+        font (.generateFont generator (font-params params))]
+    (.dispose generator)
+    font))
+
+(defn- truetype-font [{:keys [file size quality-scaling]}]
+  (let [^BitmapFont font (generate-font (.internal Gdx/files file)
+                                        {:size (* size quality-scaling)})]
+    (.setScale (.getData font) (float (/ quality-scaling)))
+    (set! (.markupEnabled (.getData font)) true)
+    (.setUseIntegerPositions font false) ; otherwise scaling to world-units (/ 1 48)px not visible
+    font))
+
+(defn- white-pixel-texture []
+  (let [pixmap (doto (Pixmap. 1 1 Pixmap$Format/RGBA8888)
+                 (.setColor Color/WHITE)
+                 (.drawPixel 0 0))
+        texture (Texture. pixmap)]
+    (.dispose pixmap)
+    texture))
+
+; touch coordinates are y-down, while screen coordinates are y-up
+; so the clamping of y is reverse, but as black bars are equal it does not matter
+(defn- unproject-mouse-position
+  "Returns vector of [x y]."
+  [viewport]
+  (let [mouse-x (clamp (.getX Gdx/input)
+                       (:left-gutter-width viewport)
+                       (:right-gutter-x    viewport))
+        mouse-y (clamp (.getY Gdx/input)
+                       (:top-gutter-height viewport)
+                       (:top-gutter-y      viewport))]
+    (let [v2 (Viewport/.unproject viewport (Vector2. mouse-x mouse-y))]
+      [(.x v2) (.y v2)])))
 
 (defn- unit-dimensions [image unit-scale]
   (if (= unit-scale 1)
@@ -70,47 +183,47 @@
   cdq.graphics/Graphics
   (mouse-position [_]
     ; TODO mapv int needed?
-    (mapv int (graphics/unproject-mouse-position ui-viewport)))
+    (mapv int (unproject-mouse-position ui-viewport)))
 
   (world-mouse-position [_]
     ; TODO clamping only works for gui-viewport ? check. comment if true
     ; TODO ? "Can be negative coordinates, undefined cells."
-    (graphics/unproject-mouse-position world-viewport))
+    (unproject-mouse-position world-viewport))
 
   (pixels->world-units [_ pixels]
     (* (int pixels) world-unit-scale))
 
   (draw-image [_ {:keys [texture-region color] :as image} position]
-    (graphics/draw-texture-region batch
-                                  texture-region
-                                  position
-                                  (unit-dimensions image @unit-scale)
-                                  0 ; rotation
-                                  color))
+    (draw-texture-region batch
+                         texture-region
+                         position
+                         (unit-dimensions image @unit-scale)
+                         0 ; rotation
+                         color))
 
   (draw-rotated-centered [_ {:keys [texture-region color] :as image} rotation [x y]]
     (let [[w h] (unit-dimensions image @unit-scale)]
-      (graphics/draw-texture-region batch
-                                    texture-region
-                                    [(- (float x) (/ (float w) 2))
-                                     (- (float y) (/ (float h) 2))]
-                                    [w h]
-                                    rotation
-                                    color)))
+      (draw-texture-region batch
+                           texture-region
+                           [(- (float x) (/ (float w) 2))
+                            (- (float y) (/ (float h) 2))]
+                           [w h]
+                           rotation
+                           color)))
 
   (set-camera-position! [_ position]
     (camera/set-position! (:camera world-viewport) position))
 
   (draw-text [_ {:keys [font scale x y text h-align up?]}]
-    (graphics/draw-text! {:font (or font default-font)
-                          :scale (* (float @unit-scale)
-                                    (float (or scale 1)))
-                          :batch batch
-                          :x x
-                          :y y
-                          :text text
-                          :h-align h-align
-                          :up? up?}))
+    (draw-text! {:font (or font default-font)
+                 :scale (* (float @unit-scale)
+                           (float (or scale 1)))
+                 :batch batch
+                 :x x
+                 :y y
+                 :text text
+                 :h-align h-align
+                 :up? up?}))
 
   (draw-ellipse [_ [x y] radius-x radius-y color]
     (set-color! shape-drawer color)
@@ -258,7 +371,7 @@
                       world-viewport
                       ui-viewport]}]
   (let [batch (SpriteBatch.)
-        shape-drawer-texture (graphics/white-pixel-texture)
+        shape-drawer-texture (white-pixel-texture)
         world-unit-scale (float (/ tile-size))]
     (map->Graphics
      {:batch batch
@@ -266,18 +379,18 @@
       :shape-drawer (ShapeDrawer. batch (TextureRegion. ^Texture shape-drawer-texture 1 0 1 1))
       :cursors (utils/mapvals
                 (fn [[file [hotspot-x hotspot-y]]]
-                  (let [pixmap (graphics/pixmap (str "cursors/" file ".png"))
+                  (let [pixmap (Pixmap. (.internal Gdx/files (str "cursors/" file ".png")))
                         cursor (.newCursor Gdx/graphics pixmap hotspot-x hotspot-y)]
                     (.dispose pixmap)
                     cursor))
                 cursors)
-      :default-font (graphics/truetype-font default-font)
+      :default-font (truetype-font default-font)
       :world-unit-scale  world-unit-scale
-      :world-viewport (graphics/world-viewport world-unit-scale world-viewport)
+      :world-viewport (->world-viewport world-unit-scale world-viewport)
       :get-tiled-map-renderer (memoize (fn [tiled-map]
                                          (tiled/renderer tiled-map
                                                          world-unit-scale
                                                          batch)))
-      :ui-viewport (graphics/fit-viewport (:width  ui-viewport)
-                                          (:height ui-viewport))
+      :ui-viewport (fit-viewport (:width  ui-viewport)
+                                 (:height ui-viewport))
       :unit-scale (atom 1)})))
