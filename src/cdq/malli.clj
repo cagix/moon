c7fc67a49923eec431564ff02c739bbebe2f640b
Michael Sappler <michaelsappler@gmail.com>
2025-05-14 11:32:47 +0530

    add stuff to cdq.malli for checking malli-schema-forms

--- src/cdq/malli.clj
+++ src/cdq/malli.clj
@@ -14,3 +14,56 @@
       [:boz {:optional false}]
       [:asdf {:optional true}]])
     [:foo :bar :baz :boz :asdf]))
+
+(defn- map-form-k->properties
+  "Given a map schema gives a map of key to key properties (like :optional)."
+  [map-schema]
+  (let [[_m _p & ks] map-schema]
+    (into {} (for [[k m? _schema] ks]
+               [k (if (map? m?) m?)]))))
+
+(comment
+ (= (map-form-k->properties
+     [:map {:closed true}
+      [:foo]
+      [:bar]
+      [:baz {:optional true}]
+      [:boz {:optional false}]
+      [:asdf {:optional true}]])
+    {:foo nil,
+     :bar nil,
+     :baz {:optional true},
+     :boz {:optional false},
+     :asdf {:optional true}}))
+
+(defn optional? [k map-schema]
+  (:optional (k (map-form-k->properties map-schema))))
+
+(comment
+ (= (optional? :foo
+     [:map {:closed true}
+      [:foo]
+      [:bar]
+      [:baz {:optional true}]
+      [:boz {:optional false}]
+      [:asdf {:optional true}]])
+    nil)
+
+ (= (optional? :baz
+               [:map {:closed true}
+                [:foo]
+                [:bar]
+                [:baz {:optional true}]
+                [:boz {:optional false}]
+                [:asdf {:optional true}]])
+    true)
+
+ (= (optional? :asdf
+               [:map {:closed true}
+                [:foo]
+                [:bar]
+                [:baz {:optional true}]
+                [:boz {:optional false}]
+                [:asdf {:optional true}]])
+    true)
+ )
--- src/cdq/schema.clj
+++ src/cdq/schema.clj
@@ -22,18 +22,8 @@
 (defn map-keys [schema schemas]
   (malli/map-keys (malli-form schema schemas)))
 
-(defn- map-form-k->properties
-  "Given a map schema gives a map of key to key properties (like :optional)."
-  [m-schema]
-  (let [[_m _p & ks] m-schema]
-    (into {} (for [[k m? _schema] ks]
-               [k (if (map? m?) m?)]))))
-
-(defn- optional? [k map-schema]
-  (:optional (k (map-form-k->properties map-schema))))
-
 (defn optional-k? [k schema schemas]
-  (optional? k (malli-form schema schemas)))
+  (malli/optional? k (malli-form schema schemas)))
 
 (defn- optional-keyset [m-schema schemas]
   (set (filter #(optional? % m-schema) (map-keys m-schema schemas))))
