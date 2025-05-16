29726c08c58accc2e4611736367f369bb05296fa
Michael Sappler <michaelsappler@gmail.com>
2025-05-16 21:22:07 +0530

    Remove stage protocol

--- src/cdq/stage.clj
+++ src/cdq/stage.clj
@@ -25,63 +25,44 @@
    (cdq.ui.player-state-draw/create)
    (cdq.ui.message/create)])
 
-(defprotocol PStage
-  (add-actor! [_ actor])
-  (draw! [_])
-  (act! [_])
-  (mouse-on-actor? [_])
-  (root [_])
-  (set-item! [stage cell item])
-  (remove-item! [stage cell])
-  (selected-skill [stage])
-  (add-skill! [stage skill])
-  (remove-skill! [stage skill])
-  (show-message! [stage text]))
+(defn add-actor! [stage actor]
+  (Stage/.addActor stage actor))
+
+(defn draw! [stage]
+  (Stage/.draw stage))
+
+(defn act! [stage]
+  (Stage/.act stage))
+
+(defn mouse-on-actor? [stage]
+  (let [[x y] (viewport/mouse-position ctx/ui-viewport)]
+    (Stage/.hit stage x y true)))
+
+(defn root [stage]
+  (Stage/.getRoot stage))
+
+(defn set-item! [stage cell item]
+  (cdq.ui.inventory/set-item! stage cell item))
+
+(defn remove-item! [stage cell]
+  (cdq.ui.inventory/remove-item! stage cell))
+
+(defn selected-skill [stage]
+  (cdq.ui.action-bar/selected-skill stage))
+
+(defn add-skill! [stage skill]
+  (cdq.ui.action-bar/add-skill! stage skill))
+
+(defn remove-skill! [stage skill]
+  (cdq.ui.action-bar/remove-skill! stage skill))
+
+(defn show-message! [stage text]
+  (cdq.ui.message/show! (root stage) text))
 
 (defn create! []
-  (let [stage (Stage. (:java-object ctx/ui-viewport)
-                      (:java-object ctx/batch))]
-    (run! #(.addActor stage %) (create-actors))
-    (reify
-      ILookup
-      (valAt [_ id]
-        (ui/find-actor-with-id (.getRoot stage) id))
-
-      (valAt [_ id not-found]
-        (or (ui/find-actor-with-id (.getRoot stage) id)
-            not-found))
-
-      PStage
-      (add-actor! [_ actor]
-        (.addActor stage actor))
-
-      (draw! [_]
-        (.draw stage))
-
-      (act! [_]
-        (.act stage))
-
-      (mouse-on-actor? [_]
-        (let [[x y] (viewport/mouse-position ctx/ui-viewport)]
-          (.hit stage x y true)))
-
-      (root [_]
-        (.getRoot stage))
-
-      (set-item! [stage cell item]
-        (cdq.ui.inventory/set-item! stage cell item))
-
-      (remove-item! [stage cell]
-        (cdq.ui.inventory/remove-item! stage cell))
-
-      (selected-skill [stage]
-        (cdq.ui.action-bar/selected-skill stage))
-
-      (add-skill! [stage skill]
-        (cdq.ui.action-bar/add-skill! stage skill))
-
-      (remove-skill! [stage skill]
-        (cdq.ui.action-bar/remove-skill! stage skill))
-
-      (show-message! [stage text]
-        (cdq.ui.message/show! (root stage) text)))))
+  (let [stage (proxy [Stage ILookup] [(:java-object ctx/ui-viewport)
+                                      (:java-object ctx/batch)]
+                (valAt [id]
+                  (ui/find-actor-with-id (Stage/.getRoot this) id)))]
+    (run! #(Stage/.addActor stage %) (create-actors))
+    stage))
