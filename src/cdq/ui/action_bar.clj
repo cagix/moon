49f089bbed0e019f21aa972851bbaf633e154fd4
Michael Sappler <michaelsappler@gmail.com>
2025-05-17 12:07:41 +0530

    add gdl.ui.button-group

--- src/cdq/ui/action_bar.clj
+++ src/cdq/ui/action_bar.clj
@@ -1,24 +1,18 @@
 (ns cdq.ui.action-bar
   (:require [cdq.info :as info]
             [gdl.ui :as ui]
-            [gdl.ui.actor :as actor])
+            [gdl.ui.actor :as actor]
+            [gdl.ui.button-group :as button-group])
   (:import (com.badlogic.gdx.scenes.scene2d Actor
-                                            Group)
-           (com.badlogic.gdx.scenes.scene2d.ui Button
-                                               ButtonGroup)))
-
-(defn- button-group [{:keys [max-check-count min-check-count]}]
-  (doto (ButtonGroup.)
-    (.setMaxCheckCount max-check-count)
-    (.setMinCheckCount min-check-count)))
+                                            Group)))
 
 (defn create []
   (ui/table {:rows [[{:actor (doto (ui/horizontal-group {:pad 2 :space 2})
                                (Actor/.setUserObject ::horizontal-group)
                                (Group/.addActor (doto (proxy [Actor] [])
                                                   (Actor/.setName "button-group")
-                                                  (Actor/.setUserObject (button-group {:max-check-count 1
-                                                                                       :min-check-count 0})))))
+                                                  (Actor/.setUserObject (button-group/create {:max-check-count 1
+                                                                                              :min-check-count 0})))))
                       :expand? true
                       :bottom? true}]]
              :id ::action-bar-table
@@ -31,7 +25,7 @@
      :button-group (Actor/.getUserObject (Group/.findActor group "button-group"))}))
 
 (defn selected-skill [stage]
-  (when-let [skill-button (ButtonGroup/.getChecked (:button-group (get-data stage)))]
+  (when-let [skill-button (button-group/checked (:button-group (get-data stage)))]
     (Actor/.getUserObject skill-button)))
 
 (defn add-skill! [stage {:keys [property/id entity/image] :as skill}]
@@ -40,12 +34,12 @@
     (Actor/.setUserObject button id)
     (actor/add-tooltip! button #(info/text skill)) ; (assoc ctx :effect/source (world/player)) FIXME
     (Group/.addActor horizontal-group button)
-    (ButtonGroup/.add button-group ^Button button)
+    (button-group/add! button-group button)
     nil))
 
 (defn remove-skill! [stage {:keys [property/id]}]
   (let [{:keys [horizontal-group button-group]} (get-data stage)
         button (get horizontal-group id)]
     (Actor/.remove button)
-    (ButtonGroup/.remove button-group ^Button button)
+    (button-group/remove! button-group button)
     nil))
