56a838b0827a2021a4a2e7ddc8cd2b747819a70b
Michael Sappler <michaelsappler@gmail.com>
2025-05-18 15:03:18 +0530

    Use minimum gdx stuff in assets

--- src/cdq/application/create/assets.clj
+++ src/cdq/application/create/assets.clj
@@ -1,7 +1,29 @@
 (ns cdq.application.create.assets
   (:require [cdq.ctx :as ctx]
             [cdq.utils :refer [bind-root]]
-            [gdl.assets :as assets]))
+            [clojure.string :as str]
+            [gdl.assets :as assets]
+            [gdl.files :as files]
+            [gdl.files.file-handle :as fh]))
+
+(defn- search [{:keys [folder
+                       asset-type-extensions]}]
+  (for [[asset-type extensions] asset-type-extensions
+        file (map #(str/replace-first % folder "")
+                  (loop [[file & remaining] (fh/list (files/internal folder))
+                         result []]
+                    (cond (nil? file)
+                          result
+
+                          (fh/directory? file)
+                          (recur (concat remaining (fh/list file)) result)
+
+                          (extensions (fh/extension file))
+                          (recur remaining (conj result (fh/path file)))
+
+                          :else
+                          (recur remaining result))))]
+    [file asset-type]))
 
 (defn do! []
-  (bind-root #'ctx/assets (assets/create (:assets ctx/config))))
+  (bind-root #'ctx/assets (assets/create (search (:assets ctx/config)))))
