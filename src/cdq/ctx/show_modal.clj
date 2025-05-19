8c45bf6521795b810b7db5496a508adc48f46de9
Michael Sappler <michaelsappler@gmail.com>
2025-05-19 20:02:29 +0530

    Add ctx.ctx.show-modal with ctx param

---
+++ src/cdq/ctx/show_modal.clj
@@ -0,0 +1,23 @@
+(ns cdq.ctx.show-modal
+  (:require [gdl.ui :as ui]))
+
+; no window movable type cursor appears here like in player idle
+; inventory still working, other stuff not, because custom listener to keypresses ? use actor listeners?
+; => input events handling
+; hmmm interesting ... can disable @ item in cursor  / moving / etc.
+(defn do! [{:keys [ctx/stage
+                   ctx/ui-viewport]}
+           {:keys [title text button-text on-click]}]
+  (assert (not (::modal stage)))
+  (ui/add! stage
+           (ui/window {:title title
+                       :rows [[(ui/label text)]
+                              [(ui/text-button button-text
+                                               (fn []
+                                                 (ui/remove! (::modal stage))
+                                                 (on-click)))]]
+                       :id ::modal
+                       :modal? true
+                       :center-position [(/ (:width  ui-viewport) 2)
+                                         (* (:height ui-viewport) (/ 3 4))]
+                       :pack? true})))
--- src/cdq/tx/show_modal.clj
+++ src/cdq/tx/show_modal.clj
@@ -1,7 +1,17 @@
 (ns cdq.tx.show-modal
   (:require [cdq.ctx :as ctx]
-            [cdq.ui.modal]
-            [gdl.ui :as ui]))
+            [cdq.ctx.show-modal :as show-modal]))
 
 (defn do! [opts]
-  (ui/add! ctx/stage (cdq.ui.modal/create opts)))
+  (show-modal/do! {:ctx/stage ctx/stage
+                   :ctx/ui-viewport ctx/ui-viewport}
+                  opts))
+
+(comment
+ (gdl.application/post-runnable!
+  (cdq.utils/handle-txs! [[:tx/show-modal {:title "hey title"
+                                           :text "my text"
+                                           :button-text "button txt"
+                                           :on-click (fn []
+                                                       (println "hoho"))}]]))
+ )
