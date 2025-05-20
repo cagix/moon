6216673750aec29a7ca48bfb2d1113feb3573fd9
Michael Sappler <michaelsappler@gmail.com>
2025-05-20 11:41:00 +0530

    pass assets to texture widgets

--- src/cdq/ui/editor/widget/image.clj
+++ src/cdq/ui/editor/widget/image.clj
@@ -1,14 +1,13 @@
 (ns cdq.ui.editor.widget.image
-  (:require #_[cdq.ctx :as ctx]
-            [cdq.schema :as schema]
+  (:require [cdq.schema :as schema]
             [cdq.ui.editor.widget :as widget]
             #_[gdl.assets :as assets]
             [gdl.ui :as ui]))
 
 ; too many ! too big ! scroll ... only show files first & preview?
 ; make tree view from folders, etc. .. !! all creatures animations showing...
-#_(defn- texture-rows []
-    (for [file (sort (assets/all-of-type ctx/assets :texture))]
+#_(defn- texture-rows [assets]
+    (for [file (sort (assets/all-of-type assets :texture))]
       [(ui/image-button (image file) (fn []))]
       #_[(ui/text-button file (fn []))]))
 
@@ -17,5 +16,5 @@
                    (fn on-clicked [])
                    {:scale 2})
   #_(ui/image-button image
-                     #(ui/add! ctx/stage (scroll-pane/choose-window (texture-rows)))
+                     #(ui/add! ctx/stage (scroll-pane/choose-window (texture-rows ctx/assets)))
                      {:dimensions [96 96]})) ; x2  , not hardcoded here
