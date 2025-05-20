9d8f14590999952cd992d68fac03b2108ae87175
Michael Sappler <michaelsappler@gmail.com>
2025-05-20 08:39:00 +0530

    Separate assets/files-search/context-bind

--- src/cdq/application/create/assets.clj
+++ src/cdq/application/create/assets.clj
@@ -1,29 +1,7 @@
 (ns cdq.application.create.assets
-  (:require [cdq.ctx :as ctx]
-            [cdq.utils :refer [bind-root]]
-            [clojure.string :as str]
-            [gdl.assets :as assets]
-            [gdl.files :as files]
-            [gdl.files.file-handle :as fh]))
-
-(defn- search [{:keys [folder
-                       asset-type-extensions]}]
-  (for [[asset-type extensions] asset-type-extensions
-        file (map #(str/replace-first % folder "")
-                  (loop [[file & remaining] (fh/list (files/internal folder))
-                         result []]
-                    (cond (nil? file)
-                          result
-
-                          (fh/directory? file)
-                          (recur (concat remaining (fh/list file)) result)
-
-                          (extensions (fh/extension file))
-                          (recur remaining (conj result (fh/path file)))
-
-                          :else
-                          (recur remaining result))))]
-    [file asset-type]))
+  (:require [cdq.assets :as assets]
+            [cdq.ctx :as ctx]
+            [cdq.utils :refer [bind-root]]))
 
 (defn do! []
-  (bind-root #'ctx/assets (assets/create (search (:assets ctx/config)))))
+  (bind-root #'ctx/assets (assets/create (:assets ctx/config))))
---
+++ src/cdq/assets.clj
@@ -0,0 +1,12 @@
+(ns cdq.assets
+  (:require [cdq.utils.files :as files]
+            [clojure.string :as str]
+            [gdl.assets :as assets]))
+
+(defn create [{:keys [folder
+                      asset-type-extensions]}]
+  (assets/create
+   (for [[asset-type extensions] asset-type-extensions
+         file (map #(str/replace-first % folder "")
+                   (files/recursively-search folder extensions))]
+     [file asset-type])))
