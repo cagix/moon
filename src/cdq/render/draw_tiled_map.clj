cef351715583728787d4cb71a57f355b91e0ac85
Michael Sappler <michaelsappler@gmail.com>
2025-05-21 10:48:07 +0530

    Move ctx/draw-tiled-map into cdq.render.draw-tiled-map

--- src/cdq/render/draw_tiled_map.clj
+++ src/cdq/render/draw_tiled_map.clj
@@ -1,6 +1,17 @@
 (ns cdq.render.draw-tiled-map
-  (:require [cdq.ctx.draw-tiled-map :as draw-tiled-map]))
+  (:require [cdq.tile-color-setter :as tile-color-setter]
+            [gdl.graphics.camera :as camera]
+            [gdl.tiled :as tiled]))
 
-(defn do! [ctx]
-  (draw-tiled-map/do! ctx)
+(defn do! [{:keys [ctx/get-tiled-map-renderer
+                   ctx/tiled-map
+                   ctx/raycaster
+                   ctx/explored-tile-corners
+                   ctx/world-viewport]}]
+  (tiled/draw! (get-tiled-map-renderer tiled-map)
+               tiled-map
+               (tile-color-setter/create raycaster
+                                         explored-tile-corners
+                                         (camera/position (:camera world-viewport)))
+               (:camera world-viewport))
   nil)
