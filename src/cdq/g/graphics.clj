a7fd793cc14b561563a3db7473eadf9d3e727b9b
Michael Sappler <michaelsappler@gmail.com>
2025-05-23 08:35:54 +0530

    Move cdq.draw into cdq.g.graphics

--- src/cdq/g/graphics.clj
+++ src/cdq/g/graphics.clj
@@ -1,51 +1,128 @@
 (ns cdq.g.graphics
-  (:require [cdq.draw :as draw]
-            [cdq.g :as g]
-            [gdl.graphics.batch :as batch]))
+  (:require [cdq.g :as g]
+            [gdl.graphics :as graphics]
+            [gdl.graphics.batch :as batch]
+            [gdl.graphics.shape-drawer :as sd]))
+
+(defn- unit-dimensions [image unit-scale]
+  (if (= unit-scale 1)
+    (:pixel-dimensions image)
+    (:world-unit-dimensions image)))
+
+(defn- with-line-width [shape-drawer width draw-fn]
+  (sd/with-line-width shape-drawer width draw-fn))
 
 (defmulti draw! (fn [[k] _ctx]
                   k))
 
-(defmethod draw! :draw/text [[_ opts] ctx]
-  (draw/text ctx opts))
+(defmethod draw! :draw/image [[_ {:keys [texture-region color] :as image} position]
+                              {:keys [ctx/batch
+                                      ctx/unit-scale]}]
+  (batch/draw-texture-region! batch
+                              texture-region
+                              position
+                              (unit-dimensions image unit-scale)
+                              0 ; rotation
+                              color))
 
-(defmethod draw! :draw/rectangle [[_ & opts] ctx]
-  (apply draw/rectangle ctx opts))
+(defmethod draw! :draw/rotated-centered [[_ {:keys [texture-region color] :as image} rotation [x y]]
+                                         {:keys [ctx/batch
+                                                 ctx/unit-scale]}]
+  (let [[w h] (unit-dimensions image unit-scale)]
+    (batch/draw-texture-region! batch
+                                texture-region
+                                [(- (float x) (/ (float w) 2))
+                                 (- (float y) (/ (float h) 2))]
+                                [w h]
+                                rotation
+                                color)))
 
-(defmethod draw! :draw/filled-rectangle [[_ & opts] ctx]
-  (apply draw/filled-rectangle ctx opts))
+(defmethod draw! :draw/centered [[_ image position] ctx]
+  (draw! [:draw/rotated-centered image 0 position] ctx))
 
-(defmethod draw! :draw/rotated-centered [[_ & opts] ctx]
-  (apply draw/rotated-centered ctx opts))
+(defmethod draw! :draw/text [[_ {:keys [font scale x y text h-align up?]}]
+                             {:keys [ctx/default-font
+                                     ctx/batch
+                                     ctx/unit-scale]}]
+  (graphics/draw-text! (or font default-font)
+                       batch
+                       {:scale (* (float unit-scale)
+                                  (float (or scale 1)))
+                        :x x
+                        :y y
+                        :text text
+                        :h-align h-align
+                        :up? up?}))
 
-(defmethod draw! :draw/centered [[_ & opts] ctx]
-  (apply draw/centered ctx opts))
+(defmethod draw! :draw/ellipse [[_ [x y] radius-x radius-y color]
+                                {:keys [ctx/shape-drawer]}]
+  (sd/set-color! shape-drawer color)
+  (sd/ellipse! shape-drawer x y radius-x radius-y))
 
-(defmethod draw! :draw/image [[_ & opts] ctx]
-  (apply draw/image ctx opts))
+(defmethod draw! :draw/filled-ellipse [[_ [x y] radius-x radius-y color]
+                                       {:keys [ctx/shape-drawer]}]
+  (sd/set-color! shape-drawer color)
+  (sd/filled-ellipse! shape-drawer x y radius-x radius-y))
 
-(defmethod draw! :draw/line [[_ & opts] ctx]
-  (apply draw/line ctx opts))
+(defmethod draw! :draw/circle [[_ [x y] radius color]
+                               {:keys [ctx/shape-drawer]}]
+  (sd/set-color! shape-drawer color)
+  (sd/circle! shape-drawer x y radius))
 
-(defmethod draw! :draw/circle [[_ & opts] ctx]
-  (apply draw/circle ctx opts))
+(defmethod draw! :draw/filled-circle [[_ [x y] radius color]
+                                      {:keys [ctx/shape-drawer]}]
+  (sd/set-color! shape-drawer color)
+  (sd/filled-circle! shape-drawer x y radius))
 
-(defmethod draw! :draw/filled-circle [[_ & opts] ctx]
-  (apply draw/filled-circle ctx opts))
+(defmethod draw! :draw/rectangle [[_ x y w h color]
+                                  {:keys [ctx/shape-drawer]}]
+  (sd/set-color! shape-drawer color)
+  (sd/rectangle! shape-drawer x y w h))
 
-(defmethod draw! :draw/ellipse [[_ & opts] ctx]
-  (apply draw/ellipse ctx opts))
+(defmethod draw! :draw/filled-rectangle [[_ x y w h color]
+                                         {:keys [ctx/shape-drawer]}]
+  (sd/set-color! shape-drawer color)
+  (sd/filled-rectangle! shape-drawer x y w h))
 
-(defmethod draw! :draw/sector [[_ & opts] ctx]
-  (apply draw/sector ctx opts))
+(defmethod draw! :draw/arc [[_ [center-x center-y] radius start-angle degree color]
+                            {:keys [ctx/shape-drawer]}]
+  (sd/set-color! shape-drawer color)
+  (sd/arc! shape-drawer center-x center-y radius start-angle degree))
 
-(defmethod draw! :draw/grid [[_ & opts] ctx]
-  (apply draw/grid ctx opts))
+(defmethod draw! :draw/sector [[_ [center-x center-y] radius start-angle degree color]
+                               {:keys [ctx/shape-drawer]}]
+  (sd/set-color! shape-drawer color)
+  (sd/sector! shape-drawer center-x center-y radius start-angle degree))
 
-(defmethod draw! :draw/with-line-width [[_ width draws] ctx]
-  (draw/with-line-width ctx width
+(defmethod draw! :draw/line [[_ [sx sy] [ex ey] color]
+                             {:keys [ctx/shape-drawer]}]
+  (sd/set-color! shape-drawer color)
+  (sd/line! shape-drawer sx sy ex ey))
+
+(defmethod draw! :draw/grid [[_ leftx bottomy gridw gridh cellw cellh color] ctx]
+  (let [w (* (float gridw) (float cellw))
+        h (* (float gridh) (float cellh))
+        topy (+ (float bottomy) (float h))
+        rightx (+ (float leftx) (float w))]
+    (doseq [idx (range (inc (float gridw)))
+            :let [linex (+ (float leftx) (* (float idx) (float cellw)))]]
+      (draw! [:draw/line [linex topy] [linex bottomy] color] ctx))
+    (doseq [idx (range (inc (float gridh)))
+            :let [liney (+ (float bottomy) (* (float idx) (float cellh)))]]
+      (draw! [:draw/line [leftx liney] [rightx liney] color] ctx))))
+
+(defmethod draw! :draw/with-line-width [[_ width draws]
+                                        {:keys [ctx/shape-drawer]
+                                         :as ctx}]
+  (sd/with-line-width shape-drawer width
     (fn []
       (g/handle-draws! ctx draws))))
+
+; doc for :draw/text:
+;  "font, h-align, up? and scale are optional.
+;  h-align one of: :center, :left, :right. Default :center.
+;  up? renders the font over y, otherwise under.
+;  scale will multiply the drawn text size with the scale."
 
 (extend-type cdq.g.Game
   g/Graphics
@@ -56,12 +133,13 @@
 
   (draw-on-world-viewport! [{:keys [ctx/batch
                                     ctx/world-viewport
-                                    ctx/world-unit-scale]
+                                    ctx/world-unit-scale
+                                    ctx/shape-drawer]
                              :as ctx} fns]
     (batch/draw-on-viewport! batch
                              world-viewport
                              (fn []
-                               (draw/with-line-width ctx world-unit-scale
+                               (sd/with-line-width shape-drawer world-unit-scale
                                  (fn []
                                    (doseq [f fns]
                                      (f (assoc ctx :ctx/unit-scale world-unit-scale)))))))))
