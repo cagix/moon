57d6598c35a1bddaf73281b4b9a0a1f656946ee7
Michael Sappler <michaelsappler@gmail.com>
2025-05-26 11:50:40 +0530

    create application context in cdq

--- src/cdq/application_state.clj
+++ src/cdq/application_state.clj
@@ -7,12 +7,75 @@
             [cdq.create.tiled-map-renderer]
             [cdq.create.ui-viewport]
             [cdq.create.world-viewport]
+            [clojure.gdx.interop :as interop]
+            [clojure.space.earlygrey.shape-drawer :as sd]
             [gdl.application]
-            [gdl.ui :as ui]))
+            [gdl.c :as c]
+            [gdl.graphics :as graphics]
+            [gdl.ui :as ui]
+            [gdl.utils :as utils]))
+
+; next step we have to somehow merge together used fields together as far as possible ?!
+; grep ctx/ ...
+
+(extend-type gdl.application.Context
+  c/Graphics
+  (draw-on-world-viewport! [{:keys [ctx/batch
+                                    ctx/world-viewport
+                                    ctx/shape-drawer
+                                    ctx/world-unit-scale]
+                             :as ctx}
+                            fns]
+    (graphics/draw-on-viewport! batch
+                                world-viewport
+                                (fn []
+                                  (sd/with-line-width shape-drawer world-unit-scale
+                                    (fn []
+                                      (doseq [f fns]
+                                        (f (assoc ctx :ctx/unit-scale world-unit-scale))))))))
+
+  (pixels->world-units [{:keys [ctx/world-unit-scale]} pixels]
+    (* pixels world-unit-scale))
+
+  (sprite [{:keys [ctx/world-unit-scale] :as ctx} texture-path]
+    (graphics/sprite (graphics/texture-region (c/texture ctx texture-path))
+                     world-unit-scale))
+
+  (sub-sprite [{:keys [ctx/world-unit-scale]} sprite [x y w h]]
+    (graphics/sprite (graphics/sub-region (:texture-region sprite) x y w h)
+                     world-unit-scale))
+
+  (sprite-sheet [{:keys [ctx/world-unit-scale] :as ctx} texture-path tilew tileh]
+    {:image (graphics/sprite (graphics/texture-region (c/texture ctx texture-path))
+                             world-unit-scale)
+     :tilew tilew
+     :tileh tileh})
+
+  (sprite-sheet->sprite [ctx {:keys [image tilew tileh]} [x y]]
+    (c/sub-sprite ctx image [(* x tilew) (* y tileh) tilew tileh])))
+
+(defn- create-state! [config]
+  (let [batch (graphics/sprite-batch)
+        shape-drawer-texture (graphics/white-pixel-texture)
+        world-unit-scale (float (/ (:tile-size config)))]
+    (gdl.application/map->Context
+     {:batch batch
+      :unit-scale 1
+      :world-unit-scale world-unit-scale
+      :shape-drawer-texture shape-drawer-texture
+      :shape-drawer (sd/create (:java-object batch)
+                               (graphics/texture-region shape-drawer-texture 1 0 1 1))
+      :cursors (utils/mapvals
+                (fn [[file [hotspot-x hotspot-y]]]
+                  (graphics/create-cursor (format (:cursor-path-format config) file)
+                                          hotspot-x
+                                          hotspot-y))
+                (:cursors config))
+      :default-font (graphics/truetype-font (:default-font config))})))
 
 (defn create! [config]
   (ui/load! (:ui config))
-  (-> (gdl.application/create-state! config)
+  (-> (create-state! config)
       (cdq.create.gdx/add-gdx!)
       (cdq.create.ui-viewport/add config)
       (cdq.create.stage/add-stage!)
--- src/gdl/application.clj
+++ src/gdl/application.clj
@@ -1,10 +1,5 @@
 (ns gdl.application
-  (:require [clojure.gdx.interop :as interop]
-            [clojure.space.earlygrey.shape-drawer :as sd]
-            [gdl.c :as c]
-            [gdl.graphics :as graphics]
-            [gdl.utils :as utils]
-            [qrecord.core :as q])
+  (:require [qrecord.core :as q])
   (:import (com.badlogic.gdx Gdx)))
 
 (defmacro post-runnable! [& exprs]
@@ -21,51 +16,4 @@
                       ctx/world-viewport
                       ctx/ui-viewport
                       ctx/tiled-map-renderer
-                      ctx/stage]
-  c/Graphics
-  (draw-on-world-viewport! [ctx fns]
-    (graphics/draw-on-viewport! batch
-                                world-viewport
-                                (fn []
-                                  (sd/with-line-width shape-drawer world-unit-scale
-                                    (fn []
-                                      (doseq [f fns]
-                                        (f (assoc ctx :ctx/unit-scale world-unit-scale))))))))
-
-  (pixels->world-units [_ pixels]
-    (* pixels world-unit-scale))
-
-  (sprite [this texture-path]
-    (graphics/sprite (graphics/texture-region (c/texture this texture-path))
-                     world-unit-scale))
-
-  (sub-sprite [_ sprite [x y w h]]
-    (graphics/sprite (graphics/sub-region (:texture-region sprite) x y w h)
-                     world-unit-scale))
-
-  (sprite-sheet [this texture-path tilew tileh]
-    {:image (graphics/sprite (graphics/texture-region (c/texture this texture-path))
-                             world-unit-scale)
-     :tilew tilew
-     :tileh tileh})
-
-  (sprite-sheet->sprite [this {:keys [image tilew tileh]} [x y]]
-    (c/sub-sprite this image [(* x tilew) (* y tileh) tilew tileh])))
-
-(defn create-state! [config]
-  (let [batch (graphics/sprite-batch)
-        shape-drawer-texture (graphics/white-pixel-texture)
-        world-unit-scale (float (/ (:tile-size config)))]
-    (map->Context {:batch batch
-                   :unit-scale 1
-                   :world-unit-scale world-unit-scale
-                   :shape-drawer-texture shape-drawer-texture
-                   :shape-drawer (sd/create (:java-object batch)
-                                            (graphics/texture-region shape-drawer-texture 1 0 1 1))
-                   :cursors (utils/mapvals
-                             (fn [[file [hotspot-x hotspot-y]]]
-                               (graphics/create-cursor (format (:cursor-path-format config) file)
-                                                       hotspot-x
-                                                       hotspot-y))
-                             (:cursors config))
-                   :default-font (graphics/truetype-font (:default-font config))})))
+                      ctx/stage])
