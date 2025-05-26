91983351b15d2f2df36ac326f70a7423064a2bca
Michael Sappler <michaelsappler@gmail.com>
2025-05-26 09:36:52 +0530

    Only define :ctx/db once in cdq.create.db

--- src/cdq/create/db.clj
+++ src/cdq/create/db.clj
@@ -2,29 +2,31 @@
   (:require [cdq.db :as db]
             [cdq.g :as g]))
 
+(def ^:private -k :ctx/db)
+
 (defn add-db [ctx config]
-  {:pre [(not (contains? ctx :ctx/db))]}
-  (assoc ctx :ctx/db (db/create (:db config))))
+  {:pre [(not (contains? ctx -k))]}
+  (assoc ctx -k (db/create (:db config))))
 
 (extend-type gdl.application.Context
   g/Database
-  (get-raw [{:keys [ctx/db]} property-id]
-    (db/get-raw db property-id))
+  (get-raw [ctx property-id]
+    (db/get-raw (-k ctx) property-id))
 
-  (build [{:keys [ctx/db] :as ctx} property-id]
-    (db/build db property-id ctx))
+  (build [ctx property-id]
+    (db/build (-k ctx) property-id ctx))
 
-  (build-all [{:keys [ctx/db] :as ctx} property-type]
-    (db/build-all db property-type ctx))
+  (build-all [ctx property-type]
+    (db/build-all (-k ctx) property-type ctx))
 
-  (property-types [{:keys [ctx/db]}]
-    (db/property-types db))
+  (property-types [ctx]
+    (db/property-types (-k ctx)))
 
-  (schemas [{:keys [ctx/db]}]
-    (:schemas db))
+  (schemas [ctx]
+    (:schemas (-k ctx)))
 
   (update-property! [ctx property]
-    (update ctx :ctx/db db/update! property))
+    (update ctx -k db/update! property))
 
   (delete-property! [ctx property-id]
-    (update ctx :ctx/db db/delete! property-id)))
+    (update ctx -k db/delete! property-id)))
