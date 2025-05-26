00c74e9713dff426c7a5ee278e23e2ff3e4c940a
Michael Sappler <michaelsappler@gmail.com>
2025-05-26 10:15:56 +0530

    cdq.create.config

--- src/cdq/application_state.clj
+++ src/cdq/application_state.clj
@@ -1,23 +1,14 @@
 (ns cdq.application-state
   (:require [cdq.create.assets]
+            [cdq.create.config]
             [cdq.create.db]
-            [cdq.g :as g]
             [gdl.application]
             [gdl.ui :as ui]))
-
-(defn add-component [ctx k v]
-  {:pre [(not (contains? ctx k))]}
-  (assoc ctx k v))
-
-(extend-type gdl.application.Context
-  g/Config
-  (config [{:keys [ctx/config]} key]
-    (get config key)))
 
 (defn create! [config]
   (ui/load! (:ui config))
   (-> (gdl.application/create-state! config)
       (cdq.create.assets/add-assets config)
-      (add-component :ctx/config config)
+      (cdq.create.config/add-config config)
       (cdq.create.db/add-db config)
       ((requiring-resolve 'cdq.game-state/create!))))
---
+++ src/cdq/create/config.clj
@@ -0,0 +1,13 @@
+(ns cdq.create.config
+  (:require [cdq.g :as g]))
+
+(def ^:private -k :ctx/config)
+
+(defn add-config [ctx config]
+  {:pre [(nil? (-k ctx))]}
+  (assoc ctx -k config))
+
+(extend-type gdl.application.Context
+  g/Config
+  (config [ctx key]
+    (get (-k ctx) key)))
