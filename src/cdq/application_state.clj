aa8b6b8286f86ae6b76b3ba43f170f71e3b12feb
Michael Sappler <michaelsappler@gmail.com>
2025-05-28 11:39:14 +0530

    Remove cdq.create.gdx

--- src/cdq/application_state.clj
+++ src/cdq/application_state.clj
@@ -1,6 +1,5 @@
 (ns cdq.application-state
   (:require [cdq.db :as db]
-            [cdq.create.gdx]
             [cdq.create.stage]
             [cdq.create.ui-viewport]
             [cdq.g :as g]
@@ -8,14 +7,63 @@
             [cdq.game-state :as game-state]
             [gdl.application]
             [gdl.assets :as assets]
-            [gdl.ui :as ui]))
+            [gdl.ui :as ui])
+  (:import (com.badlogic.gdx Gdx
+                             Input$Keys
+                             Input$Buttons)
+           (com.badlogic.gdx.graphics Color)
+           (com.badlogic.gdx.utils ScreenUtils)))
+
+(defn- button->code [button]
+  (case button
+    :left Input$Buttons/LEFT
+    ))
+
+(defn- k->code [key]
+  (case key
+    :minus  Input$Keys/MINUS
+    :equals Input$Keys/EQUALS
+    :space  Input$Keys/SPACE
+    :p      Input$Keys/P
+    :enter  Input$Keys/ENTER
+    :escape Input$Keys/ESCAPE
+    :i      Input$Keys/I
+    :e      Input$Keys/E
+    :d      Input$Keys/D
+    :a      Input$Keys/A
+    :w      Input$Keys/W
+    :s      Input$Keys/S
+    ))
+
+(defn add-gdx! [ctx]
+  (extend (class ctx)
+    g/Input
+    {:button-just-pressed?
+     (fn [_ button]
+       (.isButtonJustPressed Gdx/input (button->code button)))
+     :key-pressed?
+     (fn [_ key]
+       (.isKeyPressed Gdx/input (k->code key)))
+     :key-just-pressed?
+     (fn [_ key]
+       (.isKeyJustPressed Gdx/input (k->code key)))}
+    g/BaseGraphics
+    {:delta-time (fn [_]
+                   (.getDeltaTime Gdx/graphics))
+     :set-cursor! (fn [_ cursor]
+                    (.setCursor Gdx/graphics cursor))
+     :frames-per-second (fn [_]
+                          (.getFramesPerSecond Gdx/graphics))
+     :clear-screen! (fn [_]
+                      (ScreenUtils/clear Color/BLACK))})
+  ctx)
 
 (defn create! [config]
   (ui/load! (:ui config))
   (-> (gdl.application/map->Context {})
       (assoc :ctx/config config)
       (assoc :ctx/graphics (graphics/create config))
-      (cdq.create.gdx/add-gdx!)
+      (add-gdx!)
       (cdq.create.ui-viewport/add config)
       (cdq.create.stage/add-stage!)
       (assoc :ctx/assets (assets/create (:assets config)))
