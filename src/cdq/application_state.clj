dec54e27cf0a16e590436bc039f4a238dbda24f8
Michael Sappler <michaelsappler@gmail.com>
2025-05-28 12:42:39 +0530

    Remove cdq.create.stage

--- src/cdq/application_state.clj
+++ src/cdq/application_state.clj
@@ -1,9 +1,16 @@
 (ns cdq.application-state
   (:require [cdq.db :as db]
-            [cdq.create.stage]
             [cdq.g :as g]
             [cdq.graphics :as graphics]
             [cdq.game-state :as game-state]
+            [cdq.ui.dev-menu]
+            [cdq.ui.action-bar]
+            [cdq.ui.hp-mana-bar]
+            [cdq.ui.windows]
+            [cdq.ui.entity-info]
+            [cdq.ui.inventory]
+            [cdq.ui.player-state-draw]
+            [cdq.ui.message]
             [gdl.application]
             [gdl.assets :as assets]
             [gdl.input :as input]
@@ -45,6 +52,44 @@
     (key-just-pressed? [_ key]
       (.isKeyJustPressed input (k->code key)))))
 
+(defn- add-stage! [ctx]
+  (let [stage (ui/stage (:java-object (:ctx/ui-viewport ctx))
+                        (:batch (:ctx/graphics ctx)))]
+    (.setInputProcessor Gdx/input stage)
+    (assoc ctx :ctx/stage stage)))
+
+(defn- create-actors [{:keys [ctx/ui-viewport]
+                       :as ctx}]
+  [(cdq.ui.dev-menu/create ctx)
+   (cdq.ui.action-bar/create :id :action-bar)
+   (cdq.ui.hp-mana-bar/create [(/ (:width ui-viewport) 2)
+                               80 ; action-bar-icon-size
+                               ]
+                              ctx)
+   (cdq.ui.windows/create :id :windows
+                          :actors [(cdq.ui.entity-info/create [(:width ui-viewport) 0])
+                                   (cdq.ui.inventory/create ctx
+                                                            :id :inventory-window
+                                                            :position [(:width ui-viewport)
+                                                                       (:height ui-viewport)])])
+   (cdq.ui.player-state-draw/create)
+   (cdq.ui.message/create :name "player-message")])
+
+(extend-type gdl.application.Context
+  g/Stage
+  (find-actor-by-name [{:keys [ctx/stage]} name]
+    (-> stage
+        ui/root
+        (ui/find-actor name))) ; <- find-actor protocol & for stage use ui/root
+
+  (mouseover-actor [{:keys [ctx/ui-viewport
+                            ctx/stage]}]
+    (ui/hit stage (viewport/mouse-position ui-viewport)))
+
+  (reset-actors! [{:keys [ctx/stage] :as ctx}]
+    (ui/clear! stage)
+    (run! #(ui/add! stage %) (create-actors ctx))))
+
 (defn create! [config]
   (ui/load! (:ui config))
   (-> (gdl.application/map->Context {})
@@ -52,7 +97,7 @@
       (assoc :ctx/graphics (graphics/create Gdx/graphics config)) ; <- actually create only called here <- all libgdx create stuff here and assets/input/graphics/stage/viewport as protocols in gdl ? -> all gdx code creating together and upfactored protocols?
       (assoc :ctx/input (make-input Gdx/input))
       (assoc :ctx/ui-viewport (viewport/ui-viewport (:ui-viewport config))) ; <- even viewport construction is in here .... viewport itself a protocol  ....
-      (cdq.create.stage/add-stage!)
+      (add-stage!)
       (assoc :ctx/assets (assets/create (:assets config)))
       (assoc :ctx/db (db/create (:db config)))
       (game-state/create! (:world-fn config))))
