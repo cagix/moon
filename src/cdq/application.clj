cb5ec3f5639a56fe33365a3854354632a840701a
Michael Sappler <michaelsappler@gmail.com>
2025-05-28 13:57:58 +0530

    Remove clojure.gdx.input

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -1,29 +1,30 @@
 (ns cdq.application
   (:require [clojure.gdx.backends.lwjgl :as lwjgl]
-            [clojure.gdx.input :as input]
-            gdl.input)
+            [clojure.gdx.interop :as interop]
+            [gdl.input :as input])
   (:import (com.badlogic.gdx ApplicationAdapter
                              Gdx)))
 
-(defn- make-input [input]
-  (reify gdl.input/Input
-    (button-just-pressed? [_ button]
-      (clojure.gdx.input/button-just-pressed? input button))
+(defn- make-input []
+  (let [input Gdx/input]
+    (reify input/Input
+      (button-just-pressed? [_ button]
+        (.isButtonJustPressed input (interop/k->input-button button)))
 
-    (key-pressed? [_ key]
-      (clojure.gdx.input/key-pressed? input key))
+      (key-pressed? [_ key]
+        (.isKeyPressed input (interop/k->input-key key)))
 
-    (key-just-pressed? [_ key]
-      (clojure.gdx.input/key-just-pressed? input key))
+      (key-just-pressed? [_ key]
+        (.isKeyJustPressed input (interop/k->input-key key)))
 
-    (set-processor! [_ input-processor]
-      (.setInputProcessor input input-processor))))
+      (set-processor! [_ input-processor]
+        (.setInputProcessor input input-processor)))))
 
 (defn- get-context []
   {;:clojure.gdx/app      Gdx/app
    :clojure.gdx/files    Gdx/files
    :clojure.gdx/graphics Gdx/graphics
-   :clojure.gdx/input    (make-input Gdx/input)})
+   :clojure.gdx/input    (make-input)})
 
 (def state (atom nil))
 
