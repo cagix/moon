1fad627e07c769ba296de4a0f6f0d9205f026891
Michael Sappler <michaelsappler@gmail.com>
2025-05-29 14:04:49 +0530

    Create assets in cdq.create.assets

--- src/cdq/create/assets.clj
+++ src/cdq/create/assets.clj
@@ -1,7 +1,49 @@
 (ns cdq.create.assets
-  (:require [gdl.assets :as assets]))
+  (:require [clojure.gdx.assets.asset-manager :as asset-manager]
+            [clojure.string :as str]
+            [gdl.assets]
+            [gdl.files :as files])
+  (:import (com.badlogic.gdx.audio Sound)
+           (com.badlogic.gdx.files FileHandle)
+           (com.badlogic.gdx.graphics Texture)))
+
+(defn- recursively-search [^FileHandle folder extensions]
+  (loop [[^FileHandle file & remaining] (.list folder)
+         result []]
+    (cond (nil? file)
+          result
+
+          (.isDirectory file)
+          (recur (concat remaining (.list file)) result)
+
+          (extensions (.extension file))
+          (recur remaining (conj result (.path file)))
+
+          :else
+          (recur remaining result))))
 
 (defn do! [{:keys [ctx/files
                    ctx/config]
             :as ctx}]
-  (assoc ctx :ctx/assets (assets/create files (:assets config))))
+  (assoc ctx :ctx/assets
+         (let [{:keys [folder
+                       asset-type-extensions]} (:assets config)
+               manager (asset-manager/create
+                        (for [[asset-type extensions] asset-type-extensions
+                              file (map #(str/replace-first % folder "")
+                                        (recursively-search (files/internal files folder) extensions))]
+                          [file (case asset-type
+                                  :sound Sound
+                                  :texture Texture)]))]
+           (reify gdl.assets/Assets
+             (sound [assets path]
+               (asset-manager/safe-get manager path))
+
+             (texture [assets path]
+               (asset-manager/safe-get manager path))
+
+             (all-sounds [assets]
+               (asset-manager/all-of-type manager Sound))
+
+             (all-textures [assets]
+               (asset-manager/all-of-type manager Texture))))))
--- src/gdl/assets.clj
+++ src/gdl/assets.clj
@@ -1,49 +1,6 @@
-(ns gdl.assets
-  (:require [clojure.gdx.assets.asset-manager :as asset-manager]
-            [clojure.string :as str]
-            [gdl.files :as files])
-  (:import (com.badlogic.gdx.audio Sound)
-           (com.badlogic.gdx.files FileHandle)
-           (com.badlogic.gdx.graphics Texture)))
+(ns gdl.assets)
 
-(defn- recursively-search [^FileHandle folder extensions]
-  (loop [[^FileHandle file & remaining] (.list folder)
-         result []]
-    (cond (nil? file)
-          result
-
-          (.isDirectory file)
-          (recur (concat remaining (.list file)) result)
-
-          (extensions (.extension file))
-          (recur remaining (conj result (.path file)))
-
-          :else
-          (recur remaining result))))
-
-(defn create [files {:keys [folder
-                            asset-type-extensions]}]
-  (asset-manager/create
-   (for [[asset-type extensions] asset-type-extensions
-         file (map #(str/replace-first % folder "")
-                   (recursively-search (files/internal files folder) extensions))]
-     [file (case asset-type
-             :sound Sound
-             :texture Texture)])))
-
-(defn sound [assets path]
-  (asset-manager/safe-get assets path))
-
-(defn texture [assets path]
-  (asset-manager/safe-get assets path))
-
-(defn all-sounds [assets]
-  (asset-manager/all-of-type assets Sound))
-
-(defn all-textures [assets]
-  (asset-manager/all-of-type assets Texture))
-
-#_(defprotocol Assets
+(defprotocol Assets
   (sound [_ path])
   (texture [_ path])
   (all-sounds [_])
