7330434788e463fe2955af0b29e7afcba86b4945
Michael Sappler <michaelsappler@gmail.com>
2025-05-31 14:32:11 +0530

    cdq.create.game-state

---
+++ src/cdq/create/game_state.clj
@@ -0,0 +1,201 @@
+(ns cdq.create.game-state
+  (:require [cdq.cell :as cell]
+            [cdq.content-grid :as content-grid]
+            [cdq.entity :as entity]
+            [cdq.game]
+            [cdq.g :as g]
+            [cdq.g.player-movement-vector]
+            [cdq.g.interaction-state]
+            [cdq.g.spawn-entity]
+            [cdq.g.spawn-creature]
+            [cdq.grid :as grid]
+            [cdq.grid-impl :as grid-impl]
+            [cdq.grid2d :as g2d]
+            [cdq.potential-fields.movement :as potential-fields.movement]
+            [cdq.raycaster :as raycaster]
+            [cdq.state :as state]
+            [cdq.vector2 :as v]
+            [gdl.tiled :as tiled]
+            [gdl.utils :as utils])
+  (:import (cdq.game Context)))
+
+(defn- player-entity-props [start-position {:keys [creature-id
+                                                   free-skill-points
+                                                   click-distance-tiles]}]
+  {:position start-position
+   :creature-id creature-id
+   :components {:entity/fsm {:fsm :fsms/player
+                             :initial-state :player-idle}
+                :entity/faction :good
+                :entity/player? {:state-changed! (fn [new-state-obj]
+                                                   (when-let [cursor (state/cursor new-state-obj)]
+                                                     [[:tx/set-cursor cursor]]))
+                                 :skill-added! g/add-skill!
+                                 :skill-removed! g/remove-skill!
+                                 :item-set! g/set-item!
+                                 :item-removed! g/remove-item!}
+                :entity/free-skill-points free-skill-points
+                :entity/clickable {:type :clickable/player}
+                :entity/click-distance-tiles click-distance-tiles}})
+
+(defn- spawn-player-entity [ctx start-position player-props]
+  (g/spawn-creature! ctx
+                     (player-entity-props (utils/tile->middle start-position)
+                                          player-props)))
+
+(defn- spawn-enemies [tiled-map]
+  (for [props (for [[position creature-id] (tiled/positions-with-property tiled-map :creatures :id)]
+                {:position position
+                 :creature-id (keyword creature-id)
+                 :components {:entity/fsm {:fsm :fsms/npc
+                                           :initial-state :npc-sleeping}
+                              :entity/faction :evil}})]
+    [:tx/spawn-creature (update props :position utils/tile->middle)]))
+
+(defn- create-game-state [{:keys [ctx/config] :as ctx} world-fn]
+  (g/reset-actors! ctx)
+  (let [{:keys [tiled-map
+                start-position]} (world-fn ctx)
+        grid (grid-impl/create tiled-map)
+        z-orders [:z-order/on-ground
+                  :z-order/ground
+                  :z-order/flying
+                  :z-order/effect]
+        ctx (merge ctx
+                   {:ctx/tiled-map tiled-map
+                    :ctx/elapsed-time 0
+                    :ctx/grid grid
+                    :ctx/raycaster (raycaster/create grid)
+                    :ctx/content-grid (content-grid/create tiled-map (:content-grid-cell-size config))
+                    :ctx/explored-tile-corners (atom (g2d/create-grid (tiled/tm-width  tiled-map)
+                                                                      (tiled/tm-height tiled-map)
+                                                                      (constantly false)))
+                    :ctx/id-counter (atom 0)
+                    :ctx/entity-ids (atom {})
+                    :ctx/potential-field-cache (atom nil)
+                    :ctx/factions-iterations (:potential-field-factions-iterations config)
+                    :ctx/z-orders z-orders
+                    :ctx/render-z-order (utils/define-order z-orders)})
+        ctx (assoc ctx :ctx/player-eid (spawn-player-entity ctx
+                                                            start-position
+                                                            (:player-props config)))]
+    (g/handle-txs! ctx (spawn-enemies tiled-map))
+    ctx))
+
+(defn do! [{:keys [ctx/config] :as ctx}]
+  (create-game-state ctx (:world-fn config)))
+
+(extend-type Context
+  g/Game
+  (reset-game-state! [ctx world-fn]
+    (create-game-state ctx world-fn)))
+
+(extend-type Context
+  g/Context
+  (context-entity-add! [{:keys [ctx/entity-ids
+                                ctx/content-grid
+                                ctx/grid]}
+                        eid]
+    (let [id (entity/id @eid)]
+      (assert (number? id))
+      (swap! entity-ids assoc id eid))
+    (content-grid/add-entity! content-grid eid)
+    ; https://github.com/damn/core/issues/58
+    ;(assert (valid-position? grid @eid)) ; TODO deactivate because projectile no left-bottom remove that field or update properly for all
+    (grid/add-entity! grid eid))
+
+  (context-entity-remove! [{:keys [ctx/entity-ids
+                                   ctx/grid]}
+                           eid]
+    (let [id (entity/id @eid)]
+      (assert (contains? @entity-ids id))
+      (swap! entity-ids dissoc id))
+    (content-grid/remove-entity! eid)
+    (grid/remove-entity! grid eid))
+
+  (context-entity-moved! [{:keys [ctx/content-grid
+                                  ctx/grid]}
+                          eid]
+    (content-grid/position-changed! content-grid eid)
+    (grid/position-changed! grid eid)))
+
+(extend-type Context
+  cdq.g/Grid
+  (nearest-enemy-distance [{:keys [ctx/grid]} entity]
+    (cell/nearest-entity-distance @(grid/cell grid (mapv int (entity/position entity)))
+                                  (entity/enemy entity)))
+
+  (nearest-enemy [{:keys [ctx/grid]} entity]
+    (cell/nearest-entity @(grid/cell grid (mapv int (entity/position entity)))
+                         (entity/enemy entity)))
+
+  (potential-field-find-direction [{:keys [ctx/grid]} eid]
+    (potential-fields.movement/find-direction grid eid)))
+
+(extend-type Context
+  g/EffectContext
+  (player-effect-ctx [{:keys [ctx/mouseover-eid]
+                       :as ctx}
+                      eid]
+    (let [target-position (or (and mouseover-eid
+                                   (entity/position @mouseover-eid))
+                              (g/world-mouse-position ctx))]
+      {:effect/source eid
+       :effect/target mouseover-eid
+       :effect/target-position target-position
+       :effect/target-direction (v/direction (entity/position @eid) target-position)}))
+
+  (npc-effect-ctx [ctx eid]
+    (let [entity @eid
+          target (g/nearest-enemy ctx entity)
+          target (when (and target
+                            (g/line-of-sight? ctx entity @target))
+                   target)]
+      {:effect/source eid
+       :effect/target target
+       :effect/target-direction (when target
+                                  (v/direction (entity/position entity)
+                                               (entity/position @target)))})))
+
+; does not take into account zoom - but zoom is only for debug ???
+; vision range?
+(defn- on-screen? [ctx position]
+  (let [[x y] position
+        x (float x)
+        y (float y)
+        [cx cy] (g/camera-position ctx)
+        px (float cx)
+        py (float cy)
+        xdist (Math/abs (- x px))
+        ydist (Math/abs (- y py))]
+    (and
+     (<= xdist (inc (/ (float (g/world-viewport-width  ctx))  2)))
+     (<= ydist (inc (/ (float (g/world-viewport-height ctx)) 2))))))
+
+; TODO at wrong point , this affects targeting logic of npcs
+; move the debug flag to either render or mouseover or lets see
+(def ^:private ^:dbg-flag los-checks? true)
+
+(extend-type Context
+  g/LineOfSight
+  ; does not take into account size of entity ...
+  ; => assert bodies <1 width then
+  (line-of-sight? [{:keys [ctx/raycaster] :as ctx}
+                   source
+                   target]
+    (and (or (not (:entity/player? source))
+             (on-screen? ctx (entity/position target)))
+         (not (and los-checks?
+                   (raycaster/blocked? raycaster
+                                       (entity/position source)
+                                       (entity/position target)))))))
+
+(extend-type Context
+  g/SpawnEntity
+  (spawn-entity! [ctx position body components]
+    (cdq.g.spawn-entity/spawn-entity! ctx position body components)))
+
+(extend-type Context
+  g/Creatures
+  (spawn-creature! [ctx opts]
+    (cdq.g.spawn-creature/spawn-creature! ctx opts)))
--- src/cdq/game.clj
+++ src/cdq/game.clj
@@ -1,28 +1,15 @@
 (ns cdq.game
   (:require [cdq.resizable]
-            [cdq.cell :as cell]
-            [cdq.content-grid :as content-grid]
-            [cdq.entity :as entity]
             [cdq.g :as g]
             [cdq.g.info]
             [cdq.g.player-movement-vector]
             [cdq.g.interaction-state]
-            [cdq.g.spawn-entity]
-            [cdq.g.spawn-creature]
             [cdq.g.handle-txs]
-            [cdq.grid :as grid]
-            [cdq.grid-impl :as grid-impl]
-            [cdq.grid2d :as g2d]
-            [cdq.potential-fields.movement :as potential-fields.movement]
-            [cdq.raycaster :as raycaster]
-            [cdq.state :as state]
             [cdq.ui.editor]
             [cdq.ui.editor.overview-table]
-            [cdq.vector2 :as v]
             [cdq.malli :as m]
             [clojure.gdx.backends.lwjgl :as lwjgl]
             [gdl.application :as application]
-            [gdl.tiled :as tiled]
             [gdl.utils :as utils]
             [qrecord.core :as q])
   (:import (com.badlogic.gdx ApplicationAdapter)
@@ -81,76 +68,7 @@
              [:ctx/player-eid :some]
              [:ctx/active-entities {:optional true} :some]]))
 
-(defn- player-entity-props [start-position {:keys [creature-id
-                                                   free-skill-points
-                                                   click-distance-tiles]}]
-  {:position start-position
-   :creature-id creature-id
-   :components {:entity/fsm {:fsm :fsms/player
-                             :initial-state :player-idle}
-                :entity/faction :good
-                :entity/player? {:state-changed! (fn [new-state-obj]
-                                                   (when-let [cursor (state/cursor new-state-obj)]
-                                                     [[:tx/set-cursor cursor]]))
-                                 :skill-added! g/add-skill!
-                                 :skill-removed! g/remove-skill!
-                                 :item-set! g/set-item!
-                                 :item-removed! g/remove-item!}
-                :entity/free-skill-points free-skill-points
-                :entity/clickable {:type :clickable/player}
-                :entity/click-distance-tiles click-distance-tiles}})
-
-(defn- spawn-player-entity [ctx start-position player-props]
-  (g/spawn-creature! ctx
-                     (player-entity-props (utils/tile->middle start-position)
-                                          player-props)))
-
-(defn- spawn-enemies [tiled-map]
-  (for [props (for [[position creature-id] (tiled/positions-with-property tiled-map :creatures :id)]
-                {:position position
-                 :creature-id (keyword creature-id)
-                 :components {:entity/fsm {:fsm :fsms/npc
-                                           :initial-state :npc-sleeping}
-                              :entity/faction :evil}})]
-    [:tx/spawn-creature (update props :position utils/tile->middle)]))
-
-(defn- create-game-state [{:keys [ctx/config] :as ctx} world-fn]
-  (g/reset-actors! ctx)
-  (let [{:keys [tiled-map
-                start-position]} (world-fn ctx)
-        grid (grid-impl/create tiled-map)
-        z-orders [:z-order/on-ground
-                  :z-order/ground
-                  :z-order/flying
-                  :z-order/effect]
-        ctx (merge ctx
-                   {:ctx/tiled-map tiled-map
-                    :ctx/elapsed-time 0
-                    :ctx/grid grid
-                    :ctx/raycaster (raycaster/create grid)
-                    :ctx/content-grid (content-grid/create tiled-map (:content-grid-cell-size config))
-                    :ctx/explored-tile-corners (atom (g2d/create-grid (tiled/tm-width  tiled-map)
-                                                                      (tiled/tm-height tiled-map)
-                                                                      (constantly false)))
-                    :ctx/id-counter (atom 0)
-                    :ctx/entity-ids (atom {})
-                    :ctx/potential-field-cache (atom nil)
-                    :ctx/factions-iterations (:potential-field-factions-iterations config)
-                    :ctx/z-orders z-orders
-                    :ctx/render-z-order (utils/define-order z-orders)})
-        ctx (assoc ctx :ctx/player-eid (spawn-player-entity ctx
-                                                            start-position
-                                                            (:player-props config)))]
-    (g/handle-txs! ctx (spawn-enemies tiled-map))
-    ctx))
-
-(extend-type Context
-  g/Game
-  (reset-game-state! [ctx world-fn]
-    (create-game-state ctx world-fn)))
-
 ; We _need to create stepwise because otherwise the file becomes too big ... _
-
 (defn- create! [config]
   (let [ctx (map->Context {:config config})
         ctx (reduce (fn [ctx f]
@@ -162,8 +80,7 @@
                            cdq.create.db/do!
                            cdq.create.graphics/do!
                            cdq.create.stage/do!
-                           ]))
-        ctx (create-game-state ctx (:world-fn config))]
+                           cdq.create.game-state/do!]))]
     (m/validate-humanize schema ctx)
     ctx))
 
@@ -233,105 +150,6 @@
                            (resize! @application/state width height))))))
 
 (extend-type Context
-  g/Context
-  (context-entity-add! [{:keys [ctx/entity-ids
-                                ctx/content-grid
-                                ctx/grid]}
-                        eid]
-    (let [id (entity/id @eid)]
-      (assert (number? id))
-      (swap! entity-ids assoc id eid))
-    (content-grid/add-entity! content-grid eid)
-    ; https://github.com/damn/core/issues/58
-    ;(assert (valid-position? grid @eid)) ; TODO deactivate because projectile no left-bottom remove that field or update properly for all
-    (grid/add-entity! grid eid))
-
-  (context-entity-remove! [{:keys [ctx/entity-ids
-                                   ctx/grid]}
-                           eid]
-    (let [id (entity/id @eid)]
-      (assert (contains? @entity-ids id))
-      (swap! entity-ids dissoc id))
-    (content-grid/remove-entity! eid)
-    (grid/remove-entity! grid eid))
-
-  (context-entity-moved! [{:keys [ctx/content-grid
-                                  ctx/grid]}
-                          eid]
-    (content-grid/position-changed! content-grid eid)
-    (grid/position-changed! grid eid)))
-
-(extend-type Context
-  cdq.g/Grid
-  (nearest-enemy-distance [{:keys [ctx/grid]} entity]
-    (cell/nearest-entity-distance @(grid/cell grid (mapv int (entity/position entity)))
-                                  (entity/enemy entity)))
-
-  (nearest-enemy [{:keys [ctx/grid]} entity]
-    (cell/nearest-entity @(grid/cell grid (mapv int (entity/position entity)))
-                         (entity/enemy entity)))
-
-  (potential-field-find-direction [{:keys [ctx/grid]} eid]
-    (potential-fields.movement/find-direction grid eid)))
-
-(extend-type Context
-  g/EffectContext
-  (player-effect-ctx [{:keys [ctx/mouseover-eid]
-                       :as ctx}
-                      eid]
-    (let [target-position (or (and mouseover-eid
-                                   (entity/position @mouseover-eid))
-                              (g/world-mouse-position ctx))]
-      {:effect/source eid
-       :effect/target mouseover-eid
-       :effect/target-position target-position
-       :effect/target-direction (v/direction (entity/position @eid) target-position)}))
-
-  (npc-effect-ctx [ctx eid]
-    (let [entity @eid
-          target (g/nearest-enemy ctx entity)
-          target (when (and target
-                            (g/line-of-sight? ctx entity @target))
-                   target)]
-      {:effect/source eid
-       :effect/target target
-       :effect/target-direction (when target
-                                  (v/direction (entity/position entity)
-                                               (entity/position @target)))})))
-
-; does not take into account zoom - but zoom is only for debug ???
-; vision range?
-(defn- on-screen? [ctx position]
-  (let [[x y] position
-        x (float x)
-        y (float y)
-        [cx cy] (g/camera-position ctx)
-        px (float cx)
-        py (float cy)
-        xdist (Math/abs (- x px))
-        ydist (Math/abs (- y py))]
-    (and
-     (<= xdist (inc (/ (float (g/world-viewport-width  ctx))  2)))
-     (<= ydist (inc (/ (float (g/world-viewport-height ctx)) 2))))))
-
-; TODO at wrong point , this affects targeting logic of npcs
-; move the debug flag to either render or mouseover or lets see
-(def ^:private ^:dbg-flag los-checks? true)
-
-(extend-type Context
-  g/LineOfSight
-  ; does not take into account size of entity ...
-  ; => assert bodies <1 width then
-  (line-of-sight? [{:keys [ctx/raycaster] :as ctx}
-                   source
-                   target]
-    (and (or (not (:entity/player? source))
-             (on-screen? ctx (entity/position target)))
-         (not (and los-checks?
-                   (raycaster/blocked? raycaster
-                                       (entity/position source)
-                                       (entity/position target)))))))
-(extend-type Context
   g/InfoText
   (info-text [ctx object]
     (cdq.g.info/text ctx object)))
@@ -347,11 +165,6 @@
     (cdq.g.interaction-state/create ctx eid)))
 
 (extend-type Context
-  g/SpawnEntity
-  (spawn-entity! [ctx position body components]
-    (cdq.g.spawn-entity/spawn-entity! ctx position body components)))
-
-(extend-type Context
   g/EffectHandler
   (handle-txs! [ctx transactions]
     (doseq [transaction transactions
@@ -363,11 +176,6 @@
       (try (cdq.g.handle-txs/handle-tx! transaction ctx)
            (catch Throwable t
              (throw (ex-info "" {:transaction transaction} t)))))))
-
-(extend-type Context
-  g/Creatures
-  (spawn-creature! [ctx opts]
-    (cdq.g.spawn-creature/spawn-creature! ctx opts)))
 
 (extend-type Context
   g/EditorWindow
