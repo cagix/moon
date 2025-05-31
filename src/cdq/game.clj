bfcfa6a2633ce5b66896fc3ebc798e31a40b8fee
Michael Sappler <michaelsappler@gmail.com>
2025-05-31 15:08:31 +0530

    Remove clojure.gdx.backends.lwjgl

--- src/cdq/game.clj
+++ src/cdq/game.clj
@@ -1,9 +1,15 @@
 (ns cdq.game
-  (:require [clojure.gdx.backends.lwjgl :as lwjgl]
-            [clojure.edn :as edn]
+  (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.walk :as walk])
-  (:import (com.badlogic.gdx ApplicationAdapter)))
+  (:import (com.badlogic.gdx ApplicationAdapter)
+           (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
+                                             Lwjgl3ApplicationConfiguration)
+           (com.badlogic.gdx.utils SharedLibraryLoader
+                                   Os)
+           (java.awt Taskbar
+                     Toolkit)
+           (org.lwjgl.system Configuration)))
 
 (defn- require-symbols [form]
   (walk/postwalk (fn [form]
@@ -26,20 +32,36 @@
                 (str "Config key not found: " k))
         (get m k)))))
 
+(defn- set-mac-os-config! [{:keys [glfw-async?
+                                   dock-icon]}]
+  (.set Configuration/GLFW_LIBRARY_NAME "glfw_async")
+  (.setIconImage (Taskbar/getTaskbar)
+                 (.getImage (Toolkit/getDefaultToolkit)
+                            (io/resource dock-icon))))
+
+(defn- lwjgl3-config [config]
+  (doto (Lwjgl3ApplicationConfiguration.)
+    (.setTitle (:title config))
+    (.setWindowedMode (:width (:windowed-mode config))
+                      (:height (:windowed-mode config)))
+    (.setForegroundFPS (:foreground-fps config))))
+
 (def state (atom nil))
 
 (defn -main [config-path]
   (let [config (create-config config-path)]
-    (lwjgl/application (:lwjgl-application config)
-                       (proxy [ApplicationAdapter] []
-                         (create []
-                           (reset! state ((:create! config) config)))
+    (when (= SharedLibraryLoader/os Os/MacOsX)
+      (set-mac-os-config! (:mac-os config)))
+    (Lwjgl3Application. (proxy [ApplicationAdapter] []
+                          (create []
+                            (reset! state ((:create! config) config)))
 
-                         (dispose []
-                           ((:dispose! config) @state))
+                          (dispose []
+                            ((:dispose! config) @state))
 
-                         (render []
-                           (swap! state (:render! config)))
+                          (render []
+                            (swap! state (:render! config)))
 
-                         (resize [width height]
-                           ((:resize! config) @state width height))))))
+                          (resize [width height]
+                            ((:resize! config) @state width height)))
+                        (lwjgl3-config (:lwjgl3-config config)))))
