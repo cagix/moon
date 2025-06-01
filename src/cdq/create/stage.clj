99021dd461ecfe5ed948499dd58c46781bcd947a
Michael Sappler <michaelsappler@gmail.com>
2025-06-01 20:22:06 +0530

    gdl.ui.stage protocol

--- src/cdq/create/stage.clj
+++ src/cdq/create/stage.clj
@@ -12,6 +12,7 @@
             [cdq.ui.player-state-draw]
             [cdq.ui.windows]
             [gdl.ui :as ui]
+            [gdl.ui.stage]
             [gdl.ui.menu :as menu]))
 
 (defn- create-actors [{:keys [ctx/ui-viewport]
@@ -33,6 +34,23 @@
 
 (def ^:private -k :ctx/stage)
 
+; TODO can we make it a protocol ?
+; gdl.ui.stage ?
+; or how is it accessed?
+
+; => as ILookup -> possible ?
+
+; * act!
+; * draw!
+; * add!
+; * hit
+; * find by name string
+
+; => then we can remove stage from gdl.ui ????
+
+; also outdated context -> pass directly atom state?
+; swap! at each render?
+
 (defn do! [{:keys [ctx/ui-viewport
                    ctx/batch
                    ctx/config]
@@ -41,25 +59,22 @@
   (extend (class ctx)
     cdq.stage/Stage
     {:render-stage! (fn [ctx]
-                      (ui/act! (-k ctx) ctx)
-                      (ui/draw! (-k ctx) ctx)
-                      ctx)
+                      (gdl.ui.stage/render! (-k ctx) ctx))
 
      :reset-actors! (fn [ctx]
                       (let [stage (-k ctx)]
-                        (ui/clear! stage)
-                        (run! #(ui/add! stage %) (create-actors ctx))))
+                        (gdl.ui.stage/clear! stage)
+                        (run! #(gdl.ui.stage/add! stage %) (create-actors ctx))))
 
      :add-actor! (fn [ctx actor]
-                   (ui/add! (-k ctx) actor))
+                   (gdl.ui.stage/add! (-k ctx) actor))
 
      :mouseover-actor (fn [ctx]
-                        (ui/hit (-k ctx) (g/ui-mouse-position ctx)))
+                        (gdl.ui.stage/hit (-k ctx) (g/ui-mouse-position ctx)))
 
      :show-message! (fn [ctx message]
                       (-> (-k ctx)
-                          ui/root
-                          (ui/find-actor "player-message")
+                          (gdl.ui.stage/find-actor "player-message")
                           (cdq.ui.message/show! message)))
 
      ; no window movable type cursor appears here like in player idle
@@ -70,18 +85,18 @@
                        {:keys [title text button-text on-click]}]
                     (let [stage (-k ctx)]
                       (assert (not (::modal stage)))
-                      (ui/add! stage
-                               (ui/window {:title title
-                                           :rows [[(ui/label text)]
-                                                  [(ui/text-button button-text
-                                                                   (fn [_actor _ctx]
-                                                                     (ui/remove! (::modal stage))
-                                                                     (on-click)))]]
-                                           :id ::modal
-                                           :modal? true
-                                           :center-position [(/ (:width ui-viewport) 2)
-                                                             (* (:height ui-viewport) (/ 3 4))]
-                                           :pack? true}))))
+                      (gdl.ui.stage/add! stage
+                                         (ui/window {:title title
+                                                     :rows [[(ui/label text)]
+                                                            [(ui/text-button button-text
+                                                                             (fn [_actor _ctx]
+                                                                               (ui/remove! (::modal stage))
+                                                                               (on-click)))]]
+                                                     :id ::modal
+                                                     :modal? true
+                                                     :center-position [(/ (:width ui-viewport) 2)
+                                                                       (* (:height ui-viewport) (/ 3 4))]
+                                                     :pack? true}))))
 
      :inventory-window-visible? (fn [ctx]
                                   (-> ctx -k :windows :inventory-window ui/visible?))
@@ -102,7 +117,7 @@
                      (-> ctx -k :windows :inventory-window (inventory-window/remove-item! inventory-cell)))
 
      :open-error-window! (fn [ctx throwable]
-                           (ui/add! (-k ctx) (error-window/create throwable)))
+                           (gdl.ui.stage/add! (-k ctx) (error-window/create throwable)))
 
      :selected-skill (fn [ctx]
                        (action-bar/selected-skill (:action-bar (-k ctx))))
@@ -111,4 +126,28 @@
   (let [stage (ui/stage (:java-object ui-viewport)
                         batch)]
     (input/set-processor! ctx stage)
-    (assoc ctx -k stage)))
+    (assoc ctx -k (reify
+                    ; TODO is disposable but not sure if needed as we handle batch ourself.
+                    clojure.lang.ILookup
+                    (valAt [_ key]
+                      (key stage))
+
+                    gdl.ui.stage/Stage
+                    (render! [_ ctx]
+                      (ui/act! stage ctx)
+                      (ui/draw! stage ctx)
+                      ctx)
+
+                    (add! [_ actor] ; -> use gdl.ui/add! ?
+                      (ui/add! stage actor))
+
+                    (clear! [_]
+                      (ui/clear! stage))
+
+                    (hit [_ position]
+                      (ui/hit stage position))
+
+                    (find-actor [_ actor-name]
+                      (-> stage
+                          ui/root
+                          (ui/find-actor actor-name)))))))
