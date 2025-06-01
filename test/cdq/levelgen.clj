cde1eaa0915982605812bb378e3f2f7edf5d341b
Michael Sappler <michaelsappler@gmail.com>
2025-06-01 13:38:42 +0530

    empty levelgen test app

---
+++ test/cdq/levelgen.clj
@@ -0,0 +1,15 @@
+(ns cdq.levelgen)
+
+(defn create! [config]
+  ; camera (world-viewport)
+  ; input (world-mouse pos)
+  ; stage ( ui -> ui-viewport )
+  ; graphics ...
+  ; database ...
+  )
+
+(defn dispose! [])
+
+(defn render! [])
+
+(defn resize! [width height])
