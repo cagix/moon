3a2fb3900701f9bab7a6f795dcdf489d8ceecfc8
Michael Sappler <michaelsappler@gmail.com>
2025-06-01 15:48:08 +0530

    Draw tiled map in levelgen test

--- test/cdq/levelgen.clj
+++ test/cdq/levelgen.clj
@@ -1,8 +1,14 @@
 (ns cdq.levelgen
   (:require [cdq.create.assets]
             [cdq.create.db]
-            [cdq.level.modules :as modules])
-  (:import (com.badlogic.gdx.utils Disposable)))
+            [cdq.level.modules :as modules]
+            [clojure.gdx.utils.disposable :as disposable]
+            [gdl.graphics :as graphics]
+            [gdl.graphics.color :as color]
+            [gdl.graphics.tiled-map-renderer :as tm-renderer]
+            [gdl.viewport :as viewport]))
+
+(def tile-size 48)
 
 (defrecord Context [])
 
@@ -16,14 +22,35 @@
                                              :asset-type-extensions {:texture #{"png" "bmp"}}}})
         ctx (cdq.create.db/do!     ctx)
         ctx (cdq.create.assets/do! ctx)
-        level (modules/create ctx)]
+        level (modules/create ctx)
+        world-unit-scale (float (/ tile-size))
+        ctx (assoc ctx :ctx/world-viewport (graphics/world-viewport world-unit-scale
+                                                                    {:width 1440
+                                                                     :height 900}))
+        tiled-map (:tiled-map level)
+        batch (graphics/sprite-batch)
+        tm-renderer (tm-renderer/create tiled-map
+                                        world-unit-scale
+                                        batch)
+        ctx (assoc ctx
+                   :ctx/tm-renderer tm-renderer
+                   :ctx/tiled-map tiled-map)]
     (reset! state ctx)
     (println level)))
 
 (defn dispose! []
   (let [{:keys [ctx/assets]} @state]
-    (Disposable/.dispose assets)))
+    (disposable/dispose! assets)))
 
-(defn render! [])
+(defn render! []
+  (let [{:keys [ctx/tm-renderer
+                ctx/tiled-map
+                ctx/world-viewport]} @state]
+    (tm-renderer/draw! tm-renderer
+                       tiled-map
+                       (constantly color/white)
+                       (:camera world-viewport))))
 
-(defn resize! [width height])
+(defn resize! [width height]
+  (let [{:keys [ctx/world-viewport]} @state]
+    (viewport/resize! world-viewport width height)))
