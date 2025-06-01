18a40f33767980ec2672b164973a791de809d1ca
Michael Sappler <michaelsappler@gmail.com>
2025-06-01 15:20:23 +0530

    Successfully generate modules level at cdq.levelgen test

--- test/cdq/levelgen.clj
+++ test/cdq/levelgen.clj
@@ -1,16 +1,18 @@
 (ns cdq.levelgen
-  (:require [cdq.create.db :as create.db]
+  (:require [cdq.create.assets]
+            [cdq.create.db]
             [cdq.level.modules :as modules]))
 
 (defrecord Context [])
 
 (defn create! [config]
-  ; 1. generate level
   (let [ctx (->Context)
-        ctx (assoc ctx :ctx/config
-                   {:db {:schemas "schema.edn"
-                         :properties "properties.edn"}})
-        ctx (create.db/do! ctx)
+        ctx (assoc ctx :ctx/config {:db {:schemas "schema.edn"
+                                         :properties "properties.edn"}
+                                    :assets {:folder "resources/"
+                                             :asset-type-extensions {:texture #{"png" "bmp"}}}})
+        ctx (cdq.create.db/do!     ctx)
+        ctx (cdq.create.assets/do! ctx)
         level (modules/create ctx)]
     (println level)))
 
