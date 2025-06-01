e2e19421418a8350eb9d8038f55786bab0caaa2b
Michael Sappler <michaelsappler@gmail.com>
2025-06-01 19:14:43 +0530

    add clojure.gdx.utils.files

---
+++ src/clojure/gdx/utils/files.clj
@@ -0,0 +1,17 @@
+(ns clojure.gdx.utils.files
+  (:require [clojure.gdx.files.file-handle :as fh]))
+
+(defn recursively-search [folder extensions]
+  (loop [[file & remaining] (fh/list folder)
+         result []]
+    (cond (nil? file)
+          result
+
+          (fh/directory? file)
+          (recur (concat remaining (fh/list file)) result)
+
+          (extensions (fh/extension file))
+          (recur remaining (conj result (fh/path file)))
+
+          :else
+          (recur remaining result))))
--- src/gdl/assets.clj
+++ src/gdl/assets.clj
@@ -2,32 +2,18 @@
   (:require [clojure.gdx :as gdx]
             [clojure.gdx.assets.manager :as manager]
             [clojure.gdx.files :as files]
-            [clojure.gdx.files.file-handle :as fh]
+            [clojure.gdx.utils.files :as utils.files]
             [clojure.string :as str]
             [gdl.audio.sound])
   (:import (com.badlogic.gdx.audio Sound)
            (com.badlogic.gdx.graphics Texture)))
 
-(defn- recursively-search [folder extensions]
-  (loop [[file & remaining] (fh/list folder)
-         result []]
-    (cond (nil? file)
-          result
-
-          (fh/directory? file)
-          (recur (concat remaining (fh/list file)) result)
-
-          (extensions (fh/extension file))
-          (recur remaining (conj result (fh/path file)))
-
-          :else
-          (recur remaining result))))
-
 (defn create [{:keys [folder asset-type-extensions]}]
   (manager/create
    (for [[asset-type extensions] asset-type-extensions
          file (map #(str/replace-first % folder "")
-                   (recursively-search (files/internal (gdx/files) folder) extensions))]
+                   (utils.files/recursively-search (files/internal (gdx/files) folder)
+                                                   extensions))]
      [file (case asset-type
              :sound Sound
              :texture Texture)])))
