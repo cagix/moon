1ddb2427f7127d62754671db83dae006ad2909e9
Michael Sappler <michaelsappler@gmail.com>
2025-06-01 19:07:39 +0530

    add clojure.gdx.assets.manager

---
+++ src/clojure/gdx/assets/manager.clj
@@ -0,0 +1,18 @@
+(ns clojure.gdx.assets.manager
+  (:import (com.badlogic.gdx.assets AssetManager)))
+
+(defn create [assets]
+  (let [manager (AssetManager.)]
+    (doseq [[file class] assets]
+      (.load manager ^String file ^Class class))
+    (.finishLoading manager)
+    manager))
+
+(defn safe-get [^AssetManager this path]
+  (if (.contains this path)
+    (.get this ^String path)
+    (throw (IllegalArgumentException. (str "Asset cannot be found: " path)))))
+
+(defn all-of-type [^AssetManager assets class]
+  (filter #(= (.getAssetType assets %) class)
+          (.getAssetNames assets)))
--- src/gdl/assets.clj
+++ src/gdl/assets.clj
@@ -1,10 +1,10 @@
 (ns gdl.assets
   (:require [clojure.gdx :as gdx]
+            [clojure.gdx.assets.manager :as manager]
             [clojure.gdx.files :as files]
             [clojure.string :as str]
             [gdl.audio.sound])
-  (:import (com.badlogic.gdx.assets AssetManager)
-           (com.badlogic.gdx.audio Sound)
+  (:import (com.badlogic.gdx.audio Sound)
            (com.badlogic.gdx.files FileHandle)
            (com.badlogic.gdx.graphics Texture)))
 
@@ -23,24 +23,8 @@
           :else
           (recur remaining result))))
 
-(defn- create-asset-manager [assets]
-  (let [manager (AssetManager.)]
-    (doseq [[file class] assets]
-      (.load manager ^String file ^Class class))
-    (.finishLoading manager)
-    manager))
-
-(defn- safe-get [^AssetManager this path]
-  (if (.contains this path)
-    (.get this ^String path)
-    (throw (IllegalArgumentException. (str "Asset cannot be found: " path)))))
-
-(defn- all-of-type [^AssetManager assets class]
-  (filter #(= (.getAssetType assets %) class)
-          (.getAssetNames assets)))
-
 (defn create [{:keys [folder asset-type-extensions]}]
-  (create-asset-manager
+  (manager/create
    (for [[asset-type extensions] asset-type-extensions
          file (map #(str/replace-first % folder "")
                    (recursively-search (files/internal (gdx/files) folder) extensions))]
@@ -49,16 +33,16 @@
              :texture Texture)])))
 
 (defn sound [assets path]
-  (let [sound (safe-get assets path)]
+  (let [sound (manager/safe-get assets path)]
     (reify gdl.audio.sound/Sound
       (play! [_]
         (Sound/.play sound)))))
 
 (defn texture [assets path]
-  (safe-get assets path))
+  (manager/safe-get assets path))
 
 (defn all-sounds [assets]
-  (all-of-type assets Sound))
+  (manager/all-of-type assets Sound))
 
 (defn all-textures [assets]
-  (all-of-type assets Texture))
+  (manager/all-of-type assets Texture))
