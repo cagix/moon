18dbf120c17899faf239dd2918d5e5d53565cd55
Michael Sappler <michaelsappler@gmail.com>
2025-06-04 15:37:04 +0530

    upfactor pause-game?

--- src/cdq/render/assoc_paused.clj
+++ src/cdq/render/assoc_paused.clj
@@ -3,15 +3,14 @@
             [cdq.state :as state]
             [clojure.input :as input]))
 
-(defn- pause-game? [{:keys [ctx/input
-                            ctx/config
-                            ctx/player-eid]}]
-  (let [controls (:controls config)]
-    (or #_error
-        (and (:pausing? config)
-             (state/pause-game? (entity/state-obj @player-eid))
-             (not (or (input/key-just-pressed? input (:unpause-once controls))
-                      (input/key-pressed?      input (:unpause-continously controls))))))))
-
-(defn do! [ctx]
-  (assoc ctx :ctx/paused? (pause-game? ctx)))
+(defn do! [{:keys [ctx/input
+                   ctx/config
+                   ctx/player-eid]
+            :as ctx}]
+  (assoc ctx :ctx/paused?
+         (let [controls (:controls config)]
+           (or #_error
+               (and (:pausing? config)
+                    (state/pause-game? (entity/state-obj @player-eid))
+                    (not (or (input/key-just-pressed? input (:unpause-once controls))
+                             (input/key-pressed?      input (:unpause-continously controls)))))))))
