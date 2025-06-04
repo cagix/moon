c435575023590e138ff291acec60425f1885f1c2
Michael Sappler <michaelsappler@gmail.com>
2025-06-04 16:28:37 +0530

    Move mac-os config to clojure.gdx.lwjgl

--- src/cdq/java/awt.clj
+++
@@ -1,9 +0,0 @@
-(ns cdq.java.awt
-  (:require [clojure.java.io :as io]) ; TODO it does that with passing str also internally I checked
-  (:import (java.awt Taskbar
-                     Toolkit)))
-
-(defn set-taskbar-icon! [io-resource]
-  (.setIconImage (Taskbar/getTaskbar)
-                 (.getImage (Toolkit/getDefaultToolkit)
-                            (io/resource io-resource))))
--- src/cdq/lwjgl.clj
+++ src/cdq/lwjgl.clj
@@ -1,8 +1,6 @@
 (ns cdq.lwjgl
-  (:require [cdq.java.awt :as awt]
-            [cdq.utils :as utils]
+  (:require [cdq.utils :as utils]
             [clojure.application-listener :as application]
-            [clojure.gdx :as gdx]
             [clojure.gdx.lwjgl :as lwjgl]))
 
 (defn invoke [[f params]]
@@ -10,13 +8,6 @@
 
 (defn -main [config-path]
   (let [config (utils/load-edn-config config-path)]
-    (when (= (gdx/operating-system) :operating-system/mac)
-      (let [{:keys [glfw-async?
-                    dock-icon]} (:mac-os (::config config))]
-        (when glfw-async?
-          (lwjgl/set-glfw-async!))
-        (when dock-icon
-          (awt/set-taskbar-icon! dock-icon))))
     (lwjgl/start-application! (reify application/Listener
                                 (create! [_]
                                   ((::create! config) config))
@@ -28,4 +19,4 @@
                                   ((::resize! config) width height))
                                 (pause! [_])
                                 (resume! [_]))
-                              (dissoc (::config config) :mac-os))))
+                              (::config config))))
