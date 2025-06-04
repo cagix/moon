d137df1344f28a7668d1766fb6e58e30d14472be
Michael Sappler <michaelsappler@gmail.com>
2025-06-04 12:01:10 +0530

    pass namespace to ctx extenders -> extend-protocols

--- src/cdq/create/extend_protocols.clj
+++ src/cdq/create/extend_protocols.clj
@@ -1,27 +1,25 @@
-(ns cdq.create.ctx-extenders
+(ns cdq.create.extend-protocols
   (:require [cdq.utils :as utils]))
 
-(defn- create-method-map [protocol]
+(defn- create-method-map [protocol namespace]
   (let [{:keys [sigs
                 var]} protocol]
     (into {}
           (for [[k signature] sigs]
-            [k (requiring-resolve (symbol (str "cdq.ctx."
+            [k (requiring-resolve (symbol (str (str namespace ".")
                                                (utils/pascal->kebab (name (symbol var)))
                                                "/"
                                                (:name signature))))]))))
 
-(defn do! [ctx]
+(defn do! [ctx namespace]
   (let [protocols (map (comp deref second)
-                       (filter #(:sigs @(% 1)) (ns-publics 'cdq.ctx)))]
+                       (filter #(:sigs @(% 1)) (ns-publics namespace)))]
     (doseq [protocol protocols]
       ;(println)
       ;(println "Extending protocol: " (:on protocol))
       ;(println "With method map: ")
-      ;(clojure.pprint/pprint (create-method-map protocol))
+      ;(clojure.pprint/pprint (create-method-map protocol namespace))
       (extend (class ctx)
         protocol
-        (create-method-map protocol))))
+        (create-method-map protocol namespace))))
   ctx)
-
-
