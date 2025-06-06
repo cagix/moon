6b9a8194d5fb2747f2c3bee2becaa4625caec0c7
Michael Sappler <michaelsappler@gmail.com>
2025-06-06 22:13:50 +0530

    Pass draw-fns via config

--- src/cdq/render/draw_on_world_viewport.clj
+++ src/cdq/render/draw_on_world_viewport.clj
@@ -1,20 +1,9 @@
 (ns cdq.render.draw-on-world-viewport
   (:require [gdl.graphics :as graphics]))
 
-(def draw-fns
-  '[
-    cdq.render.draw-on-world-viewport.draw-tile-grid/do!
-    cdq.render.draw-on-world-viewport.draw-cell-debug/do!
-    cdq.render.draw-on-world-viewport.render-entities/do!
-    ; cdq.render.draw-on-world-viewport.geom-test/do!
-    cdq.render.draw-on-world-viewport.highlight-mouseover-tile/do!
-    ])
-
-(defn do! [{:keys [ctx/graphics]
-            :as ctx}]
-  (let [draw-fns (map requiring-resolve draw-fns)]
-    (graphics/draw-on-world-viewport! graphics
-                                      (fn []
-                                        (doseq [f draw-fns]
-                                          (f ctx)))))
+(defn do! [{:keys [ctx/graphics] :as ctx} draw-fns]
+  (graphics/draw-on-world-viewport! graphics
+                                    (fn []
+                                      (doseq [f draw-fns]
+                                        (f ctx))))
   ctx)
