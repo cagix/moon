bbbfe9b3b5f5d9888838ff7d778a21e221f89983
Michael Sappler <michaelsappler@gmail.com>
2025-06-06 07:13:54 +0530

    Move interop mappings to separate files

---
+++ src/clojure/gdx/interop.clj
@@ -0,0 +1,37 @@
+(ns clojure.gdx.interop)
+
+(comment
+
+ (require '[clojure.string :as str]
+          '[clojure.reflect :refer [type-reflect]])
+
+ (defn- relevant-fields [class-str field-type]
+   (->> class-str
+        symbol
+        eval
+        type-reflect
+        :members
+        (filter #(= field-type (:type %)))))
+
+ (defn- ->clojure-symbol [field]
+   (-> field :name name str/lower-case (str/replace #"_" "-") symbol))
+
+ (defn create-mapping [class-str field-type]
+   (sort-by first
+            (for [field (relevant-fields class-str field-type)]
+              [(keyword (->clojure-symbol field))
+               (symbol class-str (str (:name field)))])))
+
+ (defn generate-mapping [class-str field-type]
+   (spit "temp.clj"
+         (with-out-str
+          (println "{")
+          (doseq [[kw static-field] (create-mapping class-str field-type)]
+            (println kw static-field))
+          (println "}"))))
+
+ (generate-mapping "Input$Buttons" 'int)
+ (generate-mapping "Input$Keys"    'int) ; without Input$Keys/MAX_KEYCODE
+ (generate-mapping "Color" 'com.badlogic.gdx.graphics.Color)
+
+ )
