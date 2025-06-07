31edb7378b6196777c2da6f7ee70b68afe689340
Michael Sappler <michaelsappler@gmail.com>
2025-06-07 14:29:49 +0530

    gdl.context/create

---
+++ src/gdl/context.clj
@@ -0,0 +1,53 @@
+(ns gdl.context
+  (:require [clojure.gdx :as gdx]
+            [clojure.gdx.files :as files]
+            [clojure.gdx.files.file-handle :as file-handle]
+            [clojure.string :as str]
+            [gdl.create.audio]
+            [gdl.create.graphics]
+            [gdl.create.input]
+            [gdl.create.stage]
+            [gdl.utils.disposable :as disposable])
+  (:import (com.badlogic.gdx.utils Disposable)))
+
+(defn- recursively-search [folder extensions]
+  (loop [[file & remaining] (file-handle/list folder)
+         result []]
+    (cond (nil? file)
+          result
+
+          (file-handle/directory? file)
+          (recur (concat remaining (file-handle/list file)) result)
+
+          (extensions (file-handle/extension file))
+          (recur remaining (conj result (file-handle/path file)))
+
+          :else
+          (recur remaining result))))
+
+(extend-type Disposable
+  disposable/Disposable
+  (dispose! [object]
+    (.dispose object)))
+
+(defn- find-assets [files {:keys [folder extensions]}]
+  (map #(str/replace-first % folder "")
+       (recursively-search (files/internal files folder)
+                           extensions)))
+
+(defn create [config]
+  (let [graphics-config (update (:graphics config) :textures (partial find-assets (gdx/files)))
+        graphics (gdl.create.graphics/create-graphics (gdx/graphics)
+                                                      (gdx/files)
+                                                      graphics-config)
+        stage (gdl.create.stage/create! (:ui config)
+                                        graphics
+                                        (gdx/input))]
+    {:ctx/input (gdl.create.input/create-input (gdx/input))
+     :ctx/audio (when (:sounds config)
+                  (gdl.create.audio/create-audio (gdx/audio)
+                                                 (gdx/files)
+                                                 (find-assets (gdx/files)
+                                                              (:sounds config))))
+     :ctx/graphics graphics
+     :ctx/stage stage}))
--- src/gdl/start.clj
+++ src/gdl/start.clj
@@ -1,64 +1,13 @@
 (ns gdl.start
   (:require [clojure.edn :as edn]
-            [clojure.gdx :as gdx]
             [clojure.gdx.backends.lwjgl :as lwjgl]
-            [clojure.gdx.files :as files]
-            [clojure.gdx.files.file-handle :as file-handle]
             [clojure.gdx.utils.shared-library-loader :as shared-library-loader]
             [clojure.gdx.utils.os :as os]
             [clojure.java.awt.taskbar :as taskbar]
             [clojure.java.io :as io]
             [clojure.lwjgl.system.configuration :as lwjgl.system.configuration]
-            [clojure.string :as str]
-            [gdl.create.audio]
-            [gdl.create.graphics]
-            [gdl.create.input]
-            [gdl.create.stage]
-            [gdl.utils.disposable :as disposable])
-  (:import (com.badlogic.gdx ApplicationListener)
-           (com.badlogic.gdx.utils Disposable)))
-
-(defn- recursively-search [folder extensions]
-  (loop [[file & remaining] (file-handle/list folder)
-         result []]
-    (cond (nil? file)
-          result
-
-          (file-handle/directory? file)
-          (recur (concat remaining (file-handle/list file)) result)
-
-          (extensions (file-handle/extension file))
-          (recur remaining (conj result (file-handle/path file)))
-
-          :else
-          (recur remaining result))))
-
-(extend-type Disposable
-  disposable/Disposable
-  (dispose! [object]
-    (.dispose object)))
-
-(defn- find-assets [files {:keys [folder extensions]}]
-  (map #(str/replace-first % folder "")
-       (recursively-search (files/internal files folder)
-                           extensions)))
-
-(defn- create-context [config]
-  (let [graphics-config (update (:graphics config) :textures (partial find-assets (gdx/files)))
-        graphics (gdl.create.graphics/create-graphics (gdx/graphics)
-                                                      (gdx/files)
-                                                      graphics-config)
-        stage (gdl.create.stage/create! (:ui config)
-                                        graphics
-                                        (gdx/input))]
-    {:ctx/input (gdl.create.input/create-input (gdx/input))
-     :ctx/audio (when (:sounds config)
-                  (gdl.create.audio/create-audio (gdx/audio)
-                                                 (gdx/files)
-                                                 (find-assets (gdx/files)
-                                                              (:sounds config))))
-     :ctx/graphics graphics
-     :ctx/stage stage}))
+            [gdl.context])
+  (:import (com.badlogic.gdx ApplicationListener)))
 
 (defn- set-mac-os-config! [{:keys [glfw-async?
                                    dock-icon]}]
@@ -74,7 +23,7 @@
     (proxy [ApplicationListener] []
       (create  []
         ((requiring-resolve (:clojure.gdx.lwjgl/create! config))
-         (create-context (:gdl.application/context config))
+         (gdl.context/create (:gdl.application/context config))
          config))
       (dispose []             (req-resolve-call :clojure.gdx.lwjgl/dispose!))
       (render  []             (req-resolve-call :clojure.gdx.lwjgl/render!))
