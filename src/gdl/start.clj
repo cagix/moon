f1a3ad0c824bc965468e10d6a48d499e755c65ac
Michael Sappler <michaelsappler@gmail.com>
2025-06-07 16:25:47 +0530

    decouple listener creation from start

--- src/gdl/start.clj
+++ src/gdl/start.clj
@@ -1,7 +1,7 @@
 (ns gdl.start
-  (:require [clojure.gdx.app-listener :as app-listener]
-            [clojure.gdx.backends.lwjgl :as lwjgl]))
+  (:require [clojure.gdx.backends.lwjgl :as lwjgl]))
 
 (defn start! [{:keys [config listener]}]
   (lwjgl/application! config
-                      (app-listener/create-adapter listener)))
+                      (let [[f params] listener]
+                        (f params))))
