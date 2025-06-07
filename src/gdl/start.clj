abff6e48a3dda30bc4ebb186d9e4d7e53070af18
Michael Sappler <michaelsappler@gmail.com>
2025-06-07 15:25:59 +0530

    decouple mac-os config from start

--- src/gdl/start.clj
+++ src/gdl/start.clj
@@ -2,22 +2,14 @@
   (:require [clojure.gdx.app-listener :as app-listener]
             [clojure.gdx.backends.lwjgl :as lwjgl]
             [clojure.gdx.utils.shared-library-loader :as shared-library-loader]
-            [clojure.gdx.utils.os :as os]
-            [clojure.java.awt.taskbar :as taskbar]
-            [clojure.lwjgl.system.configuration :as lwjgl.system.configuration]))
-
-(defn- set-mac-os-config! [{:keys [glfw-async?
-                                   dock-icon]}]
-  (when glfw-async?
-    (lwjgl.system.configuration/set-glfw-library-name! "glfw_async"))
-  (when dock-icon
-    (taskbar/set-icon! dock-icon)))
+            [clojure.gdx.utils.os :as os]))
 
 (defn- operating-system []
   (get os/mapping (shared-library-loader/os)))
 
 (defn start! [config]
   (when (= (operating-system) :os/mac-osx)
-    (set-mac-os-config! (:mac-os config)))
+    (when-let [[f params] (:mac-os config)]
+      (f params)))
   (lwjgl/application! (:clojure.gdx.lwjgl/config config)
                       (app-listener/create-adapter (:listener config))))
