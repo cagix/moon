b67ba9b66f40be342db04876c0d70c17456296b3
Michael Sappler <michaelsappler@gmail.com>
2025-06-09 10:07:07 +0530

    destructure projectile size

--- src/cdq/world.clj
+++ src/cdq/world.clj
@@ -502,30 +502,31 @@
          (handle-txs! ctx))
     eid))
 
-(defmethod do! :tx/spawn-projectile [[_
-                                      {:keys [position direction faction]}
-                                      {:keys [entity/image
-                                              projectile/max-range
-                                              projectile/speed
-                                              entity-effects
-                                              projectile/piercing?] :as projectile}]
-                                     ctx]
-  (let [size (:projectile/size projectile)]
-    (spawn-entity! ctx
-                   position
-                   {:width size
-                    :height size
-                    :z-order :z-order/flying
-                    :rotation-angle (v/angle-from-vector direction)}
-                   {:entity/movement {:direction direction
-                                      :speed speed}
-                    :entity/image image
-                    :entity/faction faction
-                    :entity/delete-after-duration (/ max-range speed)
-                    :entity/destroy-audiovisual :audiovisuals/hit-wall
-                    :entity/projectile-collision {:entity-effects entity-effects
-                                                  :piercing? piercing?}})
-    nil))
+(defmethod do! :tx/spawn-projectile
+  [[_
+    {:keys [position direction faction]}
+    {:keys [entity/image
+            projectile/max-range
+            projectile/speed
+            entity-effects
+            projectile/size
+            projectile/piercing?] :as projectile}]
+   ctx]
+  (spawn-entity! ctx
+                 position
+                 {:width size
+                  :height size
+                  :z-order :z-order/flying
+                  :rotation-angle (v/angle-from-vector direction)}
+                 {:entity/movement {:direction direction
+                                    :speed speed}
+                  :entity/image image
+                  :entity/faction faction
+                  :entity/delete-after-duration (/ max-range speed)
+                  :entity/destroy-audiovisual :audiovisuals/hit-wall
+                  :entity/projectile-collision {:entity-effects entity-effects
+                                                :piercing? piercing?}})
+  nil)
 
 (defmethod do! :tx/spawn-effect
   [[_ position components]
