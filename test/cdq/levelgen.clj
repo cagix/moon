70690cff22f7132961c3257b67f24af24eb3f38c
Michael Sappler <michaelsappler@gmail.com>
2025-06-10 08:33:05 +0530

    fix levelgen create config param

--- test/cdq/levelgen.clj
+++ test/cdq/levelgen.clj
@@ -66,7 +66,7 @@
 
 (defrecord Context [])
 
-(defn create! [_]
+(defn create! [_config _params]
   (let [ctx (merge (->Context)
                    (gdl.context/create {:graphics {:textures {:folder "resources/"
                                                               :extensions #{"png" "bmp"}}
