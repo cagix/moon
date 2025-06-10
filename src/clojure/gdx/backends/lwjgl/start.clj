76634d0772b4bff7e69e133f3da49dbd5717d462
Michael Sappler <michaelsappler@gmail.com>
2025-06-10 07:11:57 +0530

    remove clojure.gdx.app-listener, lwjgl.start & create gdl.application

--- src/clojure/gdx/backends/lwjgl/start.clj
+++
@@ -1,7 +0,0 @@
-(ns clojure.gdx.backends.lwjgl.start
-  (:require [clojure.gdx.backends.lwjgl :as lwjgl]))
-
-(defn application! [{:keys [config listener]}]
-  (lwjgl/application! config
-                      (let [[f params] listener]
-                        (f params))))
--- src/gdl/application.clj
+++ src/gdl/application.clj
@@ -1,7 +1,8 @@
-(ns clojure.gdx.app-listener
+(ns gdl.application
+  (:require [clojure.gdx.backends.lwjgl :as lwjgl])
   (:import (com.badlogic.gdx ApplicationListener)))
 
-(defn create-adapter [{:keys [create dispose render resize pause resume]}]
+(defn- create-adapter [{:keys [create dispose render resize pause resume]}]
   (proxy [ApplicationListener] []
     (create  []              (when-let [[f params] create] (f params)))
     (dispose []              (when dispose (dispose)))
@@ -9,3 +10,7 @@
     (resize  [width height]  (when resize  (resize width height)))
     (pause   []              (when pause   (pause)))
     (resume  []              (when resume  (resume)))))
+
+(defn start! [{:keys [config listener]}]
+  (lwjgl/application! config
+                      (create-adapter listener)))
