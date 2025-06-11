c1346ab9cb4b63d28f4643e477f9ec1f244e2e13
Michael Sappler <michaelsappler@gmail.com>
2025-06-11 12:53:01 +0530

    Fix audio for levelgen

--- src/gdl/create/audio.clj
+++ src/gdl/create/audio.clj
@@ -5,20 +5,21 @@
             [gdl.utils.disposable :as disposable]))
 
 (defn do! [gdl {:keys [sounds]}]
-  (let [sounds (into {}
-                     (for [file (assets/find-assets (update sounds :folder (partial fs/internal gdl)))]
-                       [file (audio/sound gdl file)]))]
-    (reify
-      disposable/Disposable
-      (dispose! [_]
-        (do
-         ;(println "Disposing sounds ...")
-         (run! disposable/dispose! (vals sounds))))
+  (when sounds
+    (let [sounds (into {}
+                       (for [file (assets/find-assets (update sounds :folder (partial fs/internal gdl)))]
+                         [file (audio/sound gdl file)]))]
+      (reify
+        disposable/Disposable
+        (dispose! [_]
+          (do
+           ;(println "Disposing sounds ...")
+           (run! disposable/dispose! (vals sounds))))
 
-      gdl.audio/Audio
-      (all-sounds [_]
-        (map first sounds))
+        gdl.audio/Audio
+        (all-sounds [_]
+          (map first sounds))
 
-      (play-sound! [_ path]
-        (assert (contains? sounds path) (str path))
-        (audio/play! (get sounds path))))))
+        (play-sound! [_ path]
+          (assert (contains? sounds path) (str path))
+          (audio/play! (get sounds path)))))))
