19af7950ec011b8ea4981dea2fe5ca03addff02e
Michael Sappler <michaelsappler@gmail.com>
2025-06-11 09:53:28 +0530

    Remove comments

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -5,10 +5,6 @@
             [gdl.utils.disposable :as disp]
             [qrecord.core :as q]))
 
-(comment
- (defmacro def-record-and-schema )
- )
-
 (q/defrecord Context [ctx/config
                       ctx/db
                       ctx/graphics
@@ -18,59 +14,21 @@
   (m/schema [:map {:closed true}
              [:ctx/config :some]
              [:ctx/gdl :some]
-
              [:ctx/db :some]
-
              [:ctx/audio :some]
-
              [:ctx/stage :some]
-
              [:ctx/graphics :some]
              [:ctx/world-event-handlers :some]
-
-             ; TODO can do _first_ the renamings to ':world/'
-             ; separaately from other changes
-
-             ; our world model is most complex
              [:ctx/elapsed-time number?]
-             ; info-segment <- only [ctx/elapsed-time]
-             ; effect/handle <- only world
-             ; entity/tick! <- FIXME cdq.entity.state.player-moving -> controls
-             ; entity/create <- this is intereseting, can do only w. world?
-             ; draw-active-skill [exception]
-             ; cdq.render.update-time [exception]
-             ; cdq.ctx.effect-handler/do! - [FIXED]
-             ; cdq.ui.dev-menu [exception]
              [:ctx/delta-time {:optional true} number?]
-             ; entity/tick!
-             ; cdq.render.update-time
-             ; cdq.tx.update-animation
              [:ctx/max-delta number?]
-             ; cdq.render.update-time
              [:ctx/max-speed number?]
-             ; entity/tick!
              [:ctx/minimum-size number?]
-             ; spawn-entity!
              [:ctx/paused? {:optional true} :boolean]
-             ; dev-menu
              [:ctx/tiled-map :some]
-             ; cdq.render.draw-world-map
              [:ctx/grid :some]
-             ; context-entity-add!/remove/upd
-             ; cdq.ctx.grid
-             ; entity/tick!
-             ; cdq.render.draw-on-world-viewport.draw-cell-debug
-             ; cdq.render.draw-on-world-viewport.geom-test
-             ; ..
-             ; cdq.render.update-mouseover-entity
-             ; update-potential-fields!
              [:ctx/raycaster :some]
-             ; cdq.ctx.line-of-sight
-             ; effect/useful?
-             ; cdq.render.draw-world-map
              [:ctx/content-grid :some]
-             ; ctx add/remove/upd
-             ; calculate-active-entities
              [:ctx/explored-tile-corners :some]
              [:ctx/id-counter :some]
              [:ctx/entity-ids :some]
@@ -80,8 +38,7 @@
              [:ctx/render-z-order :some]
              [:ctx/mouseover-eid {:optional true} :any]
              [:ctx/player-eid :some]
-             [:ctx/active-entities {:optional true} :some]
-             ]))
+             [:ctx/active-entities {:optional true} :some]]))
 
 (def state (atom nil))
 
