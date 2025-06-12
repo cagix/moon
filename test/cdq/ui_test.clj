e9cfa70d999cde802540df0aaf1fe6cb54e08981
Michael Sappler <michaelsappler@gmail.com>
2025-06-12 10:04:49 +0530

    add cdq.ui-test

---
+++ test/cdq/ui_test.clj
@@ -0,0 +1,22 @@
+(ns cdq.ui-test
+  (:require [cdq.application :as application]
+            [cdq.create.world-event-handlers :refer [show-modal-window!]]
+            [cdq.ui.error-window :as error-window]
+            [gdl.ui.stage :as stage]))
+
+(comment
+ ; TODO use post-runnable! ?
+
+ (show-modal-window! @application/state
+                     {:title "Hello Modal"
+                      :text "MY TEXT"
+                      :button-text "MYBUTONTEXT"
+                      :on-click (fn []
+                                  (println "CLIEKDE"))})
+
+ (stage/add! (:ctx/stage @application/state)
+             (error-window/create (ex-info "MY MESSAGE"
+                                           {:m1 :k1
+                                            :m2 :k2}
+                                           (Throwable. "foobarcause"))))
+ )
