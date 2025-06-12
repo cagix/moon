3bd7c990e15d1ef726ca21125d7945ea3bf8b389
Michael Sappler <michaelsappler@gmail.com>
2025-06-12 22:24:54 +0530

    impl-namespaces

--- src/gdl/application.clj
+++ src/gdl/application.clj
@@ -2,23 +2,23 @@
   (:require [gdx.backends.lwjgl.application :as application]
             [gdx.utils.shared-library-loader :as shared-library-loader]))
 
-(defn- execute! [[f params]]
+(defn execute! [[f params]]
   (f params))
 
-(defn provide-impl-namespace [atype protocol implementation-ns]
-  (let [protocol @(requiring-resolve protocol)
-        method-map (update-vals (:sigs protocol)
-                                (fn [{:keys [name]}]
-                                  (requiring-resolve (symbol (str implementation-ns "/" name)))))]
-    (extend atype protocol method-map)))
+(defn provide-impl-namespace
+  ([[atype implementation-ns protocol]]
+   (let [protocol @(requiring-resolve protocol)
+         method-map (update-vals (:sigs protocol)
+                                 (fn [{:keys [name]}]
+                                   (requiring-resolve (symbol (str implementation-ns "/" name)))))]
+     (extend atype protocol method-map)))
+  ([impls]
+   (run! provide-impl-namespace impls)))
 
 (defn start! [os-config lwjgl3-config listener]
-  (provide-impl-namespace com.badlogic.gdx.Application      'gdl.app/Application                 'gdx.app)
-  (provide-impl-namespace com.badlogic.gdx.Files            'gdl.files/Files                     'gdx.files)
-  (provide-impl-namespace com.badlogic.gdx.Input            'gdl.input/Input                     'gdx.input)
-  (provide-impl-namespace com.badlogic.gdx.utils.Disposable 'gdl.utils.disposable/Disposable     'gdx.utils.disposable)
+  (provide-impl-namespace [com.badlogic.gdx.Application      'gdx.app              'gdl.app/Application            ]
+                          [com.badlogic.gdx.Files            'gdx.files            'gdl.files/Files                ]
+                          [com.badlogic.gdx.Input            'gdx.input            'gdl.input/Input                ]
+                          [com.badlogic.gdx.utils.Disposable 'gdx.utils.disposable 'gdl.utils.disposable/Disposable])
   (run! execute! (get os-config (shared-library-loader/operating-system)))
   (application/start! lwjgl3-config listener))
-
-
-
