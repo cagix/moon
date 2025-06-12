c26c171bff112edb2653293b7aeac102abaca61d
Michael Sappler <michaelsappler@gmail.com>
2025-06-12 17:26:56 +0530

    load vis-ui in gdl.application

--- src/gdl/application.clj
+++ src/gdl/application.clj
@@ -3,7 +3,6 @@
             [clojure.gdx.freetype :as freetype]
             [clojure.gdx.shape-drawer :as sd]
             [clojure.gdx.tiled :as tiled]
-            [clojure.gdx.vis-ui :as vis-ui]
             [clojure.java.io :as io]
             [clojure.string :as str]
             [gdl.audio :as audio]
@@ -25,6 +24,8 @@
            (com.badlogic.gdx.utils Align
                                    SharedLibraryLoader
                                    Os)
+           (com.kotcrab.vis.ui VisUI
+                               VisUI$SkinScale)
            (com.kotcrab.vis.ui.widget Tooltip
                                       VisLabel)
            (gdl.graphics OrthogonalTiledMapRenderer
@@ -33,6 +34,25 @@
            (java.awt Taskbar
                      Toolkit)
            (org.lwjgl.system Configuration)))
+
+(defn- load-vis-ui! [{:keys [skin-scale]}]
+  ; app crashes during startup before VisUI/dispose and we do clojure.tools.namespace.refresh-> gui elements not showing.
+  ; => actually there is a deeper issue at play
+  ; we need to dispose ALL resources which were loaded already ...
+  (when (VisUI/isLoaded)
+    (VisUI/dispose))
+  (VisUI/load (case skin-scale
+                :x1 VisUI$SkinScale/X1
+                :x2 VisUI$SkinScale/X2))
+  (-> (VisUI/getSkin)
+      (.getFont "default-font")
+      .getData
+      .markupEnabled
+      (set! true))
+  ;(set! Tooltip/DEFAULT_FADE_TIME (float 0.3))
+  ;Controls whether to fade out tooltip when mouse was moved. (default false)
+  ;(set! Tooltip/MOUSE_MOVED_FADEOUT true)
+  (set! Tooltip/DEFAULT_APPEAR_DELAY_TIME (float 0)))
 
 (defn- texture-region-drawing-dimensions
   [{:keys [unit-scale
