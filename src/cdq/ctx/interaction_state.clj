214092a1ce5f8ed6a284b2550a4c2d3764ea0133
Michael Sappler <michaelsappler@gmail.com>
2025-06-14 08:32:05 +0530

    decomplect clickable cursor and txs

--- src/cdq/ctx/interaction_state.clj
+++ src/cdq/ctx/interaction_state.clj
@@ -17,39 +17,6 @@
 (defn can-pickup-item? [entity item]
   (inventory/can-pickup-item? (:entity/inventory entity) item))
 
-(defmulti on-clicked
-  (fn [ctx eid]
-    (:type (:entity/clickable @eid))))
-
-(defmethod on-clicked :clickable/item [{:keys [ctx/player-eid
-                                               ctx/stage]}
-                                       eid]
-  (let [item (:entity/item @eid)]
-    (cond
-     (inventory-window-visible? stage)
-     [[:tx/sound "bfxr_takeit"]
-      [:tx/mark-destroyed eid]
-      [:tx/event player-eid :pickup-item item]]
-
-     (can-pickup-item? @player-eid item)
-     [[:tx/sound "bfxr_pickup"]
-      [:tx/mark-destroyed eid]
-      [:tx/pickup-item player-eid item]]
-
-     :else
-     [[:tx/sound "bfxr_denied"]
-      [:tx/show-message "Your Inventory is full"]])))
-
-(defmethod on-clicked :clickable/player [_ctx _eid]
-  [[:tx/toggle-inventory-visible]])
-
-(defn- clickable->cursor [entity & {:keys [too-far-away?]}]
-  (case (:type (:entity/clickable entity))
-    :clickable/item (if too-far-away?
-                      :cursors/hand-before-grab-gray
-                      :cursors/hand-before-grab)
-    :clickable/player :cursors/bag))
-
 (defn distance [a b]
   (v/distance (entity/position a)
               (entity/position b)))
@@ -57,23 +24,6 @@
 (defn in-click-range? [player-entity clicked-entity]
   (< (distance player-entity clicked-entity)
      (:entity/click-distance-tiles player-entity)))
-
-(defn clickable-entity-interaction [ctx player-entity clicked-eid]
-  (if (in-click-range? player-entity @clicked-eid)
-    [(clickable->cursor @clicked-eid :too-far-away? false)
-     (on-clicked ctx clicked-eid)]
-    [(clickable->cursor @clicked-eid :too-far-away? true)
-     [[:tx/sound "bfxr_denied"]
-      [:tx/show-message "Too far away"]]]))
-
-(defn skill-interaction-state [ctx eid skill-id]
-  (let [entity @eid
-        skill (skill-id (:entity/skills entity))
-        effect-ctx (ctx/player-effect-ctx ctx eid)
-        state (entity/skill-usable-state entity skill effect-ctx)]
-    (if (= state :usable)
-      [:interaction-state.skill/usable [skill effect-ctx]]
-      [:interaction-state.skill/not-usable state])))
 
 (defn interaction-state
   [{:keys [ctx/stage
@@ -86,48 +36,96 @@
 
      (and mouseover-eid
           (:entity/clickable @mouseover-eid))
-     [:interaction-state/clickable-mouseover-eid mouseover-eid]
+     [:interaction-state/clickable-mouseover-eid
+      {:clicked-eid mouseover-eid
+       :in-click-range? (in-click-range? @player-eid @mouseover-eid)}]
 
      :else
      (if-let [skill-id (selected-skill stage)]
-       (skill-interaction-state ctx player-eid skill-id)
+       (let [entity @player-eid
+             skill (skill-id (:entity/skills entity))
+             effect-ctx (ctx/player-effect-ctx ctx player-eid)
+             state (entity/skill-usable-state entity skill effect-ctx)]
+         (if (= state :usable)
+           [:interaction-state.skill/usable [skill effect-ctx]]
+           [:interaction-state.skill/not-usable state]))
        [:interaction-state/no-skill-selected]))))
-
-(defn- mouseover-actor->cursor [actor player-entity-inventory]
-  (let [inventory-slot (inventory-window/cell-with-item? actor)]
-    (cond
-     (and inventory-slot
-         (get-in player-entity-inventory inventory-slot)) :cursors/hand-before-grab
-     (ui/window-title-bar? actor) :cursors/move-window
-     (ui/button? actor) :cursors/over-button
-     :else :cursors/default)))
 
 (defn ->cursor [player-eid ctx]
   (let [[k params] (interaction-state ctx player-eid)]
     (case k
-      :interaction-state/mouseover-actor (let [mouseover-actor params]
-                                           (mouseover-actor->cursor mouseover-actor (:entity/inventory @player-eid)))
-      :interaction-state/clickable-mouseover-eid (let [mouseover-eid params
-                                                       [cursor _txs] (clickable-entity-interaction ctx @player-eid mouseover-eid)]
-                                                   cursor)
-      :interaction-state.skill/usable :cursors/use-skill
-      :interaction-state.skill/not-usable :cursors/skill-not-usable
-      :interaction-state/no-skill-selected :cursors/no-skill-selected)))
+      :interaction-state/mouseover-actor
+      (let [actor params]
+        (let [inventory-slot (inventory-window/cell-with-item? actor)]
+          (cond
+           (and inventory-slot
+                (get-in (:entity/inventory @player-eid) inventory-slot)) :cursors/hand-before-grab
+           (ui/window-title-bar? actor) :cursors/move-window
+           (ui/button? actor) :cursors/over-button
+           :else :cursors/default)))
+
+      :interaction-state/clickable-mouseover-eid
+      (let [{:keys [clicked-eid
+                    in-click-range?]} params]
+        (case (:type (:entity/clickable @clicked-eid))
+          :clickable/item (if in-click-range?
+                            :cursors/hand-before-grab
+                            :cursors/hand-before-grab-gray)
+          :clickable/player :cursors/bag))
+
+      :interaction-state.skill/usable
+      :cursors/use-skill
+
+      :interaction-state.skill/not-usable
+      :cursors/skill-not-usable
+
+      :interaction-state/no-skill-selected
+      :cursors/no-skill-selected)))
 
 (defn ->txs [ctx player-eid]
   (let [[k params] (interaction-state ctx player-eid)]
     (case k
       :interaction-state/mouseover-actor nil ; handled by ui actors themself.
-      :interaction-state/clickable-mouseover-eid (let [mouseover-eid params
-                                                       [_cursor txs] (clickable-entity-interaction ctx @player-eid mouseover-eid)]
-                                                   txs)
-      :interaction-state.skill/usable (let [[skill effect-ctx] params]
-                                        [[:tx/event player-eid :start-action [skill effect-ctx]]])
-      :interaction-state.skill/not-usable (let [state params]
-                                            [[:tx/sound "bfxr_denied"]
-                                             [:tx/show-message (case state
-                                                                 :cooldown "Skill is still on cooldown"
-                                                                 :not-enough-mana "Not enough mana"
-                                                                 :invalid-params "Cannot use this here")]])
-      :interaction-state/no-skill-selected [[:tx/sound "bfxr_denied"]
-                                            [:tx/show-message "No selected skill"]])))
+
+      :interaction-state/clickable-mouseover-eid
+      (let [{:keys [clicked-eid
+                    in-click-range?]} params]
+        (if in-click-range?
+          (case (:type (:entity/clickable @clicked-eid))
+            :clickable/player
+            [[:tx/toggle-inventory-visible]]
+
+            :clickable/item
+            (let [item (:entity/item @clicked-eid)]
+              (cond
+               (inventory-window-visible? (:ctx/stage ctx))
+               [[:tx/sound "bfxr_takeit"]
+                [:tx/mark-destroyed clicked-eid]
+                [:tx/event player-eid :pickup-item item]]
+
+               (can-pickup-item? @player-eid item)
+               [[:tx/sound "bfxr_pickup"]
+                [:tx/mark-destroyed clicked-eid]
+                [:tx/pickup-item player-eid item]]
+
+               :else
+               [[:tx/sound "bfxr_denied"]
+                [:tx/show-message "Your Inventory is full"]])))
+          [[:tx/sound "bfxr_denied"]
+           [:tx/show-message "Too far away"]]))
+
+      :interaction-state.skill/usable
+      (let [[skill effect-ctx] params]
+        [[:tx/event player-eid :start-action [skill effect-ctx]]])
+
+      :interaction-state.skill/not-usable
+      (let [state params]
+        [[:tx/sound "bfxr_denied"]
+         [:tx/show-message (case state
+                             :cooldown "Skill is still on cooldown"
+                             :not-enough-mana "Not enough mana"
+                             :invalid-params "Cannot use this here")]])
+
+      :interaction-state/no-skill-selected
+      [[:tx/sound "bfxr_denied"]
+       [:tx/show-message "No selected skill"]])))
