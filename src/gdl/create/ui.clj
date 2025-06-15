8804bb5a97e5335f7fb2b327dfdfa0b0f59e05b1
Michael Sappler <michaelsappler@gmail.com>
2025-06-15 11:26:37 +0530

    gdx.ui/load!

--- src/gdl/create/ui.clj
+++ src/gdl/create/ui.clj
@@ -4,34 +4,13 @@
             [gdl.ui.group :as group]
             [gdl.ui.stage]
             [gdl.ui.table]
+            [gdx.ui]
             [gdx.ui.table.cell :as cell])
-  (:import (com.kotcrab.vis.ui VisUI
-                               VisUI$SkinScale)
-           (com.kotcrab.vis.ui.widget Tooltip)
-           (gdl.ui CtxStage)))
-
-(defn- load-vis-ui! [{:keys [skin-scale]}]
-  ; app crashes during startup before VisUI/dispose and we do clojure.tools.namespace.refresh-> gui elements not showing.
-  ; => actually there is a deeper issue at play
-  ; we need to dispose ALL resources which were loaded already ...
-  (when (VisUI/isLoaded)
-    (VisUI/dispose))
-  (VisUI/load (case skin-scale
-                :x1 VisUI$SkinScale/X1
-                :x2 VisUI$SkinScale/X2))
-  (-> (VisUI/getSkin)
-      (.getFont "default-font")
-      .getData
-      .markupEnabled
-      (set! true))
-  ;(set! Tooltip/DEFAULT_FADE_TIME (float 0.3))
-  ;Controls whether to fade out tooltip when mouse was moved. (default false)
-  ;(set! Tooltip/MOUSE_MOVED_FADEOUT true)
-  (set! Tooltip/DEFAULT_APPEAR_DELAY_TIME (float 0)))
+  (:import (gdl.ui CtxStage)))
 
 (defn do! [{:keys [ctx/graphics
                    ctx/input] :as ctx} params]
-  (load-vis-ui! params)
+  (gdx.ui/load! params)
   (let [stage (proxy [CtxStage clojure.lang.ILookup] [(:ui-viewport graphics)
                                                       (:batch graphics)
                                                       (atom nil)]
