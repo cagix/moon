97824beedad8022be9ecccbad5fc8ce890f6231d
Michael Sappler <michaelsappler@gmail.com>
2025-08-22 20:15:52 +0530

    Remove cdq.create.player-movement-vector/do

--- src/cdq/controls.clj
+++ src/cdq/controls.clj
@@ -1,4 +1,15 @@
-(ns cdq.controls)
+(ns cdq.controls
+  (:require [clojure.input :as input]
+            [gdl.math.vector2 :as v]))
 
-(defprotocol PlayerMovementInput
-  (player-movement-vector [_]))
+(defn- WASD-movement-vector [{:keys [ctx/input]}]
+  (let [r (when (input/key-pressed? input :d) [1  0])
+        l (when (input/key-pressed? input :a) [-1 0])
+        u (when (input/key-pressed? input :w) [0  1])
+        d (when (input/key-pressed? input :s) [0 -1])]
+    (when (or r l u d)
+      (let [v (v/add-vs (remove nil? [r l u d]))]
+        (when (pos? (v/length v))
+          v)))))
+
+(def player-movement-vector WASD-movement-vector)
