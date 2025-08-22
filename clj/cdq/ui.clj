be56e5b97cd392573a1309286dc9c1340dbd9335
Michael Sappler <michaelsappler@gmail.com>
2025-08-22 18:47:49 +0530

    Remove Stage protocol

--- clj/cdq/ui.clj
+++ clj/cdq/ui.clj
@@ -1,13 +1,71 @@
-(ns cdq.ui)
+(ns cdq.ui
+  (:require [cdq.ui.action-bar :as action-bar]
+            [cdq.ui.windows.inventory :as inventory-window]
+            [cdq.ui.message]
+            [gdl.ui.actor :as actor]
+            [gdl.ui.group :as group]
+            [gdl.ui.stage :as stage]
+            [gdx.ui :as ui]))
 
-(defprotocol Stage
-  (action-bar-selected-skill [_])
-  (add-action-bar-skill! [_ item-opts])
-  (remove-action-bar-skill! [_ id])
-  (set-inventory-item! [_ inventory-cell item-opts])
-  (remove-inventory-item! [_ inventory-cell])
-  (show-player-ui-msg! [_ message])
-  (show-modal-window! [_ ui-viewport {:keys [title text button-text on-click]}])
-  (toggle-inventory-visible! [_])
-  (toggle-entity-info-window! [_])
-  (close-all-windows! [_]))
+(defn action-bar-selected-skill [stage]
+  (-> stage
+      :action-bar
+      action-bar/selected-skill))
+
+(defn add-action-bar-skill! [stage item-opts]
+  (-> stage
+      :action-bar
+      (action-bar/add-skill! item-opts)))
+
+(defn remove-action-bar-skill! [stage id]
+  (-> stage
+      :action-bar
+      (action-bar/remove-skill! id)))
+
+(defn set-inventory-item! [stage inventory-cell item-opts]
+  (-> stage
+      :windows
+      :inventory-window
+      (inventory-window/set-item! inventory-cell item-opts)))
+
+(defn remove-inventory-item! [stage inventory-cell]
+  (-> stage
+      :windows
+      :inventory-window
+      (inventory-window/remove-item! inventory-cell)))
+
+(defn show-player-ui-msg! [stage message]
+  (-> stage
+      (stage/find-actor "player-message")
+      (cdq.ui.message/show! message)))
+
+; no window movable type cursor appears here like in player idle
+; inventory still working, other stuff not, because custom listener to keypresses ? use actor listeners?
+; => input events handling
+; hmmm interesting ... can disable @ item in cursor  / moving / etc.
+(defn show-modal-window! [stage
+                          ui-viewport
+                          {:keys [title text button-text on-click]}]
+  (assert (not (::modal stage)))
+  (stage/add! stage
+              (ui/window {:title title
+                          :rows [[{:actor {:actor/type :actor.type/label
+                                           :label/text text}}]
+                                 [(ui/text-button button-text
+                                                  (fn [_actor _ctx]
+                                                    (actor/remove! (::modal stage))
+                                                    (on-click)))]]
+                          :id ::modal
+                          :modal? true
+                          :center-position [(/ (:viewport/width  ui-viewport) 2)
+                                            (* (:viewport/height ui-viewport) (/ 3 4))]
+                          :pack? true})))
+
+(defn toggle-inventory-visible! [stage]
+  (-> stage :windows :inventory-window actor/toggle-visible!))
+
+(defn toggle-entity-info-window! [stage]
+  (-> stage :windows :entity-info-window actor/toggle-visible!))
+
+(defn close-all-windows! [stage]
+  (run! #(actor/set-visible! % false) (group/children (:windows stage))))
