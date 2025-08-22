5a0bab3cdb5ccdb4e37dc779fc3f221e5426401b
Michael Sappler <michaelsappler@gmail.com>
2025-08-22 19:41:15 +0530

    Remove gdl.ui.stage/Stage protocol

--- src/gdl/ui/stage.clj
+++ src/gdl/ui/stage.clj
@@ -1,8 +1,25 @@
-(ns gdl.ui.stage)
+(ns gdl.ui.stage
+  (:require [gdx.ui.actor :as actor]
+            [gdx.ui.group :as group])
+  (:import (gdl.ui CtxStage)))
 
-(defprotocol Stage
-  (render! [_ ctx])
-  (add! [_ actor-or-decl])
-  (clear! [_])
-  (hit [_ position])
-  (find-actor [_ actor-name]))
+; FIXME outdated `.ctx` in the click/etc. listeners as they are processed outside `.act`
+(defn render! [^CtxStage stage ctx]
+  (reset! (.ctx stage) ctx)
+  (.act  stage)
+  (.draw stage)
+  ctx)
+
+(defn add! [^CtxStage stage actor-or-decl]
+  (.addActor stage (actor/construct? actor-or-decl)))
+
+(defn clear! [^CtxStage stage]
+  (.clear stage))
+
+(defn hit [^CtxStage stage [x y]]
+  (.hit stage x y true))
+
+(defn find-actor [^CtxStage stage actor-name]
+  (-> stage
+      .getRoot
+      (group/find-actor actor-name)))
