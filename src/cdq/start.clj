696d57708d391d5ac9932fc973f4541c6e993758
Michael Sappler <michaelsappler@gmail.com>
2025-08-29 01:49:09 +0530

    Move application state side effects up in -main

--- src/cdq/start.clj
+++ src/cdq/start.clj
@@ -182,106 +182,102 @@
                       ctx/world])
 
 (defn- create! [{:keys [audio files graphics input]}]
-  (reset! cdq.application/state (let [graphics (graphics/create
-                                                graphics
-                                                files
-                                                {:colors [["PRETTY_NAME" [0.84 0.8 0.52 1]]]
-                                                 :textures (cdq.assets/search files
-                                                                              {:folder "resources/"
-                                                                               :extensions #{"png" "bmp"}})
-                                                 :tile-size 48
-                                                 :ui-viewport    {:width 1440 :height 900}
-                                                 :world-viewport {:width 1440 :height 900}
-                                                 :cursor-path-format "cursors/%s.png"
-                                                 :cursors {:cursors/bag                   ["bag001"       [0   0]]
-                                                           :cursors/black-x               ["black_x"      [0   0]]
-                                                           :cursors/default               ["default"      [0   0]]
-                                                           :cursors/denied                ["denied"       [16 16]]
-                                                           :cursors/hand-before-grab      ["hand004"      [4  16]]
-                                                           :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
-                                                           :cursors/hand-grab             ["hand003"      [4  16]]
-                                                           :cursors/move-window           ["move002"      [16 16]]
-                                                           :cursors/no-skill-selected     ["denied003"    [0   0]]
-                                                           :cursors/over-button           ["hand002"      [0   0]]
-                                                           :cursors/sandclock             ["sandclock"    [16 16]]
-                                                           :cursors/skill-not-usable      ["x007"         [0   0]]
-                                                           :cursors/use-skill             ["pointer004"   [0   0]]
-                                                           :cursors/walking               ["walking"      [16 16]]}
-                                                 :default-font {:file "exocet/films.EXL_____.ttf"
-                                                                :params {:size 16
-                                                                         :quality-scaling 2
-                                                                         :enable-markup? true
-                                                                         ; false, otherwise scaling to world-units not visible
-                                                                         :use-integer-positions? false}}})
-                                      ctx (map->Context {:app (cdq.app/create
-                                                               {:schema [:map {:closed true}
-                                                                         [:ctx/app :some]
-                                                                         [:ctx/files :some]
-                                                                         [:ctx/config :some]
-                                                                         [:ctx/input :some]
-                                                                         [:ctx/db :some]
-                                                                         [:ctx/audio :some]
-                                                                         [:ctx/stage :some]
-                                                                         [:ctx/graphics :some]
-                                                                         [:ctx/world :some]]
-                                                                :stacktraces {:print-level 3
-                                                                              :print-depth 24}})
-                                                         :audio  (cdq.audio/create audio files {:sounds "sounds.edn"})
-                                                         :config app-config
-                                                         :db (cdq.create.db/do! {:schemas "schema.edn"
-                                                                                 :properties "properties.edn"})
-                                                         :files    files
-                                                         :graphics graphics
-                                                         :input    input
-                                                         :stage (cdq.create.ui/do! graphics input {:skin-scale :x1})})]
-                                  (-> ctx
-                                      (cdq.ctx/reset-game-state! [(requiring-resolve 'cdq.level.from-tmx/create)
-                                                                  {:tmx-file "maps/vampire.tmx"
-                                                                   :start-position [32 71]}])
-                                      cdq.app/validate))))
+  (let [graphics (graphics/create
+                  graphics
+                  files
+                  {:colors [["PRETTY_NAME" [0.84 0.8 0.52 1]]]
+                   :textures (cdq.assets/search files
+                                                {:folder "resources/"
+                                                 :extensions #{"png" "bmp"}})
+                   :tile-size 48
+                   :ui-viewport    {:width 1440 :height 900}
+                   :world-viewport {:width 1440 :height 900}
+                   :cursor-path-format "cursors/%s.png"
+                   :cursors {:cursors/bag                   ["bag001"       [0   0]]
+                             :cursors/black-x               ["black_x"      [0   0]]
+                             :cursors/default               ["default"      [0   0]]
+                             :cursors/denied                ["denied"       [16 16]]
+                             :cursors/hand-before-grab      ["hand004"      [4  16]]
+                             :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
+                             :cursors/hand-grab             ["hand003"      [4  16]]
+                             :cursors/move-window           ["move002"      [16 16]]
+                             :cursors/no-skill-selected     ["denied003"    [0   0]]
+                             :cursors/over-button           ["hand002"      [0   0]]
+                             :cursors/sandclock             ["sandclock"    [16 16]]
+                             :cursors/skill-not-usable      ["x007"         [0   0]]
+                             :cursors/use-skill             ["pointer004"   [0   0]]
+                             :cursors/walking               ["walking"      [16 16]]}
+                   :default-font {:file "exocet/films.EXL_____.ttf"
+                                  :params {:size 16
+                                           :quality-scaling 2
+                                           :enable-markup? true
+                                           ; false, otherwise scaling to world-units not visible
+                                           :use-integer-positions? false}}})
+        ctx (map->Context {:app (cdq.app/create
+                                 {:schema [:map {:closed true}
+                                           [:ctx/app :some]
+                                           [:ctx/files :some]
+                                           [:ctx/config :some]
+                                           [:ctx/input :some]
+                                           [:ctx/db :some]
+                                           [:ctx/audio :some]
+                                           [:ctx/stage :some]
+                                           [:ctx/graphics :some]
+                                           [:ctx/world :some]]
+                                  :stacktraces {:print-level 3
+                                                :print-depth 24}})
+                           :audio  (cdq.audio/create audio files {:sounds "sounds.edn"})
+                           :config app-config
+                           :db (cdq.create.db/do! {:schemas "schema.edn"
+                                                   :properties "properties.edn"})
+                           :files    files
+                           :graphics graphics
+                           :input    input
+                           :stage (cdq.create.ui/do! graphics input {:skin-scale :x1})})]
+    (-> ctx
+        (cdq.ctx/reset-game-state! [(requiring-resolve 'cdq.level.from-tmx/create)
+                                    {:tmx-file "maps/vampire.tmx"
+                                     :start-position [32 71]}])
+        cdq.app/validate)))
 
 ; TODO call dispose! on all components
-(defn- dispose! []
-  (let [{:keys [ctx/audio
-                ctx/graphics
-                ctx/world]} @cdq.application/state]
-    (Disposable/.dispose audio)
-    (Disposable/.dispose graphics)
-    (Disposable/.dispose (:world/tiled-map world))
-    ; TODO vis-ui dispose
-    ; TODO what else disposable?
-    ; => :ctx/tiled-map definitely and also dispose when re-creting gamestate.
-    ))
+(defn- dispose! [{:keys [ctx/audio
+                         ctx/graphics
+                         ctx/world]}]
+  (Disposable/.dispose audio)
+  (Disposable/.dispose graphics)
+  (Disposable/.dispose (:world/tiled-map world))
+  ; TODO vis-ui dispose
+  ; TODO what else disposable?
+  ; => :ctx/tiled-map definitely and also dispose when re-creting gamestate.
+  )
 
 ; TODO call resize! on all components
-(defn- resize! [width height]
-  (let [{:keys [ctx/graphics]} @cdq.application/state]
-    (graphics/resize-viewports! graphics width height)))
+(defn- resize! [{:keys [ctx/graphics]} width height]
+  (graphics/resize-viewports! graphics width height))
 
-(defn- render! []
-  (swap! cdq.application/state
-         (fn [ctx]
-           (reduce (fn [ctx f] (f ctx))
-                   (-> ctx
-                       cdq.app/validate
-                       cdq.app/run-runnables!)
-                   (map requiring-resolve
-                        '[cdq.render.debug-data-view/do!
-                          cdq.render.assoc-active-entities/do!
-                          cdq.render.set-camera-on-player/do!
-                          cdq.render.clear-screen/do!
-                          cdq.render.draw-world-map/do!
-                          cdq.render.draw-on-world-viewport/do!
-                          cdq.render.stage/do!
-                          cdq.render.set-cursor/do!
-                          cdq.render.player-state-handle-input/do!
-                          cdq.render.update-mouseover-entity/do!
-                          cdq.render.assoc-paused/do!
-                          cdq.render.tick-world/do!
-                          cdq.render.remove-destroyed-entities/do! ; do not pause as pickup item should be destroyed
-                          cdq.render.camera-controls/do!
-                          cdq.render.check-window-hotkeys/do!
-                          cdq.app/validate])))))
+(defn- render! [ctx]
+  (reduce (fn [ctx f] (f ctx))
+          (-> ctx
+              cdq.app/validate
+              cdq.app/run-runnables!)
+          (map requiring-resolve
+               '[cdq.render.debug-data-view/do!
+                 cdq.render.assoc-active-entities/do!
+                 cdq.render.set-camera-on-player/do!
+                 cdq.render.clear-screen/do!
+                 cdq.render.draw-world-map/do!
+                 cdq.render.draw-on-world-viewport/do!
+                 cdq.render.stage/do!
+                 cdq.render.set-cursor/do!
+                 cdq.render.player-state-handle-input/do!
+                 cdq.render.update-mouseover-entity/do!
+                 cdq.render.assoc-paused/do!
+                 cdq.render.tick-world/do!
+                 cdq.render.remove-destroyed-entities/do! ; do not pause as pickup item should be destroyed
+                 cdq.render.camera-controls/do!
+                 cdq.render.check-window-hotkeys/do!
+                 cdq.app/validate])))
 
 (defn -main []
   (install-effects!)
@@ -291,7 +287,11 @@
   (lwjgl/start-application! {:title "Cyber Dungeon Quest"
                              :windowed-mode {:width 1440 :height 900}
                              :foreground-fps 60}
-                            {:create! create!
-                             :dispose! dispose!
-                             :render! render!
-                             :resize! resize!}))
+                            {:create! (fn [context]
+                                        (reset! cdq.application/state (create! context)))
+                             :dispose! (fn []
+                                         (dispose! @cdq.application/state))
+                             :render! (fn []
+                                        (swap! cdq.application/state render!))
+                             :resize! (fn [width height]
+                                        (resize! @cdq.application/state width height))}))
