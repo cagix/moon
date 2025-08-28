9dbc6540294ae71e9363f89080162be3d16df45a
Michael Sappler <michaelsappler@gmail.com>
2025-08-28 22:54:49 +0530

    entity->tick to clojure

--- src/cdq/render/tick_entities.clj
+++ src/cdq/render/tick_entities.clj
@@ -3,9 +3,39 @@
             [cdq.entity :as entity]
             [cdq.ui.error-window :as error-window]
             [cdq.app :as app]
-            [cdq.ui.stage :as stage]))
+            [cdq.ui.stage :as stage]
+            cdq.entity.alert-friendlies-after-duration
+            cdq.entity.animation
+            cdq.entity.delete-after-animation-stopped
+            cdq.entity.delete-after-duration
+            cdq.entity.movement
+            cdq.entity.projectile-collision
+            cdq.entity.skills
+            cdq.entity.state.active-skill
+            cdq.entity.state.npc-idle
+            cdq.entity.state.npc-moving
+            cdq.entity.state.npc-sleeping
+            cdq.entity.state.stunned
+            cdq.entity.string-effect
+            cdq.entity.temp-modifier))
 
-(defn- tick-entity! [{:keys [ctx/world] :as ctx} eid entity->tick]
+(def ^:private entity->tick
+  {:entity/alert-friendlies-after-duration cdq.entity.alert-friendlies-after-duration/tick!
+   :entity/animation cdq.entity.animation/tick!
+   :entity/delete-after-animation-stopped? cdq.entity.delete-after-animation-stopped/tick!
+   :entity/delete-after-duration cdq.entity.delete-after-duration/tick!
+   :entity/movement cdq.entity.movement/tick!
+   :entity/projectile-collision cdq.entity.projectile-collision/tick!
+   :entity/skills cdq.entity.skills/tick!
+   :active-skill cdq.entity.state.active-skill/tick!
+   :npc-idle cdq.entity.state.npc-idle/tick!
+   :npc-moving cdq.entity.state.npc-moving/tick!
+   :npc-sleeping cdq.entity.state.npc-sleeping/tick!
+   :stunned cdq.entity.state.stunned/tick!
+   :entity/string-effect cdq.entity.string-effect/tick!
+   :entity/temp-modifier cdq.entity.temp-modifier/tick!})
+
+(defn- tick-entity! [{:keys [ctx/world] :as ctx} eid]
   (doseq [k (keys @eid)]
     (try (when-let [v (k @eid)]
            (ctx/handle-txs! ctx (when-let [f (entity->tick k)]
@@ -20,11 +50,10 @@
   [{:keys [ctx/app
            ctx/stage
            ctx/world]
-    :as ctx}
-   entity->tick]
+    :as ctx}]
   (try
    (doseq [eid (:world/active-entities world)]
-     (tick-entity! ctx eid entity->tick))
+     (tick-entity! ctx eid))
    (catch Throwable t
      (app/pretty-pst app t)
      (stage/add! stage (error-window/create t))
