c8fe89aded1decae0fc5c970b82f4d1ea8e5ae94
Michael Sappler <michaelsappler@gmail.com>
2025-08-28 22:30:08 +0530

    Upfactor state->handle-input

--- src/cdq/render/player_state_handle_input.clj
+++ src/cdq/render/player_state_handle_input.clj
@@ -1,9 +1,16 @@
 (ns cdq.render.player-state-handle-input
-  (:require [cdq.ctx :as ctx]))
+  (:require [cdq.ctx :as ctx]
+            cdq.entity.state.player-idle
+            cdq.entity.state.player-item-on-cursor
+            cdq.entity.state.player-moving))
+
+(def ^:private state->handle-input
+  {:player-idle           cdq.entity.state.player-idle/handle-input
+   :player-item-on-cursor cdq.entity.state.player-item-on-cursor/handle-input
+   :player-moving         cdq.entity.state.player-moving/handle-input})
 
 (defn do! [{:keys [ctx/world]
-            :as ctx}
-           {:keys [state->handle-input]}]
+            :as ctx}]
   (let [player-eid (:world/player-eid world)
         handle-input (state->handle-input (:state (:entity/fsm @player-eid)))
         txs (if handle-input
