0c7529a29245005ce7728d07aa8d0987cc31f7c5
Michael Sappler <michaelsappler@gmail.com>
2025-08-29 16:11:10 +0530

    move world/create to world

--- src/cdq/ctx/game.clj
+++ src/cdq/ctx/game.clj
@@ -1,10 +1,6 @@
 (ns cdq.ctx.game
   (:require [cdq.ctx :as ctx]
-            [cdq.content-grid :as content-grid]
             [cdq.db :as db]
-            [cdq.grid2d :as g2d]
-            [cdq.grid-impl :as grid-impl]
-            [cdq.raycaster :as raycaster]
             [cdq.utils :as utils]
             [cdq.utils.tiled :as tiled]
             [cdq.world :as w]
@@ -17,22 +13,24 @@
             cdq.entity.state.player-item-on-cursor
             cdq.ui.player-state-draw
             cdq.ui.message
-            [cdq.ui.stage :as stage]
-            [qrecord.core :as q]))
+            [cdq.ui.stage :as stage]))
 
 (defn- create-ui-actors [ctx]
   [(cdq.ui.dev-menu/create ctx ;graphics db
-                           {:world-fns '[[cdq.level.from-tmx/create {:tmx-file "maps/vampire.tmx"
-                                                                     :start-position [32 71]}]
-                                         [cdq.level.uf-caves/create {:tile-size 48
-                                                                     :texture "maps/uf_terrain.png"
-                                                                     :spawn-rate 0.02
-                                                                     :scaling 3
-                                                                     :cave-size 200
-                                                                     :cave-style :wide}]
-                                         [cdq.level.modules/create {:world/map-size 5,
-                                                                    :world/max-area-level 3,
-                                                                    :world/spawn-rate 0.05}]]
+                           {:world-fns [[(requiring-resolve 'cdq.level.from-tmx/create)
+                                         {:tmx-file "maps/vampire.tmx"
+                                          :start-position [32 71]}]
+                                        [(requiring-resolve 'cdq.level.uf-caves/create)
+                                         {:tile-size 48
+                                          :texture "maps/uf_terrain.png"
+                                          :spawn-rate 0.02
+                                          :scaling 3
+                                          :cave-size 200
+                                          :cave-style :wide}]
+                                        [(requiring-resolve 'cdq.level.modules/create)
+                                         {:world/map-size 5,
+                                          :world/max-area-level 3,
+                                          :world/spawn-rate 0.05}]]
                             ;icons, etc. , components ....
                             :info "[W][A][S][D] - Move\n[I] - Inventory window\n[E] - Entity Info window\n[-]/[=] - Zoom\n[P]/[SPACE] - Unpause"})
     (cdq.ui.action-bar/create {:id :action-bar}) ; padding.... !, etc.
@@ -71,80 +69,13 @@
     (stage/add! stage actor))
   ctx)
 
-(defn- create-explored-tile-corners [tiled-map]
-  (atom (g2d/create-grid (:tiled-map/width  tiled-map)
-                         (:tiled-map/height tiled-map)
-                         (constantly false))))
-
-(q/defrecord World [world/tiled-map
-                    world/start-position
-                    world/grid
-                    world/explored-tile-corners
-                    world/content-grid
-                    world/raycaster
-                    world/potential-field-cache
-                    world/factions-iterations
-                    world/id-counter
-                    world/entity-ids
-                    world/elapsed-time
-                    world/max-delta
-                    world/max-speed
-                    world/minimum-size
-                    world/z-orders
-                    world/render-z-order
-
-                    ; added later
-                    world/delta-time
-                    world/paused?
-                    world/active-entities
-                    world/mouseover-eid
-                    world/player-eid])
-
-(defn- create-world
-  [{:keys [tiled-map
-           start-position]
-    :as config}]
-  (let [grid (grid-impl/create tiled-map)
-        z-orders [:z-order/on-ground
-                  :z-order/ground
-                  :z-order/flying
-                  :z-order/effect]
-        ; so that at low fps the game doesn't jump faster between frames used @ movement to set a max speed so entities don't jump over other entities when checking collisions
-        max-delta 0.04
-        ; setting a min-size for colliding bodies so movement can set a max-speed for not
-        ; skipping bodies at too fast movement
-        ; TODO assert at properties load
-        minimum-size 0.39 ; == spider smallest creature size.
-        ; set max speed so small entities are not skipped by projectiles
-        ; could set faster than max-speed if I just do multiple smaller movement steps in one frame
-        max-speed (/ minimum-size max-delta)]
-    (merge (map->World {})
-           {:world/tiled-map tiled-map
-            :world/start-position start-position
-            :world/grid grid
-            :world/explored-tile-corners (create-explored-tile-corners tiled-map)
-            :world/content-grid (content-grid/create (:tiled-map/width  tiled-map)
-                                                     (:tiled-map/height tiled-map)
-                                                     (:content-grid-cell-size config))
-            :world/raycaster (raycaster/create grid)
-            :world/potential-field-cache (atom nil)
-            :world/factions-iterations (:potential-field-factions-iterations config)
-            :world/id-counter (atom 0)
-            :world/entity-ids (atom {})
-            :world/elapsed-time 0
-            :world/max-delta max-delta
-            :world/max-speed max-speed
-            :world/minimum-size minimum-size
-            :world/z-orders z-orders
-            :world/render-z-order (utils/define-order z-orders)})))
-
 (defn- add-ctx-world
   [{:keys [ctx/config]
     :as ctx}
    world-fn]
-  (assoc ctx :ctx/world (create-world (merge (:cdq.ctx.game/world config)
-                                             (let [[f params] world-fn]
-                                               (f ctx params))))))
+  (assoc ctx :ctx/world (w/create (merge (:cdq.ctx.game/world config)
+                                         (let [[f params] world-fn]
+                                           (f ctx params))))))
 
 (defn- spawn-player!
   [{:keys [ctx/config
--- src/cdq/world.clj
+++ src/cdq/world.clj
@@ -3,6 +3,8 @@
             [cdq.content-grid :as content-grid]
             [cdq.entity :as entity]
             [cdq.grid :as grid]
+            [cdq.grid-impl :as grid-impl]
+            [cdq.grid2d :as g2d]
             [cdq.raycaster :as raycaster]
             [cdq.malli :as m]
             [cdq.math.vector2 :as v]
@@ -189,3 +191,70 @@
                                                                (:entity/body creature-property)))
                      (assoc :entity/destroy-audiovisual :audiovisuals/creature-die)
                      (utils/safe-merge components))))
+
+(defn- create-explored-tile-corners [tiled-map]
+  (atom (g2d/create-grid (:tiled-map/width  tiled-map)
+                         (:tiled-map/height tiled-map)
+                         (constantly false))))
+
+(q/defrecord World [world/tiled-map
+                    world/start-position
+                    world/grid
+                    world/explored-tile-corners
+                    world/content-grid
+                    world/raycaster
+                    world/potential-field-cache
+                    world/factions-iterations
+                    world/id-counter
+                    world/entity-ids
+                    world/elapsed-time
+                    world/max-delta
+                    world/max-speed
+                    world/minimum-size
+                    world/z-orders
+                    world/render-z-order
+
+                    ; added later
+                    world/delta-time
+                    world/paused?
+                    world/active-entities
+                    world/mouseover-eid
+                    world/player-eid])
+
+(defn create
+  [{:keys [tiled-map
+           start-position]
+    :as config}]
+  (let [grid (grid-impl/create tiled-map)
+        z-orders [:z-order/on-ground
+                  :z-order/ground
+                  :z-order/flying
+                  :z-order/effect]
+        ; so that at low fps the game doesn't jump faster between frames used @ movement to set a max speed so entities don't jump over other entities when checking collisions
+        max-delta 0.04
+        ; setting a min-size for colliding bodies so movement can set a max-speed for not
+        ; skipping bodies at too fast movement
+        ; TODO assert at properties load
+        minimum-size 0.39 ; == spider smallest creature size.
+        ; set max speed so small entities are not skipped by projectiles
+        ; could set faster than max-speed if I just do multiple smaller movement steps in one frame
+        max-speed (/ minimum-size max-delta)]
+    (merge (map->World {})
+           {:world/tiled-map tiled-map
+            :world/start-position start-position
+            :world/grid grid
+            :world/explored-tile-corners (create-explored-tile-corners tiled-map)
+            :world/content-grid (content-grid/create (:tiled-map/width  tiled-map)
+                                                     (:tiled-map/height tiled-map)
+                                                     (:content-grid-cell-size config))
+            :world/raycaster (raycaster/create grid)
+            :world/potential-field-cache (atom nil)
+            :world/factions-iterations (:potential-field-factions-iterations config)
+            :world/id-counter (atom 0)
+            :world/entity-ids (atom {})
+            :world/elapsed-time 0
+            :world/max-delta max-delta
+            :world/max-speed max-speed
+            :world/minimum-size minimum-size
+            :world/z-orders z-orders
+            :world/render-z-order (utils/define-order z-orders)})))
