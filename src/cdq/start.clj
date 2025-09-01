549352a15d758c874fa0a43bd3f784505bdfaa4f
Michael Sappler <michaelsappler@gmail.com>
2025-09-02 00:02:00 +0530

    cdq.start.edn

--- src/cdq/start.clj
+++ src/cdq/start.clj
@@ -1,17 +1,13 @@
 (ns cdq.start
-  (:require cdq.start.install-entity-components
-            cdq.start.install-effects
-            cdq.start.install-txs
-            cdq.start.install-editor-widgets
-            cdq.start.set-icon
-            cdq.start.gdx-app)
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io])
   (:gen-class))
 
 (defn -main []
-  (doseq [f [cdq.start.install-entity-components/do!
-             cdq.start.install-effects/do!
-             cdq.start.install-txs/do!
-             cdq.start.install-editor-widgets/do!
-             cdq.start.set-icon/do!
-             cdq.start.gdx-app/do!]]
-    (f)))
+  (let [fns (->> "cdq.start.edn"
+                 io/resource
+                 slurp
+                 edn/read-string
+                 (map requiring-resolve))]
+    (doseq [f fns]
+      (f))))
