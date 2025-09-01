7405bb6f55e18e4a27e34781dda839446d2ce00d
Michael Sappler <michaelsappler@gmail.com>
2025-09-02 04:40:15 +0530

    Move interaction state to player idle

--- src/cdq/entity/state/player_idle.clj
+++ src/cdq/entity/state/player_idle.clj
@@ -1,9 +1,87 @@
 (ns cdq.entity.state.player-idle
-  (:require [cdq.controls :as controls]
-            [cdq.interaction-state]
+  (:require [cdq.ctx.input :as input]
+            [cdq.ctx.stage :as stage]
+            [cdq.controls :as controls]
+            [cdq.gdx.math.vector2 :as v]
             [cdq.inventory :as inventory]
-            [cdq.ctx.input :as input]
-            [cdq.ui.actor :as actor]))
+            [cdq.ui.actor :as actor]
+            [cdq.ui.windows.inventory :as inventory-window]
+            [cdq.world.entity :as entity]))
+
+(defn distance [a b]
+  (v/distance (entity/position a)
+              (entity/position b)))
+
+(defn in-click-range? [player-entity clicked-entity]
+  (< (distance player-entity clicked-entity)
+     (:entity/click-distance-tiles player-entity)))
+
+(defn- player-effect-ctx [mouseover-eid world-mouse-position player-eid]
+  (let [target-position (or (and mouseover-eid
+                                 (entity/position @mouseover-eid))
+                            world-mouse-position)]
+    {:effect/source player-eid
+     :effect/target mouseover-eid
+     :effect/target-position target-position
+     :effect/target-direction (v/direction (entity/position @player-eid) target-position)}))
+
+(defn interaction-state
+  [{:keys [ctx/mouseover-actor
+           ctx/mouseover-eid
+           ctx/stage
+           ctx/world-mouse-position]}
+   player-eid]
+  (cond
+   mouseover-actor
+   [:interaction-state/mouseover-actor mouseover-actor]
+
+   (and mouseover-eid
+        (:entity/clickable @mouseover-eid))
+   [:interaction-state/clickable-mouseover-eid
+    {:clicked-eid mouseover-eid
+     :in-click-range? (in-click-range? @player-eid @mouseover-eid)}]
+
+   :else
+   (if-let [skill-id (stage/action-bar-selected-skill stage)]
+     (let [entity @player-eid
+           skill (skill-id (:entity/skills entity))
+           effect-ctx (player-effect-ctx mouseover-eid world-mouse-position player-eid)
+           state (entity/skill-usable-state entity skill effect-ctx)]
+       (if (= state :usable)
+         [:interaction-state.skill/usable [skill effect-ctx]]
+         [:interaction-state.skill/not-usable state]))
+     [:interaction-state/no-skill-selected])))
+
+(defn ->cursor [player-eid ctx]
+  (let [[k params] (interaction-state ctx player-eid)]
+    (case k
+      :interaction-state/mouseover-actor
+      (let [actor params]
+        (let [inventory-slot (inventory-window/cell-with-item? actor)]
+          (cond
+           (and inventory-slot
+                (get-in (:entity/inventory @player-eid) inventory-slot)) :cursors/hand-before-grab
+           (actor/window-title-bar? actor) :cursors/move-window
+           (actor/button?           actor) :cursors/over-button
+           :else :cursors/default)))
+
+      :interaction-state/clickable-mouseover-eid
+      (let [{:keys [clicked-eid
+                    in-click-range?]} params]
+        (case (:type (:entity/clickable @clicked-eid))
+          :clickable/item (if in-click-range?
+                            :cursors/hand-before-grab
+                            :cursors/hand-before-grab-gray)
+          :clickable/player :cursors/bag))
+
+      :interaction-state.skill/usable
+      :cursors/use-skill
+
+      :interaction-state.skill/not-usable
+      :cursors/skill-not-usable
+
+      :interaction-state/no-skill-selected
+      :cursors/no-skill-selected)))
 
 (defn inventory-window-visible? [stage]
   (-> stage :windows :inventory-window actor/visible?))
@@ -12,7 +90,7 @@
   (inventory/can-pickup-item? (:entity/inventory entity) item))
 
 (defn interaction-state->txs [ctx player-eid]
-  (let [[k params] (cdq.interaction-state/interaction-state ctx player-eid)]
+  (let [[k params] (interaction-state ctx player-eid)]
     (case k
       :interaction-state/mouseover-actor nil ; handled by ui actors themself.
 
