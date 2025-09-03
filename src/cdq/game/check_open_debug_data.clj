cd26bcecb3543428e408973f49c07c1f916f611e
Michael Sappler <michaelsappler@gmail.com>
2025-09-03 20:08:00 +0530

    add game.open-debug-data-window

--- src/cdq/game/check_open_debug_data.clj
+++ src/cdq/game/check_open_debug_data.clj
@@ -1,23 +1,9 @@
 (ns cdq.game.check-open-debug-data
-  (:require [cdq.ctx.input :as input]
-            [cdq.dev.data-view :as data-view]
-            [cdq.ui.stage :as stage]
-            [cdq.world.grid :as grid]))
+  (:require [cdq.ctx.input :as input]))
 
 (defn do!
-  [{:keys [ctx/input
-           ctx/stage
-           ctx/mouseover-eid
-           ctx/world
-           ctx/world-mouse-position]
+  [{:keys [ctx/input]
     :as ctx}]
   (when (input/button-just-pressed? input :right)
-    (let [data (or (and mouseover-eid @mouseover-eid)
-                   @(grid/cell (:world/grid world)
-                               (mapv int world-mouse-position)))]
-      (stage/add! stage
-                  (data-view/table-view-window {:title "Data View"
-                                                :data data
-                                                :width 500
-                                                :height 500}))))
+    ((requiring-resolve 'cdq.game.open-debug-data-window/do!) ctx))
   ctx)
