682ec3eaf815e8ad82e2be03a7ba25a509ebc1ab
Michael Sappler <michaelsappler@gmail.com>
2025-09-05 21:53:56 +0530

    Remove cdq.entity.timer

--- src/cdq/tx/add_text_effect.clj
+++ src/cdq/tx/add_text_effect.clj
@@ -1,6 +1,16 @@
 (ns cdq.tx.add-text-effect
-  (:require [cdq.entity.timers :as timers]))
+  (:require [cdq.timer :as timer]))
+
+(defn- add-text-effect [entity text duration elapsed-time]
+  (assoc entity
+         :entity/string-effect
+         (if-let [string-effect (:entity/string-effect entity)]
+           (-> string-effect
+               (update :text str "\n" text)
+               (update :counter timer/increment duration))
+           {:text text
+            :counter (timer/create elapsed-time duration)})))
 
 (defn do! [[_ eid text duration] {:keys [ctx/elapsed-time]}]
-  (swap! eid timers/add-text-effect text duration elapsed-time)
+  (swap! eid add-text-effect text duration elapsed-time)
   nil)
