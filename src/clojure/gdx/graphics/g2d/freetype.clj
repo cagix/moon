1e836bda20e9bc83bd276132fd655a6a6291833d
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 00:39:51 +0530

    Add src/clojure/gdx/graphics/g2d/freetype/parameter

--- src/clojure/gdx/graphics/g2d/freetype.clj
+++ src/clojure/gdx/graphics/g2d/freetype.clj
@@ -1,17 +1,9 @@
 (ns clojure.gdx.graphics.g2d.freetype
   (:require [clojure.gdx.graphics.texture.filter :as texture-filter]
-            [clojure.gdx.graphics.g2d.bitmap-font :as bitmap-font])
-  (:import (com.badlogic.gdx.graphics.g2d.freetype FreeTypeFontGenerator
-                                                   FreeTypeFontGenerator$FreeTypeFontParameter)))
+            [clojure.gdx.graphics.g2d.bitmap-font :as bitmap-font]
+            [clojure.gdx.graphics.g2d.freetype.parameter :as parameter])
+  (:import (com.badlogic.gdx.graphics.g2d.freetype FreeTypeFontGenerator)))
 
-(defn- create-font-params [{:keys [size
-                                   min-filter
-                                   mag-filter]}]
-  (let [params (FreeTypeFontGenerator$FreeTypeFontParameter.)]
-    (set! (.size params) size)
-    (set! (.minFilter params) min-filter)
-    (set! (.magFilter params) mag-filter)
-    params))
 
 (defn generate-font
   [file-handle
@@ -20,11 +12,10 @@
            enable-markup?
            use-integer-positions?]}]
   (let [generator (FreeTypeFontGenerator. file-handle)
-        font (.generateFont generator
-                            (create-font-params {:size (* size quality-scaling)
-                                                 ; :texture-filter/linear because scaling to world-units
-                                                 :min-filter (texture-filter/k->value :linear)
-                                                 :mag-filter (texture-filter/k->value :linear)}))]
+        font (.generateFont generator (parameter/create {:size (* size quality-scaling)
+                                                         ; :texture-filter/linear because scaling to world-units
+                                                         :min-filter (texture-filter/k->value :linear)
+                                                         :mag-filter (texture-filter/k->value :linear)}))]
     (bitmap-font/configure! font {:scale (/ quality-scaling)
                                   :enable-markup? enable-markup?
                                   :use-integer-positions? use-integer-positions?})))
