336816f55b0514a1680a9c0e95d6591675d48718
Michael Sappler <michaelsappler@gmail.com>
2025-09-05 22:23:14 +0530

    Do not use entity/position at cdq.effects.target-all

--- src/cdq/effects/target_all.clj
+++ src/cdq/effects/target_all.clj
@@ -1,6 +1,5 @@
 (ns cdq.effects.target-all
-  (:require [cdq.entity :as entity]
-            [cdq.raycaster :as raycaster]))
+  (:require [cdq.raycaster :as raycaster]))
 
 (defn- creatures-in-los-of
   [{:keys [ctx/active-entities
@@ -15,31 +14,29 @@
 (defn applicable? [_ _]
   true)
 
+; TODO
 (defn useful? [_ _effect-ctx _ctx]
-  ; TODO
   false)
 
 (defn handle [[_ {:keys [entity-effects]}] {:keys [effect/source]} ctx]
   (let [source* @source]
     (apply concat
            (for [target (creatures-in-los-of ctx source*)]
-             [[:tx/spawn-line {:start (entity/position source*) #_(start-point source* target*)
-                               :end (entity/position @target)
-                               :duration 0.05
-                               :color [1 0 0 0.75]
-                               :thick? true}]
-              ; some sound .... or repeat smae sound???
-              ; skill do sound  / skill start sound >?
-              ; problem : nested effect/do-all! , we are still having direction/target-position
-              ; at sub-effects
-              ; and no more safe - merge
-              ; find a way to pass ctx / effect-ctx separate ?
-              [:tx/effect {:effect/source source :effect/target target} entity-effects]]))))
+             [[:tx/spawn-line
+               {:start (:body/position (:entity/body source*)) #_(start-point source* target*)
+                :end (:body/position (:entity/body @target))
+                :duration 0.05
+                :color [1 0 0 0.75]
+                :thick? true}]
+              [:tx/effect
+               {:effect/source source
+                :effect/target target}
+               entity-effects]]))))
 
 (defn render [_ {:keys [effect/source]} ctx]
   (let [source* @source]
     (for [target* (map deref (creatures-in-los-of ctx source*))]
       [:draw/line
-       (entity/position source*) #_(start-point source* target*)
-       (entity/position target*)
+       (:body/position (:entity/body source*)) #_(start-point source* target*)
+       (:body/position (:entity/body target*))
        [1 0 0 0.5]])))
