2d8d2a38287cbd669e19921b786f7aef8017d6f4
Michael Sappler <michaelsappler@gmail.com>
2025-09-07 01:37:07 +0530

    fix cdq.start

--- src/cdq/start.clj
+++ src/cdq/start.clj
@@ -2,14 +2,14 @@
   (:require [clojure.edn :as edn]
             [clojure.gdx.utils.shared-library-loader :as shared-library-loader]
             [clojure.java.io :as io]
-            [clojure.lwjgl.system.configuration :as lwjgl-configuration]
+            [clojure.lwjgl.system.configuration :as lwjgl-configuration])
   (:gen-class))
 
 (defn- bind-roots! [bindings]
   (doseq [[var-sym value] bindings]
     (clojure.lang.Var/.bindRoot (requiring-resolve var-sym) @(requiring-resolve value))))
 
-(defn- set-mac-os-config! []
+(defn set-mac-os-config! []
   (when (= (shared-library-loader/operating-system) :mac)
     (lwjgl-configuration/set-glfw-library-name! "glfw_async")))
 
