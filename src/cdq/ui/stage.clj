136589863b7c567c76dc6d41f6ffa2ce1276eeb4
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 15:48:41 +0530

    Move CtxStage to clojure.gdx.scenes.scene2d.Stage

--- src/cdq/ui/stage.clj
+++ src/cdq/ui/stage.clj
@@ -3,18 +3,18 @@
             [clojure.gdx.scenes.scene2d.group :as group]
             [clojure.gdx.scenes.scene2d.stage :as stage])
   (:import (clojure.lang ILookup)
-           (cdq.ui CtxStage)))
+           (clojure.gdx.scenes.scene2d Stage)))
 
 (defn create [viewport batch]
-  (proxy [CtxStage ILookup] [viewport batch (atom nil)]
+  (proxy [Stage ILookup] [viewport batch (atom nil)]
     (valAt [id]
       (group/find-actor-with-id (stage/root this) id))))
 
-(defn get-ctx [^CtxStage stage]
-  @(.ctx ^CtxStage stage))
+(defn get-ctx [^Stage stage]
+  @(.ctx ^Stage stage))
 
-(defn set-ctx! [^CtxStage stage ctx]
-  (reset! (.ctx ^CtxStage stage) ctx))
+(defn set-ctx! [^Stage stage ctx]
+  (reset! (.ctx ^Stage stage) ctx))
 
 (defn add! [stage actor-or-decl]
   (stage/add! stage (ui/construct? actor-or-decl)))
