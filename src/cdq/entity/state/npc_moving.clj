4933dd1ac5d6914d815896221ec67e08e5c2a4d5
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 22:31:24 +0530

    Cleanup cdq.entity.state.npc-moving

--- src/cdq/entity/state/npc_moving.clj
+++ src/cdq/entity/state/npc_moving.clj
@@ -1,19 +1,24 @@
 (ns cdq.entity.state.npc-moving
-  (:require [cdq.stats :as modifiers]
+  (:require [cdq.stats :as stats]
             [cdq.timer :as timer]))
 
-(defn tick! [{:keys [counter]} eid {:keys [ctx/elapsed-time]}]
-  (when (timer/stopped? elapsed-time counter)
-    [[:tx/event eid :timer-finished]]))
+(def reaction-time-multiplier 0.016)
 
+; can put the params movement-vector
+; and pass with enter/exit also
+; so this can be only a timer then
 (defn create [eid movement-vector {:keys [ctx/elapsed-time]}]
   {:movement-vector movement-vector
-   :counter (timer/create elapsed-time
-                          (* (modifiers/get-stat-value (:creature/stats @eid) :entity/reaction-time)
-                             0.016))})
+   :timer (timer/create elapsed-time
+                        (* (stats/get-stat-value (:creature/stats @eid) :entity/reaction-time)
+                           reaction-time-multiplier))})
 
 (defn enter [{:keys [movement-vector]} eid]
   [[:tx/set-movement eid movement-vector]])
 
 (defn exit [_ eid _ctx]
   [[:tx/dissoc eid :entity/movement]])
+
+(defn tick! [{:keys [timer]} eid {:keys [ctx/elapsed-time]}]
+  (when (timer/stopped? elapsed-time timer)
+    [[:tx/event eid :timer-finished]]))
