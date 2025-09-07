6000d1406882ba7e546f082082840fbcee206c9b
Michael Sappler <michaelsappler@gmail.com>
2025-09-07 16:17:30 +0530

    property-type-overview-window

--- src/cdq/ui/dev_menu/menus/db.clj
+++ src/cdq/ui/dev_menu/menus/db.clj
@@ -8,10 +8,8 @@
             [clojure.string :as str]
             [clojure.vis-ui.widget :as widget]))
 
-(defn- open-editor-overview-window!
-  [{:keys [ctx/stage]
-    :as ctx}
-   property-type]
+(defn- property-type-overview-window
+  [ctx property-type]
   (let [window (widget/window {:title "Edit"
                                :modal? true
                                :close-button? true
@@ -22,14 +20,14 @@
                                    ctx/stage]
                             :as ctx}]
                         (stage/add! stage (actor/build (cdq.editor.widget.map/property-editor-window ctx (db/get-raw db id)))))]
-    (table/add! window (cdq.editor.overview-table/create ctx
-                                                         property-type
-                                                         on-clicked-id))
+    (table/add! window (cdq.editor.overview-table/create ctx property-type on-clicked-id))
     (.pack window)
-    (stage/add! stage window)))
+    window))
 
 (defn create [{:keys [ctx/db]} _]
   (for [property-type (sort (db/property-types db))]
     {:label (str/capitalize (name property-type))
-     :on-click (fn [_actor ctx]
-                 (open-editor-overview-window! ctx property-type))}))
+     :on-click (fn [_actor
+                    {:keys [ctx/stage]
+                     :as ctx}]
+                 (stage/add! stage (property-type-overview-window ctx property-type)))}))
