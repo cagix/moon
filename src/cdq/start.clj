064b57cb7d9e51903b8b24eeed75a27b9703af33
Michael Sappler <michaelsappler@gmail.com>
2025-09-07 19:53:44 +0530

    Make config a map

--- src/cdq/start.clj
+++ src/cdq/start.clj
@@ -1,11 +1,20 @@
 (ns cdq.start
   (:require [clojure.edn :as edn]
+            [clojure.gdx.backends.lwjgl :as lwjgl]
+            [clojure.gdx.utils.shared-library-loader :as shared-library-loader]
             [clojure.java.io :as io])
   (:gen-class))
 
+(defn- execute! [[f params]]
+  ((requiring-resolve f) params))
+
 (defn -main []
-  (doseq [[f config] (-> "cdq.start.edn"
-                         io/resource
-                         slurp
-                         edn/read-string)]
-    ((requiring-resolve f) config)))
+  (let [{:keys [operating-sytem->executables
+                listener
+                config]} (-> "cdq.start.edn" io/resource slurp edn/read-string)]
+    (->> (shared-library-loader/operating-system)
+         operating-sytem->executables
+         (run! execute!))
+    (lwjgl/start-application! (let [[f params] listener]
+                                ((requiring-resolve f) params))
+                              config)))
