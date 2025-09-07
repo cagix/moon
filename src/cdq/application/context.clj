1c5dae36adc72a4e3b6a9a96dfbec96d5f549088
Michael Sappler <michaelsappler@gmail.com>
2025-09-07 22:25:33 +0530

    pass pipelines

--- src/cdq/application/context.clj
+++ src/cdq/application/context.clj
@@ -1,8 +1,6 @@
-(ns cdq.application.context
-  (:require cdq.gdx-app.dispose
-            cdq.gdx-app.resize))
+(ns cdq.application.context)
 
-(defn create [ctx]
+(defn create [ctx create-pipeline]
   (reduce (fn [ctx f]
             (let [result (if (vector? f)
                            (let [[f params] f]
@@ -12,66 +10,12 @@
                 ctx
                 result)))
           ctx
-          '[cdq.create.txs/do!
-            cdq.create.effects/do!
-            cdq.create.editor-widgets/do!
-            cdq.create.ui-actors/do!
-            cdq.create.draw-on-world-viewport/do!
-            cdq.create.config/do!
-            cdq.create.vis-ui/do!
-            cdq.create.colors/do!
-            cdq.create.draw-fns/do!
-            cdq.create.render-layers/do!
-            cdq.create.info/do!
-            cdq.create.sprite-batch/do!
-            cdq.create.graphics/do!
-            cdq.create.textures/do!
-            cdq.create.audio/do!
-            cdq.create.cursors/do!
-            cdq.create.db/do!
-            cdq.create.world-unit-scale/do!
-            cdq.create.ui-viewport/do!
-            cdq.create.input/do!
-            cdq.create.stage/do!
-            cdq.create.input-processor/do!
-            cdq.create.tiled-map-renderer/do!
-            cdq.create.world-viewport/do!
-            cdq.create.default-font/do!
-            cdq.create.unit-scale/do!
-            cdq.create.shape-drawer-texture/do!
-            cdq.create.shape-drawer/do!
-            cdq.reset-game-state/do!
-            cdq.create.frame/do!]))
+          create-pipeline))
 
-(def dispose cdq.gdx-app.dispose/do!)
-
-(defn render [ctx]
+(defn render [ctx pipeline]
   (reduce (fn [ctx f]
             (if-let [new-ctx ((requiring-resolve f) ctx)]
               new-ctx
               ctx))
           ctx
-          '[cdq.render.validate/do!
-            cdq.render.assoc-mouseover-keys/do!
-            cdq.render.update-mouseover-eid/do!
-            cdq.render.check-open-debug-data/do! ; TODO FIXME its not documented I forgot rightclick can open debug data view!
-            cdq.render.assoc-active-entities/do!
-            cdq.render.set-camera-on-player/do!
-            cdq.render.clear-screen/do!
-            cdq.render.draw-world-map/do!
-            cdq.render.draw-on-world-viewport/do!
-            cdq.render.render-stage/do!
-            cdq.render.set-cursor/do!
-            cdq.render.player-state-handle-input/do!
-            cdq.render.assoc-paused/do!
-            cdq.render.tick-world/do!
-            cdq.render.remove-destroyed-entities/do! ; do not pause as pickup item should be destroyed
-            cdq.render.handle-key-input/do!
-            cdq.render.dissoc-mouseover-keys/do!
-            cdq.render.validate/do!
-            ; :cdq.render/validate
-            ; -> render multifn / method map ?
-            ; tx just  method map ?
-            ]))
-
-(def resize cdq.gdx-app.resize/do!)
+          pipeline))
--- src/cdq/application/lwjgl.clj
+++ src/cdq/application/lwjgl.clj
@@ -1,5 +1,7 @@
 (ns cdq.application.lwjgl
-  (:require [cdq.application :as application]
+  (:require cdq.gdx-app.dispose
+            cdq.gdx-app.resize
+            [cdq.application :as application]
             [cdq.application.context :as ctx]
             [cdq.application.context.record :as ctx-record]
             [cdq.malli :as m]
@@ -9,13 +11,67 @@
   (lwjgl/start-application!
    {:create! (fn []
                (reset! application/state (ctx/create
-                                          (ctx-record/map->Context {:schema (m/schema ctx-record/schema)}))))
+                                          (ctx-record/map->Context {:schema (m/schema ctx-record/schema)})
+                                          '[cdq.create.txs/do!
+                                            cdq.create.effects/do!
+                                            cdq.create.editor-widgets/do!
+                                            cdq.create.ui-actors/do!
+                                            cdq.create.draw-on-world-viewport/do!
+                                            cdq.create.config/do!
+                                            cdq.create.vis-ui/do!
+                                            cdq.create.colors/do!
+                                            cdq.create.draw-fns/do!
+                                            cdq.create.render-layers/do!
+                                            cdq.create.info/do!
+                                            cdq.create.sprite-batch/do!
+                                            cdq.create.graphics/do!
+                                            cdq.create.textures/do!
+                                            cdq.create.audio/do!
+                                            cdq.create.cursors/do!
+                                            cdq.create.db/do!
+                                            cdq.create.world-unit-scale/do!
+                                            cdq.create.ui-viewport/do!
+                                            cdq.create.input/do!
+                                            cdq.create.stage/do!
+                                            cdq.create.input-processor/do!
+                                            cdq.create.tiled-map-renderer/do!
+                                            cdq.create.world-viewport/do!
+                                            cdq.create.default-font/do!
+                                            cdq.create.unit-scale/do!
+                                            cdq.create.shape-drawer-texture/do!
+                                            cdq.create.shape-drawer/do!
+                                            cdq.reset-game-state/do!
+                                            cdq.create.frame/do!]
+                                          )))
     :dispose! (fn []
-                (ctx/dispose @application/state))
+                (cdq.gdx-app.dispose/do! @application/state))
     :render! (fn []
-               (swap! application/state ctx/render))
+               (swap! application/state ctx/render
+                      '[cdq.render.validate/do!
+                        cdq.render.assoc-mouseover-keys/do!
+                        cdq.render.update-mouseover-eid/do!
+                        cdq.render.check-open-debug-data/do! ; TODO FIXME its not documented I forgot rightclick can open debug data view!
+                        cdq.render.assoc-active-entities/do!
+                        cdq.render.set-camera-on-player/do!
+                        cdq.render.clear-screen/do!
+                        cdq.render.draw-world-map/do!
+                        cdq.render.draw-on-world-viewport/do!
+                        cdq.render.render-stage/do!
+                        cdq.render.set-cursor/do!
+                        cdq.render.player-state-handle-input/do!
+                        cdq.render.assoc-paused/do!
+                        cdq.render.tick-world/do!
+                        cdq.render.remove-destroyed-entities/do! ; do not pause as pickup item should be destroyed
+                        cdq.render.handle-key-input/do!
+                        cdq.render.dissoc-mouseover-keys/do!
+                        cdq.render.validate/do!
+                        ; :cdq.render/validate
+                        ; -> render multifn / method map ?
+                        ; tx just  method map ?
+                        ]
+                      ))
     :resize! (fn [width height]
-               (ctx/resize @application/state width height))
+               (cdq.gdx-app.resize/do! @application/state width height))
     :pause! (fn [])
     :resume! (fn [])}
    {:title "Cyber Dungeon Quest"
