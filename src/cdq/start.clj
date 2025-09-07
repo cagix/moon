9c25e4cf3fc66ec8055adea24f0bd8f34f2caa47
Michael Sappler <michaelsappler@gmail.com>
2025-09-07 16:03:32 +0530

    multifn, start and dispatch to cdq.start

--- src/cdq/start.clj
+++ src/cdq/start.clj
@@ -1,11 +1,50 @@
 (ns cdq.start
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io])
+  (:import (clojure.lang MultiFn
+                         Var))
   (:gen-class))
+
+(defn- add-methods* [system-vars ns-sym k & {:keys [optional?]}]
+  (doseq [system-var (map requiring-resolve system-vars)
+          :let [_ (assert (var? system-var))
+                method-sym (symbol (str ns-sym "/" (:name (meta system-var))))
+                method-var (resolve method-sym)]]
+    (assert (or optional? method-var)
+            (str "Cannot find required `" (:name (meta system-var)) "` function in " ns-sym " - method-sym: " method-sym))
+    (when method-var
+      (assert (keyword? k))
+      (assert (var? method-var) (pr-str method-var))
+      (let [system @system-var]
+        (when (k (methods system))
+          (println "WARNING: Overwriting method" (:name (meta method-var)) "on" k))
+        ;(println "MultiFn/.addMethod " system-var  k method-var)
+        (MultiFn/.addMethod system k method-var)))))
+
+(defn- install-methods [{:keys [required optional]} ns-sym k]
+  (add-methods* required ns-sym k)
+  (add-methods* optional ns-sym k :optional? true))
+
+(defn add-methods! [[multis components]]
+  (doseq [[ns-sym k] components]
+    (require ns-sym)
+    (install-methods multis ns-sym k)))
+
+(defn dispatch!
+  [{:keys [on
+           k->executions]}]
+  (doseq [[f params] (k->executions ((requiring-resolve on)))]
+    ((requiring-resolve f) params)))
+
+(defn bind-roots! [bindings]
+  (doseq [[var-sym value] bindings]
+    (Var/.bindRoot (requiring-resolve var-sym) @(requiring-resolve value))))
 
 (defn -main []
   (doseq [[f config] (-> "cdq.start.edn"
                          io/resource
                          slurp
                          edn/read-string)]
-    ((requiring-resolve f) config)))
+    ((or (ns-resolve 'cdq.start f)
+         (requiring-resolve f))
+     config)))
--- src/cdq/var.clj
+++
@@ -1,6 +0,0 @@
-(ns cdq.var
-  (:import (clojure.lang Var)))
-
-(defn bind-roots! [bindings]
-  (doseq [[var-sym value] bindings]
-    (Var/.bindRoot (requiring-resolve var-sym) @(requiring-resolve value))))
