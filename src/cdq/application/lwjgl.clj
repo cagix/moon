3b80b9cbf575601581b54526b10215caf294a671
Michael Sappler <michaelsappler@gmail.com>
2025-09-07 19:43:23 +0530

    Delete cdq.core/dispatch!

--- src/cdq/application/lwjgl.clj
+++ src/cdq/application/lwjgl.clj
@@ -1,8 +1,17 @@
 (ns cdq.application.lwjgl
-  (:require [clojure.gdx.backends.lwjgl :as lwjgl]))
+  (:require [clojure.gdx.backends.lwjgl :as lwjgl]
+            [clojure.gdx.utils.shared-library-loader :as shared-library-loader]))
+
+(defn execute! [[f params]]
+  ((requiring-resolve f) params))
 
 (defn start!
-  [{:keys [listener config]}]
+  [{:keys [operating-sytem->executables
+           listener
+           config]}]
+  (->> (shared-library-loader/operating-system)
+       operating-sytem->executables
+       (run! execute!))
   (lwjgl/start-application! (let [[f params] listener]
                               ((requiring-resolve f) params))
                             config))
--- src/cdq/core.clj
+++
@@ -1,7 +0,0 @@
-(ns cdq.core)
-
-(defn dispatch!
-  [{:keys [on
-           k->executions]}]
-  (doseq [[f params] (k->executions ((requiring-resolve on)))]
-    ((requiring-resolve f) params)))
