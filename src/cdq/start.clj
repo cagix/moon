92efa7a266b1ed1df435d6c18974b93030d26e1f
Michael Sappler <michaelsappler@gmail.com>
2025-09-07 19:56:03 +0530

    create listener in start

--- src/cdq/start.clj
+++ src/cdq/start.clj
@@ -8,6 +8,47 @@
 (defn- execute! [[f params]]
   ((requiring-resolve f) params))
 
+(defn- create-listener
+  [{:keys [state
+           initial-record
+           create-pipeline
+           dispose-fn
+           render-pipeline
+           resize-fn]}]
+  (let [state @(requiring-resolve state)
+        create! (fn []
+                  (reduce (fn [ctx f]
+                            (let [result (if (vector? f)
+                                           (let [[f params] f]
+                                             ((requiring-resolve f) ctx params))
+                                           ((requiring-resolve f) ctx))]
+                              (if (nil? result)
+                                ctx
+                                result)))
+                          ((requiring-resolve initial-record))
+                          create-pipeline))
+        dispose! (fn [ctx]
+                   (requiring-resolve dispose-fn) ctx)
+        render! (fn [ctx]
+                  (reduce (fn [ctx f]
+                            (if-let [new-ctx ((requiring-resolve f) ctx)]
+                              new-ctx
+                              ctx))
+                          ctx
+                          render-pipeline))
+        resize! (fn [ctx width height]
+                  ((requiring-resolve resize-fn) ctx width height))]
+    {:create! (fn []
+                (reset! state (create!)))
+     :dispose! (fn []
+                 (dispose! @state))
+     :render! (fn []
+                (swap! state render!))
+     :resize! (fn [width height]
+                (resize! @state width height))
+     :pause! (fn [])
+     :resume! (fn [])}))
+
 (defn -main []
   (let [{:keys [operating-sytem->executables
                 listener
@@ -15,6 +56,5 @@
     (->> (shared-library-loader/operating-system)
          operating-sytem->executables
          (run! execute!))
-    (lwjgl/start-application! (let [[f params] listener]
-                                ((requiring-resolve f) params))
+    (lwjgl/start-application! (create-listener listener)
                               config)))
