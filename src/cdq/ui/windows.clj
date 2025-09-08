2efb3e135f2d31f1f6c6c27ec3af9368450dc414
Michael Sappler <michaelsappler@gmail.com>
2025-09-08 22:11:10 +0530

    move state clicked inventory cell to state

--- src/cdq/ui/windows.clj
+++ src/cdq/ui/windows.clj
@@ -1,50 +1,10 @@
 (ns cdq.ui.windows
   (:require [cdq.ctx :as ctx]
             [cdq.info :as info]
-            [cdq.inventory :as inventory]
             cdq.ui.windows.entity-info
             cdq.ui.windows.inventory))
 
-(def state->clicked-inventory-cell
-  {:player-idle           (fn [eid cell]
-                            ; TODO no else case
-                            (when-let [item (get-in (:entity/inventory @eid) cell)]
-                              [[:tx/sound "bfxr_takeit"]
-                               [:tx/event eid :pickup-item item]
-                               [:tx/remove-item eid cell]]))
-   :player-item-on-cursor (fn [eid cell]
-                            (let [entity @eid
-                                  inventory (:entity/inventory entity)
-                                  item-in-cell (get-in inventory cell)
-                                  item-on-cursor (:entity/item-on-cursor entity)]
-                              (cond
-                               ; PUT ITEM IN EMPTY CELL
-                               (and (not item-in-cell)
-                                    (inventory/valid-slot? cell item-on-cursor))
-                               [[:tx/sound "bfxr_itemput"]
-                                [:tx/dissoc eid :entity/item-on-cursor]
-                                [:tx/set-item eid cell item-on-cursor]
-                                [:tx/event eid :dropped-item]]
-
-                               ; STACK ITEMS
-                               (and item-in-cell
-                                    (inventory/stackable? item-in-cell item-on-cursor))
-                               [[:tx/sound "bfxr_itemput"]
-                                [:tx/dissoc eid :entity/item-on-cursor]
-                                [:tx/stack-item eid cell item-on-cursor]
-                                [:tx/event eid :dropped-item]]
-
-                               ; SWAP ITEMS
-                               (and item-in-cell
-                                    (inventory/valid-slot? cell item-on-cursor))
-                               [[:tx/sound "bfxr_itemput"]
-                                ; need to dissoc and drop otherwise state enter does not trigger picking it up again
-                                ; TODO? coud handle pickup-item from item-on-cursor state also
-                                [:tx/dissoc eid :entity/item-on-cursor]
-                                [:tx/remove-item eid cell]
-                                [:tx/set-item eid cell item-on-cursor]
-                                [:tx/event eid :dropped-item]
-                                [:tx/event eid :pickup-item item-in-cell]])))})
+(declare state->clicked-inventory-cell)
 
 (comment
 
