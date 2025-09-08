f2b979fdfc5a73a8e6168eab9ecd8ea5f05ec135
Michael Sappler <michaelsappler@gmail.com>
2025-09-08 15:22:26 +0530

    merge render layrs and extend scene2d

--- src/cdq/extend_scene2d.clj
+++
@@ -1,9 +0,0 @@
-(ns cdq.extend-scene2d
-  (:require [clojure.gdx.scenes.scene2d :as scene2d]
-            [cdq.ctx]))
-
-(defn do! [ctx]
-  (extend-type (class ctx)
-    clojure.gdx.scenes.scene2d/Context
-    (handle-draws! [ctx draws]
-      (cdq.ctx/handle-draws! ctx draws))))
