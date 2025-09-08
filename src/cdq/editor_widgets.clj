31b263938ec4d75244d0db9b47d18f71942124ef
Michael Sappler <michaelsappler@gmail.com>
2025-09-08 16:33:07 +0530

    add cdq.editor.window

--- src/cdq/editor_widgets.clj
+++ src/cdq/editor_widgets.clj
@@ -1,17 +1,15 @@
 (ns cdq.editor-widgets
-  (:require [cdq.application :as application]
+  (:require cdq.editor-window
             [cdq.audio :as audio]
             [cdq.db :as db]
             [cdq.image :as image]
             [cdq.property :as property]
             [cdq.schemas :as schemas]
-            [cdq.stacktrace :as stacktrace]
             [cdq.editor.overview-table]
             [cdq.editor.widget :as editor-widget]
             [cdq.ui.widget]
             [cdq.utils :as utils]
             [clojure.edn :as edn]
-            [clojure.gdx.input :as input]
             [clojure.gdx.scenes.scene2d.actor :as actor]
             [clojure.gdx.scenes.scene2d.group :as group]
             [clojure.gdx.scenes.scene2d.stage :as stage]
@@ -129,56 +127,6 @@
                             (fn [_actor ctx]
                               (redo-rows ctx (disj property-ids id)))))])))
 
-
-; Move config to ctx/editor (functions, etc.)
-; and pure ui building but with editor config
-; so it dispatches on ctx/editor ....
-; which can have implementation/protocol
-; and input stage ui functions can also returns txs
-; [:tx.stage/add [:ui.actor/property-editor-window options]]
-(defn property-editor-window
-  [{:keys [ctx/db
-           ctx/ui-viewport]
-    :as ctx}
-   property]
-  (let [schema (get (:schemas db) (property/type property))
-        widget (editor-widget/create schema nil property ctx)
-        with-window-close (fn [f]
-                            (fn [actor {:keys [ctx/stage] :as ctx}]
-                              (try (f ctx)
-                                   (actor/remove! (window/find-ancestor actor))
-                                   (catch Throwable t
-                                     (stacktrace/pretty-print t)
-                                     (stage/add! stage (cdq.ui.widget/error-window t))))))
-        clicked-save-fn (with-window-close (fn [{:keys [ctx/db]}]
-                                             (swap! application/state update :ctx/db
-                                                    db/update!
-                                                    (editor-widget/value schema nil widget (:schemas db)))))
-        clicked-delete-fn (with-window-close (fn [_ctx]
-                                               (swap! application/state update :ctx/db
-                                                      db/delete!
-                                                      (:property/id property))))
-        extra-act-fn (fn [actor _delta {:keys [ctx/input] :as ctx}]
-                       (when (input/key-just-pressed? input :enter)
-                         (clicked-save-fn actor ctx)))
-        scrollpane-height (:viewport/height ui-viewport)]
-    (doto (widget/window {:title (str "[SKY]Property[]")
-                          :id :property-editor-window
-                          :modal? true
-                          :close-button? true
-                          :center? true
-                          :close-on-escape? true
-                          :rows [[(cdq.ui.widget/scroll-pane-cell scrollpane-height
-                                                                  [[{:actor widget :colspan 2}]
-                                                                   [{:actor (widget/text-button "Save [LIGHT_GRAY](ENTER)[]" clicked-save-fn)
-                                                                     :center? true}
-                                                                    {:actor (widget/text-button "Delete" clicked-delete-fn)
-                                                                     :center? true}]])]]
-                          :actors [{:actor/type :actor.type/actor
-                                    :act extra-act-fn}]
-                          :cell-defaults {:pad 5}})
-      (.pack))))
-
 (def ^:private property-k-sort-order
   [:property/id
    :property/pretty-name
@@ -211,7 +159,7 @@
   (let [window (:property-editor-window stage)
         prop-value (window->property-value window (:schemas db))]
     (actor/remove! window)
-    (stage/add! stage (property-editor-window ctx prop-value))))
+    (stage/add! stage (cdq.editor-window/property-editor-window ctx prop-value))))
 
 (defn- find-kv-widget [table k]
   (utils/find-first (fn [actor]
---
+++ src/cdq/editor_window.clj
@@ -0,0 +1,55 @@
+(ns cdq.editor-window
+  (:require [cdq.application :as application]
+            [cdq.db :as db]
+            [cdq.property :as property]
+            [cdq.stacktrace :as stacktrace]
+            [cdq.editor.widget :as editor-widget]
+            [cdq.ui.widget]
+            [clojure.gdx.input :as input]
+            [clojure.gdx.scenes.scene2d.actor :as actor]
+            [clojure.gdx.scenes.scene2d.stage :as stage]
+            [clojure.gdx.scenes.scene2d.ui.window :as window]
+            [clojure.vis-ui.widget :as widget]))
+
+(defn property-editor-window
+  [{:keys [ctx/db
+           ctx/ui-viewport]
+    :as ctx}
+   property]
+  (let [schema (get (:schemas db) (property/type property))
+        widget (editor-widget/create schema nil property ctx)
+        with-window-close (fn [f]
+                            (fn [actor {:keys [ctx/stage] :as ctx}]
+                              (try (f ctx)
+                                   (actor/remove! (window/find-ancestor actor))
+                                   (catch Throwable t
+                                     (stacktrace/pretty-print t)
+                                     (stage/add! stage (cdq.ui.widget/error-window t))))))
+        clicked-save-fn (with-window-close (fn [{:keys [ctx/db]}]
+                                             (swap! application/state update :ctx/db
+                                                    db/update!
+                                                    (editor-widget/value schema nil widget (:schemas db)))))
+        clicked-delete-fn (with-window-close (fn [_ctx]
+                                               (swap! application/state update :ctx/db
+                                                      db/delete!
+                                                      (:property/id property))))
+        extra-act-fn (fn [actor _delta {:keys [ctx/input] :as ctx}]
+                       (when (input/key-just-pressed? input :enter)
+                         (clicked-save-fn actor ctx)))
+        scrollpane-height (:viewport/height ui-viewport)]
+    (doto (widget/window {:title (str "[SKY]Property[]")
+                          :id :property-editor-window
+                          :modal? true
+                          :close-button? true
+                          :center? true
+                          :close-on-escape? true
+                          :rows [[(cdq.ui.widget/scroll-pane-cell scrollpane-height
+                                                                  [[{:actor widget :colspan 2}]
+                                                                   [{:actor (widget/text-button "Save [LIGHT_GRAY](ENTER)[]" clicked-save-fn)
+                                                                     :center? true}
+                                                                    {:actor (widget/text-button "Delete" clicked-delete-fn)
+                                                                     :center? true}]])]]
+                          :actors [{:actor/type :actor.type/actor
+                                    :act extra-act-fn}]
+                          :cell-defaults {:pad 5}})
+      (.pack))))
