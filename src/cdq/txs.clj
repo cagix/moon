18cfd26c7618e6d8398f8e5348feb3f78883137b
Michael Sappler <michaelsappler@gmail.com>
2025-09-09 16:02:36 +0530

    simplify load txs

--- src/cdq/txs.clj
+++ src/cdq/txs.clj
@@ -1,42 +1,45 @@
 (ns cdq.txs)
 
 (def txs-fn-map
-  (into {}
-        (for [[k sym] '{:tx/assoc cdq.tx.assoc/do!
-                        :tx/assoc-in cdq.tx.assoc-in/do!
-                        :tx/dissoc cdq.tx.dissoc/do!
-                        :tx/effect cdq.tx.effect/do!
-                        :tx/mark-destroyed cdq.tx.mark-destroyed/do!
-                        :tx/mod-add cdq.tx.mod-add/do!
-                        :tx/mod-remove cdq.tx.mod-remove/do!
-                        :tx/pay-mana-cost cdq.tx.pay-mana-cost/do!
-                        :tx/pickup-item cdq.tx.pickup-item/do!
-                        :tx/set-cooldown cdq.tx.set-cooldown/do!
-                        :tx/add-text-effect cdq.tx.add-text-effect/do!
-                        :tx/add-skill cdq.tx.add-skill/do!
-                        :tx/player-add-skill cdq.tx.player-add-skill/do!
-                        :tx/set-item cdq.tx.set-item/do!
-                        :tx/remove-item cdq.tx.remove-item/do!
-                        :tx/player-set-item cdq.tx.player-set-item/do!
-                        :tx/player-remove-item cdq.tx.player-remove-item/do!
-                        :tx/event cdq.tx.event/do!
-                        :tx/toggle-inventory-visible cdq.tx.toggle-inventory-visible/do!
-                        :tx/show-message cdq.tx.show-message/do!
-                        :tx/show-modal cdq.tx.show-modal/do!
-                        :tx/sound cdq.tx.sound/do!
-                        :tx/state-exit cdq.tx.state-exit/do!
-                        :tx/state-enter cdq.tx.state-enter/do!
-                        :tx/audiovisual cdq.tx.audiovisual/do!
-                        :tx/spawn-alert cdq.tx.spawn-alert/do!
-                        :tx/spawn-line cdq.tx.spawn-line/do!
-                        :tx/deal-damage cdq.tx.deal-damage/do!
-                        :tx/move-entity cdq.tx.move-entity/do!
-                        :tx/spawn-projectile cdq.tx.spawn-projectile/do!
-                        :tx/spawn-effect cdq.tx.spawn-effect/do!
-                        :tx/spawn-item cdq.tx.spawn-item/do!
-                        :tx/spawn-creature cdq.tx.spawn-creature/do!
-                        :tx/spawn-entity cdq.tx.spawn-entity/do!}
-              :let [f (requiring-resolve sym)]]
-          (do
-           (assert f (str "Cannot resolve " sym))
-           [k f]))))
+  (let [sym-format "cdq.tx.%s/do!"
+        ks [:tx/assoc
+            :tx/assoc-in
+            :tx/dissoc
+            :tx/effect
+            :tx/mark-destroyed
+            :tx/mod-add
+            :tx/mod-remove
+            :tx/pay-mana-cost
+            :tx/pickup-item
+            :tx/set-cooldown
+            :tx/add-text-effect
+            :tx/add-skill
+            :tx/player-add-skill
+            :tx/set-item
+            :tx/remove-item
+            :tx/player-set-item
+            :tx/player-remove-item
+            :tx/event
+            :tx/toggle-inventory-visible
+            :tx/show-message
+            :tx/show-modal
+            :tx/sound
+            :tx/state-exit
+            :tx/state-enter
+            :tx/audiovisual
+            :tx/spawn-alert
+            :tx/spawn-line
+            :tx/deal-damage
+            :tx/move-entity
+            :tx/spawn-projectile
+            :tx/spawn-effect
+            :tx/spawn-item
+            :tx/spawn-creature
+            :tx/spawn-entity]]
+    (into {}
+          (for [k ks
+                :let [sym (symbol (format sym-format (name k)))
+                      f (requiring-resolve sym)]]
+            (do
+             (assert f (str "Cannot resolve " sym))
+             [k f])))))
