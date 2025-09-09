8d6007394a79f9ebe5a897f76ea6be2416d427c5
Michael Sappler <michaelsappler@gmail.com>
2025-09-09 20:17:41 +0530

    simplify modules world fn

--- src/cdq/world_fns/modules.clj
+++ src/cdq/world_fns/modules.clj
@@ -2,82 +2,8 @@
   (:require [cdq.area-level-grid :as area-level-grid]
             [cdq.grid2d :as g2d]
             [cdq.level.helper :as helper]
+            [cdq.world-fns.module :as module]
             [clojure.gdx.maps.tiled :as tiled]))
-
-(def modules-file "maps/modules.tmx") ; used @ tst
-(def modules-width  32) ; usd @ test
-(def modules-height 20) ; used @ test
-(def modules-scale [modules-width modules-height])
-
-(def ^:private number-modules-x 8)
-(def ^:private number-modules-y 4)
-(def ^:private module-offset-tiles 1)
-(def ^:private transition-modules-row-width 4)
-(def ^:private transition-modules-row-height 4)
-(def ^:private transition-modules-offset-x 4)
-(def ^:private floor-modules-row-width 4)
-(def ^:private floor-modules-row-height 4)
-(def ^:private floor-idxvalue 0)
-
-(defn- module-index->tiled-map-positions [[module-x module-y]]
-  (let [start-x (* module-x (+ modules-width  module-offset-tiles))
-        start-y (* module-y (+ modules-height module-offset-tiles))]
-    (for [x (range start-x (+ start-x modules-width))
-          y (range start-y (+ start-y modules-height))]
-      [x y])))
-
-(defn- floor->module-index []
-  [(rand-int floor-modules-row-width)
-   (rand-int floor-modules-row-height)])
-
-(defn- transition-idxvalue->module-index [idxvalue]
-  [(+ (rem idxvalue transition-modules-row-width)
-      transition-modules-offset-x)
-   (int (/ idxvalue transition-modules-row-height))])
-
-(defn- place-module* [scaled-grid
-                      unscaled-position
-                      & {:keys [transition?
-                                transition-neighbor?]}]
-  (let [idxvalue (if transition?
-                   (helper/transition-idx-value unscaled-position transition-neighbor?)
-                   floor-idxvalue)
-        tiled-map-positions (module-index->tiled-map-positions
-                             (if transition?
-                               (transition-idxvalue->module-index idxvalue)
-                               (floor->module-index)))
-        offsets (for [x (range modules-width)
-                      y (range modules-height)]
-                  [x y])
-        offset->tiled-map-position (zipmap offsets tiled-map-positions)
-        scaled-position (mapv * unscaled-position modules-scale)]
-    (reduce (fn [grid offset]
-              (assoc grid
-                     (mapv + scaled-position offset)
-                     (offset->tiled-map-position offset)))
-            scaled-grid
-            offsets)))
-
-(defn place-module [modules-tiled-map
-                    scaled-grid
-                    unscaled-grid
-                    unscaled-floor-positions
-                    unscaled-transition-positions]
-  (let [_ (assert (and (= (:tiled-map/width modules-tiled-map)
-                          (* number-modules-x (+ modules-width module-offset-tiles)))
-                       (= (:tiled-map/height modules-tiled-map)
-                          (* number-modules-y (+ modules-height module-offset-tiles)))))
-        scaled-grid (reduce (fn [scaled-grid unscaled-position]
-                              (place-module* scaled-grid unscaled-position :transition? false))
-                            scaled-grid
-                            unscaled-floor-positions)
-        scaled-grid (reduce (fn [scaled-grid unscaled-position]
-                              (place-module* scaled-grid unscaled-position :transition? true
-                                            :transition-neighbor? #(#{:transition :wall}
-                                                                    (get unscaled-grid %))))
-                            scaled-grid
-                            unscaled-transition-positions)]
-    (helper/grid->tiled-map modules-tiled-map scaled-grid)))
 
 ; * unique max 16 modules, not random take @ #'floor->module-index, also special start, end modules, rare modules...
 ; * at the beginning enemies very close, different area different spawn-rate !
@@ -103,13 +29,13 @@
                    (= #{:wall :ground :transition} (set (g2d/cells grid)))
                    (= #{:ground :transition} (set (g2d/cells grid))))
                   (str "(set (g2d/cells grid)): " (set (g2d/cells grid))))
-        scale modules-scale
+        scale module/modules-scale
         scaled-grid (helper/scale-grid grid scale)
-        tiled-map (place-module (tiled/tmx-tiled-map modules-file)
-                                 scaled-grid
-                                 grid
-                                 (filter #(= :ground     (get grid %)) (g2d/posis grid))
-                                 (filter #(= :transition (get grid %)) (g2d/posis grid)))
+        tiled-map (module/place-module (tiled/tmx-tiled-map module/modules-file)
+                                       scaled-grid
+                                       grid
+                                       (filter #(= :ground     (get grid %)) (g2d/posis grid))
+                                       (filter #(= :transition (get grid %)) (g2d/posis grid)))
         start-position (mapv * start scale)
         can-spawn? #(= "all" (tiled/movement-property tiled-map %))
         _ (assert (can-spawn? start-position)) ; assuming hoping bottom left is movable
