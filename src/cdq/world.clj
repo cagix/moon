a9108bb010630b89d8d894f7981a1dfcf1d6f0ac
Michael Sappler <michaelsappler@gmail.com>
2025-09-10 01:30:24 +0530

    Remove world-impl, causes problems

--- src/cdq/world.clj
+++ src/cdq/world.clj
@@ -1,7 +1,53 @@
-(ns cdq.world)
+(ns cdq.world
+  (:require [cdq.ctx :as ctx]
+            [cdq.content-grid :as content-grid]
+            [cdq.entity-tick]
+            [cdq.raycaster :as raycaster]
+            [cdq.potential-fields.update :as potential-fields.update]))
 
-(defprotocol World
-  (creatures-in-los-of [_ entity])
-  (assoc-active-entities [_])
-  (update-potential-fields! [_])
-  (tick-entities! [_]))
+(defn- tick-component! [k v eid ctx]
+  (when-let [f (cdq.entity-tick/entity->tick k)]
+    (f v eid ctx)))
+
+(defn- tick-entity! [ctx eid]
+  (doseq [k (keys @eid)]
+    (try (when-let [v (k @eid)]
+           (ctx/handle-txs! ctx (tick-component! k v eid ctx)))
+         (catch Throwable t
+           (throw (ex-info "entity-tick"
+                           {:k k
+                            :entity/id (:entity/id @eid)}
+                           t))))))
+
+(defn creatures-in-los-of
+  [{:keys [ctx/active-entities
+           ctx/raycaster]}
+   entity]
+  (->> active-entities
+       (filter #(:entity/species @%))
+       (filter #(raycaster/line-of-sight? raycaster entity @%))
+       (remove #(:entity/player? @%))))
+
+(defn assoc-active-entities
+  [{:keys [ctx/content-grid
+           ctx/player-eid]
+    :as ctx}]
+  (assoc ctx :ctx/active-entities (content-grid/active-entities content-grid @player-eid)))
+
+(defn update-potential-fields!
+  [{:keys [ctx/factions-iterations
+           ctx/potential-field-cache
+           ctx/grid
+           ctx/active-entities]}]
+  (doseq [[faction max-iterations] factions-iterations]
+    (potential-fields.update/tick! potential-field-cache
+                                   grid
+                                   faction
+                                   active-entities
+                                   max-iterations)))
+
+(defn tick-entities!
+  [{:keys [ctx/active-entities]
+    :as ctx}]
+  (doseq [eid active-entities]
+    (tick-entity! ctx eid)))
