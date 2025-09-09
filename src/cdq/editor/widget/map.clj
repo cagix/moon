319b814e63131a7f0d0f2cbb3b1531a55c1c9bf4
Michael Sappler <michaelsappler@gmail.com>
2025-09-10 01:30:37 +0530

    declare remove component button

--- src/cdq/editor/widget/map.clj
+++ src/cdq/editor/widget/map.clj
@@ -15,21 +15,19 @@
   (name k) ;(str "[GRAY]:" (namespace k) "[]/" (name k))
   )
 
-(defn- remove-component-button [k table]
-  (widget/text-button "-"
-                      (fn [_actor ctx]
-                        (actor/remove! (utils/find-first (fn [actor]
-                                                           (and (actor/user-object actor)
-                                                                (= k ((actor/user-object actor) 0))))
-                                                         (group/children table)))
-                        (editor-window/rebuild! ctx))))
-
 (defn- label-cell
   [{:keys [display-remove-component-button? k table label-text]}]
   {:actor {:actor/type :actor.type/table
            :cell-defaults {:pad 2}
            :rows [[{:actor (when display-remove-component-button?
-                             (remove-component-button k table))
+                             {:actor/type :actor.type/text-button
+                              :text "-"
+                              :on-clicked (fn [_actor ctx]
+                                            (actor/remove! (utils/find-first (fn [actor]
+                                                                               (and (actor/user-object actor)
+                                                                                    (= k ((actor/user-object actor) 0))))
+                                                                             (group/children table)))
+                                            (editor-window/rebuild! ctx))})
                     :left? true}
                    {:actor {:actor/type :actor.type/label
                             :label/text label-text}}]]}
