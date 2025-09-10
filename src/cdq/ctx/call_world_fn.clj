463c24d2f7134c67c59b5fd9606a05845808e921
Michael Sappler <michaelsappler@gmail.com>
2025-09-10 16:12:38 +0530

    reset-game-state is just a few functions pipelined

---
+++ src/cdq/ctx/call_world_fn.clj
@@ -0,0 +1,21 @@
+(ns cdq.ctx.call-world-fn
+  (:require [cdq.db :as db]
+            [clojure.gdx.utils.disposable :as disposable]))
+
+(defn do!
+  [{:keys [ctx/db
+           ctx/textures]
+    :as ctx}
+   world-fn]
+  (let [{:keys [tiled-map
+                start-position]} (let [[f params] world-fn]
+                                   ((requiring-resolve f)
+                                    (assoc params
+                                           :creature-properties (db/all-raw db :properties/creatures)
+                                           :textures textures)))]
+    (assert tiled-map)
+    (assert start-position)
+    (when-let [tiled-map (:world/tiled-map (:ctx/world ctx))]
+      (disposable/dispose! tiled-map))
+    (assoc ctx :ctx/world {:world/tiled-map tiled-map
+                           :world/start-position start-position})))
