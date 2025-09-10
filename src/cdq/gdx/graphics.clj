a6bafd093659b6f4d43c2db0e5e829e93ea75361
Michael Sappler <michaelsappler@gmail.com>
2025-09-10 17:45:13 +0530

    create cdq.gdx.graphics

--- src/cdq/gdx/graphics.clj
+++ src/cdq/gdx/graphics.clj
@@ -1,10 +1,94 @@
 (ns cdq.gdx.graphics
-  (:require [cdq.utils :as utils]
+  (:require [cdq.utils :as utils] ; <- name with clojure !
+
+            ;; pass file-handles !
+            [clojure.gdx :as gdx]
+            [clojure.gdx.files :as files]
+            ;;
+
             [clojure.earlygrey.shape-drawer :as sd]
-            [clojure.gdx.graphics.g2d.batch :as batch]
+            [clojure.gdx.graphics :as graphics]
+            [clojure.gdx.graphics.camera :as camera]
             [clojure.gdx.graphics.color :as color]
+            [clojure.gdx.graphics.texture :as texture]
+            [clojure.gdx.graphics.tiled-map-renderer :as tm-renderer]
+            [clojure.gdx.graphics.pixmap :as pixmap]
+            [clojure.gdx.graphics.g2d.batch :as batch]
             [clojure.gdx.graphics.g2d.bitmap-font :as bitmap-font]
-            [clojure.gdx.graphics.g2d.texture-region :as texture-region]))
+            [clojure.gdx.graphics.g2d.freetype :as freetype]
+            [clojure.gdx.graphics.g2d.texture-region :as texture-region]
+            [clojure.gdx.graphics.g2d.sprite-batch :as sprite-batch]
+            [clojure.gdx.utils.viewport :as viewport]))
+
+(def font-file "exocet/films.EXL_____.ttf")
+(def font-params {:size 16
+             :quality-scaling 2
+             :enable-markup? true
+             ; false, otherwise scaling to world-units not visible
+             :use-integer-positions? false})
+
+(def world-viewport-width 1440)
+(def world-viewport-height 900)
+
+(def path-format "cursors/%s.png")
+
+(def cursor-data
+  {:cursors/bag                   ["bag001"       [0   0]]
+   :cursors/black-x               ["black_x"      [0   0]]
+   :cursors/default               ["default"      [0   0]]
+   :cursors/denied                ["denied"       [16 16]]
+   :cursors/hand-before-grab      ["hand004"      [4  16]]
+   :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
+   :cursors/hand-grab             ["hand003"      [4  16]]
+   :cursors/move-window           ["move002"      [16 16]]
+   :cursors/no-skill-selected     ["denied003"    [0   0]]
+   :cursors/over-button           ["hand002"      [0   0]]
+   :cursors/sandclock             ["sandclock"    [16 16]]
+   :cursors/skill-not-usable      ["x007"         [0   0]]
+   :cursors/use-skill             ["pointer004"   [0   0]]
+   :cursors/walking               ["walking"      [16 16]]})
+
+(defn create
+  [graphics
+   {:keys [
+           ; cursor-data
+           ; path-format
+           textures-to-load
+           world-unit-scale
+           ; world-viewport-width
+           ; world-viewport-height
+           ; font-file
+           ; font-params
+           ]}]
+  (let [batch (sprite-batch/create)
+        shape-drawer-texture (let [pixmap (doto (pixmap/create)
+                                            (pixmap/set-color! color/white)
+                                            (pixmap/draw-pixel! 0 0))
+                                   texture (texture/create pixmap)]
+                               (pixmap/dispose! pixmap)
+                               texture)]
+    {:ctx/batch batch
+     :ctx/tiled-map-renderer (tm-renderer/create world-unit-scale batch)
+     :ctx/graphics graphics
+     :ctx/textures (into {} (for [[path file-handle] textures-to-load]
+                              [path (texture/from-file file-handle)]))
+     :ctx/cursors (update-vals cursor-data
+                               (fn [[file [hotspot-x hotspot-y]]]
+                                 (let [pixmap (pixmap/create (files/internal (gdx/files) (format path-format file)))
+                                       cursor (graphics/cursor graphics pixmap hotspot-x hotspot-y)]
+                                   (.dispose pixmap)
+                                   cursor)))
+     :ctx/world-viewport (let [world-width  (* world-viewport-width world-unit-scale)
+                               world-height (* world-viewport-height world-unit-scale)]
+                           (viewport/fit world-width
+                                         world-height
+                                         (camera/orthographic :y-down? false
+                                                              :world-width world-width
+                                                              :world-height world-height)))
+     :ctx/default-font (freetype/generate-font (files/internal (gdx/files) font-file)
+                                               font-params)
+     :ctx/shape-drawer-texture shape-drawer-texture
+     :ctx/shape-drawer (sd/create batch (texture/region shape-drawer-texture 1 0 1 1))}))
 
 (defn draw-texture-region!
   [{:keys [ctx/batch
