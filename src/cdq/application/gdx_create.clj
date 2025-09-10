4cb809abb307ba7f3b26dd4e6c80235dd8eb5c88
Michael Sappler <michaelsappler@gmail.com>
2025-09-10 21:43:34 +0530

    configure after gdx create

--- src/cdq/application/gdx_create.clj
+++ src/cdq/application/gdx_create.clj
@@ -12,45 +12,30 @@
 
 (defn after-gdx-create!
   [ctx]
-  (vis-ui/load! {:skin-scale :x1})
-  (let [graphics (cdq.gdx.graphics/create
-                  (gdx/graphics)
-                  {
-                   :ui-viewport {:width 1440
-                                 :height 900}
-                   :default-font {:file-handle (files/internal (gdx/files) "exocet/films.EXL_____.ttf")
-                                  :params {:size 16
-                                           :quality-scaling 2
-                                           :enable-markup? true
-                                           :use-integer-positions? false} ; false, otherwise scaling to world-units not visible
-                                  }
-                   :colors {"PRETTY_NAME" [0.84 0.8 0.52 1]}
-                   :cursors (update-vals {:cursors/bag                   ["bag001"       [0   0]]
-                                          :cursors/black-x               ["black_x"      [0   0]]
-                                          :cursors/default               ["default"      [0   0]]
-                                          :cursors/denied                ["denied"       [16 16]]
-                                          :cursors/hand-before-grab      ["hand004"      [4  16]]
-                                          :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
-                                          :cursors/hand-grab             ["hand003"      [4  16]]
-                                          :cursors/move-window           ["move002"      [16 16]]
-                                          :cursors/no-skill-selected     ["denied003"    [0   0]]
-                                          :cursors/over-button           ["hand002"      [0   0]]
-                                          :cursors/sandclock             ["sandclock"    [16 16]]
-                                          :cursors/skill-not-usable      ["x007"         [0   0]]
-                                          :cursors/use-skill             ["pointer004"   [0   0]]
-                                          :cursors/walking               ["walking"      [16 16]]}
-                                         (fn [[short-path hotspot]]
-                                           [(files/internal (gdx/files) (format "cursors/%s.png" short-path))
-                                            hotspot]))
-                   :world-unit-scale (float (/ 48))
-                   :world-viewport {:width 1440 :height 900}
-                   :textures-to-load (cdq.files/search (gdx/files)
-                                                       {:folder "resources/"
-                                                        :extensions #{"png" "bmp"}})
-                   })
+  (vis-ui/load! (:cdq.vis-ui (:ctx/config ctx)))
+  (let [files (gdx/files)
+        {:keys [ui-viewport
+                default-font
+                colors
+                cursors
+                tile-size
+                world-viewport
+                texture-folder]} (:after-gdx-create (:ctx/config ctx))
+        graphics-config {:ui-viewport ui-viewport
+                         :default-font {:file-handle (files/internal files (:path default-font))
+                                        :params (:params default-font)}
+                         :colors colors
+                         :cursors (update-vals (:data cursors)
+                                               (fn [[short-path hotspot]]
+                                                 [(files/internal files (format (:path-format cursors) short-path))
+                                                  hotspot]))
+                         :world-unit-scale (float (/ tile-size))
+                         :world-viewport world-viewport
+                         :textures-to-load (cdq.files/search files texture-folder)}
+        graphics (cdq.gdx.graphics/create (gdx/graphics) graphics-config)
         input (gdx/input)
         stage (scene2d/stage (:ctx/ui-viewport graphics)
-                             (:ctx/batch graphics))]
+                             (:ctx/batch       graphics))]
     (input/set-processor! input stage)
     (merge ctx
            {:ctx/graphics (assoc graphics :ctx/draw-fns (:ctx/draw-fns ctx))
@@ -62,4 +47,4 @@
                                (for [sound-name (->> sound-names io/resource slurp edn/read-string)
                                      :let [path (format path-format sound-name)]]
                                  [sound-name
-                                  (audio/sound (gdx/audio) (files/internal (gdx/files) path))])))})))
+                                  (audio/sound (gdx/audio) (files/internal files path))])))})))
