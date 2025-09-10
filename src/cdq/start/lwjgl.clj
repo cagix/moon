eb62f1750163794a512ccc7c62c2e17e56cab602
Michael Sappler <michaelsappler@gmail.com>
2025-09-10 22:04:13 +0530

    upfactor listener

--- src/cdq/start/lwjgl.clj
+++ src/cdq/start/lwjgl.clj
@@ -1,10 +1,25 @@
 (ns cdq.start.lwjgl
-  (:require [clojure.gdx.backends.lwjgl :as lwjgl]))
+  (:require [cdq.ctx :as ctx]
+            [clojure.gdx.backends.lwjgl :as lwjgl]))
 
 (defn do!
-  [{:keys [ctx/config]
+  [{:keys [ctx/application-state
+           ctx/config]
     :as ctx}]
-  (let [{:keys [listener
-                config]} (:cdq.start.lwjgl config)]
-    (lwjgl/start-application! ((requiring-resolve listener) ctx)
-                              config)))
+  (lwjgl/start-application!
+   (let [config (:cdq.start.lwjgl.listener config)]
+     {:create! (fn []
+                 (reset! application-state ((requiring-resolve (:reset-game-state config))
+                                            (let [ctx ((requiring-resolve (:after-gdx-create config)) ctx)]
+                                              (ctx/reset-stage! ctx)
+                                              ctx)
+                                            (:starting-world config))))
+      :dispose! (fn []
+                  ((requiring-resolve (:dispose config)) @application-state))
+      :render! (fn []
+                 (swap! application-state (requiring-resolve (:render config))))
+      :resize! (fn [width height]
+                 ((requiring-resolve (:resize config)) @application-state width height))
+      :pause! (fn [])
+      :resume! (fn [])})
+   (:config (:cdq.start.lwjgl config))))
