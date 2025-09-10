638a9fd2bc43da541e691ac115247527d8bc05e6
Michael Sappler <michaelsappler@gmail.com>
2025-09-10 14:40:38 +0530

    Move raycaster stuff to raycaster

--- src/cdq/application/reset_game_state.clj
+++ src/cdq/application/reset_game_state.clj
@@ -8,41 +8,18 @@
             [cdq.grid :as grid]
             [cdq.grid.cell :as cell]
             [cdq.grid2d :as g2d]
-            [cdq.math.raycaster :as raycaster]
-            [cdq.raycaster]
-            [cdq.path-rays :as path-rays]
+            [cdq.raycaster :as raycaster]
             [cdq.utils :as utils]
             [clojure.gdx.maps.tiled :as tiled]
             [clojure.gdx.utils.disposable :as disposable]))
 
-(defn- set-arr [arr cell cell->blocked?]
-  (let [[x y] (:position cell)]
-    (aset arr x y (boolean (cell->blocked? cell)))))
-
-(defn- create-raycaster-arr [grid]
+(defn- create-raycaster [grid]
   (let [width  (g2d/width  (.g2d grid))
         height (g2d/height (.g2d grid))
-        arr (make-array Boolean/TYPE width height)]
-    (doseq [cell (g2d/cells (.g2d grid))]
-      (set-arr arr @cell cell/blocks-vision?))
-    [arr width height]))
-
-(defn- create-raycaster [grid]
-  (let [arr (create-raycaster-arr grid)]
-    (reify cdq.raycaster/Raycaster
-      (blocked? [_ start end]
-        (raycaster/blocked? arr start end))
-
-      (path-blocked? [_ start target path-w]
-        (let [[start1,target1,start2,target2] (path-rays/create-double-ray-endpositions start target path-w)]
-          (or
-           (raycaster/blocked? arr start1 target1)
-           (raycaster/blocked? arr start2 target2))))
-
-      (line-of-sight? [_ source target]
-        (not (raycaster/blocked? arr
-                                 (:body/position (:entity/body source))
-                                 (:body/position (:entity/body target))))))))
+        cells  (for [cell (map deref (g2d/cells (.g2d grid)))]
+                 [(:position cell)
+                  (boolean (cell/blocks-vision? cell))])]
+    (raycaster/create width height cells)))
 
 ; could use inside tiles only for >1 tile bodies (for example size 4.5 use 4x4 tiles for occupied)
 ; => only now there are no >1 tile entities anyway
--- src/cdq/raycaster.clj
+++ src/cdq/raycaster.clj
@@ -1,6 +1,23 @@
-(ns cdq.raycaster)
+(ns cdq.raycaster
+  (:require [cdq.math.raycaster :as raycaster]
+            [cdq.path-rays :as path-rays]))
 
-(defprotocol Raycaster
-  (blocked? [_ start end])
-  (path-blocked? [_ start target path-w])
-  (line-of-sight? [_ source target]))
+(defn create [width height cells]
+  (let [arr (make-array Boolean/TYPE width height)]
+    (doseq [[[x y] blocks-vision?] cells]
+      (aset arr x y (boolean blocks-vision?)))
+    [arr width height]))
+
+(defn blocked? [raycaster start end]
+  (raycaster/blocked? raycaster start end))
+
+(defn path-blocked? [raycaster start target path-w]
+  (let [[start1,target1,start2,target2] (path-rays/create-double-ray-endpositions start target path-w)]
+    (or
+     (raycaster/blocked? raycaster start1 target1)
+     (raycaster/blocked? raycaster start2 target2))))
+
+(defn line-of-sight? [raycaster source target]
+  (not (raycaster/blocked? raycaster
+                           (:body/position (:entity/body source))
+                           (:body/position (:entity/body target)))))
