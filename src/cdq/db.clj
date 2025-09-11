0cc3f3b34a64586fa34c0441e4d212f5c32ce8b1
Michael Sappler <michaelsappler@gmail.com>
2025-09-11 15:10:53 +0530

    Remove db protocol

--- src/cdq/db.clj
+++ src/cdq/db.clj
@@ -5,14 +5,17 @@
             [clojure.edn :as edn]
             [clojure.java.io :as io]))
 
-(defprotocol Database
-  (property-types [_])
-  (update! [_ property])
-  (delete! [_ property-id])
-  (get-raw [_ property-id])
-  (all-raw [_ property-type])
-  (build [_ property-id])
-  (build-all [_ property-type]))
+(defn create
+  [{:keys [schemas
+           properties]}]
+  (let [properties-file (io/resource properties)
+        properties (-> properties-file slurp edn/read-string)]
+    (assert (or (empty? properties)
+                (apply distinct? (map :property/id properties))))
+    (run! (partial schemas/validate schemas) properties)
+    {:data (zipmap (map :property/id properties) properties)
+     :file properties-file
+     :schemas schemas}))
 
 (defn save-vals! [data-vals file]
   (->> data-vals
@@ -26,49 +29,47 @@
   (save-vals! (vals data)
               file))
 
-(defrecord DB [data file schemas]
-  Database
-  (property-types [_]
-    (schemas/property-types schemas))
+(defn property-types [{:keys [schemas]}]
+  (schemas/property-types schemas))
 
-  (update! [this {:keys [property/id] :as property}]
-    (assert (contains? property :property/id))
-    (assert (contains? data id))
-    (schemas/validate schemas property)
-    (let [new-db (update this :data assoc id property)]
-      (save! new-db)
-      new-db))
+(defn update!
+  [{:keys [data schemas]
+    :as this}
+   {:keys [property/id] :as property}]
+  (assert (contains? property :property/id))
+  (assert (contains? data id))
+  (schemas/validate schemas property)
+  (let [new-db (update this :data assoc id property)]
+    (save! new-db)
+    new-db))
 
-  (delete! [this property-id]
-    (assert (contains? data property-id))
-    (let [new-db (update this :data dissoc property-id)]
-      (save! new-db)
-      new-db))
+(defn delete!
+  [{:keys [data]
+    :as this}
+   property-id]
+  (assert (contains? data property-id))
+  (let [new-db (update this :data dissoc property-id)]
+    (save! new-db)
+    new-db))
 
-  (get-raw [_ property-id]
-    (utils/safe-get data property-id))
+(defn get-raw [{:keys [data]} property-id]
+  (utils/safe-get data property-id))
 
-  (all-raw [_ property-type]
-    (->> (vals data)
-         (filter #(= property-type (property/type %)))))
+(defn all-raw [{:keys [data]} property-type]
+  (->> (vals data)
+       (filter #(= property-type (property/type %)))))
 
-  (build [this property-id]
-    (schemas/build-values schemas
-                          (get-raw this property-id)
-                          this))
+(defn build
+  [{:keys [schemas]
+    :as this}
+   property-id]
+  (schemas/build-values schemas
+                        (get-raw this property-id)
+                        this))
 
-  (build-all [this property-type]
-    (map #(schemas/build-values schemas % this)
-         (all-raw this property-type))))
-
-(defn create
-  [{:keys [schemas
-           properties]}]
-  (let [properties-file (io/resource properties)
-        properties (-> properties-file slurp edn/read-string)]
-    (assert (or (empty? properties)
-                (apply distinct? (map :property/id properties))))
-    (run! (partial schemas/validate schemas) properties)
-    (map->DB {:data (zipmap (map :property/id properties) properties)
-              :file properties-file
-              :schemas schemas})))
+(defn build-all
+  [{:keys [schemas]
+    :as this}
+   property-type]
+  (map #(schemas/build-values schemas % this)
+       (all-raw this property-type)))
