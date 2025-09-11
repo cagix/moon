a9020f71cf9f834f4295b5ab4a23249ac2ce87cd
Michael Sappler <michaelsappler@gmail.com>
2025-09-11 18:22:26 +0530

    remove default schema editor widgets

--- src/cdq/editor/widgets.clj
+++ src/cdq/editor/widgets.clj
@@ -1,27 +1,48 @@
 (ns cdq.editor.widgets)
 
 (def k->methods*
-  '{:s/map {:create cdq.schema.map/create
-            :value cdq.schema.map/value}
-    :default {:create cdq.editor.widget.default/create
+  '{:s/animation {:create cdq.schema.animation/create
+                  :value cdq.editor.widget.default/value}
+
+    :s/boolean {:create cdq.schema.boolean/create
+                :value cdq.schema.boolean/value}
+
+    :s/enum {:create cdq.schema.enum/create
+             :value cdq.schema.enum/value}
+
+    :s/image {:create cdq.schema.image/create
               :value cdq.editor.widget.default/value}
+
+    :s/map {:create cdq.schema.map/create
+            :value cdq.schema.map/value}
+
     :s/number {:create cdq.editor.widget.edn/create
                :value cdq.editor.widget.edn/value}
-    :s/val-max {:create cdq.editor.widget.edn/create
-                :value cdq.editor.widget.edn/value}
-    :s/string {:create cdq.schema.string/create
-               :value cdq.schema.string/value}
-    :s/boolean {:create cdq.schema.boolean/create
-              :value cdq.schema.boolean/value}
-    :s/enum {:create cdq.schema.enum/create
-           :value cdq.schema.enum/value}
-    :s/sound {:create cdq.schema.sound/create}
-    :s/one-to-one {:create cdq.schema.one-to-one/create
-                   :value cdq.schema.one-to-one/value}
+
     :s/one-to-many {:create cdq.schema.one-to-many/create
                     :value cdq.schema.one-to-many/value}
-    :s/image     {:create cdq.schema.image/create}
-    :s/animation {:create cdq.schema.animation/create}})
+
+    :s/one-to-one {:create cdq.schema.one-to-one/create
+                   :value cdq.schema.one-to-one/value}
+
+    :s/qualified-keyword {:create cdq.editor.widget.default/create
+                          :value cdq.editor.widget.default/value}
+
+    :s/some {:create cdq.editor.widget.default/create
+             :value cdq.editor.widget.default/value}
+
+    :s/sound {:create cdq.schema.sound/create
+              :value cdq.editor.widget.default/value}
+
+    :s/string {:create cdq.schema.string/create
+               :value cdq.schema.string/value}
+
+    :s/val-max {:create cdq.editor.widget.edn/create
+                :value cdq.editor.widget.edn/value}
+
+    :s/vector {:create cdq.editor.widget.default/create
+               :value cdq.editor.widget.default/value}
+    })
 
 (require 'cdq.effects)
 
--- src/cdq/schema.clj
+++ src/cdq/schema.clj
@@ -17,14 +17,12 @@
 (declare k->methods)
 
 (defn create [schema attribute v ctx]
-  (if-let [f (:create (k->methods (get-type schema)))]
-    (f schema attribute v ctx)
-    ((:create (k->methods :default)) schema attribute v ctx)))
+  (let [f (:create (k->methods (get-type schema)))]
+    (f schema attribute v ctx)))
 
 (defn value [schema attribute widget schemas]
-  (if-let [f (:value (k->methods (get-type schema)))]
-    (f schema attribute widget schemas)
-    ((:value (k->methods :default)) schema attribute widget schemas)))
+  (let [f (:value (k->methods (get-type schema)))]
+    (f schema attribute widget schemas)))
 
 (defn build [ctx schema k v]
   (let [widget (actor/build? (create schema k v ctx))]
