95f0f349c03bf5da04c57c4e872c5e9184ffb90f
Michael Sappler <michaelsappler@gmail.com>
2025-09-12 02:03:39 +0530

    upfactor create world

--- src/cdq/create/world.clj
+++ src/cdq/create/world.clj
@@ -1,5 +1,13 @@
 (ns cdq.create.world
-  (:require [cdq.db :as db]))
+  (:require [cdq.content-grid :as content-grid]
+            [cdq.ctx :as ctx]
+            [cdq.db :as db]
+            [cdq.grid.cell :as cell]
+            [cdq.grid2d :as g2d]
+            [cdq.math.raycaster :as raycaster]
+            [cdq.utils :as utils]
+            [clojure.gdx.maps.tiled :as tiled]
+            [clojure.gdx.utils.disposable :as disposable]))
 
 (defn- call-world-fn
   [{:keys [ctx/db
@@ -10,12 +18,137 @@
           :creature-properties (db/all-raw db :properties/creatures)
           :graphics graphics)))
 
+(defn- asdf
+  [ctx
+   {:keys [tiled-map
+           start-position]}]
+  (assert tiled-map)
+  (assert start-position)
+  (when-let [tiled-map (:world/tiled-map (:ctx/world ctx))]
+    (disposable/dispose! tiled-map))
+  (assoc ctx :ctx/world {:world/tiled-map tiled-map
+                         :world/start-position start-position}))
+
+(defn- create-explored-tile-corners [tiled-map]
+  (atom (g2d/create-grid (:tiled-map/width  tiled-map)
+                         (:tiled-map/height tiled-map)
+                         (constantly false))))
+
+(defn- grid->raycaster [g2d]
+  (let [width  (g2d/width  g2d)
+        height (g2d/height g2d)
+        cells  (for [cell (map deref (g2d/cells g2d))]
+                 [(:position cell)
+                  (boolean (cell/blocks-vision? cell))])]
+    (raycaster/create width height cells)))
+
+(defrecord RCell [position
+                  middle ; only used @ potential-field-follow-to-enemy -> can remove it.
+                  adjacent-cells
+                  movement
+                  entities
+                  occupied
+                  good
+                  evil]
+  cell/Cell
+  (blocked? [_ z-order]
+    (case movement
+      :none true ; wall
+      :air (case z-order ; water/doodads
+             :z-order/flying false
+             :z-order/ground true)
+      :all false)) ; ground/floor
+
+  (blocks-vision? [_]
+    (= movement :none))
+
+  (occupied-by-other? [_ eid]
+    (some #(not= % eid) occupied))
+
+  (nearest-entity [this faction]
+    (-> this faction :eid))
+
+  (nearest-entity-distance [this faction]
+    (-> this faction :distance))
+
+  (pf-blocked? [this]
+    (cell/blocked? this :z-order/ground)))
+
+(defn- create-grid-cell [position movement]
+  {:pre [(#{:none :air :all} movement)]}
+  (map->RCell
+   {:position position
+    :middle (utils/tile->middle position)
+    :movement movement
+    :entities #{}
+    :occupied #{}}))
+
+(defn- create-grid [tiled-map]
+  (g2d/create-grid (:tiled-map/width  tiled-map)
+                   (:tiled-map/height tiled-map)
+                   (fn [position]
+                     (atom (create-grid-cell position
+                                             (case (tiled/movement-property tiled-map position)
+                                               "none" :none
+                                               "air"  :air
+                                               "all"  :all))))))
+
+(defn- build-world
+  [{:keys [ctx/config
+           ctx/world]
+    :as ctx}]
+  (merge ctx
+         (let [tiled-map (:world/tiled-map world)
+               grid (create-grid tiled-map)]
+           {:ctx/grid grid
+            :ctx/content-grid (content-grid/create (:tiled-map/width  tiled-map)
+                                                   (:tiled-map/height tiled-map)
+                                                   (:content-grid-cell-size (:world config)))
+            :ctx/explored-tile-corners (create-explored-tile-corners tiled-map)
+            :ctx/raycaster (grid->raycaster grid)
+            :ctx/elapsed-time 0
+            :ctx/max-speed (/ (:ctx/minimum-size ctx)
+                              (:ctx/max-delta ctx))
+            :ctx/potential-field-cache (atom nil)
+            :ctx/factions-iterations (:potential-field-factions-iterations (:world config))
+            :ctx/id-counter (atom 0)
+            :ctx/entity-ids (atom {})
+            :ctx/render-z-order (utils/define-order (:ctx/z-orders ctx))})))
+
+(defn- spawn-player!
+  [{:keys [ctx/config
+           ctx/db
+           ctx/entity-ids
+           ctx/world]
+    :as ctx}]
+  (ctx/handle-txs! ctx
+                   [[:tx/spawn-creature (let [{:keys [creature-id
+                                                      components]} (:cdq.game/player-props config)]
+                                          {:position (utils/tile->middle (:world/start-position world))
+                                           :creature-property (db/build db creature-id)
+                                           :components components})]])
+  (let [eid (get @entity-ids 1)]
+    (assert (:entity/player? @eid))
+    (assoc ctx :ctx/player-eid eid)))
+
+(defn- spawn-enemies!
+  [{:keys [ctx/config
+           ctx/db
+           ctx/world]
+    :as ctx}]
+  (doseq [[position creature-id] (tiled/positions-with-property (:world/tiled-map world) "creatures" "id")]
+    (ctx/handle-txs! ctx
+                     [[:tx/spawn-creature {:position (utils/tile->middle position)
+                                           :creature-property (db/build db (keyword creature-id))
+                                           :components (:cdq.game/enemy-components config)}]]))
+  ctx)
+
 (defn do!
   ([ctx]
    (do! ctx (:starting-world (:cdq.create.world (:ctx/config ctx)))))
   ([ctx world-fn]
    (-> ctx
-       ((requiring-resolve 'cdq.ctx.call-world-fn/do!) (call-world-fn ctx world-fn))
-       ((requiring-resolve 'cdq.ctx.build-world/do!))
-       ((requiring-resolve 'cdq.ctx.spawn-player/do!))
-       ((requiring-resolve 'cdq.ctx.spawn-enemies/do!)))))
+       (asdf (call-world-fn ctx world-fn))
+       build-world
+       spawn-player!
+       spawn-enemies!)))
