794ced35413a863f13276d33823e2b4ecf7ccaa9
Michael Sappler <michaelsappler@gmail.com>
2025-09-12 14:20:48 +0530

    Refactor handle-txs!

--- src/cdq/ctx.clj
+++ src/cdq/ctx.clj
@@ -1,4 +1,27 @@
 (ns cdq.ctx)
 
-(defprotocol TransactionHandler
-  (handle-txs! [_ transactions]))
+(defn handle-txs!
+  [{:keys [ctx/txs-fn-map]
+    :as ctx}
+   transactions]
+  (loop [ctx ctx
+         transactions transactions
+         handled-transactions []]
+    (if (seq transactions)
+      (let [[k & params :as transaction] (first transactions)]
+        (if transaction
+          (let [_ (assert (vector? transaction))
+                f (get txs-fn-map k)
+                new-transactions (try
+                                  (apply f ctx params)
+                                  (catch Throwable t
+                                    (throw (ex-info "Error handling transaction"
+                                                    {:transaction transaction}
+                                                    t))))]
+            (recur ctx
+                   (concat (or new-transactions []) (rest transactions))
+                   (conj handled-transactions transaction)))
+          (recur ctx
+                 (rest transactions)
+                 handled-transactions)))
+      handled-transactions)))
--- src/cdq/start/txs.clj
+++ src/cdq/start/txs.clj
@@ -1,39 +1,5 @@
 (ns cdq.start.txs
-  (:require [cdq.ctx]))
-
-(defn- handle-tx!
-  [{:keys [ctx/txs-fn-map]
-    :as ctx}
-   {k 0 :as tx}]
-  (apply (get txs-fn-map k) ctx (rest tx)))
-
-(defn- create-fn-map [{:keys [ks sym-format]}]
-  (into {}
-        (for [k ks
-              :let [sym (symbol (format sym-format (name k)))
-                    f (requiring-resolve sym)]]
-          (do
-           (assert f (str "Cannot resolve " sym))
-           [k f]))))
+  (:require [cdq.fn-map :as fn-map]))
 
 (defn do! [ctx]
-  (extend-type (class ctx)
-    cdq.ctx/TransactionHandler
-    (handle-txs! [ctx transactions]
-      (loop [ctx ctx
-             txs transactions
-             handled []]
-        (if (seq txs)
-          (let [tx (first txs)]
-            (if tx
-              (let [_ (assert (vector? tx))
-                    new-txs (try
-                             (handle-tx! ctx tx)
-                             (catch Throwable t
-                               (throw (ex-info "Error handling transaction" {:transaction tx} t))))]
-                (recur ctx
-                       (concat (or new-txs []) (rest txs))
-                       (conj handled tx)))
-              (recur ctx (rest txs) handled)))
-          handled))))
-  (update ctx :ctx/txs-fn-map create-fn-map))
+  (update ctx :ctx/txs-fn-map fn-map/create))
