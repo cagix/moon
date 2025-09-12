295b35b272fcbec0f04d51a9b0320a60cca265ca
Michael Sappler <michaelsappler@gmail.com>
2025-09-12 20:47:03 +0530

    Reintegrate clojure.dev-loop and rename defproject to cdq

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject moon "-SNAPSHOT"
+(defproject cdq "-SNAPSHOT"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [
                  [com.badlogicgames.gdx/gdx                   "1.13.5"]
@@ -7,27 +7,24 @@
                  [com.badlogicgames.gdx/gdx-freetype-platform "1.13.5" :classifier "natives-desktop"]
                  [com.badlogicgames.gdx/gdx-platform          "1.13.5" :classifier "natives-desktop"]
                  [com.github.cdorrat/reduce-fsm "fe1c914d68"]
-                 [com.github.damn/clojure.dev-loop "ef54a03"]
                  [com.kotcrab.vis/vis-ui "1.5.2"]
                  [fr.reuz/qrecord "0.1.0"]
                  [metosin/malli "0.13.0"]
+                 [nrepl "0.9.0"]
                  [space.earlygrey/shapedrawer "2.5.0"]
+                 [org.clj-commons/pretty "3.2.0"]
                  [org.clojure/clojure "1.12.0"]
+                 [org.clojure/tools.namespace "1.3.0"]
                  ]
-
   :java-source-paths ["src"]
-
   :aliases {
             "dev"      ["run" "-m" "clojure.dev-loop" "((requiring-resolve 'clojure.start/-main))"]
             "levelgen" ["run" "-m" "clojure.dev-loop" "((requiring-resolve 'cdq.levelgen/-main))"]
             "ns"       ["hiera" ":layout" ":horizontal"]
             }
-
   :plugins [[lein-hiera "2.0.0"]
             [lein-codox "0.10.8"]]
-
   :target-path "target/%s/" ; https://stackoverflow.com/questions/44246924/clojure-tools-namespace-refresh-fails-with-no-namespace-foo
-
   :jvm-opts ["-Xms256m"
              "-Xmx256m"
              "-Dvisualvm.display.name=CDQ"
