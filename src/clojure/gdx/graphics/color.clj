4cee2626dae5aed25c50ce8b7501c85fb4327d74
Michael Sappler <michaelsappler@gmail.com>
2025-09-12 22:32:28 +0530

    implement float-bits

--- src/clojure/gdx/graphics/color.clj
+++ src/clojure/gdx/graphics/color.clj
@@ -1,11 +1,13 @@
 (ns clojure.gdx.graphics.color
-  (:import (com.badlogic.gdx.graphics Color)))
+  (:import (com.badlogic.gdx.graphics Color)
+           (com.badlogic.gdx.utils NumberUtils)))
 
 (defn create ^Color [[r g b a]]
   (Color. r g b a))
 
 (defn float-bits [[r g b a]]
-  (Color/toFloatBits (float r)
-                     (float g)
-                     (float b)
-                     (float a)))
+  (let [color (bit-or (bit-shift-left (int (* 255 (float a))) 24)
+                      (bit-shift-left (int (* 255 (float b))) 16)
+                      (bit-shift-left (int (* 255 (float g))) 8)
+                      (int (* 255 (float r))))]
+    (NumberUtils/intToFloatColor (unchecked-int color))))
