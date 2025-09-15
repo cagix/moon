fc6e480c2137c924d205bd28706817c2264b63b4
Michael Sappler <michaelsappler@gmail.com>
2025-09-15 13:36:13 +0530

    Effect protocol

--- src/cdq/effect.clj
+++ src/cdq/effect.clj
@@ -1,35 +1,10 @@
 (ns cdq.effect)
 
-(comment
- (defprotocol Effect
-   (fooz [_ effect-ctx]))
-
- (extend-type clojure.lang.PersistentVector
-   Effect
-   (fooz [[k] effect-ctx]
-     (str "dispatching on k internally")))
-
- (fooz [:effect/bar 123] {}))
-
-(declare k->method-map)
-
-(defn applicable? [{k 0 :as component} effect-ctx]
-  ((:applicable? (k->method-map k)) component effect-ctx))
-
-(defn handle [{k 0 :as component} effect-ctx ctx]
-  ((:handle (k->method-map k)) component effect-ctx ctx))
-
-(defn useful? [{k 0 :as component} effect-ctx ctx]
-  (if-let [f (:useful? (k->method-map k))]
-    (f component effect-ctx ctx)
-    true))
-
-(defn render [{k 0 :as component} effect-ctx ctx]
-  (if-let [f (:render (k->method-map k))]
-    (f component effect-ctx ctx)
-    nil))
-
-; TODO this doesnt go here
+(defprotocol Effect
+  (applicable? [_ effect-ctx])
+  (useful?     [_ effect-ctx ctx])
+  (handle      [_ effect-ctx ctx])
+  (render      [_ effect-ctx ctx]))
 
 (defn filter-applicable? [effect-ctx effect]
   (filter #(applicable? % effect-ctx) effect))
--- src/cdq/start/load_effects.clj
+++ src/cdq/start/load_effects.clj
@@ -1,13 +1,35 @@
 (ns cdq.start.load-effects
-  (:require cdq.effect
+  (:require [cdq.effect :as effect]
             [clojure.edn :as edn]
             [clojure.java.io :as io]
-            [clojure.symbol :as symbol]))
+            [clojure.symbol :as symbol])
+  (:import (clojure.lang MapEntry
+                         PersistentVector)))
 
 (defn do! [ctx]
-  (.bindRoot #'cdq.effect/k->method-map (symbol/require-resolve-symbols
-                                         (-> "effects_fn_map.edn"
-                                             io/resource
-                                             slurp
-                                             edn/read-string)))
+  (let [k->fn (-> "effects_fn_map.edn"
+                  io/resource
+                  slurp
+                  edn/read-string
+                  symbol/require-resolve-symbols)
+        fn-map {:applicable? (fn [{k 0 :as component} effect-ctx]
+                               ((:applicable? (k->fn k)) component effect-ctx))
+
+                :handle (fn [{k 0 :as component} effect-ctx ctx]
+                          ((:handle (k->fn k)) component effect-ctx ctx))
+
+                :useful? (fn [{k 0 :as component} effect-ctx ctx]
+                           (if-let [f (:useful? (k->fn k))]
+                             (f component effect-ctx ctx)
+                             true))
+
+                :render (fn [{k 0 :as component} effect-ctx ctx]
+                          (if-let [f (:render (k->fn k))]
+                            (f component effect-ctx ctx)
+                            nil))}]
+    (extend MapEntry         effect/Effect fn-map)
+    (extend PersistentVector effect/Effect fn-map)
+    ; TODO both have IPersistentVector ?
+    ; and other stuff in common
+    )
   ctx)
