65ea77e250917ef5b5eea50b13e6044d5c028041
Michael Sappler <michaelsappler@gmail.com>
2025-09-15 19:23:38 +0530

    Remove cdq.start.lwjgl config

--- src/cdq/start/lwjgl.clj
+++ src/cdq/start/lwjgl.clj
@@ -2,24 +2,23 @@
   (:require [clojure.gdx.backends.lwjgl :as lwjgl]))
 
 (defn do!
-  [{:keys [ctx/application-state
-           ctx/config]
-    :as ctx}]
+  [{:keys [ctx/application-state]
+    :as ctx}
+   {:keys [create
+           dispose
+           render
+           resize
+           config]}]
   (reset! application-state ctx)
-  (let [{:keys [create
-                dispose
-                render
-                resize
-                config]} (:cdq.start.lwjgl config)]
-    (lwjgl/start-application!
-     {:create! (fn [gdx]
-                 (swap! application-state create gdx))
-      :dispose! (fn []
-                  (swap! application-state dispose))
-      :render! (fn []
-                 (swap! application-state render))
-      :resize! (fn [width height]
-                 (swap! application-state resize width height))
-      :pause! (fn [])
-      :resume! (fn [])}
-     config)))
+  (lwjgl/start-application!
+   {:create! (fn [gdx]
+               (swap! application-state create gdx))
+    :dispose! (fn []
+                (swap! application-state dispose))
+    :render! (fn []
+               (swap! application-state render))
+    :resize! (fn [width height]
+               (swap! application-state resize width height))
+    :pause! (fn [])
+    :resume! (fn [])}
+   config))
