0e585b372e3172ff82f9b1bb7d2be9687f29eff4
Michael Sappler <michaelsappler@gmail.com>
2025-09-15 21:15:43 +0530

    Effect APersistentVector

--- src/cdq/start/load_effects.clj
+++ src/cdq/start/load_effects.clj
@@ -3,33 +3,29 @@
             [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.symbol :as symbol])
-  (:import (clojure.lang MapEntry
-                         PersistentVector)))
+  (:import (clojure.lang APersistentVector)))
 
 (defn do! [ctx edn-resource]
   (let [k->fn (-> edn-resource
                   io/resource
                   slurp
                   edn/read-string
-                  symbol/require-resolve-symbols)
-        fn-map {:applicable? (fn [{k 0 :as component} effect-ctx]
-                               ((:applicable? (k->fn k)) component effect-ctx))
+                  symbol/require-resolve-symbols)]
+    (extend APersistentVector
+      effect/Effect
+      {:applicable? (fn [{k 0 :as component} effect-ctx]
+                      ((:applicable? (k->fn k)) component effect-ctx))
 
-                :handle (fn [{k 0 :as component} effect-ctx ctx]
-                          ((:handle (k->fn k)) component effect-ctx ctx))
+       :handle (fn [{k 0 :as component} effect-ctx ctx]
+                 ((:handle (k->fn k)) component effect-ctx ctx))
 
-                :useful? (fn [{k 0 :as component} effect-ctx ctx]
-                           (if-let [f (:useful? (k->fn k))]
-                             (f component effect-ctx ctx)
-                             true))
+       :useful? (fn [{k 0 :as component} effect-ctx ctx]
+                  (if-let [f (:useful? (k->fn k))]
+                    (f component effect-ctx ctx)
+                    true))
 
-                :render (fn [{k 0 :as component} effect-ctx ctx]
-                          (if-let [f (:render (k->fn k))]
-                            (f component effect-ctx ctx)
-                            nil))}]
-    (extend MapEntry         effect/Effect fn-map)
-    (extend PersistentVector effect/Effect fn-map)
-    ; TODO both have IPersistentVector ?
-    ; and other stuff in common
-    )
+       :render (fn [{k 0 :as component} effect-ctx ctx]
+                 (if-let [f (:render (k->fn k))]
+                   (f component effect-ctx ctx)
+                   nil))}))
   ctx)
