6c605a7a1e70d0af1ce546e17061ff99f4f20904
Michael Sappler <michaelsappler@gmail.com>
2025-09-16 03:44:59 +0530

    formatting, forgot help

--- src/cdq/effects/sound.clj
+++ src/cdq/effects/sound.clj
@@ -1,6 +1,10 @@
 (ns cdq.effects.sound)
 
-(defn applicable? [_ _ctx] true)
-(defn useful? [_ _effect-ctx _ctx] false)
+(defn applicable? [_ _ctx]
+  true)
+
+(defn useful? [_ _effect-ctx _ctx]
+  false)
+
 (defn handle [[_ sound] _effect-ctx _ctx]
   [[:tx/sound sound]])
---
+++ src/cdq/ui/dev_menu/menus/help.clj
@@ -0,0 +1,4 @@
+(ns cdq.ui.dev-menu.menus.help)
+
+(defn create-items [_ctx text]
+  [{:label text}])
