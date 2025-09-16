466130700fff6031bdcfad093c4458bc991bf4b4
Michael Sappler <michaelsappler@gmail.com>
2025-09-16 15:10:05 +0530

    pass update-labels to dev menu

--- src/cdq/ui/dev_menu.clj
+++ src/cdq/ui/dev_menu.clj
@@ -1,29 +1,16 @@
 (ns cdq.ui.dev-menu
   (:require [cdq.ui.menu :as menu]))
 
-(def config
-  '[[cdq.ui.dev-menu.update-labels.mouseover-entity-id/create "images/mouseover.png"]
-    [cdq.ui.dev-menu.update-labels.elapsed-time/create "images/clock.png"]
-    [cdq.ui.dev-menu.update-labels.paused/item]
-    [cdq.ui.dev-menu.update-labels.ui-mouse-position/item]
-    [cdq.ui.dev-menu.update-labels.world-mouse-position/item]
-    [cdq.ui.dev-menu.update-labels.zoom/create "images/zoom.png"]
-    [cdq.ui.dev-menu.update-labels.fps/create "images/fps.png"]])
-
 (defn create
   [{:keys [ctx/graphics]
     :as ctx}
-   {:keys [menus]}]
+   {:keys [menus
+           update-labels]}]
   (menu/create
    {:menus (for [menu menus]
              (update menu :items (fn [[f params]]
                                    (f ctx params))))
-    :update-labels (let [textures (:ctx/textures graphics)
-                         ->texture (fn [path]
-                                     (assert (contains? textures path))
-                                     (get textures path))]
-                     (for [[sym icon] config
-                           :let [avar (requiring-resolve sym)]]
-                       (if icon
-                         (avar (->texture icon))
-                         @avar)))}))
+    :update-labels (for [[avar icon] update-labels]
+                     (if icon
+                       (avar (get (:ctx/textures graphics) icon))
+                       @avar))}))
