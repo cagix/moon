6c416a563bf864bd2037b1d1da9d8b8be40857ab
Michael Sappler <michaelsappler@gmail.com>
2025-09-16 22:18:17 +0530

    impl input at create input

--- src/cdq/create/input.clj
+++ src/cdq/create/input.clj
@@ -1,5 +1,23 @@
-(ns cdq.create.input)
+(ns cdq.create.input
+  (:require [cdq.ctx.input]
+            [clojure.input :as input]
+            [cdq.gdx.math.vector2 :as v]))
 
 (defn do! [{:keys [ctx/gdx]
             :as ctx}]
   (assoc ctx :ctx/input (:clojure.gdx/input gdx)))
+
+(defn- WASD-movement-vector [input]
+  (let [r (when (input/key-pressed? input :d) [1  0])
+        l (when (input/key-pressed? input :a) [-1 0])
+        u (when (input/key-pressed? input :w) [0  1])
+        d (when (input/key-pressed? input :s) [0 -1])]
+    (when (or r l u d)
+      (let [v (v/add-vs (remove nil? [r l u d]))]
+        (when (pos? (v/length v))
+          v)))))
+
+(extend-type com.badlogic.gdx.Input
+  cdq.ctx.input/Input
+  (player-movement-vector [input]
+    (WASD-movement-vector input)))
