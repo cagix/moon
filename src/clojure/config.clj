c66aae6c36124d29c6dc97b249a51eb45216ce1e
Michael Sappler <michaelsappler@gmail.com>
2025-09-16 17:17:05 +0530

    Move clojure.symbol in clojure.config

--- src/clojure/config.clj
+++ src/clojure/config.clj
@@ -1,11 +1,25 @@
 (ns clojure.config
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
-            [clojure.symbol :as symbol]))
+            [clojure.walk :as walk]))
+
+(defn- java-class? [s]
+  (boolean (re-matches #".*\.[A-Z][A-Za-z0-9_]*" s)))
+
+(defn- require-resolve-symbols [form]
+  (if (symbol? form)
+    (if (java-class? (str form))
+      form
+      (if (namespace form)
+        (let [var (requiring-resolve form)]
+          (assert var form)
+          var)
+        form))
+    form))
 
 (defn edn-resource [path]
   (->> path
        io/resource
        slurp
        (edn/read-string {:readers {'edn/resource edn-resource}})
-       symbol/require-resolve-symbols))
+       (walk/postwalk require-resolve-symbols)))
