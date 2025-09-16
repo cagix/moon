71128ca0922a6cadb0c4457ddd80f9c3fd92cc95
Michael Sappler <michaelsappler@gmail.com>
2025-09-16 20:06:14 +0530

    clean up projectile collision

--- src/cdq/entity/projectile_collision.clj
+++ src/cdq/entity/projectile_collision.clj
@@ -12,15 +12,11 @@
   [{:keys [entity-effects already-hit-bodies piercing?]}
    eid
    {:keys [ctx/world]}]
-  ; TODO this could be called from body on collision
-  ; for non-solid
-  ; means non colliding with other entities
-  ; but still collding with other stuff here ? o.o
   (let [grid (:world/grid world)
         entity @eid
-        cells* (map deref (g2d/get-cells grid (geom/body->touched-tiles (:entity/body entity)))) ; just use cached-touched -cells
-        hit-entity (first (filter #(and (not (contains? already-hit-bodies %)) ; not filtering out own id
-                                        (not= (:entity/faction entity) ; this is not clear in the componentname & what if they dont have faction - ??
+        cells* (map deref (g2d/get-cells grid (geom/body->touched-tiles (:entity/body entity))))
+        hit-entity (first (filter #(and (not (contains? already-hit-bodies %))
+                                        (not= (:entity/faction entity)
                                               (:entity/faction @%))
                                         (:body/collides? (:entity/body @%))
                                         (body/overlaps? (:entity/body entity)
@@ -31,7 +27,13 @@
     [(when destroy?
        [:tx/mark-destroyed eid])
      (when hit-entity
-       [:tx/assoc-in eid [:entity/projectile-collision :already-hit-bodies] (conj already-hit-bodies hit-entity)] ; this is only necessary in case of not piercing ...
-       )
+       [:tx/assoc-in
+        eid
+        [:entity/projectile-collision
+         :already-hit-bodies]
+        (conj already-hit-bodies hit-entity)])
      (when hit-entity
-       [:tx/effect {:effect/source eid :effect/target hit-entity} entity-effects])]))
+       [:tx/effect
+        {:effect/source eid
+         :effect/target hit-entity}
+        entity-effects])]))
