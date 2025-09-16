cf33ca716590b8b252758c75029519b2f9a94f6b
Michael Sappler <michaelsappler@gmail.com>
2025-09-16 14:46:52 +0530

    remove tx/reset-game-state

--- src/cdq/ui/dev_menu/menus/select_world.clj
+++ src/cdq/ui/dev_menu/menus/select_world.clj
@@ -1,8 +1,16 @@
 (ns cdq.ui.dev-menu.menus.select-world
-  (:require [cdq.ctx :as ctx]))
+  (:require [cdq.ctx :as ctx]
+            [cdq.ctx.world :as world]))
 
-(defn create [_ctx {:keys [world-fns]}]
+(defn create
+  [_ctx
+   {:keys [world-fns
+           create-world]}]
   (for [world-fn world-fns]
     {:label (str "Start " (first world-fn))
-     :on-click (fn [_actor ctx]
-                 (ctx/handle-txs! ctx [[:tx/reset-game-state world-fn]]))}))
+     :on-click (fn [_actor {:keys [ctx/application-state
+                                   ctx/world]
+                            :as ctx}]
+                 (ctx/handle-txs! ctx [[:tx/reset-stage]])
+                 (world/dispose! world)
+                 (swap! application-state create-world world-fn))}))
