b4dbb33ba759d3ed5fdd9158a0d0bc286b8cd6e0
Michael Sappler <michaelsappler@gmail.com>
2025-09-16 16:43:58 +0530

    Make module lwjgl app deeper

--- src/clojure/gdx/backends/lwjgl.clj
+++ src/clojure/gdx/backends/lwjgl.clj
@@ -1,10 +1,23 @@
 (ns clojure.gdx.backends.lwjgl
-  (:require [clojure.gdx.backends.lwjgl.application :as application]
-            [clojure.gdx.backends.lwjgl.application.configuration :as config]))
+  (:require [clojure.gdx.application :as application]
+            [clojure.gdx.backends.lwjgl.application.configuration :as config]
+            [clojure.gdx.backends.lwjgl.application.gl-debug-message-severity :as gl-debug-message-severity]
+            [clojure.gdx.backends.lwjgl.window.configuration :as window-config])
+  (:import (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application)))
 
-(def start-application!            application/start!)
-(def new-window!                   application/new-window!)
-(def set-gl-debug-message-control! application/set-gl-debug-message-control!)
+(defn start-application! [listener config]
+  (Lwjgl3Application. (application/listener listener)
+                      (config/create config)))
+
+(defn new-window! [application listener config]
+  (Lwjgl3Application/.newWindow application
+                                listener
+                                (window-config/create config)))
+
+(defn set-gl-debug-message-control! [severity enabled?]
+  (Lwjgl3Application/setGLDebugMessageControl (gl-debug-message-severity/k->value severity)
+                                              (boolean enabled?)))
+
 (def display-mode!                 config/display-mode!)
 (def display-modes!                config/display-modes!)
 (def primary-monitor!              config/primary-monitor!)
