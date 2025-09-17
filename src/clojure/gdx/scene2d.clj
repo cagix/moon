07ba65a9cbda38ec2b6947f83990de69965000e7
Michael Sappler <michaelsappler@gmail.com>
2025-09-17 18:35:10 +0530

    defmethods scene2d/build

--- src/clojure/gdx/scene2d.clj
+++ src/clojure/gdx/scene2d.clj
@@ -1,18 +1,143 @@
 (ns clojure.gdx.scene2d
   (:require [cdq.start]
-            [clojure.gdx.scene2d.actor :as actor]
             [clojure.scene2d :as scene2d]
+            [clojure.scene2d.actor :as actor]
+            [clojure.scene2d.ctx :as ctx]
             [clojure.scene2d.group :as group]
+            [clojure.scene2d.stage :as stage]
             [clojure.scene2d.ui.table :as table]
             [clojure.walk :as walk]
-            [com.badlogic.gdx.scenes.scene2d.ui.cell :as cell])
+            [com.badlogic.gdx.math.vector2 :as vector2]
+            [com.badlogic.gdx.scenes.scene2d.ui.cell :as cell]
+            [com.badlogic.gdx.scenes.scene2d.touchable :as touchable])
   (:import (clojure.lang MultiFn)
            (com.badlogic.gdx.scenes.scene2d Actor
                                             Group)
            (com.badlogic.gdx.scenes.scene2d.ui HorizontalGroup
                                                Stack
                                                Table
+                                               Widget
                                                WidgetGroup)))
+
+(extend-type Actor
+  clojure.scene2d.actor/Actor
+  (get-stage [actor]
+    (.getStage actor))
+
+  (get-x [actor]
+    (.getX actor))
+
+  (get-y [actor]
+    (.getY actor))
+
+  (get-name [actor]
+    (.getName actor))
+
+  (user-object [actor]
+    (.getUserObject actor))
+
+  (set-user-object! [actor object]
+    (.setUserObject actor object))
+
+  (visible? [actor]
+    (.isVisible actor))
+
+  (set-visible! [actor visible?]
+    (.setVisible actor visible?))
+
+  (set-touchable! [actor touchable]
+    (.setTouchable actor (touchable/k->value touchable)))
+
+  (remove! [actor]
+    (.remove actor))
+
+  (parent [actor]
+    (.getParent actor))
+
+  (stage->local-coordinates [actor position]
+    (-> actor
+        (.stageToLocalCoordinates (vector2/->java position))
+        vector2/->clj))
+
+  (hit [actor [x y]]
+    (.hit actor x y true))
+
+  (set-name! [actor name]
+    (.setName actor name))
+
+  (set-position! [actor x y]
+    (.setPosition actor x y))
+
+  (get-width [actor]
+    (.getWidth actor))
+
+  (get-height [actor]
+    (.getHeight actor))
+
+  (add-listener! [actor listener]
+    (.addListener actor listener)))
+
+(def ^:private opts-fn-map
+  {:actor/name actor/set-name!
+   :actor/user-object actor/set-user-object!
+   :actor/visible?  actor/set-visible!
+   :actor/touchable actor/set-touchable!
+   :actor/listener actor/add-listener!
+   :actor/position (fn [actor [x y]]
+                     (actor/set-position! actor x y))
+   :actor/center-position (fn [actor [x y]]
+                            (actor/set-position! actor
+                                                 (- x (/ (actor/get-width  actor) 2))
+                                                 (- y (/ (actor/get-height actor) 2))))})
+
+(defn actor-opts! [actor opts]
+  (doseq [[k v] opts
+          :let [f (get opts-fn-map k)]
+          :when f]
+    (f actor v))
+  actor)
+
+(defn- create*
+  [{:keys [actor/act
+           actor/draw]
+    :as opts}]
+  (doto (proxy [Actor] []
+          (act [delta] ; TODO call proxy super required ?-> fixes tooltips in pure scene2d?
+            (act this delta))
+          (draw [batch parent-alpha]
+            (draw this batch parent-alpha)))
+    (actor-opts! opts)))
+
+(defn- get-ctx [actor]
+  (when-let [stage (actor/get-stage actor)]
+    (stage/get-ctx stage)))
+
+(defn- try-act [actor delta f]
+  (when-let [ctx (get-ctx actor)]
+    (f actor delta ctx)))
+
+(defn- try-draw [actor f]
+  (when-let [ctx (get-ctx actor)]
+    (ctx/draw! ctx (f actor ctx))))
+
+(defmethod scene2d/build :actor.type/actor
+  [{:keys [act draw]
+    :as opts}]
+  (create*
+   (assoc opts
+          :actor/act (fn [actor delta]
+                       (when act
+                         (try-act actor delta act)))
+          :actor/draw (fn [actor _batch _parent-alpha]
+                        (when draw
+                          (try-draw actor draw))))))
+
+(defmethod scene2d/build :actor.type/widget
+  [opts]
+  (proxy [Widget] []
+    (draw [_batch _parent-alpha]
+      (when-let [f (:draw opts)]
+        (try-draw this f)))))
 
 (extend-type Group
   clojure.scene2d.group/Group
@@ -34,7 +159,7 @@
                               actor-or-decl
                               (scene2d/build actor-or-decl))))
         (:group/actors opts))
-  (actor/set-opts! group opts))
+  (actor-opts! group opts))
 
 (defmethod scene2d/build :actor.type/group [opts]
   (doto (Group.)
@@ -111,14 +236,7 @@
 
 (def impls (walk/postwalk
             cdq.start/require-resolve-symbols
-            '[[clojure.scene2d/build
-               :actor.type/actor
-               clojure.gdx.scene2d.actor/create]
-
-              [clojure.scene2d/build
-               :actor.type/widget
-               clojure.gdx.scene2d.actor/create-widget]
-
+            '[
               [clojure.scene2d/build
                :actor.type/menu-bar
                clojure.gdx.scene2d.actor.menu-bar/create]
--- src/clojure/vis_ui/label.clj
+++ src/clojure/vis_ui/label.clj
@@ -1,7 +1,7 @@
 (ns clojure.vis-ui.label
-  (:require [clojure.gdx.scene2d.actor :as actor])
+  (:require [clojure.gdx.scene2d])
   (:import (com.kotcrab.vis.ui.widget VisLabel)))
 
 (defn create [{:keys [label/text] :as opts}]
   (doto (VisLabel. ^CharSequence text)
-    (actor/set-opts! opts)))
+    (clojure.gdx.scene2d/actor-opts! opts)))
