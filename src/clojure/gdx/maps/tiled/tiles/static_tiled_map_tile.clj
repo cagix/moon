d5d1d332b57ea4d22be333f42b448eac5d5322d7
Michael Sappler <michaelsappler@gmail.com>
2025-09-18 15:00:46 +0530

    Remove comments

--- src/clojure/gdx/maps/tiled/tiles/static_tiled_map_tile.clj
+++ src/clojure/gdx/maps/tiled/tiles/static_tiled_map_tile.clj
@@ -3,16 +3,12 @@
            (com.badlogic.gdx.maps.tiled.tiles StaticTiledMapTile)))
 
 (def copy
-  "Memoized function. Copies the given `StaticTiledMapTile`.
-
-  Tiles are usually shared by multiple cells, see: https://libgdx.com/wiki/graphics/2d/tile-maps#cells"
   (memoize
    (fn [^StaticTiledMapTile tile]
      (assert tile)
      (StaticTiledMapTile. tile))))
 
 (defn create
-  "Creates a `StaticTiledMapTile` with the given `texture-region` and property."
   [texture-region property-name property-value]
   {:pre [texture-region
          (string? property-name)]}
