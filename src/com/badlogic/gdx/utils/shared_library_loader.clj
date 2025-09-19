99ca3fe3ccf09e3b58f16ffd9185aeae5fa73708
Michael Sappler <michaelsappler@gmail.com>
2025-09-19 22:50:47 +0530

    os

--- src/com/badlogic/gdx/utils/shared_library_loader.clj
+++ src/com/badlogic/gdx/utils/shared_library_loader.clj
@@ -1,13 +1,6 @@
 (ns com.badlogic.gdx.utils.shared-library-loader
-  (:import (com.badlogic.gdx.utils SharedLibraryLoader
-                                   Os)))
-
-(def ^:private os->clj
-  {Os/Android :android
-   Os/IOS     :ios
-   Os/Linux   :linux
-   Os/MacOsX  :mac
-   Os/Windows :windows})
+  (:require [com.badlogic.gdx.utils.os :as os])
+  (:import (com.badlogic.gdx.utils SharedLibraryLoader)))
 
 (defn operating-system []
-  (os->clj SharedLibraryLoader/os))
+  (os/value->keyword SharedLibraryLoader/os))
