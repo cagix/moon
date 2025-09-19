27d1bdbb772553d8ba184ad0ea5e2eba98d26f78
Michael Sappler <michaelsappler@gmail.com>
2025-09-19 22:50:24 +0530

    Remove unused require

--- src/cdq/render/draw_on_world_viewport.clj
+++ src/cdq/render/draw_on_world_viewport.clj
@@ -1,14 +1,12 @@
 (ns cdq.render.draw-on-world-viewport
-  (:require [cdq.graphics :as graphics]
-            [cdq.graphics]))
+  (:require [cdq.graphics :as graphics]))
 
 (defn do!
   [{:keys [ctx/graphics]
     :as ctx}
    draw-fns]
-  (cdq.graphics/draw-on-world-viewport!
-   graphics
-   (fn []
-     (doseq [[f & params] draw-fns]
-       (graphics/handle-draws! graphics (apply f ctx params)))))
+  (graphics/draw-on-world-viewport! graphics
+                                    (fn []
+                                      (doseq [[f & params] draw-fns]
+                                        (graphics/handle-draws! graphics (apply f ctx params)))))
   ctx)
