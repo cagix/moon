8222ccc07be618fd811cccb9ac4524cfdbd86444
Michael Sappler <michaelsappler@gmail.com>
2025-09-20 19:10:00 +0530

    Remove cdq.application.listener

--- src/cdq/game.clj
+++ src/cdq/game.clj
@@ -1,15 +1,24 @@
 (ns cdq.game
-  (:require [clojure.config :as config]
+  (:require [cdq.application :as application]
+            [clojure.config :as config]
             [com.badlogic.gdx.backends.lwjgl3 :as lwjgl])
   (:gen-class))
-
-(defn- call [[f params]]
-  (f params))
 
 (defn -main [path]
   (let [{:keys [os->executions
                 listener
                 config]} (-> path config/edn-resource)]
-    (lwjgl/start-application! (call listener)
+    (lwjgl/start-application! {:create! (fn [gdx]
+                                          (let [[f pipeline] (:create listener)]
+                                            (f application/state gdx pipeline)))
+                               :dispose! (fn []
+                                           ((:dispose listener) application/state))
+                               :render! (fn []
+                                          (let [[f pipeline] (:render listener)]
+                                            (f application/state pipeline)))
+                               :resize! (fn [width height]
+                                          ((:resize listener) application/state width height))
+                               :pause! (fn [])
+                               :resume! (fn [])}
                               config
                               os->executions)))
