90b0bdf696524ad2b48441552756828006ff11c7
Michael Sappler <michaelsappler@gmail.com>
2025-09-21 17:19:33 +0530

    split up create

--- src/cdq/application/create.clj
+++ src/cdq/application/create.clj
@@ -1,41 +1,7 @@
 (ns cdq.application.create
   (:require [cdq.application :as application]
-            [cdq.ctx :as ctx]
             [clojure.config :as config]
-            [clojure.utils :as utils]
-            [malli.core :as m]
-            [malli.utils]
-            [qrecord.core :as q]))
-
-(def ^:private schema
-  (m/schema [:map {:closed true}
-             [:ctx/audio :some]
-             [:ctx/editor :some]
-             [:ctx/db :some]
-             [:ctx/graphics :some]
-
-             ; FIXME
-             [:ctx/input :some]
-             [:ctx/controls :some]
-
-             ; FIXME
-             [:ctx/stage :some]
-             [:ctx/vis-ui :some]
-             [:ctx/ui-actors :some]
-
-             [:ctx/world :some]
-
-             ; FIXME
-             [:ctx/mouseover-actor :any]
-             [:ctx/ui-mouse-position :some]
-             [:ctx/world-mouse-position :some]
-             [:ctx/interaction-state :some]]))
-
-(q/defrecord Context [ctx/graphics]
-  ctx/Validation
-  (validate [this]
-    (malli.utils/validate-humanize schema this)
-    this))
+            [clojure.utils :as utils]))
 
 (def ^:private create-pipeline (config/edn-resource "create.edn"))
 
@@ -45,9 +11,8 @@
            gdl/graphics
            gdl/input]}]
   (reset! application/state (utils/pipeline
-                             (merge (map->Context {})
-                                    {:ctx/audio audio
-                                     :ctx/files files
-                                     :ctx/graphics graphics
-                                     :ctx/input input})
+                             {:ctx/audio audio
+                              :ctx/files files
+                              :ctx/graphics graphics
+                              :ctx/input input}
                              create-pipeline)))
---
+++ src/cdq/application/create/initial_record.clj
@@ -0,0 +1,8 @@
+(ns cdq.application.create.initial-record
+  (:require [qrecord.core :as q]))
+
+(q/defrecord Context [ctx/graphics])
+
+(defn merge-into-record [ctx]
+  (merge (map->Context {})
+         ctx))
