95de6e1b22434a815effae36415315041cf37c2f
Michael Sappler <michaelsappler@gmail.com>
2025-09-22 09:58:13 +0530

    Go back to vector clojurize edn config

--- src/gdl/backends/desktop.clj
+++ src/gdl/backends/desktop.clj
@@ -1,24 +1,15 @@
 (ns gdl.backends.desktop
-  (:require [clojure.java.awt.taskbar]
-            [com.badlogic.gdx.backends.lwjgl3 :as lwjgl]
+  (:require [com.badlogic.gdx.backends.lwjgl3 :as lwjgl]
             [com.badlogic.gdx.utils :as utils]
-            [org.lwjgl.system.configuration]
             [gdl.backends.gdx.extends.audio]
             [gdl.backends.gdx.extends.files]
             [gdl.backends.gdx.extends.graphics]
             [gdl.backends.gdx.extends.input]))
 
-(defn- set-mac-os-settings!
-  [{:keys [glfw-async?
-           taskbar-icon]}]
-  (when glfw-async?
-    (org.lwjgl.system.configuration/set-glfw-library-name! "glfw_async"))
-  (when taskbar-icon
-    (clojure.java.awt.taskbar/set-icon-image! taskbar-icon)))
+(defn dispatch-on-os [os->executions]
+  (doseq [[f & params] (os->executions (utils/operating-system))]
+    (apply f params)))
 
 (defn application
-  [config]
-  (when (= (utils/operating-system) :mac)
-    (set-mac-os-settings! (:mac config)))
-  (lwjgl/start-application! (:listener config)
-                            (dissoc config :mac :listener)))
+  [{:keys [listener config]}]
+  (lwjgl/start-application! listener config))
--- src/gdl/backends/desktop/main.clj
+++ src/gdl/backends/desktop/main.clj
@@ -1,16 +1,7 @@
 (ns gdl.backends.desktop.main
-  (:require [clojure.edn :as edn]
-            [clojure.java.io :as io]
-            [gdl.backends.desktop :as desktop])
+  (:require [clojure.config :as config])
   (:gen-class))
 
-(defn -main [edn-resource]
-  (-> edn-resource
-      io/resource
-      slurp
-      edn/read-string
-      (update :listener update-vals (fn [sym]
-                                      (let [avar (requiring-resolve sym)]
-                                        (assert avar sym)
-                                        avar)))
-      desktop/application))
+(defn -main [path]
+  (doseq [[f & params] (config/edn-resource path)]
+    (apply f params)))
