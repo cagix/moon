05bd60bd9faafdaab42941e3bba9648889eac9fa
Michael Sappler <michaelsappler@gmail.com>
2025-09-24 22:10:31 +0530

    move tx/spawn-entity in handle-txs

--- src/cdq/application/create/handle_txs.clj
+++ src/cdq/application/create/handle_txs.clj
@@ -1,25 +1,183 @@
 (ns cdq.application.create.handle-txs
-  (:require [cdq.audio :as audio]
+  (:require [cdq.animation :as animation]
+            [cdq.audio :as audio]
+            [cdq.body :as body]
+            [cdq.creature :as creature]
             [cdq.ctx]
             [cdq.db :as db]
             [cdq.effect :as effect]
+            [cdq.entity :as entity]
             [cdq.entity.state :as state]
+            [cdq.entity.stats]
+            [cdq.gdx.math.geom :as geom]
             [cdq.graphics :as graphics]
             [cdq.inventory :as inventory]
+            [cdq.malli :as m]
             [cdq.stage]
             [cdq.stats :as stats]
             [cdq.timer :as timer]
-            cdq.tx.spawn-entity
             [cdq.world :as world]
             [cdq.world.content-grid :as content-grid]
             [cdq.world.grid :as grid]
             [clj-commons.pretty.repl :as pretty-repl]
+            [clojure.grid2d :as g2d]
             [clojure.math.vector2 :as v]
             [clojure.repl]
             [clojure.scene2d :as scene2d]
             [clojure.scene2d.stage :as stage]
             [clojure.tx-handler :as tx-handler]
-            [clojure.utils :as utils]))
+            [clojure.utils :as utils]
+            [qrecord.core :as q]))
+
+(defrecord Animation [frames frame-duration looping? cnt maxcnt]
+  cdq.animation/Animation
+  (tick [this delta]
+    (let [maxcnt (float maxcnt)
+          newcnt (+ (float cnt) (float delta))]
+      (assoc this :cnt (cond (< newcnt maxcnt) newcnt
+                             looping? (min maxcnt (- newcnt maxcnt))
+                             :else maxcnt))))
+
+  (restart [this]
+    (assoc this :cnt 0))
+
+  (stopped? [_]
+    (and (not looping?) (>= cnt maxcnt)))
+
+  (current-frame [this]
+    (frames (min (int (/ (float cnt) (float frame-duration)))
+                 (dec (count frames))))))
+
+(defn- create-animation
+  [{:keys [animation/frames
+           animation/frame-duration
+           animation/looping?]}
+   _ctx]
+  (map->Animation
+   {:frames (vec frames)
+    :frame-duration frame-duration
+    :looping? looping?
+    :cnt 0
+    :maxcnt (* (count frames) (float frame-duration))}))
+
+(q/defrecord Body [body/position
+                   body/width
+                   body/height
+                   body/collides?
+                   body/z-order
+                   body/rotation-angle]
+  cdq.body/Body
+  (overlaps? [body other-body]
+    (geom/overlaps? (geom/body->gdx-rectangle body)
+                    (geom/body->gdx-rectangle other-body)))
+
+  (touched-tiles [body]
+    (geom/body->touched-tiles body))
+
+  (distance [body other-body]
+    (v/distance (:body/position body)
+                (:body/position other-body))))
+
+(defn- create-body
+  [{[x y] :position
+    :keys [position
+           width
+           height
+           collides?
+           z-order
+           rotation-angle]}
+   {:keys [ctx/world]}]
+  (assert position)
+  (assert width)
+  (assert height)
+  (assert (>= width  (if collides? (:world/minimum-size world) 0)))
+  (assert (>= height (if collides? (:world/minimum-size world) 0)))
+  (assert (or (boolean? collides?) (nil? collides?)))
+  (assert ((set (:world/z-orders world)) z-order))
+  (assert (or (nil? rotation-angle)
+              (<= 0 rotation-angle 360)))
+  (map->Body
+   {:position (mapv float position)
+    :width  (float width)
+    :height (float height)
+    :collides? collides?
+    :z-order z-order
+    :rotation-angle (or rotation-angle 0)}))
+
+(def ^:private create-fns {:entity/animation create-animation
+                           :entity/body      create-body
+                           :entity/delete-after-duration (fn [duration {:keys [ctx/world]}]
+                                                           (timer/create (:world/elapsed-time world) duration))
+                           :entity/projectile-collision (fn [v _ctx]
+                                                          (assoc v :already-hit-bodies #{}))
+                           :creature/stats cdq.entity.stats/create})
+
+(defn- create-component [[k v] ctx]
+  (if-let [f (create-fns k)]
+    (f v ctx)
+    v))
+
+(defn- create-fsm
+  [{:keys [fsm initial-state]}
+   eid
+   {:keys [ctx/world]}]
+  ; fsm throws when initial-state is not part of states, so no need to assert initial-state
+  ; initial state is nil, so associng it. make bug report at reduce-fsm?
+  [[:tx/assoc eid :entity/fsm (assoc ((get (:world/fsms world) fsm) initial-state nil) :state initial-state)]
+   [:tx/assoc eid initial-state (state/create [initial-state nil] eid world)]])
+
+(defn- create-inventory []
+  (->> inventory/empty-inventory
+       (map (fn [[slot [width height]]]
+              [slot (g2d/create-grid width height (constantly nil))]))
+       (into {})))
+
+(defn- create!-inventory [items eid _ctx]
+  (cons [:tx/assoc eid :entity/inventory (create-inventory)]
+        (for [item items]
+          [:tx/pickup-item eid item])))
+
+(def ^:private create!-fns
+  {:entity/fsm                             create-fsm
+   :entity/inventory                       create!-inventory
+   :entity/delete-after-animation-stopped? (fn [_ eid _ctx]
+                                             (-> @eid :entity/animation :looping? not assert)
+                                             nil)
+   :entity/skills                          (fn [skills eid _ctx]
+                                             (cons [:tx/assoc eid :entity/skills nil]
+                                                   (for [skill skills]
+                                                     [:tx/add-skill eid skill])))})
+
+(defn- after-create-component [[k v] eid ctx]
+  (when-let [f (create!-fns k)]
+    (f v eid ctx)))
+
+(q/defrecord Entity [entity/body]
+  entity/Entity
+  (position [_]
+    (:body/position body))
+
+  (distance [_ other-entity]
+    (body/distance body
+                   (:entity/body other-entity))))
+
+(extend-type Entity
+  creature/Skills
+  (skill-usable-state [entity
+                       {:keys [skill/cooling-down? skill/effects] :as skill}
+                       effect-ctx]
+    (cond
+     cooling-down?
+     :cooldown
+
+     (stats/not-enough-mana? (:creature/stats entity) skill)
+     :not-enough-mana
+
+     (not (seq (filter #(effect/applicable? % effect-ctx) effects)))
+     :invalid-params
+
+     :else
+     :usable)))
 
 (def ^:private txs-fn-map
   {
@@ -292,7 +450,33 @@
                               (assoc :entity/destroy-audiovisual :audiovisuals/creature-die)
                               (utils/safe-merge components))]])
 
-   :tx/spawn-entity cdq.tx.spawn-entity/do!
+   :tx/spawn-entity (fn [{:keys [ctx/world]
+                          :as ctx}
+                         entity]
+                      (let [{:keys [world/content-grid
+                                    world/entity-ids
+                                    world/grid
+                                    world/id-counter
+                                    world/spawn-entity-schema]} world
+                            _ (m/validate-humanize spawn-entity-schema entity)
+                            entity (reduce (fn [m [k v]]
+                                             (assoc m k (create-component [k v] ctx)))
+                                           {}
+                                           entity)
+                            _ (assert (and (not (contains? entity :entity/id))))
+                            entity (assoc entity :entity/id (swap! id-counter inc))
+                            entity (merge (map->Entity {}) entity)
+                            eid (atom entity)]
+                        (let [id (:entity/id @eid)]
+                          (assert (number? id))
+                          (swap! entity-ids assoc id eid))
+                        (content-grid/add-entity! content-grid eid)
+                        ; https://github.com/damn/core/issues/58
+                        ;(assert (valid-position? grid @eid))
+                        (grid/set-touched-cells! grid eid)
+                        (when (:body/collides? (:entity/body @eid))
+                          (grid/set-occupied-cells! grid eid))
+                        (mapcat #(after-create-component % eid ctx) @eid)))
    }
   )
 
