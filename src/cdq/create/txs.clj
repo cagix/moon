f0a388f96c93fbbf00762270abb31dffb4d02c62
Michael Sappler <michaelsappler@gmail.com>
2025-09-24 10:38:16 +0530

    Move deal-damage to effect

--- src/cdq/create/txs.clj
+++ src/cdq/create/txs.clj
@@ -3,7 +3,6 @@
             [cdq.db :as db]
             [cdq.effect :as effect]
             [cdq.editor :as editor]
-            [cdq.entity :as entity]
             [cdq.entity.state :as state]
             [cdq.graphics :as graphics]
             [cdq.inventory :as inventory]
@@ -25,42 +24,7 @@
             [clojure.scene2d.actor :as actor]
             [clojure.scene2d.group :as group]
             [clojure.scene2d.stage :as stage]
-            [clojure.rand :refer [rand-int-between]]
             [clojure.repl]))
-
-(defn calc-damage
-  ([source target damage]
-   (update (calc-damage source damage)
-           :damage/min-max
-           cdq.entity.stats/apply-max
-           (:entity/modifiers target)
-           :modifier/damage-receive-max))
-  ([source damage]
-   (update damage
-           :damage/min-max
-           #(-> %
-                (cdq.entity.stats/apply-min (:entity/modifiers source) :modifier/damage-deal-min)
-                (cdq.entity.stats/apply-max (:entity/modifiers source) :modifier/damage-deal-max)))))
-
-; not in stats because projectile as source doesnt have stats
-; FIXME I don't see it triggering with 10 armor save ... !
-(defn- effective-armor-save [source-stats target-stats]
-  (max (- (or (cdq.stats/get-stat-value source-stats :entity/armor-save)   0)
-          (or (cdq.stats/get-stat-value target-stats :entity/armor-pierce) 0))
-       0))
-
-(comment
-
- (effective-armor-save {} {:entity/modifiers {:modifiers/armor-save {:op/inc 10}}
-                           :entity/armor-save 0})
- ; broken
- (let [source* {:entity/armor-pierce 0.4}
-       target* {:entity/armor-save   0.5}]
-   (effective-armor-save source* target*))
- )
-
-(def print-level 3)
-(def print-depth 24)
 
 (def txs-fn-map
   {
@@ -111,10 +75,12 @@
                          (do
                           #_(tx/stack-item ctx eid cell item))
                          [[:tx/set-item eid cell item]])))
-   :tx/print-stacktrace (fn [_ctx throwable]
-                          (binding [*print-level* print-level]
-                            (pretty-repl/pretty-pst throwable print-depth))
-                          nil)
+   :tx/print-stacktrace (let[print-level 3
+                             print-depth 24]
+                          (fn [_ctx throwable]
+                            (binding [*print-level* print-level]
+                              (pretty-repl/pretty-pst throwable print-depth))
+                            nil))
    :tx/show-error-window (fn [{:keys [ctx/stage]} throwable]
                            (stage/add! stage (scene2d/build
                                               {:actor/type :actor.type/window
@@ -260,39 +226,6 @@
                       start
                       {:entity/line-render {:thick? thick? :end end :color color}
                        :entity/delete-after-duration duration}]])
-   :tx/deal-damage (fn
-                     [_ctx
-                      source
-                      target
-                      damage]
-                     (let [source* @source
-                           target* @target
-                           hp (cdq.stats/get-hitpoints (:creature/stats target*))]
-                       (cond
-                        (zero? (hp 0))
-                        nil
-
-                        ; TODO find a better way
-                        (not (:creature/stats target*))
-                        nil
-
-                        (and (:creature/stats source*)
-                             (:creature/stats target*)
-                             (< (rand) (effective-armor-save (:creature/stats source*)
-                                                             (:creature/stats target*))))
-                        [[:tx/add-text-effect target "[WHITE]ARMOR" 0.3]]
-
-                        :else
-                        (let [min-max (:damage/min-max (calc-damage (:creature/stats source*)
-                                                                    (:creature/stats target*)
-                                                                    damage))
-                              dmg-amount (rand-int-between min-max)
-                              new-hp-val (max (- (hp 0) dmg-amount)
-                                              0)]
-                          [[:tx/assoc-in target [:creature/stats :entity/hp 0] new-hp-val]
-                           [:tx/event    target (if (zero? new-hp-val) :kill :alert)]
-                           [:tx/audiovisual (entity/position target*) :audiovisuals/damage]
-                           [:tx/add-text-effect target (str "[RED]" dmg-amount "[]") 0.3]]))))
    :tx/move-entity (fn
                      [{:keys [ctx/world]} eid body direction rotate-in-movement-direction?]
                      (let [{:keys [world/content-grid
--- src/cdq/effect.clj
+++ src/cdq/effect.clj
@@ -3,9 +3,11 @@
             cdq.effects.target-all
             [cdq.effects.target-entity :as target-entity]
             [cdq.entity :as entity]
+            [cdq.entity.stats]
             [cdq.entity.faction :as faction]
             [cdq.stats :as stats]
-            [cdq.timer :as timer])
+            [cdq.timer :as timer]
+            [clojure.rand :refer [rand-int-between]])
   (:import (clojure.lang APersistentVector)))
 
 (defprotocol Effect
@@ -23,6 +25,37 @@
   (->> effect
        (filter-applicable? effect-ctx)
        (some #(useful? % effect-ctx ctx))))
+
+; not in stats because projectile as source doesnt have stats
+; FIXME I don't see it triggering with 10 armor save ... !
+(defn- effective-armor-save [source-stats target-stats]
+  (max (- (or (stats/get-stat-value source-stats :entity/armor-save)   0)
+          (or (stats/get-stat-value target-stats :entity/armor-pierce) 0))
+       0))
+
+(comment
+
+ (effective-armor-save {} {:entity/modifiers {:modifiers/armor-save {:op/inc 10}}
+                           :entity/armor-save 0})
+ ; broken
+ (let [source* {:entity/armor-pierce 0.4}
+       target* {:entity/armor-save   0.5}]
+   (effective-armor-save source* target*))
+ )
+
+(defn- calc-damage
+  ([source target damage]
+   (update (calc-damage source damage)
+           :damage/min-max
+           cdq.entity.stats/apply-max
+           (:entity/modifiers target)
+           :modifier/damage-receive-max))
+  ([source damage]
+   (update damage
+           :damage/min-max
+           #(-> %
+                (cdq.entity.stats/apply-min (:entity/modifiers source) :modifier/damage-deal-min)
+                (cdq.entity.stats/apply-max (:entity/modifiers source) :modifier/damage-deal-max)))))
 
 (defn- entity->melee-damage [{:keys [creature/stats]}]
   (let [strength (or (stats/get-stat-value stats :entity/strength) 0)]
@@ -93,13 +126,44 @@
                                                    (faction/enemy (:entity/faction @source)))))
                             :handle (fn [_ {:keys [effect/source effect/target]} _ctx]
                                       [[:tx/assoc target :entity/faction (:entity/faction @source)]])}
+
+
    :effects.target/damage {:applicable? (fn [_ {:keys [effect/target]}]
                                           (and target
                                                #_(:entity/hp @target))) ; not exist anymore ... bugfix .... -> is 'creature?'
+
                            :handle (fn [[_ damage]
                                         {:keys [effect/source effect/target]}
                                         _ctx]
-                                     [[:tx/deal-damage source target damage]])}
+                                     (let [source* @source
+                                           target* @target
+                                           hp (stats/get-hitpoints (:creature/stats target*))]
+                                       (cond
+                                        (zero? (hp 0))
+                                        nil
+
+                                        ; TODO find a better way
+                                        (not (:creature/stats target*))
+                                        nil
+
+                                        (and (:creature/stats source*)
+                                             (:creature/stats target*)
+                                             (< (rand) (effective-armor-save (:creature/stats source*)
+                                                                             (:creature/stats target*))))
+                                        [[:tx/add-text-effect target "[WHITE]ARMOR" 0.3]]
+
+                                        :else
+                                        (let [min-max (:damage/min-max (calc-damage (:creature/stats source*)
+                                                                                    (:creature/stats target*)
+                                                                                    damage))
+                                              dmg-amount (rand-int-between min-max)
+                                              new-hp-val (max (- (hp 0) dmg-amount)
+                                                              0)]
+                                          [[:tx/assoc-in target [:creature/stats :entity/hp 0] new-hp-val]
+                                           [:tx/event    target (if (zero? new-hp-val) :kill :alert)]
+                                           [:tx/audiovisual (entity/position target*) :audiovisuals/damage]
+                                           [:tx/add-text-effect target (str "[RED]" dmg-amount "[]") 0.3]]))))}
+
    :effects.target/kill {:applicable? (fn [_ {:keys [effect/target]}]
                                         (and target
                                              (:entity/fsm @target)))
