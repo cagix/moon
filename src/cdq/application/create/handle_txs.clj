0b335c2b39d141a39f1a0bd9c548cb050c538889
Michael Sappler <michaelsappler@gmail.com>
2025-09-24 22:07:24 +0530

    Remove cdq.tx.spawn-creature

--- src/cdq/application/create/handle_txs.clj
+++ src/cdq/application/create/handle_txs.clj
@@ -9,7 +9,6 @@
             [cdq.stage]
             [cdq.stats :as stats]
             [cdq.timer :as timer]
-            cdq.tx.spawn-creature
             cdq.tx.spawn-entity
             [cdq.world :as world]
             [cdq.world.content-grid :as content-grid]
@@ -269,7 +268,29 @@
                        :entity/clickable {:type :clickable/item
                                           :text (:property/pretty-name item)}}]])
 
-   :tx/spawn-creature cdq.tx.spawn-creature/do!
+   ; # :z-order/flying has no effect for now
+   ; * entities with :z-order/flying are not flying over water,etc. (movement/air)
+   ; because using potential-field for z-order/ground
+   ; -> would have to add one more potential-field for each faction for z-order/flying
+   ; * they would also (maybe) need a separate occupied-cells if they don't collide with other
+   ; * they could also go over ground units and not collide with them
+   ; ( a test showed then flying OVER player entity )
+   ; -> so no flying units for now
+   :tx/spawn-creature (fn [_ctx
+                           {:keys [position
+                                   creature-property
+                                   components]}]
+                        (assert creature-property)
+                        [[:tx/spawn-entity
+                          (-> creature-property
+                              (assoc :entity/body (let [{:keys [body/width body/height #_body/flying?]} (:entity/body creature-property)]
+                                                    {:position position
+                                                     :width  width
+                                                     :height height
+                                                     :collides? true
+                                                     :z-order :z-order/ground #_(if flying? :z-order/flying :z-order/ground)}))
+                              (assoc :entity/destroy-audiovisual :audiovisuals/creature-die)
+                              (utils/safe-merge components))]])
 
    :tx/spawn-entity cdq.tx.spawn-entity/do!
    }
