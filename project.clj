79f1b3cd1551132621e6a3e943e4445b1f447c3a
Michael Sappler <michaelsappler@gmail.com>
2025-09-25 17:14:56 +0530

    shape drawer in separate project

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
-(def libgdx-version "1.13.5")
-
 (defproject cdq "-SNAPSHOT"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [
-                 [com.badlogic.gdx                       "1.13.5"]
-                 [com.badlogic.gdx.backends.lwjgl3       "1.13.5"]
-                 [com.badlogic.gdx.graphics.g2d.freetype "1.13.5"]
-                 [com.badlogicgames.gdx/gdx-freetype-platform ~libgdx-version :classifier "natives-desktop"]
+                 [com.badlogic.gdx                            "1.13.5"]
+                 [com.badlogic.gdx.backends.lwjgl3            "1.13.5"]
+                 [com.badlogic.gdx.graphics.g2d.freetype      "1.13.5"]
+                 [com.badlogicgames.gdx/gdx-freetype-platform "1.13.5" :classifier "natives-desktop"]
                  [com.kotcrab.vis/vis-ui "1.5.2"]
-                 [space.earlygrey/shapedrawer "2.5.0"]
+                 [space.earlygrey.shape-drawer "2.5.0"]
                  [com.github.cdorrat/reduce-fsm "fe1c914d68"]
                  [fr.reuz/qrecord "0.1.0"]
                  [metosin/malli "0.13.0"]
--- src/gdl/graphics/shape_drawer.clj
+++ src/gdl/graphics/shape_drawer.clj
@@ -1,5 +1,5 @@
 (ns gdl.graphics.shape-drawer
-  (:require [com.badlogic.gdx.graphics.color :as color])
+  (:require [space.earlygrey.shape-drawer :as sd])
   (:import (space.earlygrey.shapedrawer ShapeDrawer)))
 
 (defprotocol PShapeDrawer
@@ -15,79 +15,18 @@
   (rectangle! [_ x y w h])
   (sector! [_ center-x center-y radius start-radians radians]))
 
-(defn create [batch texture-region]
-  (ShapeDrawer. batch texture-region))
+(def create sd/create)
 
-(extend-type ShapeDrawer
+(extend ShapeDrawer
   PShapeDrawer
-  (set-color! [this color]
-    (.setColor this (color/float-bits color)))
-
-  (with-line-width [this width draw-fn]
-    (let [old-line-width (.getDefaultLineWidth this)]
-      (.setDefaultLineWidth this (float (* width old-line-width)))
-      (draw-fn)
-      (.setDefaultLineWidth this (float old-line-width))))
-
-  (arc! [this center-x center-y radius start-radians radians]
-    (.arc this
-          (float center-x)
-          (float center-y)
-          (float radius)
-          (float start-radians)
-          (float radians)))
-
-  (circle! [this x y radius]
-    (.circle this
-             (float x)
-             (float y)
-             (float radius)))
-
-  (ellipse! [this x y radius-x radius-y]
-    (.ellipse this
-              (float x)
-              (float y)
-              (float radius-x)
-              (float radius-y)))
-
-  (filled-circle! [this x y radius]
-    (.filledCircle this
-                   (float x)
-                   (float y)
-                   (float radius)))
-
-  (filled-ellipse! [this x y radius-x radius-y]
-    (.filledEllipse this
-                    (float x)
-                    (float y)
-                    (float radius-x)
-                    (float radius-y)))
-
-  (filled-rectangle! [this x y w h]
-    (.filledRectangle this
-                      (float x)
-                      (float y)
-                      (float w)
-                      (float h)))
-
-  (line! [this sx sy ex ey]
-    (.line this
-           (float sx)
-           (float sy)
-           (float ex)
-           (float ey)))
-
-  (rectangle! [this x y w h]
-    (.rectangle this
-                (float x)
-                (float y)
-                (float w)
-                (float h)))
-
-  (sector! [this center-x center-y radius start-radians radians]
-    (.sector this
-             (float center-x)
-             (float center-y)
-             (float radius)
-             (float start-radians)
-             (float radians))))
+  {:set-color! sd/set-color!
+   :with-line-width sd/with-line-width
+   :arc! sd/arc!
+   :circle! sd/circle!
+   :ellipse! sd/ellipse!
+   :filled-circle! sd/filled-circle!
+   :filled-ellipse! sd/filled-ellipse!
+   :filled-rectangle! sd/filled-rectangle!
+   :line! sd/line!
+   :rectangle! sd/rectangle!
+   :sector! sd/sector!})
