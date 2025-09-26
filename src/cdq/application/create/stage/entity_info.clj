dc4709593d97b7cd1f440c4e15cf5d74353ac2ca
Michael Sappler <michaelsappler@gmail.com>
2025-09-27 01:24:47 +0530

    clean up entity info window

--- src/cdq/application/create/stage/entity_info.clj
+++ src/cdq/application/create/stage/entity_info.clj
@@ -6,50 +6,29 @@
             [com.badlogic.gdx.scenes.scene2d.ui.label :as label]
             [com.badlogic.gdx.scenes.scene2d.ui.widget-group :as widget-group]))
 
-(comment
-
- ; items then have 2x pretty-name
- #_(.setText (.getTitleLabel window)
-             (info-text [:property/pretty-name (:property/pretty-name entity)])
-             "Entity Info")
- )
-
-(def disallowed-keys [:entity/skills
-                      #_:entity/fsm
-                      :entity/faction
-                      :active-skill])
-
-; TODO details how the text looks move to info
-; only for :
-; * skill
-; * entity -> all sub-types
-; * item
-; => can test separately !?
-
-(defn- ->label-text [entity world]
-  ; don't use select-keys as it loses Entity record type
-  (info/info-text (apply dissoc entity disallowed-keys)
-                  world))
-
 (defn create [stage]
-  (let [y-position 0
-        position [(stage/viewport-width stage)
-                  y-position]
+  (let [title "info"
+        actor-name "cdq.ui.windows.entity-info"
+        visible? false
+        position [(stage/viewport-width stage) 0]
+        set-label-text! (fn [{:keys [ctx/world]}]
+                          (if-let [eid (:world/mouseover-eid world)]
+                            (info/info-text (apply dissoc @eid [:entity/skills
+                                                                :entity/faction
+                                                                :active-skill])
+                                            world)
+                            ""))
         label (scene2d/build {:actor/type :actor.type/label
                               :label/text ""})
         window (scene2d/build {:actor/type :actor.type/window
-                               :title "Info"
-                               :actor/name "cdq.ui.windows.entity-info"
-                               :actor/visible? false
+                               :title title
+                               :actor/name actor-name
+                               :actor/visible? visible?
                                :actor/position position
                                :rows [[{:actor label :expand? true}]]})]
-    ; do not change window size ... -> no need to invalidate layout, set the whole stage up again
-    ; => fix size somehow.
     (group/add! window (scene2d/build
                         {:actor/type :actor.type/actor
-                         :act (fn [_this _delta {:keys [ctx/world]}]
-                                (label/set-text! label (str (if-let [eid (:world/mouseover-eid world)]
-                                                              (->label-text @eid world)
-                                                              "")))
+                         :act (fn [_this _delta ctx]
+                                (label/set-text! label (str (set-label-text! ctx)))
                                 (widget-group/pack! window))}))
     window))
