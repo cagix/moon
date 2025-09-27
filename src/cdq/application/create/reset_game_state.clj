b499681ca590c1d30e1d70b10e556d364c9b429e
Michael Sappler <michaelsappler@gmail.com>
2025-09-27 20:24:38 +0530

    spawn-enemies only 1x handle-txs!

--- src/cdq/application/create/reset_game_state.clj
+++ src/cdq/application/create/reset_game_state.clj
@@ -14,11 +14,15 @@
   [{:keys [ctx/db
            ctx/world]
     :as ctx}]
-  (doseq [[position creature-id] (tiled/positions-with-property (:world/tiled-map world) "creatures" "id")]
-    (ctx/handle-txs! ctx
-                     [[:tx/spawn-creature {:position (mapv (partial + 0.5) position)
-                                           :creature-property (db/build db (keyword creature-id))
-                                           :components (:world/enemy-components world)}]]))
+  (ctx/handle-txs!
+   ctx
+   (for [[position creature-id] (tiled/positions-with-property
+                                 (:world/tiled-map world)
+                                 "creatures"
+                                 "id")]
+     [:tx/spawn-creature {:position (mapv (partial + 0.5) position)
+                          :creature-property (db/build db (keyword creature-id))
+                          :components (:world/enemy-components world)}]))
   ctx)
 
 (defn- spawn-player!
