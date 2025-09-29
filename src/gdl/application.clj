16492db223c83eecc1499d084ab681e4046be0e8
Michael Sappler <michaelsappler@gmail.com>
2025-09-29 15:22:46 +0530

    Move backend port to other project

--- src/gdl/application.clj
+++ src/gdl/application.clj
@@ -1,25 +1,30 @@
 (ns gdl.application
-  (:require [com.badlogic.gdx.backends.lwjgl :as lwjgl])
   (:import (com.badlogic.gdx ApplicationListener
-                             Gdx)))
+                             Gdx)
+           (com.badlogic.gdx.backends.lwjgl3 Lwjgl3Application
+                                             Lwjgl3ApplicationConfiguration)))
 
 (defn start! [config]
-  (lwjgl/application
-   (reify ApplicationListener
-     (create [_]
-       ((:create config) {:ctx/audio    Gdx/audio
-                          :ctx/files    Gdx/files
-                          :ctx/graphics Gdx/graphics
-                          :ctx/input    Gdx/input}))
-     (dispose [_]
-       ((:dispose config)))
-     (render [_]
-       ((:render config)))
-     (resize [_ width height]
-       ((:resize config) width height))
-     (pause [_])
-     (resume [_]))
-   config))
+  (.set org.lwjgl.system.Configuration/GLFW_LIBRARY_NAME "glfw_async")
+  (Lwjgl3Application. (reify ApplicationListener
+                        (create [_]
+                          ((:create config) {:ctx/audio    Gdx/audio
+                                             :ctx/files    Gdx/files
+                                             :ctx/graphics Gdx/graphics
+                                             :ctx/input    Gdx/input}))
+                        (dispose [_]
+                          ((:dispose config)))
+                        (render [_]
+                          ((:render config)))
+                        (resize [_ width height]
+                          ((:resize config) width height))
+                        (pause [_])
+                        (resume [_]))
+                      (doto (Lwjgl3ApplicationConfiguration.)
+                        (.setWindowedMode (:width (:windowed-mode config))
+                                          (:height (:windowed-mode config)))
+                        (.setTitle (:title config))
+                        (.setForegroundFPS (:foreground-fps config)))))
 
 (defn post-runnable! [f]
   (.postRunnable Gdx/app f))
