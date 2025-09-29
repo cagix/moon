f862a3aa8f4fd805d11f7b8ff50aca856084caea
Michael Sappler <michaelsappler@gmail.com>
2025-09-29 19:20:18 +0530

    Actor protocol

--- src/clojure/gdx.clj
+++ src/clojure/gdx.clj
@@ -15,7 +15,10 @@
             clojure.graphics.viewport
             clojure.input
 
-            gdl.scene2d.stage
+            [gdl.scene2d :as scene2d]
+            [gdl.scene2d.actor :as actor]
+            [gdl.scene2d.ctx :as ctx]
+            [gdl.scene2d.stage :as stage]
 
             [clojure.string :as str]
             [com.badlogic.gdx.graphics.color :as color]
@@ -24,6 +27,7 @@
             [com.badlogic.gdx.input.keys    :as input-keys]
             [com.badlogic.gdx.math.vector2 :as vector2]
             [com.badlogic.gdx.math.vector3 :as vector3]
+            [com.badlogic.gdx.scenes.scene2d.touchable :as touchable]
             [com.badlogic.gdx.utils.align :as align])
   (:import (clojure.lang ILookup)
            (com.badlogic.gdx ApplicationListener
@@ -48,6 +52,7 @@
                                           SpriteBatch)
            (com.badlogic.gdx.graphics.g2d.freetype FreeTypeFontGenerator
                                                    FreeTypeFontGenerator$FreeTypeFontParameter)
+           (com.badlogic.gdx.scenes.scene2d Actor)
            (com.badlogic.gdx.utils Disposable)
            (com.badlogic.gdx.utils.viewport FitViewport)
            (gdl.scene2d Stage)
@@ -480,3 +485,116 @@
 
   (viewport [stage]
     (.getViewport stage)))
+
+(defn- get-ctx [actor]
+  (when-let [stage (actor/get-stage actor)]
+    (stage/get-ctx stage)))
+
+(def ^:private opts-fn-map
+  {:actor/name (fn [a v] (actor/set-name! a v))
+   :actor/user-object (fn [a v] (actor/set-user-object! a v))
+   :actor/visible?  (fn [a v] (actor/set-visible! a v))
+   :actor/touchable (fn [a v] (actor/set-touchable! a v))
+   :actor/listener (fn [a v] (actor/add-listener! a v))
+   :actor/position (fn [actor [x y]]
+                     (actor/set-position! actor x y))
+   :actor/center-position (fn [actor [x y]]
+                            (actor/set-position! actor
+                                                 (- x (/ (actor/get-width  actor) 2))
+                                                 (- y (/ (actor/get-height actor) 2))))})
+
+(extend-type Actor
+  gdl.scene2d.actor/Actor
+  (get-stage [actor]
+    (.getStage actor))
+
+  (get-x [actor]
+    (.getX actor))
+
+  (get-y [actor]
+    (.getY actor))
+
+  (get-name [actor]
+    (.getName actor))
+
+  (user-object [actor]
+    (.getUserObject actor))
+
+  (set-user-object! [actor object]
+    (.setUserObject actor object))
+
+  (visible? [actor]
+    (.isVisible actor))
+
+  (set-visible! [actor visible?]
+    (.setVisible actor visible?))
+
+  (set-touchable! [actor touchable]
+    (.setTouchable actor (touchable/k->value touchable)))
+
+  (remove! [actor]
+    (.remove actor))
+
+  (parent [actor]
+    (.getParent actor))
+
+  (stage->local-coordinates [actor position]
+    (-> actor
+        (.stageToLocalCoordinates (vector2/->java position))
+        vector2/->clj))
+
+  (hit [actor [x y]]
+    (.hit actor x y true))
+
+  (set-name! [actor name]
+    (.setName actor name))
+
+  (set-position! [actor x y]
+    (.setPosition actor x y))
+
+  (get-width [actor]
+    (.getWidth actor))
+
+  (get-height [actor]
+    (.getHeight actor))
+
+  (add-listener! [actor listener]
+    (.addListener actor listener))
+
+  (set-opts! [actor opts]
+    (doseq [[k v] opts
+            :let [f (get opts-fn-map k)]
+            :when f]
+      (f actor v))
+    actor)
+
+  (act [actor delta f]
+    (when-let [ctx (get-ctx actor)]
+      (f actor delta ctx)))
+
+  (draw [actor f]
+    (when-let [ctx (get-ctx actor)]
+      (ctx/draw! ctx (f actor ctx)))))
+
+(defn- create-actor*
+  [{:keys [actor/act
+           actor/draw]
+    :as opts}]
+  (doto (proxy [Actor] []
+          (act [delta]
+            (act this delta)
+            (proxy-super act delta))
+          (draw [batch parent-alpha]
+            (draw this batch parent-alpha)))
+    (actor/set-opts! opts)))
+
+(defmethod scene2d/build :actor.type/actor
+  [opts]
+  (create-actor*
+   (assoc opts
+          :actor/act (fn [actor delta]
+                       (when (:act opts)
+                         (actor/act actor delta (:act opts))))
+          :actor/draw (fn [actor _batch _parent-alpha]
+                        (when (:draw opts)
+                          (actor/draw actor (:draw opts)))))))
