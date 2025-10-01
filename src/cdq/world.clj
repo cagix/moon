f5541cd28f719805bbaeae72e79216036b05d24b
Michael Sappler <michaelsappler@gmail.com>
2025-10-01 15:26:11 +0530

    split up world create

--- src/cdq/world.clj
+++ src/cdq/world.clj
@@ -1,13 +1,13 @@
 (ns cdq.world
-  (:require [cdq.malli :as m]
+  (:require [cdq.world.assoc-entity-spawn-schema :as assoc-entity-spawn-schema]
             [cdq.world.content-grid :as content-grid]
+            [cdq.world.create-fsms :as create-fsms]
             [cdq.world.grid :as grid]
             [cdq.world.grid.cell :as cell]
-            [gdl.grid2d :as g2d]
-            [gdl.utils :as utils]
             [com.badlogic.gdx.utils.disposable :as disposable]
             [com.badlogic.gdx.maps.tiled :as tiled]
-            [reduce-fsm :as fsm]))
+            [gdl.grid2d :as g2d]
+            [gdl.utils :as utils]))
 
 (defrecord RCell [position
                   middle
@@ -144,63 +144,6 @@
            :world/paused? false
            :world/mouseover-eid nil)))
 
-(comment
-
- ; 1. quote the data structur ebecause of arrows
- ; 2.
- (eval `(fsm/fsm-inc ~data))
- )
-
-(def ^:private npc-fsm
-  (fsm/fsm-inc
-   [[:npc-sleeping
-     :kill -> :npc-dead
-     :stun -> :stunned
-     :alert -> :npc-idle]
-    [:npc-idle
-     :kill -> :npc-dead
-     :stun -> :stunned
-     :start-action -> :active-skill
-     :movement-direction -> :npc-moving]
-    [:npc-moving
-     :kill -> :npc-dead
-     :stun -> :stunned
-     :timer-finished -> :npc-idle]
-    [:active-skill
-     :kill -> :npc-dead
-     :stun -> :stunned
-     :action-done -> :npc-idle]
-    [:stunned
-     :kill -> :npc-dead
-     :effect-wears-off -> :npc-idle]
-    [:npc-dead]]))
-
-(def ^:private player-fsm
-  (fsm/fsm-inc
-   [[:player-idle
-     :kill -> :player-dead
-     :stun -> :stunned
-     :start-action -> :active-skill
-     :pickup-item -> :player-item-on-cursor
-     :movement-input -> :player-moving]
-    [:player-moving
-     :kill -> :player-dead
-     :stun -> :stunned
-     :no-movement-input -> :player-idle]
-    [:active-skill
-     :kill -> :player-dead
-     :stun -> :stunned
-     :action-done -> :player-idle]
-    [:stunned
-     :kill -> :player-dead
-     :effect-wears-off -> :player-idle]
-    [:player-item-on-cursor
-     :kill -> :player-dead
-     :stun -> :stunned
-     :drop-item -> :player-idle
-     :dropped-item -> :player-idle]
-    [:player-dead]]))
-
 (defn- calculate-max-speed
   [{:keys [world/minimum-size
            world/max-delta]
@@ -212,44 +155,10 @@
     :as world}]
   (assoc world :world/render-z-order (utils/define-order z-orders)))
 
-(defn- create-fsms
-  [world]
-  (assoc world :world/fsms {:fsms/player player-fsm
-                            :fsms/npc npc-fsm}))
-
-(def ^:private components-schema
-  (m/schema [:map {:closed true}
-             [:entity/body :some]
-             [:entity/image {:optional true} :some]
-             [:entity/animation {:optional true} :some]
-             [:entity/alert-friendlies-after-duration {:optional true} :some]
-             [:entity/line-render {:optional true} :some]
-             [:entity/delete-after-duration {:optional true} :some]
-             [:entity/destroy-audiovisual {:optional true} :some]
-             [:entity/fsm {:optional true} :some]
-             [:entity/player? {:optional true} :some]
-             [:entity/free-skill-points {:optional true} :some]
-             [:entity/click-distance-tiles {:optional true} :some]
-             [:entity/clickable {:optional true} :some]
-             [:property/id {:optional true} :some]
-             [:property/pretty-name {:optional true} :some]
-             [:creature/level {:optional true} :some]
-             [:entity/faction {:optional true} :some]
-             [:entity/species {:optional true} :some]
-             [:entity/movement {:optional true} :some]
-             [:entity/skills {:optional true} :some]
-             [:entity/stats {:optional true} :some]
-             [:entity/inventory    {:optional true} :some]
-             [:entity/item {:optional true} :some]
-             [:entity/projectile-collision {:optional true} :some]]))
-
-(defn- entity-schema [world]
-  (assoc world :world/spawn-entity-schema components-schema))
-
 (defn create [initial-config world-fn-result]
   (-> (merge (map->RWorld {}) initial-config)
-      entity-schema
-      create-fsms
+      assoc-entity-spawn-schema/step
+      create-fsms/step
       calculate-max-speed
       define-render-z-order
       (assoc-state world-fn-result)))
