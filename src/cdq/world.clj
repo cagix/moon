fd8797b759b43bc8aabde54e9a662002f28584df
Michael Sappler <michaelsappler@gmail.com>
2025-10-01 14:28:18 +0530

    Move effect impl to cdq.effect

--- src/cdq/world.clj
+++ src/cdq/world.clj
@@ -1,6 +1,5 @@
 (ns cdq.world
-  (:require [cdq.effect :as effect]
-            [cdq.entity.body :as body]
+  (:require [cdq.entity.body :as body]
             [cdq.entity.faction :as faction]
             [cdq.malli :as m]
             [cdq.world.content-grid :as content-grid]
@@ -406,73 +405,3 @@
       calculate-max-speed
       define-render-z-order
       (assoc-state world-fn-result)))
-
-(def ^:private k->fn
-  '{:effects/audiovisual {:applicable? cdq.effects.audiovisual/applicable?
-                          :useful?     cdq.effects.audiovisual/useful?
-                          :handle      cdq.effects.audiovisual/handle}
-
-    :effects/projectile {:applicable? cdq.effects.projectile/applicable?
-                         :useful?     cdq.effects.projectile/useful?
-                         :handle      cdq.effects.projectile/handle}
-
-    :effects/spawn {:applicable? cdq.effects.spawn/applicable?
-                    :handle      cdq.effects.spawn/handle}
-
-    :effects/target-all {:applicable? cdq.effects.target-all/applicable?
-                         :useful?     cdq.effects.target-all/useful?
-                         :handle      cdq.effects.target-all/handle
-                         :draw        cdq.effects.target-all/draw}
-
-    :effects/target-entity {:applicable? cdq.effects.target-entity/applicable?
-                            :useful?     cdq.effects.target-entity/useful?
-                            :handle      cdq.effects.target-entity/handle
-                            :draw        cdq.effects.target-entity/draw}
-
-    :effects.target/audiovisual {:applicable? cdq.effects.target.audiovisual/applicable?
-                                 :useful?     cdq.effects.target.audiovisual/useful?
-                                 :handle      cdq.effects.target.audiovisual/handle}
-
-    :effects.target/convert {:applicable? cdq.effects.target.convert/applicable?
-                             :handle      cdq.effects.target.convert/handle}
-
-    :effects.target/damage {:applicable? cdq.effects.target.damage/applicable?
-                            :handle      cdq.effects.target.damage/handle}
-
-    :effects.target/kill {:applicable? cdq.effects.target.kill/applicable?
-                          :handle      cdq.effects.target.kill/handle}
-
-    :effects.target/melee-damage {:applicable? cdq.effects.target.melee-damage/applicable?
-                                  :handle      cdq.effects.target.melee-damage/handle}
-
-    :effects.target/spiderweb {:applicable? cdq.effects.target.spiderweb/applicable?
-                               :handle      cdq.effects.target.spiderweb/handle}
-
-    :effects.target/stun {:applicable? cdq.effects.target.stun/applicable?
-                          :handle      cdq.effects.target.stun/handle}})
-
-(alter-var-root #'k->fn update-vals
-                (fn [k->fn]
-                  (update-vals k->fn
-                               (fn [sym]
-                                 (let [avar (requiring-resolve sym)]
-                                   (assert avar sym)
-                                   avar)))))
-
-(extend clojure.lang.APersistentVector
-  effect/Effect
-  {:applicable? (fn [{k 0 :as component} effect-ctx]
-                  ((:applicable? (k->fn k)) component effect-ctx))
-
-   :handle (fn [{k 0 :as component} effect-ctx world]
-             ((:handle (k->fn k)) component effect-ctx world))
-
-   :useful? (fn [{k 0 :as component} effect-ctx world]
-              (if-let [f (:useful? (k->fn k))]
-                (f component effect-ctx world)
-                true))
-
-   :draw (fn [{k 0 :as component} effect-ctx ctx]
-           (if-let [f (:draw (k->fn k))]
-             (f component effect-ctx ctx)
-             nil))})
