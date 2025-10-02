3d507a457a11248bc468a44ceb3cd700778e6b7e
Michael Sappler <michaelsappler@gmail.com>
2025-10-02 23:43:12 +0530

    actor-fns vector

--- src/cdq/ctx/build_stage_actors.clj
+++ src/cdq/ctx/build_stage_actors.clj
@@ -9,18 +9,24 @@
             [com.badlogic.gdx.scenes.scene2d :as scene2d]
             [com.badlogic.gdx.scenes.scene2d.stage :as stage]))
 
+(defn create-windows [ctx actor-fns]
+  {:actor/type :actor.type/group
+   :actor/name "cdq.ui.windows"
+   :group/actors (for [[actor-fn & params] actor-fns]
+                   (apply actor-fn ctx params))})
+
+(def actor-fns
+  [[cdq.ui.dev-menu/create]
+   [cdq.ui.action-bar/create]
+   [cdq.ui.hp-mana-bar/create]
+   [create-windows [[cdq.ui.entity-info-window/create]
+                    [cdq.ctx.create-inventory-window/create]]]
+   [cdq.ui.player-state-draw/create]
+   [cdq.ui.message/create]])
+
 (defn do!
   [{:keys [ctx/stage]
     :as ctx}]
-  (let [actors [(cdq.ui.dev-menu/create ctx)
-                (cdq.ui.action-bar/create ctx)
-                (cdq.ui.hp-mana-bar/create ctx)
-                {:actor/type :actor.type/group
-                 :actor/name "cdq.ui.windows"
-                 :group/actors [(cdq.ui.entity-info-window/create ctx)
-                                (cdq.ctx.create-inventory-window/create ctx)]}
-                (cdq.ui.player-state-draw/create ctx)
-                (cdq.ui.message/create ctx)]]
-    (doseq [actor actors]
-      (stage/add! stage (scene2d/build actor))))
+  (doseq [[actor-fn & params] actor-fns]
+    (stage/add! stage (scene2d/build (apply actor-fn ctx params))))
   ctx)
