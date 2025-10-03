b3d2b598e3b2d4634cef5a83e87f3a55c67f60b6
Michael Sappler <michaelsappler@gmail.com>
2025-10-03 10:33:16 +0530

    cdq.audio only protocol

--- src/cdq/audio.clj
+++ src/cdq/audio.clj
@@ -1,35 +1,5 @@
-(ns cdq.audio
-  (:require [gdl.audio :as audio]
-            [gdl.audio.sound :as sound]
-            [gdl.disposable :as disposable]
-            [gdl.files :as files]))
+(ns cdq.audio)
 
 (defprotocol Audio
   (all-sounds [_])
   (play-sound! [_ sound-name]))
-
-(defn- audio-impl [audio sound-names->file-handles]
-  (let [sounds (update-vals sound-names->file-handles
-                            (fn [file-handle]
-                              (audio/new-sound audio file-handle)))]
-    (reify
-      Audio
-      (all-sounds [_]
-        (map first sounds))
-
-      (play-sound! [_ sound-name]
-        (assert (contains? sounds sound-name) (str sound-name))
-        (sound/play! (get sounds sound-name)))
-
-      disposable/Disposable
-      (dispose! [_]
-        (run! disposable/dispose! (vals sounds))))))
-
-(defn create
-  [audio files sound-names path-format]
-  (let [sound-names->file-handles
-        (into {}
-              (for [sound-name sound-names]
-                [sound-name
-                 (files/internal files (format path-format sound-name))]))]
-    (audio-impl audio sound-names->file-handles)))
--- src/cdq/ctx/create/audio.clj
+++ src/cdq/ctx/create/audio.clj
@@ -1,15 +1,42 @@
 (ns cdq.ctx.create.audio
-  (:require [cdq.audio :as audio]
+  (:require [cdq.audio]
             [clojure.edn :as edn]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [gdl.audio :as audio]
+            [gdl.audio.sound :as sound]
+            [gdl.disposable :as disposable]
+            [gdl.files :as files]))
 
 (def ^:private sound-names (->> "sounds.edn" io/resource slurp edn/read-string))
 (def ^:private path-format "sounds/%s.wav")
 
+(defn- audio-impl [audio sound-names->file-handles]
+  (let [sounds (update-vals sound-names->file-handles
+                            (fn [file-handle]
+                              (audio/new-sound audio file-handle)))]
+    (reify
+      cdq.audio/Audio
+      (all-sounds [_]
+        (map first sounds))
+
+      (play-sound! [_ sound-name]
+        (assert (contains? sounds sound-name) (str sound-name))
+        (sound/play! (get sounds sound-name)))
+
+      disposable/Disposable
+      (dispose! [_]
+        (run! disposable/dispose! (vals sounds))))))
+
+(defn- create
+  [audio files sound-names path-format]
+  (let [sound-names->file-handles
+        (into {}
+              (for [sound-name sound-names]
+                [sound-name
+                 (files/internal files (format path-format sound-name))]))]
+    (audio-impl audio sound-names->file-handles)))
+
 (defn do! [{:keys [ctx/audio
                    ctx/files]
             :as ctx}]
-  (assoc ctx :ctx/audio (audio/create audio
-                                      files
-                                      sound-names
-                                      path-format)))
+  (assoc ctx :ctx/audio (create audio files sound-names path-format)))
