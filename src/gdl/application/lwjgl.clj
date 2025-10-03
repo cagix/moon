d9878b210dabb9dcd3afa85db0b18a510cc2b481
Michael Sappler <michaelsappler@gmail.com>
2025-10-03 18:55:39 +0530

    formatting, remove comments

--- src/gdl/application/lwjgl.clj
+++ src/gdl/application/lwjgl.clj
@@ -22,11 +22,6 @@
                method-map (update-vals (:sigs protocol)
                                        (fn [{:keys [name]}]
                                          (requiring-resolve (symbol (str implementation-ns-sym "/" name)))))]
-           ;(println "extend ")
-           ;(println "atype: " atype)
-           ;(println "protocol: " protocol)
-           ;(println "method-map:")
-           ;(clojure.pprint/pprint method-map)
            (extend atype protocol method-map))
          (catch Throwable t
            (throw (ex-info "Cant extend"
@@ -38,19 +33,33 @@
 (defn start! [listener config]
   (extend-types
    [
-    ['com.badlogic.gdx.Audio            'com.badlogic.gdx.audio             'gdl.audio/Audio]
-    ['com.badlogic.gdx.audio.Sound      'com.badlogic.gdx.audio.sound       'gdl.audio.sound/Sound]
-    ['com.badlogic.gdx.Files            'com.badlogic.gdx.files             'gdl.files/Files]
+    ['com.badlogic.gdx.Audio
+     'com.badlogic.gdx.audio
+     'gdl.audio/Audio]
+    ['com.badlogic.gdx.audio.Sound
+     'com.badlogic.gdx.audio.sound
+     'gdl.audio.sound/Sound]
+    ['com.badlogic.gdx.Files
+     'com.badlogic.gdx.files
+     'gdl.files/Files]
     ['com.badlogic.gdx.graphics.OrthographicCamera
      'com.badlogic.gdx.graphics.orthographic-camera
      'gdl.graphics.orthographic-camera/Camera]
     ['com.badlogic.gdx.utils.viewport.Viewport
      'com.badlogic.gdx.utils.viewport
      'gdl.graphics.viewport/Viewport]
-    ['com.badlogic.gdx.files.FileHandle 'com.badlogic.gdx.files.file-handle 'gdl.files.file-handle/FileHandle]
-    ['com.badlogic.gdx.utils.Disposable 'com.badlogic.gdx.utils.disposable  'gdl.disposable/Disposable]
-    ['com.badlogic.gdx.scenes.scene2d.Actor 'com.badlogic.gdx.scenes.scene2d.actor 'gdl.scene2d.actor/Actor]
-    ['com.badlogic.gdx.scenes.scene2d.Actor 'clojure.scene2d.tooltip 'gdl.scene2d.actor/Tooltip]
+    ['com.badlogic.gdx.files.FileHandle
+     'com.badlogic.gdx.files.file-handle
+     'gdl.files.file-handle/FileHandle]
+    ['com.badlogic.gdx.utils.Disposable
+     'com.badlogic.gdx.utils.disposable
+     'gdl.disposable/Disposable]
+    ['com.badlogic.gdx.scenes.scene2d.Actor
+     'com.badlogic.gdx.scenes.scene2d.actor
+     'gdl.scene2d.actor/Actor]
+    ['com.badlogic.gdx.scenes.scene2d.Actor
+     'clojure.scene2d.tooltip
+     'gdl.scene2d.actor/Tooltip]
     ]
    )
   (lwjgl-system/set-glfw-library-name! "glfw_async")
@@ -60,17 +69,6 @@
                                        :ctx/files    Gdx/files
                                        :ctx/graphics Gdx/graphics
                                        :ctx/input    Gdx/input}]
-                            ;(set! Gdx/app      nil)
-                            ;(set! Gdx/graphics nil)
-                            ;(set! Gdx/audio    nil)
-                            ;(set! Gdx/input    nil)
-                            ;(set! Gdx/files    nil)
-                            ;(set! Gdx/net      nil)
-                            ;(set! Gdx/gl       nil)
-                            ;(set! Gdx/gl20     nil)
-                            ;(set! Gdx/gl30     nil)
-                            ;(set! Gdx/gl31     nil)
-                            ;(set! Gdx/gl32     nil)
                             (create listener state)))
                         (dispose [_]
                           (dispose listener))
