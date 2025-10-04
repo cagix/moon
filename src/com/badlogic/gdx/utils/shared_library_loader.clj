947a9777107753d0f28f2186237ff2aa617c5dbf
Michael Sappler <michaelsappler@gmail.com>
2025-10-04 12:18:58 +0530

    Move OS with shared-library-loader

--- src/com/badlogic/gdx/utils/shared_library_loader.clj
+++ src/com/badlogic/gdx/utils/shared_library_loader.clj
@@ -1,6 +1,13 @@
 (ns com.badlogic.gdx.utils.shared-library-loader
-  (:require [com.badlogic.gdx.utils.os :as os])
-  (:import (com.badlogic.gdx.utils SharedLibraryLoader)))
+  (:import (com.badlogic.gdx.utils SharedLibraryLoader
+                                   Os)))
+
+(def ^:private os->keyword
+  {Os/Android :android
+   Os/IOS     :ios
+   Os/Linux   :linux
+   Os/MacOsX  :mac
+   Os/Windows :windows})
 
 (defn os []
-  (os/value->keyword SharedLibraryLoader/os))
+  (os->keyword SharedLibraryLoader/os))
