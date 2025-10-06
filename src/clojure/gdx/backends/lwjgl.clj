027dbcf804c17bd92e4894b587c15d1a99721670
Michael Sappler <michaelsappler@gmail.com>
2025-10-06 15:25:35 +0530

    gdx and application listener send away

--- src/clojure/gdx/backends/lwjgl.clj
+++ src/clojure/gdx/backends/lwjgl.clj
@@ -1,8 +1,25 @@
 (ns clojure.gdx.backends.lwjgl
-  (:require [com.badlogic.gdx.application.listener :as listener]
+  (:require [clojure.gdx]
+            [com.badlogic.gdx :as gdx]
+            [com.badlogic.gdx.application.listener :as listener]
             [com.badlogic.gdx.backends.lwjgl3.application :as application]
             [com.badlogic.gdx.backends.lwjgl3.application.config :as config]))
 
-(defn create [listener config]
-  (application/create (listener/create listener)
+(defrecord Context []
+  clojure.gdx/Audio
+  (sound [{:keys [clojure.gdx/audio
+                  clojure.gdx/files]}
+          path]
+    (.newSound audio (.internal files path))))
+
+(defn create [{:keys [create] :as listener} config]
+  (application/create (listener/create
+                       (assoc listener :create
+                              (fn []
+                                (create (map->Context
+                                         (let [{:keys [audio files graphics input]} (gdx/context)]
+                                           {:clojure.gdx/audio    audio
+                                            :clojure.gdx/files    files
+                                            :clojure.gdx/graphics graphics
+                                            :clojure.gdx/input    input}))))))
                       (config/create config)))
