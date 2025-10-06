2b19deb767c14e63c29e5c5bd5cd41edcdf8654a
Michael Sappler <michaelsappler@gmail.com>
2025-10-06 22:47:01 +0530

    add badlogic group

--- src/clojure/scene2d/build/group.clj
+++ src/clojure/scene2d/build/group.clj
@@ -1,32 +1,32 @@
 (ns clojure.scene2d.build.group
   (:require [clojure.scene2d :as scene2d]
             [clojure.scene2d.actor :as actor]
-            [clojure.scene2d.group :as group])
-  (:import (com.badlogic.gdx.scenes.scene2d Actor
-                                            Group)))
+            [clojure.scene2d.group :as pgroup]
+            [com.badlogic.gdx.scenes.scene2d.group :as group])
+  (:import (com.badlogic.gdx.scenes.scene2d Actor)))
 
-(extend-type Group
-  group/Group
+(extend-type com.badlogic.gdx.scenes.scene2d.Group
+  pgroup/Group
   (add! [group actor]
-    (.addActor group actor))
+    (group/add-actor! group actor))
 
   (find-actor [group name]
-    (.findActor group name))
+    (group/find-actor group name))
 
   (clear-children! [group]
-    (.clearChildren group))
+    (group/clear-children! group))
 
   (children [group]
-    (.getChildren group))
+    (group/children group))
 
   (set-opts! [group opts]
     (run! (fn [actor-or-decl]
-            (group/add! group (if (instance? Actor actor-or-decl)
-                                actor-or-decl
-                                (scene2d/build actor-or-decl))))
+            (pgroup/add! group (if (instance? Actor actor-or-decl)
+                                 actor-or-decl
+                                 (scene2d/build actor-or-decl))))
           (:group/actors opts))
     (actor/set-opts! group opts)))
 
 (defmethod scene2d/build :actor.type/group [opts]
-  (doto (Group.)
-    (group/set-opts! opts)))
+  (doto (group/create)
+    (pgroup/set-opts! opts)))
