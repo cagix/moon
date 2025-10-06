1b42343b15ca02f14a92012e8905b3c851db7006
Michael Sappler <michaelsappler@gmail.com>
2025-10-06 18:13:36 +0530

    configure yourself

--- src/clojure/gdx/graphics/g2d/bitmap_font.clj
+++ src/clojure/gdx/graphics/g2d/bitmap_font.clj
@@ -5,10 +5,7 @@
             [clojure.string :as str])
   (:import (com.badlogic.gdx.graphics.g2d BitmapFont)))
 
-(def set-use-integer-positions! bitmap-font/set-use-integer-positions!)
-
-; used
-(defn set-scale! [font scale-xy]
+(defn- set-scale! [font scale-xy]
   (data/set-scale! (bitmap-font/data font) scale-xy))
 
 (defn- text-height [font text]
@@ -17,16 +14,10 @@
       count
       (* (bitmap-font/line-height font))))
 
-; used
-(defn enable-markup!
-  [font boolean]
-  (data/enable-markup! (bitmap-font/data font) boolean))
-
-(defn scale-x
+(defn- scale-x
   [font]
   (data/scale-x (bitmap-font/data font)))
 
-; used
 (defn draw! [^BitmapFont font batch {:keys [scale text x y up? h-align target-width wrap?]}]
   {:pre [(or (nil? h-align)
              (contains? align/k->value h-align))]}
