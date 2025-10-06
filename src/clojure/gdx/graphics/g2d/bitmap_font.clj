5549afd813c0652c59319c844307d7e0560ec782
Michael Sappler <michaelsappler@gmail.com>
2025-10-06 17:58:29 +0530

    move bitmapfont data

--- src/clojure/gdx/graphics/g2d/bitmap_font.clj
+++ src/clojure/gdx/graphics/g2d/bitmap_font.clj
@@ -1,18 +1,15 @@
 (ns clojure.gdx.graphics.g2d.bitmap-font
   (:require [com.badlogic.gdx.graphics.g2d.bitmap-font :as bitmap-font]
+            [com.badlogic.gdx.graphics.g2d.bitmap-font.data :as data]
             [com.badlogic.gdx.utils.align :as align]
             [clojure.string :as str])
-  (:import (com.badlogic.gdx.graphics.g2d BitmapFont
-                                          BitmapFont$BitmapFontData)))
+  (:import (com.badlogic.gdx.graphics.g2d BitmapFont)))
 
 (def set-use-integer-positions! bitmap-font/set-use-integer-positions!)
 
 ; used
-(defn set-scale!
-  "Scales the font by the specified amount in both directions.
-  throws IllegalArgumentException if scaleX or scaleY is zero."
-  [font scale-xy]
-  (.setScale (bitmap-font/data font) scale-xy))
+(defn set-scale! [font scale-xy]
+  (data/set-scale! (bitmap-font/data font) scale-xy))
 
 (defn- text-height [font text]
   (-> text
@@ -23,11 +20,11 @@
 ; used
 (defn enable-markup!
   [font boolean]
-  (set! (.markupEnabled (bitmap-font/data font)) boolean))
+  (data/enable-markup! (bitmap-font/data font) boolean))
 
 (defn scale-x
   [font]
-  (.scaleX (bitmap-font/data font)))
+  (data/scale-x (bitmap-font/data font)))
 
 ; used
 (defn draw! [^BitmapFont font batch {:keys [scale text x y up? h-align target-width wrap?]}]
