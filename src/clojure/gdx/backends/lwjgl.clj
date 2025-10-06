b3b579400e5abce428301ddc786f24215e289cab
Michael Sappler <michaelsappler@gmail.com>
2025-10-06 17:34:38 +0530

    clojure.gdx.input move

--- src/clojure/gdx/backends/lwjgl.clj
+++ src/clojure/gdx/backends/lwjgl.clj
@@ -1,8 +1,12 @@
 (ns clojure.gdx.backends.lwjgl
   (:require [clojure.gdx.audio]
+            [clojure.gdx.input]
             [com.badlogic.gdx :as gdx]
             [com.badlogic.gdx.audio :as audio]
             [com.badlogic.gdx.files :as files]
+            [com.badlogic.gdx.input :as input]
+            [com.badlogic.gdx.input.buttons :as input-buttons]
+            [com.badlogic.gdx.input.keys    :as input-keys]
             [com.badlogic.gdx.application.listener :as listener]
             [com.badlogic.gdx.backends.lwjgl3.application :as application]
             [com.badlogic.gdx.backends.lwjgl3.application.config :as config]))
@@ -25,3 +29,25 @@
                                             :clojure.gdx/graphics graphics
                                             :clojure.gdx/input    input}))))))
                       (config/create config)))
+
+(extend-type com.badlogic.gdx.Input
+  clojure.gdx.input/Input
+  (button-just-pressed? [this button]
+    {:pre [(contains? input-buttons/k->value button)]}
+    (input/button-just-pressed? this (input-buttons/k->value button)))
+
+  (key-pressed? [this key]
+    (assert (contains? input-keys/k->value key)
+            (str "(pr-str key): "(pr-str key)))
+    (input/key-pressed? this (input-keys/k->value key)))
+
+  (key-just-pressed? [this key]
+    {:pre [(contains? input-keys/k->value key)]}
+    (input/key-just-pressed? this (input-keys/k->value key)))
+
+  (set-processor! [this input-processor]
+    (input/set-processor! this input-processor))
+
+  (mouse-position [this]
+    [(input/x this)
+     (input/y this)]))
