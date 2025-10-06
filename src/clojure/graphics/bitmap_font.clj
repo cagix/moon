d5b220ae2cde6dfe31182e8b8b87e9b7d4d1f7ca
Michael Sappler <michaelsappler@gmail.com>
2025-10-06 17:51:49 +0530

    Remove unused code

--- src/clojure/graphics/bitmap_font.clj
+++ src/clojure/graphics/bitmap_font.clj
@@ -1,7 +1,4 @@
 (ns clojure.graphics.bitmap-font)
 
 (defprotocol BitmapFont
-  (configure! [_ {:keys [scale
-                         enable-markup?
-                         use-integer-positions?]}])
   (draw! [_ batch {:keys [scale text x y up? h-align target-width wrap?]}]))
