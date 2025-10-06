f845b3032b828e4105b8fe416137dbe7c015e41a
Michael Sappler <michaelsappler@gmail.com>
2025-10-06 20:27:38 +0530

    maps tiled tiles to com.badlogic.gdx

--- src/clojure/gdx/maps/tiled.clj
+++ src/clojure/gdx/maps/tiled.clj
@@ -1,6 +1,6 @@
 (ns clojure.gdx.maps.tiled
   (:require [com.badlogic.gdx.maps.map-properties :as properties]
-            [clojure.gdx.maps.tiled.tiles :as tiles])
+            [com.badlogic.gdx.maps.tiled.tiles :as tiles])
   (:import (com.badlogic.gdx.maps.tiled TiledMap
                                         TiledMapTileLayer
                                         TiledMapTileLayer$Cell
