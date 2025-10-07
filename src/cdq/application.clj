10c82b883f638eb34d540bcf04f2c127fc50510c
Michael Sappler <michaelsappler@gmail.com>
2025-10-08 04:33:33 +0530

    create! separate fn

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -174,13 +174,63 @@
                            handled-txs
                            :strict? false))))
 
-(def state (atom nil))
-
 (defn pipeline [ctx pipeline]
   (reduce (fn [ctx [f & args]]
             (apply f ctx args))
           ctx
           pipeline))
+
+(defn- create! []
+  (vis-ui/load! {:skin-scale :x1})
+  (pipeline {:ctx/gdx {:clojure.gdx/audio    Gdx/audio
+                       :clojure.gdx/files    Gdx/files
+                       :clojure.gdx/graphics Gdx/graphics
+                       :clojure.gdx/input    Gdx/input}}
+            [[(fn [ctx]
+                (merge (map->Context {})
+                       ctx))]
+             [cdq.ctx.create.db/do!]
+             [cdq.ctx.create.graphics/do! {:tile-size 48
+                                           :ui-viewport {:width 1440
+                                                         :height 900}
+                                           :world-viewport {:width 1440
+                                                            :height 900}
+                                           :texture-folder {:folder "resources/"
+                                                            :extensions #{"png" "bmp"}}
+                                           :default-font {:path "exocet/films.EXL_____.ttf"
+                                                          :params {:size 16
+                                                                   :quality-scaling 2
+                                                                   :enable-markup? true
+                                                                   :use-integer-positions? false
+                                                                   ; :texture-filter/linear because scaling to world-units
+                                                                   :min-filter :linear
+                                                                   :mag-filter :linear}}
+                                           :colors {"PRETTY_NAME" [0.84 0.8 0.52 1]}
+                                           :cursors {:path-format "cursors/%s.png"
+                                                     :data {:cursors/bag                   ["bag001"       [0   0]]
+                                                            :cursors/black-x               ["black_x"      [0   0]]
+                                                            :cursors/default               ["default"      [0   0]]
+                                                            :cursors/denied                ["denied"       [16 16]]
+                                                            :cursors/hand-before-grab      ["hand004"      [4  16]]
+                                                            :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
+                                                            :cursors/hand-grab             ["hand003"      [4  16]]
+                                                            :cursors/move-window           ["move002"      [16 16]]
+                                                            :cursors/no-skill-selected     ["denied003"    [0   0]]
+                                                            :cursors/over-button           ["hand002"      [0   0]]
+                                                            :cursors/sandclock             ["sandclock"    [16 16]]
+                                                            :cursors/skill-not-usable      ["x007"         [0   0]]
+                                                            :cursors/use-skill             ["pointer004"   [0   0]]
+                                                            :cursors/walking               ["walking"      [16 16]]}}}]
+             [cdq.ctx.create.stage/do! '[[cdq.ctx.create.ui.dev-menu/create cdq.ctx.create.world/do!]
+                                         [cdq.ctx.create.ui.action-bar/create]
+                                         [cdq.ctx.create.ui.hp-mana-bar/create]
+                                         [cdq.ctx.create.ui.windows/create [[cdq.ctx.create.ui.windows.entity-info/create]
+                                                                            [cdq.ctx.create.ui.windows.inventory/create]]]
+                                         [cdq.ctx.create.ui.player-state-draw/create]
+                                         [cdq.ctx.create.ui.message/create]]]
+             [cdq.ctx.create.input/do!]
+             [cdq.ctx.create.audio/do!]
+             [cdq.ctx.create.world/do! "world_fns/vampire.edn"]]))
 
 (defn- resize! [{:keys [ctx/graphics]} width height]
   (ui-viewport/update!    graphics width height)
@@ -195,6 +245,8 @@
   (graphics/dispose! graphics)
   (world/dispose! world))
 
+(def state (atom nil))
+
 (defn -main []
   (let [app (-> "cdq.application.edn"
                 io/resource
@@ -206,56 +258,7 @@
     (.set Configuration/GLFW_LIBRARY_NAME "glfw_async")
     (Lwjgl3Application. (reify ApplicationListener
                           (create [_]
-                            (vis-ui/load! {:skin-scale :x1})
-                            (reset! state (pipeline {:ctx/gdx {:clojure.gdx/audio    Gdx/audio
-                                                               :clojure.gdx/files    Gdx/files
-                                                               :clojure.gdx/graphics Gdx/graphics
-                                                               :clojure.gdx/input    Gdx/input}}
-                                                    [[(fn [ctx]
-                                                        (merge (map->Context {})
-                                                               ctx))]
-                                                     [cdq.ctx.create.db/do!]
-                                                     [cdq.ctx.create.graphics/do! {:tile-size 48
-                                                                                   :ui-viewport {:width 1440
-                                                                                                 :height 900}
-                                                                                   :world-viewport {:width 1440
-                                                                                                    :height 900}
-                                                                                   :texture-folder {:folder "resources/"
-                                                                                                    :extensions #{"png" "bmp"}}
-                                                                                   :default-font {:path "exocet/films.EXL_____.ttf"
-                                                                                                  :params {:size 16
-                                                                                                           :quality-scaling 2
-                                                                                                           :enable-markup? true
-                                                                                                           :use-integer-positions? false
-                                                                                                           ; :texture-filter/linear because scaling to world-units
-                                                                                                           :min-filter :linear
-                                                                                                           :mag-filter :linear}}
-                                                                                   :colors {"PRETTY_NAME" [0.84 0.8 0.52 1]}
-                                                                                   :cursors {:path-format "cursors/%s.png"
-                                                                                             :data {:cursors/bag                   ["bag001"       [0   0]]
-                                                                                                    :cursors/black-x               ["black_x"      [0   0]]
-                                                                                                    :cursors/default               ["default"      [0   0]]
-                                                                                                    :cursors/denied                ["denied"       [16 16]]
-                                                                                                    :cursors/hand-before-grab      ["hand004"      [4  16]]
-                                                                                                    :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
-                                                                                                    :cursors/hand-grab             ["hand003"      [4  16]]
-                                                                                                    :cursors/move-window           ["move002"      [16 16]]
-                                                                                                    :cursors/no-skill-selected     ["denied003"    [0   0]]
-                                                                                                    :cursors/over-button           ["hand002"      [0   0]]
-                                                                                                    :cursors/sandclock             ["sandclock"    [16 16]]
-                                                                                                    :cursors/skill-not-usable      ["x007"         [0   0]]
-                                                                                                    :cursors/use-skill             ["pointer004"   [0   0]]
-                                                                                                    :cursors/walking               ["walking"      [16 16]]}}}]
-                                                     [cdq.ctx.create.stage/do! '[[cdq.ctx.create.ui.dev-menu/create cdq.ctx.create.world/do!]
-                                                                                 [cdq.ctx.create.ui.action-bar/create]
-                                                                                 [cdq.ctx.create.ui.hp-mana-bar/create]
-                                                                                 [cdq.ctx.create.ui.windows/create [[cdq.ctx.create.ui.windows.entity-info/create]
-                                                                                                                    [cdq.ctx.create.ui.windows.inventory/create]]]
-                                                                                 [cdq.ctx.create.ui.player-state-draw/create]
-                                                                                 [cdq.ctx.create.ui.message/create]]]
-                                                     [cdq.ctx.create.input/do!]
-                                                     [cdq.ctx.create.audio/do!]
-                                                     [cdq.ctx.create.world/do! "world_fns/vampire.edn"]])))
+                            (reset! state (create!)))
                           (dispose [_]
                             (dispose! @state))
                           (render [_]
