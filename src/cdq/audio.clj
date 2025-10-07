95222dd4e322f2a52de01e6ac9667eb6bf3cecc9
Michael Sappler <michaelsappler@gmail.com>
2025-10-08 02:21:31 +0530

    Remove clojure.gdx.audio.sound

--- src/cdq/audio.clj
+++ src/cdq/audio.clj
@@ -1,12 +1,12 @@
 (ns cdq.audio
-  (:require [clojure.gdx.audio.sound :as sound]))
+  (:import (com.badlogic.gdx.audio Sound)))
 
 (defn sound-names [sounds]
   (map first sounds))
 
 (defn play! [sounds sound-name]
   (assert (contains? sounds sound-name) (str sound-name))
-  (sound/play! (get sounds sound-name)))
+  (Sound/.play (get sounds sound-name)))
 
 (defn dispose! [sounds]
-  (run! sound/dispose! (vals sounds)))
+  (run! Sound/.dispose (vals sounds)))
