745db494d351863431a2babbedcd8d577e2259c2
Michael Sappler <michaelsappler@gmail.com>
2025-10-08 20:36:09 +0530

    pass graphics/audio config

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -3,8 +3,50 @@
             [cdq.game.dispose :as dispose]
             [cdq.game.render :as render]
             [cdq.game.resize :as resize]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [com.badlogic.gdx.backends.lwjgl :as lwjgl])
   (:gen-class))
+
+(def config
+  {
+   :audio {
+           :sound-names (->> "sounds.edn" io/resource slurp edn/read-string)
+           :path-format "sounds/%s.wav"
+           }
+
+   :graphics {:tile-size 48
+              :ui-viewport {:width 1440
+                            :height 900}
+              :world-viewport {:width 1440
+                               :height 900}
+              :texture-folder {:folder "resources/"
+                               :extensions #{"png" "bmp"}}
+              :default-font {:path "exocet/films.EXL_____.ttf"
+                             :params {:size 16
+                                      :quality-scaling 2
+                                      :enable-markup? true
+                                      :use-integer-positions? false
+                                      ; :texture-filter/linear because scaling to world-units
+                                      :min-filter :linear
+                                      :mag-filter :linear}}
+              :cursors {:path-format "cursors/%s.png"
+                        :data {:cursors/bag                   ["bag001"       [0   0]]
+                               :cursors/black-x               ["black_x"      [0   0]]
+                               :cursors/default               ["default"      [0   0]]
+                               :cursors/denied                ["denied"       [16 16]]
+                               :cursors/hand-before-grab      ["hand004"      [4  16]]
+                               :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
+                               :cursors/hand-grab             ["hand003"      [4  16]]
+                               :cursors/move-window           ["move002"      [16 16]]
+                               :cursors/no-skill-selected     ["denied003"    [0   0]]
+                               :cursors/over-button           ["hand002"      [0   0]]
+                               :cursors/sandclock             ["sandclock"    [16 16]]
+                               :cursors/skill-not-usable      ["x007"         [0   0]]
+                               :cursors/use-skill             ["pointer004"   [0   0]]
+                               :cursors/walking               ["walking"      [16 16]]}}}
+   }
+  )
 
 (def state (atom nil))
 
@@ -19,7 +61,7 @@
     :fps 60
 
     :create! (fn [gdx]
-               (reset! state (create/do! gdx)))
+               (reset! state (create/do! gdx config)))
 
     :dispose! (fn []
                 (dispose/do! @state))
--- src/cdq/game/create.clj
+++ src/cdq/game/create.clj
@@ -6,55 +6,16 @@
             [cdq.game.create.input-processor :as create-input-processor]
             [cdq.game.create.audio :as create-audio]
             [cdq.game.create.world :as create-world]
-            [clojure.edn :as edn]
-            [clojure.java.io :as io]
             [qrecord.core :as q]))
 
 (q/defrecord Context [])
 
-(def graphics-params
-  {:tile-size 48
-   :ui-viewport {:width 1440
-                 :height 900}
-   :world-viewport {:width 1440
-                    :height 900}
-   :texture-folder {:folder "resources/"
-                    :extensions #{"png" "bmp"}}
-   :default-font {:path "exocet/films.EXL_____.ttf"
-                  :params {:size 16
-                           :quality-scaling 2
-                           :enable-markup? true
-                           :use-integer-positions? false
-                           ; :texture-filter/linear because scaling to world-units
-                           :min-filter :linear
-                           :mag-filter :linear}}
-   :cursors {:path-format "cursors/%s.png"
-             :data {:cursors/bag                   ["bag001"       [0   0]]
-                    :cursors/black-x               ["black_x"      [0   0]]
-                    :cursors/default               ["default"      [0   0]]
-                    :cursors/denied                ["denied"       [16 16]]
-                    :cursors/hand-before-grab      ["hand004"      [4  16]]
-                    :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
-                    :cursors/hand-grab             ["hand003"      [4  16]]
-                    :cursors/move-window           ["move002"      [16 16]]
-                    :cursors/no-skill-selected     ["denied003"    [0   0]]
-                    :cursors/over-button           ["hand002"      [0   0]]
-                    :cursors/sandclock             ["sandclock"    [16 16]]
-                    :cursors/skill-not-usable      ["x007"         [0   0]]
-                    :cursors/use-skill             ["pointer004"   [0   0]]
-                    :cursors/walking               ["walking"      [16 16]]}}})
-
-(def audio-config {
-                   :sound-names (->> "sounds.edn" io/resource slurp edn/read-string)
-                   :path-format "sounds/%s.wav"
-                   })
-
-(defn do! [gdx]
+(defn do! [gdx config]
   (-> {:ctx/gdx gdx}
       map->Context
       create-tx-handler/do!
       create-db/do!
-      (create-graphics/do! graphics-params)
+      (create-graphics/do! (:graphics config))
       (create-ui/do! '[[cdq.ctx.create.ui.dev-menu/create cdq.game.create.world/do!]
                        [cdq.ctx.create.ui.action-bar/create]
                        [cdq.ctx.create.ui.hp-mana-bar/create]
@@ -63,5 +24,5 @@
                        [cdq.ctx.create.ui.player-state-draw/create]
                        [cdq.ctx.create.ui.message/create]])
       create-input-processor/do!
-      (create-audio/do! audio-config)
+      (create-audio/do! (:audio config))
       (create-world/do! "world_fns/vampire.edn")))
