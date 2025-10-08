58d4b9f3e1cb0e5fd35268de8cd7611426c3e51e
Michael Sappler <michaelsappler@gmail.com>
2025-10-08 15:29:13 +0530

    Pass Gdx fields to cdq.game

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -13,14 +13,22 @@
   (.set Configuration/GLFW_LIBRARY_NAME "glfw_async")
   (Lwjgl3Application. (reify ApplicationListener
                         (create [_]
-                          (reset! state (game/create!)))
+                          (reset! state (game/create! {:audio    Gdx/audio
+                                                       :files    Gdx/files
+                                                       :graphics Gdx/graphics
+                                                       :input    Gdx/input})))
+
                         (dispose [_]
                           (game/dispose! @state))
+
                         (render [_]
                           (swap! state game/render!))
+
                         (resize [_ width height]
                           (game/resize! @state width height))
+
                         (pause [_])
+
                         (resume [_]))
                       (doto (Lwjgl3ApplicationConfiguration.)
                         (.setTitle "Cyber Dungeon Quest")
--- src/cdq/game.clj
+++ src/cdq/game.clj
@@ -46,8 +46,7 @@
             [qrecord.core :as q]
             [reduce-fsm :as fsm])
   (:import (cdq.ui Stage)
-           (com.badlogic.gdx Files
-                             Gdx)))
+           (com.badlogic.gdx Files)))
 
 (defn- call-world-fn
   [world-fn creature-properties graphics]
@@ -655,53 +654,50 @@
    :world-viewport world-viewport
    :textures-to-load (files/search files texture-folder)})
 
-(defn create-graphics
-  [ctx
-   params]
-  (assoc ctx :ctx/graphics (cdq.impl.graphics/create! (handle-files Gdx/files params)
-                                                      Gdx/graphics)))
-
 (defn create-input [{:keys [ctx/stage]
-                     :as ctx}]
-  (let [input Gdx/input]
-    (.setInputProcessor input stage)
-    (assoc ctx :ctx/input input)))
+                     :as ctx} input]
+  (.setInputProcessor input stage)
+  (assoc ctx :ctx/input input))
 
-(defn create! []
+(def graphics-params
+  {:tile-size 48
+   :ui-viewport {:width 1440
+                 :height 900}
+   :world-viewport {:width 1440
+                    :height 900}
+   :texture-folder {:folder "resources/"
+                    :extensions #{"png" "bmp"}}
+   :default-font {:path "exocet/films.EXL_____.ttf"
+                  :params {:size 16
+                           :quality-scaling 2
+                           :enable-markup? true
+                           :use-integer-positions? false
+                           ; :texture-filter/linear because scaling to world-units
+                           :min-filter :linear
+                           :mag-filter :linear}}
+   :colors {"PRETTY_NAME" [0.84 0.8 0.52 1]}
+   :cursors {:path-format "cursors/%s.png"
+             :data {:cursors/bag                   ["bag001"       [0   0]]
+                    :cursors/black-x               ["black_x"      [0   0]]
+                    :cursors/default               ["default"      [0   0]]
+                    :cursors/denied                ["denied"       [16 16]]
+                    :cursors/hand-before-grab      ["hand004"      [4  16]]
+                    :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
+                    :cursors/hand-grab             ["hand003"      [4  16]]
+                    :cursors/move-window           ["move002"      [16 16]]
+                    :cursors/no-skill-selected     ["denied003"    [0   0]]
+                    :cursors/over-button           ["hand002"      [0   0]]
+                    :cursors/sandclock             ["sandclock"    [16 16]]
+                    :cursors/skill-not-usable      ["x007"         [0   0]]
+                    :cursors/use-skill             ["pointer004"   [0   0]]
+                    :cursors/walking               ["walking"      [16 16]]}}})
+
+(defn create! [{:keys [audio files graphics input]}]
   (-> {}
       map->Context
       (assoc :ctx/db (cdq.impl.db/create))
-      (create-graphics {:tile-size 48
-                        :ui-viewport {:width 1440
-                                      :height 900}
-                        :world-viewport {:width 1440
-                                         :height 900}
-                        :texture-folder {:folder "resources/"
-                                         :extensions #{"png" "bmp"}}
-                        :default-font {:path "exocet/films.EXL_____.ttf"
-                                       :params {:size 16
-                                                :quality-scaling 2
-                                                :enable-markup? true
-                                                :use-integer-positions? false
-                                                ; :texture-filter/linear because scaling to world-units
-                                                :min-filter :linear
-                                                :mag-filter :linear}}
-                        :colors {"PRETTY_NAME" [0.84 0.8 0.52 1]}
-                        :cursors {:path-format "cursors/%s.png"
-                                  :data {:cursors/bag                   ["bag001"       [0   0]]
-                                         :cursors/black-x               ["black_x"      [0   0]]
-                                         :cursors/default               ["default"      [0   0]]
-                                         :cursors/denied                ["denied"       [16 16]]
-                                         :cursors/hand-before-grab      ["hand004"      [4  16]]
-                                         :cursors/hand-before-grab-gray ["hand004_gray" [4  16]]
-                                         :cursors/hand-grab             ["hand003"      [4  16]]
-                                         :cursors/move-window           ["move002"      [16 16]]
-                                         :cursors/no-skill-selected     ["denied003"    [0   0]]
-                                         :cursors/over-button           ["hand002"      [0   0]]
-                                         :cursors/sandclock             ["sandclock"    [16 16]]
-                                         :cursors/skill-not-usable      ["x007"         [0   0]]
-                                         :cursors/use-skill             ["pointer004"   [0   0]]
-                                         :cursors/walking               ["walking"      [16 16]]}}})
+      (assoc :ctx/graphics (cdq.impl.graphics/create! (handle-files files graphics-params)
+                                                      graphics))
       (ui/create! '[[cdq.ctx.create.ui.dev-menu/create cdq.application/create-world]
                     [cdq.ctx.create.ui.action-bar/create]
                     [cdq.ctx.create.ui.hp-mana-bar/create]
@@ -709,8 +705,8 @@
                                                        [cdq.ctx.create.ui.windows.inventory/create]]]
                     [cdq.ctx.create.ui.player-state-draw/create]
                     [cdq.ctx.create.ui.message/create]])
-      create-input
-      (assoc :ctx/audio (audio/create Gdx/audio (files/sound-names->file-handles Gdx/files)))
+      (create-input input)
+      (assoc :ctx/audio (audio/create audio (files/sound-names->file-handles files)))
       (create-world "world_fns/vampire.edn")))
 
 (defn resize! [{:keys [ctx/graphics]} width height]
