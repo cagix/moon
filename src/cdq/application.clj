161f863c51b3a880b5aeddf7faca4e42a5ca2ab3
Michael Sappler <michaelsappler@gmail.com>
2025-10-08 14:45:38 +0530

    cdq.impl.ui

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -1,21 +1,8 @@
 (ns cdq.application
   (:require cdq.impl.db
+            cdq.impl.ui
             cdq.impl.world
             cdq.info-impl
-            clojure.scene2d.builds
-            cdq.scene2d.build.editor-overview-window
-            cdq.scene2d.build.editor-window
-            cdq.scene2d.build.map-widget-table
-            clojure.scene2d.build.actor
-            clojure.scene2d.build.group
-            clojure.scene2d.build.horizontal-group
-            clojure.scene2d.build.scroll-pane
-            clojure.scene2d.build.separator-horizontal
-            clojure.scene2d.build.separator-vertical
-            clojure.scene2d.build.stack
-            clojure.scene2d.build.widget
-            cdq.ui.actor-information
-            cdq.ui.error-window
             [cdq.audio :as audio]
             [cdq.db :as db]
             [cdq.files :as files]
