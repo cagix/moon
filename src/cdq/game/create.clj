9e1deac349136a7f41c0422fed8cd2a5c21dcf23
Michael Sappler <michaelsappler@gmail.com>
2025-10-09 10:35:27 +0530

    create-pipeline.edn

--- src/cdq/game/create.clj
+++ src/cdq/game/create.clj
@@ -1,30 +1,12 @@
 (ns cdq.game.create
-  (:require [cdq.game.create.record :as create-record]
-            [cdq.game.create.get-gdx :as get-gdx]
-            [cdq.game.create.tx-handler :as create-tx-handler]
-            [cdq.game.create.db :as create-db]
-            [cdq.game.create.graphics :as create-graphics]
-            [cdq.game.create.ui :as create-ui]
-            [cdq.game.create.input-processor :as create-input-processor]
-            [cdq.game.create.audio :as create-audio]
-            [cdq.game.create.world :as create-world]
-            [clojure.config :as config]))
+  (:require [clojure.config :as config]))
 
 (defn pipeline [ctx steps]
   (reduce (fn [ctx [f & params]]
-            (apply f ctx params))
+            (apply (requiring-resolve f) ctx params))
           ctx
           steps))
 
 (defn do! []
-  (let [config (config/edn-resource "config.edn")]
-    (pipeline {}
-              [[create-record/do!]
-               [get-gdx/do!]
-               [create-tx-handler/do!]
-               [create-db/do!]
-               [create-graphics/do! (:graphics config)]
-               [create-ui/do! (:ui config)]
-               [create-input-processor/do!]
-               [create-audio/do! (:audio config)]
-               [create-world/do! (:world config)]])))
+  (pipeline {}
+            (config/edn-resource "create-pipeline.edn")))
