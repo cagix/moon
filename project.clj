2bdbe27929b542357d3d6a6bd2fc0ba4366a93cd
Michael Sappler <michaelsappler@gmail.com>
2025-10-11 02:11:03 +0530

    add comments

--- project.clj
+++ project.clj
@@ -1,7 +1,60 @@
+; This is the trick - 1 class one API
+; no "pass gdx" or "vector2/convert" or "lwjgl-config" !!!!
+; because _some_ 1 class-APIs actually do something (table cell, lwjgl config, etc. ?)
+; only smoothen out API / just names / consistency
+; one _tiny_ step
+; what is the smallest step I can take
+; make it into a function/library
+; e.g. colors not together with float-bits
+
+; => minimal dependencies
+; => maximum design
+
+
 (defproject cdq "-SNAPSHOT"
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [
                  [com.github.cdorrat/reduce-fsm "fe1c914d68"]
+
+                 ; _SMALL_ libraries !!!!
+                 ; hundreds !
+                 ; powerful !
+                 ; realesas3 your whole game as l;ibraryes (world, audio, graphics,ui, editor , tests)
+
+                 ; but keep it in 1 project !!!
+
+                 ; => pull out _1_ class _1_ namespace 'views/facades/simler models'
+                 ; then can do package-based e.g. vis-ui one 'API' namespace?
+                 ; or combined creator (full API for cdq wrapper ? what is visible what noget-t ?)
+
+
+                 ; TODO FIXME !! THOSE SMALL HELPERS LIKE vector2 or the smoohtening of names
+                 ; or the potential to see a simpler API !
+
+                 ; consistency starts at the _bottom_ e.g. Lwjgl3Application/create does _nothing_
+                 ; but I can independently reqrite it !
+                 ; consistency ! rules !
+                 #_(comment
+
+                  (ns clojure.vis-ui.window
+                    (:import (com.kotcrab.vis.ui.widget VisWindow)))
+
+                  (defn create
+                    [{:keys [title
+                             close-button?
+                             center?
+                             close-on-escape?]}]
+                    (let [show-window-border? true
+                          window (VisWindow. ^String title (boolean show-window-border?))]
+                      (when close-button?    (.addCloseButton window))
+                      (when center?          (.centerWindow   window))
+                      (when close-on-escape? (.closeOnEscape  window))
+                      window))
+                  )
+
+                 ; => 'cdq.ui' or 'gdl.ui' is then a combination between
+                 ; scene2d and visui
+                 ; and you should pass the skin
                  [com.badlogicgames.gdx/gdx-backend-lwjgl3    "1.13.5"]
                  [com.badlogicgames.gdx/gdx-platform          "1.13.5" :classifier "natives-desktop"]
                  [com.badlogicgames.gdx/gdx-freetype          "1.13.5"]
