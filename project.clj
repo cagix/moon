2ea48cc591f8684749dc0026ce9f8537a34ff16a
Michael Sappler <michaelsappler@gmail.com>
2025-10-12 19:25:20 +0530

    add gdx core as shape drawer brings older version

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [
                  [com.github.cdorrat/reduce-fsm "fe1c914d68"]
+                 [com.badlogicgames.gdx/gdx "1.13.5"]
                  [com.github.damn/clojure.gdx.backends.lwjgl "482fadc0f9"]
                  [com.badlogicgames.gdx/gdx-freetype          "1.13.5"]
                  [com.badlogicgames.gdx/gdx-freetype-platform "1.13.5" :classifier "natives-desktop"]
