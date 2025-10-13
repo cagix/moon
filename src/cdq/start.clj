fe9837fc480a05baa7adae55d62f6de656c75463
Michael Sappler <michaelsappler@gmail.com>
2025-10-13 08:55:07 +0530

    upfactor application listener

--- src/cdq/start.clj
+++ src/cdq/start.clj
@@ -10,6 +10,23 @@
   (let [{:keys [listener config]} (config/edn-resource "runs.edn")]
     (lwjgl-config/set-glfw-library-name! "glfw_async")
     (application/create (listener/create
-                         (let [[f & params] listener]
-                           (apply (requiring-resolve f) params)))
+                         (let [{:keys [state
+                                       create
+                                       dispose
+                                       render
+                                       resize]} listener]
+                           (let [state @(requiring-resolve state)]
+                             {:create (fn []
+                                        (reset! state (let [[f params] create]
+                                                        ((requiring-resolve f) params))))
+                              :dispose (fn []
+                                         ((requiring-resolve dispose) @state))
+                              :render (fn []
+                                        (swap! state (fn [ctx]
+                                                       (let [[f params] render]
+                                                         ((requiring-resolve f) ctx params)))))
+                              :resize (fn [width height]
+                                        ((requiring-resolve resize) @state width height))
+                              :pause (fn [])
+                              :resume (fn [])})))
                         (app-config/create config))))
