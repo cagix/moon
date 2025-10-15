65bf38647d3bcac7daa5c4b1b227484ea284c001
Michael Sappler <michaelsappler@gmail.com>
2025-10-15 11:00:02 +0530

    enum , image, one-to-many schema widgets together

--- src/cdq/ui/editor/schema.clj
+++ src/cdq/ui/editor/schema.clj
@@ -1,11 +1,24 @@
 (ns cdq.ui.editor.schema
-  (:require [cdq.graphics :as graphics]
-            [cdq.ui.build.table :as table]
+  (:require [cdq.db :as db]
+            [cdq.graphics :as graphics]
+            [cdq.ui.build.table :as build-table]
+            [cdq.ui.editor.property :as property]
+            [cdq.ui.editor.overview-window :as editor-overview-window]
+            [cdq.ui.stage :as stage]
+            [cdq.ui.table :as table]
+            [cdq.ui.tooltip :as tooltip]
+            [cdq.ui.window :as window]
+            [clojure.edn :as edn]
             [clojure.gdx.scene2d.actor :as actor]
-            [clojure.scene2d.vis-ui.image-button :as image-button]
-            [clojure.utils :as utils]
+            [clojure.gdx.scene2d.group :as group]
+            [clojure.gdx.scene2d.ui.widget-group :as widget-group]
             [clojure.scene2d.vis-ui.check-box :as check-box]
-            [clojure.vis-ui.label :as label]))
+            [clojure.scene2d.vis-ui.image :as image]
+            [clojure.scene2d.vis-ui.image-button :as image-button]
+            [clojure.scene2d.vis-ui.text-button :as text-button]
+            [clojure.utils :as utils]
+            [clojure.vis-ui.label :as label]
+            [clojure.vis-ui.select-box :as select-box]))
 
 (defmulti create (fn [[schema-k :as _schema] v ctx]
                    schema-k))
@@ -23,7 +36,7 @@
 
 (defmethod create :s/animation
   [_ animation {:keys [ctx/graphics]}]
-  (table/create
+  (build-table/create
    {:rows [(for [image (:animation/frames animation)]
              {:actor (image-button/create
                       {:drawable/texture-region (graphics/texture-region graphics image)
@@ -41,3 +54,80 @@
 (defmethod value :s/boolean
   [_ widget _schemas]
   (check-box/checked? widget))
+
+(defmethod create :s/enum [schema v _ctx]
+  (select-box/create
+   {:items (map utils/->edn-str (rest schema))
+    :selected (utils/->edn-str v)}))
+
+(defmethod value :s/enum [_  widget _schemas]
+  (edn/read-string (select-box/selected widget)))
+
+(defmethod create :s/image
+  [schema  image {:keys [ctx/graphics]}]
+  (image-button/create
+   {:drawable/texture-region (graphics/texture-region graphics image)
+    :drawable/scale 2})
+  #_(ui/image-button image
+                     (fn [_actor ctx]
+                       (c/add-actor! ctx (scroll-pane/choose-window (texture-rows ctx))))
+                     {:dimensions [96 96]})) ; x2  , not hardcoded here
+
+
+; too many ! too big ! scroll ... only show files first & preview?
+; make tree view from folders, etc. .. !! all creatures animations showing...
+#_(defn- texture-rows [ctx]
+    (for [file (sort (assets/all-of-type assets :texture))]
+      [(image-button/create {:texture-region (texture/region (assets file))})]
+      #_[(text-button/create file
+                             (fn [_actor _ctx]))]))
+
+(defn- add-one-to-many-rows
+  [{:keys [ctx/db
+           ctx/graphics]}
+   table
+   property-type
+   property-ids]
+  (let [redo-rows (fn [ctx property-ids]
+                    (group/clear-children! table)
+                    (add-one-to-many-rows ctx table property-type property-ids)
+                    (widget-group/pack! (window/find-ancestor table)))]
+    (table/add-rows!
+     table
+     [[{:actor (text-button/create
+                {:text "+"
+                 :on-clicked (fn [_actor {:keys [ctx/db
+                                                 ctx/graphics
+                                                 ctx/stage]}]
+                               (stage/add-actor!
+                                stage
+                                (editor-overview-window/create
+                                 {:db db
+                                  :graphics graphics
+                                  :property-type property-type
+                                  :clicked-id-fn (fn [actor id ctx]
+                                                   (actor/remove! (window/find-ancestor actor))
+                                                   (redo-rows ctx (conj property-ids id)))})))})}]
+      (for [property-id property-ids]
+        (let [property (db/get-raw db property-id)
+              texture-region (graphics/texture-region graphics (property/image property))
+              image-widget (image/create
+                            {:image/object texture-region
+                             :actor/user-object property-id})]
+          {:actor (tooltip/add! image-widget (property/tooltip property))}))
+      (for [id property-ids]
+        {:actor (text-button/create
+                 {:text "-"
+                  :on-clicked (fn [_actor ctx]
+                                (redo-rows ctx (disj property-ids id)))})})])))
+
+(defmethod create :s/one-to-many [[_ property-type] property-ids ctx]
+  (let [table (build-table/create
+               {:cell-defaults {:pad 5}})]
+    (add-one-to-many-rows ctx table property-type property-ids)
+    table))
+
+(defmethod value :s/one-to-many [_  widget _schemas]
+  (->> (group/children widget)
+       (keep actor/user-object)
+       set))
--- src/cdq/ui/editor/schemas_impl.clj
+++ src/cdq/ui/editor/schemas_impl.clj
@@ -2,19 +2,11 @@
 
 (def fn-map
   '{
-    :s/enum {cdq.ui.editor.schema/create       cdq.ui.editor.schema.enum/create
-             cdq.ui.editor.schema/value        cdq.ui.editor.schema.enum/value}
-
-    :s/image {cdq.ui.editor.schema/create       cdq.ui.editor.schema.image/create}
-
     :s/map {cdq.ui.editor.schema/create       cdq.ui.editor.schema.map/create
             cdq.ui.editor.schema/value        cdq.ui.editor.schema.map/value}
 
     :s/number {cdq.ui.editor.schema/create       cdq.ui.editor.widget.edn/create
                cdq.ui.editor.schema/value        cdq.ui.editor.widget.edn/value}
-
-    :s/one-to-many {cdq.ui.editor.schema/create       cdq.ui.editor.schema.one-to-many/create
-                    cdq.ui.editor.schema/value        cdq.ui.editor.schema.one-to-many/value}
 
     :s/one-to-one {cdq.ui.editor.schema/create       cdq.ui.editor.schema.one-to-one/create
                    cdq.ui.editor.schema/value        cdq.ui.editor.schema.one-to-one/value}
