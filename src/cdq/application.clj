3036ef8a27ed2ba3f5a58fe46fe669a848db6e9d
Michael Sappler <michaelsappler@gmail.com>
2025-10-16 01:19:00 +0530

    call command

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -10,22 +10,31 @@
 
 (def state (atom nil))
 
+(defn start-lwjgl-app! [config]
+  (application/create (listener/create
+                       {:create (fn []
+                                  (reset! state (game/create! (gdx/context) config)))
+
+                        :dispose (fn []
+                                   (game/dispose! @state))
+
+                        :render (fn []
+                                  (swap! state game/render!))
+
+                        :resize (fn [width height]
+                                  (game/resize! @state width height))
+
+                        :pause (fn [])
+                        :resume (fn [])})
+                      (app-config/create (:lwjgl-application config))))
+
+(defn call [[f & params]]
+  (apply f params))
+
 (defn -main []
   (let [config (config/edn-resource "config.edn")]
-    (lwjgl-config/set-glfw-library-name! "glfw_async")
-    (application/create (listener/create
-                         {:create (fn []
-                                    (reset! state (game/create! (gdx/context) config)))
-
-                          :dispose (fn []
-                                     (game/dispose! @state))
-
-                          :render (fn []
-                                    (swap! state game/render!))
-
-                          :resize (fn [width height]
-                                    (game/resize! @state width height))
-
-                          :pause (fn [])
-                          :resume (fn [])})
-                        (app-config/create (:lwjgl-application config)))))
+    (doseq [command [
+                     [clojure.lwjgl.system.configuration/set-glfw-library-name! "glfw_async"]
+                     [start-lwjgl-app! config]
+                     ]]
+      (call command))))
