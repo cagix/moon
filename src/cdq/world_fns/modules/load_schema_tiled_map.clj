4170151012561b4ef8a47e6c7de68baa510b11e4
Michael Sappler <michaelsappler@gmail.com>
2025-10-15 13:13:06 +0530

    world-fns.modules in 1 ns

--- src/cdq/world_fns/modules/load_schema_tiled_map.clj
+++
@@ -1,5 +0,0 @@
-(ns cdq.world-fns.modules.load-schema-tiled-map
-  (:require [clojure.gdx.maps.tiled.tmx :as tmx]))
-
-(defn step [w]
-  (assoc w :schema-tiled-map (tmx/load-map "maps/modules.tmx")))
--- src/cdq/world_fns/modules/print.clj
+++
@@ -1,7 +0,0 @@
-(ns cdq.world-fns.modules.print
-  (:require [clojure.grid2d.utils :as helper]))
-
-(defn do! [{:keys [grid] :as world-fn-ctx}]
-  (helper/printgrid grid)
-  (println " - ")
-  world-fn-ctx)
