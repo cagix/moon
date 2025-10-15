2c81d55afbcb28a527a335e4beff6aa7befd5a76
Michael Sappler <michaelsappler@gmail.com>
2025-10-15 10:53:42 +0530

    Consolidate ui schema editor widgets

--- src/cdq/ui/editor/schema.clj
+++ src/cdq/ui/editor/schema.clj
@@ -1,7 +1,43 @@
-(ns cdq.ui.editor.schema)
+(ns cdq.ui.editor.schema
+  (:require [cdq.graphics :as graphics]
+            [cdq.ui.build.table :as table]
+            [clojure.gdx.scene2d.actor :as actor]
+            [clojure.scene2d.vis-ui.image-button :as image-button]
+            [clojure.utils :as utils]
+            [clojure.scene2d.vis-ui.check-box :as check-box]
+            [clojure.vis-ui.label :as label]))
 
 (defmulti create (fn [[schema-k :as _schema] v ctx]
                    schema-k))
 
 (defmulti value (fn [[schema-k :as _schema] widget schemas]
                   schema-k))
+
+(defmethod create :default
+  [_ v _ctx]
+  (label/create (utils/truncate (utils/->edn-str v) 60)))
+
+(defmethod value :default
+  [_  widget _schemas]
+  ((actor/user-object widget) 1))
+
+(defmethod create :s/animation
+  [_ animation {:keys [ctx/graphics]}]
+  (table/create
+   {:rows [(for [image (:animation/frames animation)]
+             {:actor (image-button/create
+                      {:drawable/texture-region (graphics/texture-region graphics image)
+                       :drawable/scale 2})})]
+    :cell-defaults {:pad 1}}))
+
+(defmethod create :s/boolean
+  [_ checked? _ctx]
+  (assert (boolean? checked?))
+  (check-box/create
+   :text ""
+   :on-clicked (fn [_])
+   :checked? checked?))
+
+(defmethod value :s/boolean
+  [_ widget _schemas]
+  (check-box/checked? widget))
--- src/cdq/ui/editor/schemas_impl.clj
+++ src/cdq/ui/editor/schemas_impl.clj
@@ -2,17 +2,10 @@
 
 (def fn-map
   '{
-    :s/animation {cdq.ui.editor.schema/create       cdq.ui.editor.schema.animation/create
-                  cdq.ui.editor.schema/value        cdq.ui.editor.widget.default/value}
-
-    :s/boolean {cdq.ui.editor.schema/create       cdq.ui.editor.schema.boolean/create
-                cdq.ui.editor.schema/value        cdq.ui.editor.schema.boolean/value}
-
     :s/enum {cdq.ui.editor.schema/create       cdq.ui.editor.schema.enum/create
              cdq.ui.editor.schema/value        cdq.ui.editor.schema.enum/value}
 
-    :s/image {cdq.ui.editor.schema/create       cdq.ui.editor.schema.image/create
-              cdq.ui.editor.schema/value        cdq.ui.editor.widget.default/value}
+    :s/image {cdq.ui.editor.schema/create       cdq.ui.editor.schema.image/create}
 
     :s/map {cdq.ui.editor.schema/create       cdq.ui.editor.schema.map/create
             cdq.ui.editor.schema/value        cdq.ui.editor.schema.map/value}
@@ -26,14 +19,7 @@
     :s/one-to-one {cdq.ui.editor.schema/create       cdq.ui.editor.schema.one-to-one/create
                    cdq.ui.editor.schema/value        cdq.ui.editor.schema.one-to-one/value}
 
-    :s/qualified-keyword {cdq.ui.editor.schema/create       cdq.ui.editor.widget.default/create
-                          cdq.ui.editor.schema/value        cdq.ui.editor.widget.default/value}
-
-    :s/some {cdq.ui.editor.schema/create       cdq.ui.editor.widget.default/create
-             cdq.ui.editor.schema/value        cdq.ui.editor.widget.default/value}
-
-    :s/sound {cdq.ui.editor.schema/create       cdq.ui.editor.schema.sound/create
-              cdq.ui.editor.schema/value        cdq.ui.editor.widget.default/value}
+    :s/sound {cdq.ui.editor.schema/create       cdq.ui.editor.schema.sound/create}
 
     :s/string {cdq.ui.editor.schema/create       cdq.ui.editor.schema.string/create
                cdq.ui.editor.schema/value        cdq.ui.editor.schema.string/value}
@@ -41,8 +27,6 @@
     :s/val-max {cdq.ui.editor.schema/create       cdq.ui.editor.widget.edn/create
                 cdq.ui.editor.schema/value        cdq.ui.editor.widget.edn/value}
 
-    :s/vector {cdq.ui.editor.schema/create       cdq.ui.editor.widget.default/create
-               cdq.ui.editor.schema/value        cdq.ui.editor.widget.default/value}
     }
   )
 
