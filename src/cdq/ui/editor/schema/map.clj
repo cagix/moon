724309e7b36903f977d4c8a5fea89b68d4bbb7ea
Michael Sappler <michaelsappler@gmail.com>
2025-10-15 11:07:35 +0530

    Move cdq.scene2d.build.map-widget-table into cdq.ui.editor.schema.map

--- src/cdq/ui/editor/schema/map.clj
+++ src/cdq/ui/editor/schema/map.clj
@@ -1,11 +1,155 @@
 (ns cdq.ui.editor.schema.map
-  (:require [malli.utils :as mu]
-            [cdq.scene2d.build.map-widget-table :as bmap-widget-table]
-            [cdq.ui.editor.map-widget-table :as map-widget-table]
+  (:require [cdq.db.schemas :as schemas]
+            [cdq.ui.editor.schema]
+            [cdq.ui.table :as table]
+            [cdq.ui.build.table :as btable]
             [cdq.ui.editor.value-widget :as value-widget]
+            [cdq.ui.editor.map-widget-table :as map-widget-table]
+            [cdq.ui.editor.window :as editor-window]
             [cdq.malli :as malli]
+            [clojure.gdx.scene2d.actor :as actor]
+            [clojure.gdx.scene2d.group :as group]
+            [clojure.gdx.scene2d.stage :as stage]
+            [clojure.gdx.scene2d.ui.widget-group :as widget-group]
+            [clojure.scene2d.vis-ui.text-button :as text-button]
+            [clojure.scene2d.vis-ui.window :as window]
+            [clojure.set :as set]
             [clojure.utils :as utils]
-            [clojure.set :as set]))
+            [clojure.vis-ui.label :as label]
+            [clojure.vis-ui.separator :as separator]
+            [malli.utils :as mu]))
+
+(defn- rebuild!
+  [{:keys [ctx/db
+           ctx/stage]
+    :as ctx}]
+  (let [window (-> stage
+                   stage/root
+                   (group/find-actor "cdq.ui.editor.window"))
+        map-widget-table (-> window
+                             (group/find-actor "cdq.ui.widget.scroll-pane-table")
+                             (group/find-actor "scroll-pane-table")
+                             (group/find-actor "cdq.db.schema.map.ui.widget"))
+        property (map-widget-table/get-value map-widget-table (:db/schemas db))]
+    (actor/remove! window)
+    (stage/add-actor! stage (editor-window/create
+                             {:ctx ctx
+                              :property property}))))
+
+(defn- k->label-text [k]
+  (name k) ;(str "[GRAY]:" (namespace k) "[]/" (name k))
+  )
+
+(defn- component-row*
+  [{:keys [editor-widget
+           display-remove-component-button?
+           k
+           table
+           label-text]}]
+  [{:actor (btable/create
+            {:cell-defaults {:pad 2}
+             :rows [[{:actor (when display-remove-component-button?
+                               (text-button/create
+                                {:text "-"
+                                 :on-clicked (fn [_actor ctx]
+                                               (actor/remove! (first (filter (fn [actor]
+                                                                               (and (actor/user-object actor)
+                                                                                    (= k ((actor/user-object actor) 0))))
+                                                                             (group/children table))))
+                                               (rebuild! ctx))}))
+                      :left? true}
+                     {:actor (label/create label-text)}]]})
+    :right? true}
+   {:actor (separator/vertical)
+    :pad-top 2
+    :pad-bottom 2
+    :fill-y? true
+    :expand-y? true}
+   {:actor editor-widget
+    :left? true}])
+
+(defn- component-row [editor-widget k optional-key? table]
+  (component-row*
+   {:editor-widget editor-widget
+    :display-remove-component-button? optional-key?
+    :k k
+    :table table
+    :label-text (k->label-text k)}))
+
+(defn- add-component-window [schemas schema map-widget-table]
+  (let [window (window/create {:title "Choose"
+                               :modal? true
+                               :close-button? true
+                               :center? true
+                               :close-on-escape? true
+                               :cell-defaults {:pad 5}})
+        remaining-ks (sort (remove (set (keys (cdq.ui.editor.schema/value schema map-widget-table schemas)))
+                                   (mu/map-keys (malli/form schema schemas))))]
+    (table/add-rows!
+     window
+     (for [k remaining-ks]
+       [{:actor (text-button/create
+                 {:text (name k)
+                  :on-clicked (fn [_actor ctx]
+                                (actor/remove! window)
+                                (table/add-rows! map-widget-table [(component-row (value-widget/build ctx
+                                                                                                      (get schemas k)
+                                                                                                      k
+                                                                                                      (schemas/default-value schemas k))
+                                                                                  k
+                                                                                  (mu/optional? k (malli/form schema schemas))
+                                                                                  map-widget-table)])
+                                (rebuild! ctx))})}]))
+    (widget-group/pack! window)
+    window))
+
+(defn- horiz-sep [colspan]
+  (fn []
+    [{:actor (separator/horizontal)
+      :pad-top 2
+      :pad-bottom 2
+      :colspan colspan
+      :fill-x? true
+      :expand-x? true}]))
+
+(defn- interpose-f [f coll]
+  (drop 1 (interleave (repeatedly f) coll)))
+
+(defn create-map-widget-table
+  [{:keys [schema
+           k->widget
+           k->optional?
+           ks-sorted
+           opt?]}]
+  (let [table (btable/create
+               {:cell-defaults {:pad 5}
+                :actor/name "cdq.db.schema.map.ui.widget"})
+        colspan 3
+        component-rows (interpose-f (horiz-sep colspan)
+                                    (map (fn [k]
+                                           (component-row (k->widget k)
+                                                          k
+                                                          (k->optional? k)
+                                                          table))
+                                         ks-sorted))]
+    (table/add-rows!
+     table
+     (concat [(when opt?
+                [{:actor (text-button/create
+                          {:text "Add component"
+                           :on-clicked (fn [_actor {:keys [ctx/db
+                                                           ctx/stage]}]
+                                         (stage/add-actor! stage (add-component-window (:db/schemas db) schema table)))})
+                  :colspan colspan}])]
+             [(when opt?
+                [{:actor (separator/horizontal)
+                  :pad-top 2
+                  :pad-bottom 2
+                  :colspan colspan
+                  :fill-x? true
+                  :expand-x? true}])]
+             component-rows))
+    table))
 
 (def ^:private property-k-sort-order
   [:property/id
@@ -31,7 +175,7 @@
    {:keys [ctx/db]
     :as ctx}]
   (let [schemas (:db/schemas db)]
-    (bmap-widget-table/create
+    (create-map-widget-table
      {:schema schema
       :k->widget (into {}
                        (for [[k v] m]
