d50e1c48f1e99c04fe4a41b2b222ad98379e1a0c
Michael Sappler <michaelsappler@gmail.com>
2025-10-15 17:03:54 +0530

    Remove cdq.ui.build.horizontal-group

--- src/cdq/ui/build/horizontal_group.clj
+++
@@ -1,7 +0,0 @@
-(ns cdq.ui.build.horizontal-group
-  (:require [clojure.gdx.scene2d.ui.horizontal-group :as horizontal-group]
-            [cdq.ui.group :as group]))
-
-(defn create [opts]
-  (doto (horizontal-group/create opts)
-    (group/set-opts! opts)))
