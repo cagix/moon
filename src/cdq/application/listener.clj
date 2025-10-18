3d738243d9c3d0d27da9c7edfe4cf213005f2370
Michael Sappler <michaelsappler@gmail.com>
2025-10-18 15:52:53 +0530

    test set gdx nil

--- src/cdq/application/listener.clj
+++ src/cdq/application/listener.clj
@@ -5,10 +5,36 @@
 
 (def state (atom nil))
 
+(import '(com.badlogic.gdx Gdx))
+
+(defn- set-gdx-nil! []
+  (set! Gdx/app nil)
+  (set! Gdx/graphics nil)
+  (set! Gdx/audio nil)
+  (set! Gdx/input nil)
+  (set! Gdx/files nil)
+  (set! Gdx/net nil)
+  (set! Gdx/gl nil)
+  (set! Gdx/gl20 nil)
+  (set! Gdx/gl30 nil)
+  (set! Gdx/gl31 nil)
+  (set! Gdx/gl32 nil))
+
+; TODO make the change local
+; just Lwjgl3ApplicationListener
+; gets passed application
+
 (defn create [config]
   (listener/create
    {:create (fn []
-              (reset! state ((:create config) (gdx/context) config)))
+              ; pass application
+              ; or 'ctx' is actually application
+
+              (let [gdx (gdx/context)]
+                #_(set-gdx-nil!)
+                ; com.badlogic.gdx.graphics.glutils/VertexBufferObject
+                ; Gdx.gl20.glGenBuffer();
+                (reset! state ((:create config) gdx config))))
 
     :dispose (fn []
                ((:dispose config) @state))
