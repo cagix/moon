69282b62d26c85427ee37934be527558476c1dbc
Michael Sappler <michaelsappler@gmail.com>
2025-10-23 16:02:09 +0530

    cdq.ctx

--- src/cdq/application.clj
+++ src/cdq/application.clj
@@ -1,42 +1,5 @@
 (ns cdq.application
-  (:require [cdq.audio :as audio]
-            [cdq.db.impl]
-            [cdq.game.create.add-actors]
-            [cdq.game.create.dev-menu-config]
-            [cdq.game.create.txs]
-            [cdq.game.create.world]
-            [cdq.game.render.assoc-active-entities]
-            [cdq.game.render.assoc-interaction-state]
-            [cdq.game.render.assoc-paused]
-            [cdq.game.render.check-open-debug]
-            [cdq.game.render.clear-screen]
-            [cdq.game.render.dissoc-interaction-state]
-            [cdq.game.render.draw-on-world-viewport]
-            [cdq.game.render.draw-world-map]
-            [cdq.game.render.get-stage-ctx]
-            [cdq.game.render.player-state-handle-input]
-            [cdq.game.render.remove-destroyed-entities]
-            [cdq.game.render.render-stage]
-            [cdq.game.render.set-camera-on-player]
-            [cdq.game.render.set-cursor]
-            [cdq.game.render.tick-entities]
-            [cdq.game.render.update-mouse]
-            [cdq.game.render.update-mouseover-eid]
-            [cdq.game.render.update-potential-fields]
-            [cdq.game.render.update-world-time]
-            [cdq.game.render.validate]
-            [cdq.game.render.validate]
-            [cdq.game.render.window-camera-controls]
-            [cdq.graphics :as graphics]
-            [cdq.graphics.impl]
-            [cdq.ui :as ui]
-            [cdq.ui.build.editor-window]
-            [cdq.ui.dev-menu]
-            [cdq.ui.editor.overview-window]
-            [cdq.ui.editor.window]
-            [cdq.ui.impl]
-            [cdq.world :as world]
-            [cdq.world.impl]
+  (:require [cdq.ctx :as ctx]
             [clojure.edn :as edn]
             [clojure.java.io :as io]
             [qrecord.core :as q])
@@ -48,68 +11,7 @@
            (org.lwjgl.system Configuration))
   (:gen-class))
 
-(q/defrecord Context [])
-
-(defn- create!
-  [^Application app config]
-  (let [audio    (.getAudio    app)
-        files    (.getFiles    app)
-        graphics (.getGraphics app)
-        input    (.getInput    app)
-        graphics (cdq.graphics.impl/create! graphics files (:graphics config))
-        stage (cdq.ui.impl/create! graphics {:dev-menu cdq.game.create.dev-menu-config/create})
-        ctx (-> (map->Context {})
-                (assoc :ctx/graphics graphics)
-                (assoc :ctx/stage stage)
-                cdq.game.create.txs/do!
-                (assoc :ctx/audio (cdq.audio/create audio files (:audio config)))
-                (assoc :ctx/db (cdq.db.impl/create))
-                (assoc :ctx/input input)
-                (assoc :ctx/config {:world-impl cdq.world.impl/create}))]
-    (.setInputProcessor input stage)
-    (cdq.game.create.add-actors/step stage ctx)
-    (cdq.game.create.world/step ctx (:world config))))
-
-(defn- dispose!
-  [{:keys [ctx/audio
-           ctx/graphics
-           ctx/stage
-           ctx/world]}]
-  (audio/dispose! audio)
-  (graphics/dispose! graphics)
-  (ui/dispose! stage)
-  (world/dispose! world))
-
-(defn- render! [ctx]
-  (-> ctx
-      cdq.game.render.get-stage-ctx/step
-      cdq.game.render.validate/step
-      cdq.game.render.update-mouse/step
-      cdq.game.render.update-mouseover-eid/step
-      cdq.game.render.check-open-debug/step
-      cdq.game.render.assoc-active-entities/step
-      cdq.game.render.set-camera-on-player/step
-      cdq.game.render.clear-screen/step
-      cdq.game.render.draw-world-map/step
-      cdq.game.render.draw-on-world-viewport/step
-      cdq.game.render.assoc-interaction-state/step
-      cdq.game.render.set-cursor/step
-      cdq.game.render.player-state-handle-input/step
-      cdq.game.render.dissoc-interaction-state/step
-      cdq.game.render.assoc-paused/step
-      cdq.game.render.update-world-time/step
-      cdq.game.render.update-potential-fields/step
-      cdq.game.render.tick-entities/step
-      cdq.game.render.remove-destroyed-entities/step
-      cdq.game.render.window-camera-controls/step
-      cdq.game.render.render-stage/step
-      cdq.game.render.validate/step))
-
-(defn- resize! [{:keys [ctx/graphics]} width height]
-  (graphics/update-ui-viewport! graphics width height)
-  (graphics/update-world-vp! graphics width height))
-
-(defn edn-resource [path]
+(defn- edn-resource [path]
   (->> path
        io/resource
        slurp
@@ -122,16 +24,20 @@
     (.set Configuration/GLFW_LIBRARY_NAME "glfw_async")
     (Lwjgl3Application. (reify ApplicationListener
                           (create [_]
-                            (reset! state (create! Gdx/app config)))
+                            (reset! state (ctx/create! {:audio    Gdx/audio
+                                                        :files    Gdx/files
+                                                        :graphics Gdx/graphics
+                                                        :input    Gdx/input}
+                                                       config)))
 
                           (dispose [_]
-                            (dispose! @state))
+                            (ctx/dispose! @state))
 
                           (render [_]
-                            (swap! state render!))
+                            (swap! state ctx/render!))
 
                           (resize [_ width height]
-                            (resize! @state width height))
+                            (ctx/resize! @state width height))
 
                           (pause [_])
 
